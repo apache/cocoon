495d5b3002df230245c6e430e820dc35ecd517c4
Ricardo Rocha <ricardo@apache.org>
2000-01-07 06:04:53 +0000

    1.4:Fixed create-entity-ref-nodes (false) to allow for external entity expansion
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1045613 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/CVSROOT/src/org/apache/cocoon/parser/XercesParser.java
+++ trunk/CVSROOT/src/org/apache/cocoon/parser/XercesParser.java
@@ -1,4 +1,4 @@
-/*-- $Id: XercesParser.java,v 1.3 2000-01-03 01:43:27 stefano Exp $ -- 
+/*-- $Id: XercesParser.java,v 1.4 2000-01-07 06:04:53 ricardo Exp $ -- 
 
  ============================================================================
                    The Apache Software License, Version 1.1
@@ -63,7 +63,7 @@ import org.apache.cocoon.framework.*;
  * This class implements an XML parser using the Apache Xerces XML parser.
  *
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
- * @version $Revision: 1.3 $ $Date: 2000-01-03 01:43:27 $
+ * @version $Revision: 1.4 $ $Date: 2000-01-07 06:04:53 $
  */
 
 public class XercesParser extends AbstractParser implements Status {
@@ -75,7 +75,7 @@ public class XercesParser extends AbstractParser implements Status {
         DOMParser parser = new DOMParser();
 	    // parser.setEntityResolver(resolver);
         parser.setFeature("http://xml.org/sax/features/validation", validate);
-        parser.setFeature("http://apache.org/xml/features/dom/create-entity-ref-nodes", true);
+        parser.setFeature("http://apache.org/xml/features/dom/create-entity-ref-nodes", false);
         parser.setFeature("http://apache.org/xml/features/validation/warn-on-duplicate-attdef", validate);
         parser.setFeature("http://apache.org/xml/features/validation/warn-on-undeclared-elemdef", validate);
         parser.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", true);
