ba2f0cb740cc5d1b87830ab31f32de0182614ea1
Ricardo Rocha <ricardo@apache.org>
2000-01-09 02:47:03 +0000

    Made all methods static
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1045630 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/CVSROOT/src/org/apache/cocoon/processor/xsp/library/XSPCookieLibrary.java
+++ trunk/CVSROOT/src/org/apache/cocoon/processor/xsp/library/XSPCookieLibrary.java
@@ -100,49 +100,49 @@ public class XSPCookieLibrary {
     return element;
   }
 
-  public Element getComment(Cookie cookie, Document document) {
+  public static Element getComment(Cookie cookie, Document document) {
     Element element = document.createElement("cookie:comment");
     element.appendChild(document.createTextNode(cookie.getComment()));
     return element;
   }
 
-  public Element getDomain(Cookie cookie, Document document) {
+  public static Element getDomain(Cookie cookie, Document document) {
     Element element = document.createElement("cookie:domain");
     element.appendChild(document.createTextNode(cookie.getDomain()));
     return element;
   }
 
-  public Element getMaxAge(Cookie cookie, Document document) {
+  public static Element getMaxAge(Cookie cookie, Document document) {
     Element element = document.createElement("cookie:max-age");
     element.appendChild(document.createTextNode(String.valueOf(cookie.getMaxAge())));
     return element;
   }
 
-  public Element getName(Cookie cookie, Document document) {
+  public static Element getName(Cookie cookie, Document document) {
     Element element = document.createElement("cookie:name");
     element.appendChild(document.createTextNode(cookie.getName()));
     return element;
   }
 
-  public Element getPath(Cookie cookie, Document document) {
+  public static Element getPath(Cookie cookie, Document document) {
     Element element = document.createElement("cookie:path");
     element.appendChild(document.createTextNode(cookie.getPath()));
     return element;
   }
 
-  public Element getSecure(Cookie cookie, Document document) {
+  public static Element getSecure(Cookie cookie, Document document) {
     Element element = document.createElement("cookie:secure");
     element.appendChild(document.createTextNode(String.valueOf(cookie.getSecure())));
     return element;
   }
 
-  public Element getValue(Cookie cookie, Document document) {
+  public static Element getValue(Cookie cookie, Document document) {
     Element element = document.createElement("cookie:secure");
     element.appendChild(document.createTextNode(cookie.getValue()));
     return element;
   }
 
-  public Element getVersion(Cookie cookie, Document document) {
+  public static Element getVersion(Cookie cookie, Document document) {
     Element element = document.createElement("cookie:version");
     element.appendChild(document.createTextNode(String.valueOf(cookie.getVersion())));
     return element;
