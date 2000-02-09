ec35234d38926005b45f64c9785d20f9d7f4811c
Pier Fumagalli <pier@apache.org>
2000-02-09 01:32:58 +0000

    FilterFactory was not throwing the proper ConfigurationException.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1045739 13f79535-47bb-0310-9956-ffa450edef68

--- branches/unlabeled-1.1.2/CVSROOT/src/org/apache/cocoon/filters/FilterFactory.java
+++ branches/unlabeled-1.1.2/CVSROOT/src/org/apache/cocoon/filters/FilterFactory.java
@@ -8,6 +8,7 @@
 package org.apache.cocoon.filters;
 
 import org.apache.cocoon.framework.Configurable;
+import org.apache.cocoon.framework.ConfigurationException;
 
 /**
  * The <code>FilterFactory</code> interface must be implemented by all those
@@ -17,12 +18,13 @@ import org.apache.cocoon.framework.Configurable;
  *         Exoffice Technologies, INC.</a>
  * @author Copyright 1999 &copy; <a href="http://www.apache.org">The Apache
  *         Software Foundation</a>. All rights reserved.
- * @version CVS $Revision: 1.1.2.2 $ $Date: 2000-02-09 01:18:30 $
+ * @version CVS $Revision: 1.1.2.3 $ $Date: 2000-02-09 01:32:58 $
  * @since Cocoon 2.0
  */
 public interface FilterFactory extends Configurable {
     /**
      * Return a non configured instance of a <code>Filter</code>.
      */
-    public Filter getFilter();
+    public Filter getFilter()
+    throws ConfigurationException;
 }
