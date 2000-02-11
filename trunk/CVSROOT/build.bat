8f8b2e1a8f7a4fb639ccb8df16729b2300285517
Stefano Mazzocchi <stefano@apache.org>
2000-02-11 01:35:11 +0000

    the best and simplest possible build system ever created!
    just type build and you're done :)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1045769 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/CVSROOT/build.bat
+++ trunk/CVSROOT/build.bat
@@ -1,3 +1,6 @@
 @echo off
 
-java -classpath "%CLASSPATH%;lib\ant.jar;lib\xml.jar" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+set LOCALCLASSPATH="lib\xerces_1_0_1.jar;lib\xalan_0_19_2;lib\fop_0_12_0;lib\servlet_2_2.jar;lib\ant.jar;lib\xml.jar"
+
+%JAVA_HOME%\bin\java.exe -classpath "%JAVA_HOME%\lib\tools.jar;%CLASSPATH%;%LOCALCLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+
--- trunk/CVSROOT/build.sh
+++ trunk/CVSROOT/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-java -classpath "$CLASSPATH:./lib/ant.jar:./lib/xml.jar" org.apache.tools.ant.Main $*
+LOCALCLASSPATH=lib/xerces_1_0_1.jar:lib/xalan_0_19_2:lib/fop_0_12_0:lib/servlet_2_2.jar:lib/ant.jar:lib/xml.jar
+
+$JAVA_HOME/bin/java -classpath "$JAVA_HOME/lib/tools.jar:$CLASSPATH:$LOCALCLASSPATH" org.apache.tools.ant.Main $*
