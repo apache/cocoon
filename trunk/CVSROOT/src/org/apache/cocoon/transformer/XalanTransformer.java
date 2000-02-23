759b600a9b1a6ccda4e21c93ee17adadf4a0c5a1
Stefano Mazzocchi <stefano@apache.org>
2000-02-23 00:49:06 +0000

    More coherent naming with Xerces (cosmetics)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1045814 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/CVSROOT/src/org/apache/cocoon/transformer/XalanTransformer.java
+++ trunk/CVSROOT/src/org/apache/cocoon/transformer/XalanTransformer.java
@@ -1,36 +1,36 @@
-/*-- $Id: XalanTransformer.java,v 1.7 2000-02-13 18:29:42 stefano Exp $ -- 
+/*-- $Id: XalanTransformer.java,v 1.8 2000-02-23 00:49:06 stefano Exp $ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
  ============================================================================
- 
+
  Copyright (C) @year@ The Apache Software Foundation. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of  source code must  retain the above copyright  notice,
     this list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
- 
+
  3. The end-user documentation included with the redistribution, if any, must
     include  the following  acknowledgment:  "This product includes  software
     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
- 
+
  4. The names "Cocoon" and  "Apache Software Foundation"  must not be used to
     endorse  or promote  products derived  from this  software without  prior
     written permission. For written permission, please contact
     apache@apache.org.
- 
+
  5. Products  derived from this software may not  be called "Apache", nor may
     "Apache" appear  in their name,  without prior written permission  of the
     Apache Software Foundation.
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -41,12 +41,12 @@
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
  on  behalf of the Apache Software  Foundation and was  originally created by
- Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache 
+ Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
  Software Foundation, please see <http://www.apache.org/>.
- 
+
  */
 package org.apache.cocoon.transformer;
 
@@ -69,7 +69,7 @@ import org.xml.sax.SAXException;
  * Xalan XSLT processor.
  *
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
- * @version $Revision: 1.7 $ $Date: 2000-02-13 18:29:42 $
+ * @version $Revision: 1.8 $ $Date: 2000-02-23 00:49:06 $
  */
 
 public class XalanTransformer extends AbstractActor implements Transformer, Status {
@@ -96,9 +96,9 @@ public class XalanTransformer extends AbstractActor implements Transformer, Stat
     }
 
     public String getStatus() {
-        return "Apache Xalan XSLT Processor";
+        return "Xalan XSLT Processor";
     }
- 
+
     class XMLParser extends XMLParserLiaisonDefault {
         Parser parser;
         Document document;
@@ -113,13 +113,13 @@ public class XalanTransformer extends AbstractActor implements Transformer, Stat
 
         public void parse(InputSource in) throws IOException, SAXException {
             this.document = this.parser.parse(in, false);
-            
+
             // The Xalan stylesheet is normally built from SAX events,
             // so if a DocumentHandler is specified, we need to produce
             // SAX events from the DOM tree.
             if (m_docHandler != null) {
                 (new TreeWalker(m_docHandler)).traverse(this.document);
-                
+
                 // Note that when cocoon transitions to being more SAX based,
                 // this function will be called recursivly while the parser is
                 // still in the middle of a parse, and thus the parser will have
@@ -127,7 +127,7 @@ public class XalanTransformer extends AbstractActor implements Transformer, Stat
                 // is not (to my knowledge) reentrant.
             }
         }
-     
+
         public Document getDocument() {
             return this.document;
         }
@@ -135,7 +135,7 @@ public class XalanTransformer extends AbstractActor implements Transformer, Stat
         public boolean getShouldExpandEntityRefs() {
             return true;
         }
-        
+
         public boolean supportsSAX() {
             return true;
         }
