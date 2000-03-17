3639d5c400f894a5b533a144dcdb3abf092b35d8
Stefano Mazzocchi <stefano@apache.org>
2000-03-17 16:47:24 +0000

    fixed bug 45
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1045914 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/CVSROOT/src/org/apache/cocoon/processor/xsp/XSPUtil.java
+++ trunk/CVSROOT/src/org/apache/cocoon/processor/xsp/XSPUtil.java
@@ -1,36 +1,36 @@
-/*-- $Id: XSPUtil.java,v 1.12 2000-03-13 21:26:45 ricardo Exp $ -- 
+/*-- $Id: XSPUtil.java,v 1.13 2000-03-17 16:47:24 stefano Exp $ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
  ============================================================================
- 
+
  Copyright (C) @year@ The Apache Software Foundation. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of  source code must  retain the above copyright  notice,
     this list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
- 
+
  3. The end-user documentation included with the redistribution, if any, must
     include  the following  acknowledgment:  "This product includes  software
     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
- 
+
  4. The names "Cocoon" and  "Apache Software Foundation"  must not be used to
     endorse  or promote  products derived  from this  software without  prior
     written permission. For written permission, please contact
     apache@apache.org.
- 
+
  5. Products  derived from this software may not  be called "Apache", nor may
     "Apache" appear  in their name,  without prior written permission  of the
     Apache Software Foundation.
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -41,14 +41,14 @@
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
  on  behalf of the Apache Software  Foundation and was  originally created by
- Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache 
+ Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
  Software Foundation, please see <http://www.apache.org/>.
- 
+
  */
- 
+
  package org.apache.cocoon.processor.xsp;
 
 import java.io.*;
@@ -63,7 +63,7 @@ import org.apache.cocoon.Utils;
 
 /**
  * @author <a href="mailto:ricardo@apache.org">Ricardo Rocha</a>
- * @version $Revision: 1.12 $ $Date: 2000-03-13 21:26:45 $
+ * @version $Revision: 1.13 $ $Date: 2000-03-17 16:47:24 $
  */
 public class XSPUtil {
   public static String pathComponent(String filename) {
@@ -177,6 +177,7 @@ public class XSPUtil {
         return factory.createCDATASection(node.getNodeValue());
       }
       case Node.COMMENT_NODE: {
+        return factory.createComment(node.getNodeValue());
       }
       case Node.ENTITY_REFERENCE_NODE: {
         return factory.createEntityReference(node.getNodeValue());
@@ -195,7 +196,8 @@ public class XSPUtil {
         NodeList nodeList = input.getChildNodes();
         int childCount = nodeList.getLength();
         for (int i = 0; i < childCount; i++) {
-          output.appendChild(cloneNode(nodeList.item(i), factory));
+          Node child = cloneNode(nodeList.item(i), factory);
+          if (child != null) output.appendChild(child);
         }
 
         return output;
