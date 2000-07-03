4564a3b5b075ca2610df2e2ca95a243f7f08c289
Giacomo Pati <giacomo@apache.org>
2000-07-03 15:19:13 +0000

    PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046250 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/arch/config/SitemapConfigurationBuilder.java
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+package org.apache.arch.config;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Vector;
+import org.xml.sax.Attributes;
+import org.xml.sax.AttributeList;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.ext.LexicalHandler;
+
+/**
+ * This utility class will create a <code>Configuration</code> tree from an
+ * XML file parsed with a SAX version 1 or 2 parser.
+ *
+ * @author <a href="mailto:fumagalli@exoffice.com">Pierpaolo Fumagalli</a>
+ *         (Apache Software Foundation, Exoffice Technologies)
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2000-07-03 15:19:13 $
+ */
+public class SitemapConfigurationBuilder {
+
+    /**
+     * Return a new Configuration object.
+     */
+    public Configuration newConfiguration(String name) {
+        return(new ConfigurationImpl (name));
+    }
+
+    /**
+     * Add an attribute to this configuration element, returning its old
+     * value or <b>null</b>.
+     */
+    public void addAttribute(String name, String value, Configuration conf) {
+        ((ConfigurationImpl)conf).addAttribute(name,value);
+    }
+}
