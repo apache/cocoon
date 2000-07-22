ca8ae13a0afcf4ce332dd1b3ceb91a49b6fcd9a8
Giacomo Pati <giacomo@apache.org>
2000-07-22 22:40:55 +0000

    Another piece of the generated sitemap
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046327 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/reading/Reader.java
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+package org.apache.cocoon.reading;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.sitemap.SitemapComponent;
+
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author <a href="mailto:Giacomo.Pati@pwr.ch">Giacomo Pati</a>
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2000-07-22 22:40:55 $
+ */
+public interface Reader extends SitemapComponent {
+
+    /**
+     * Set the <code>OutputStream</code> where the XML should be serialized.
+     */
+    public void setOutputStream(OutputStream out);
+    public void generate() throws IOException, ProcessingException;
+}
