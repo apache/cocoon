39789a0fb0308a2dce96f4a0e2b05de671f40e0a
Stefano Mazzocchi <stefano@apache.org>
2000-08-17 17:06:25 +0000

    updated FOP and changed name ( the sitemap was updated)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046375 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/serialization/FOPSerializer.java
@@ -0,0 +1,97 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+package org.apache.cocoon.serialization;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import org.apache.fop.apps.Driver;
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.apps.Version;
+
+import org.apache.fop.messaging.MessageListener;
+import org.apache.fop.messaging.MessageHandler;
+import org.apache.fop.messaging.MessageEvent;
+
+import org.apache.cocoon.xml.util.XMLConsumerBridge;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+/**
+ * FOP serializer.
+ *
+ * @author <a href="mailto:giacomo.pati@pwr.ch">Giacomo Pati</a>
+ *         (PWR Organisation &amp; Entwicklung)
+ * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2000-08-17 17:06:25 $
+ *
+ */
+public class FOPSerializer extends XMLConsumerBridge implements Serializer, MessageListener {
+
+    /**
+     * The FOP driver 
+     */
+    private Driver driver = null;
+
+    /**
+     * Create the FOP driver 
+     */
+    public FOPSerializer() {
+        this.driver = new Driver();
+        this.driver.setRenderer("org.apache.fop.render.pdf.PDFRenderer", Version.getVersion());
+        this.driver.addElementMapping("org.apache.fop.fo.StandardElementMapping");
+        this.driver.addElementMapping("org.apache.fop.svg.SVGElementMapping");
+
+        // the use of static resources sucks for servlet enviornments
+        // since we could have multiple FOP that all logs in this pipe
+        // It's a concurrency and security nightmare! (SM)
+        MessageHandler.setOutputMethod(MessageHandler.EVENT);
+        MessageHandler.addListener(this);
+    }
+    
+    /**
+     * Set the <code>OutputStream</code> where the XML should be serialized.
+     */
+    public void setOutputStream(OutputStream out) {
+        this.setBridgedContentHandler(this.driver.getContentHandler());
+        this.driver.setWriter(new PrintWriter(out));
+     }
+ 
+    /** 
+     * Receive notification of the end of a document. 
+     */ 
+    public void endDocument() throws SAXException { 
+        super.endDocument();
+        try { 
+            driver.format(); 
+            driver.render(); 
+        } catch (IOException e) { 
+            throw new SAXException (e); 
+        } catch (FOPException e) { 
+            throw new SAXException (e); 
+        } 
+    }
+
+    /**
+     * Return the MIME type.
+     */
+    public String getMimeType() {
+        return "application/pdf";
+    }
+    
+    /** 
+     * Receive FOP events.
+     */ 
+    public void processMessage (MessageEvent event) {
+        // XXX (SM) 
+        // we should consume the events in some meaningful way
+        // for example formatting them on the metapipeline
+    }
+}
