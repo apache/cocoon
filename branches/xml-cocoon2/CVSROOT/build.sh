4c166abff20799e51ec1eb44e0836137d315e89f
Stefano Mazzocchi <stefano@apache.org>
2000-09-19 00:33:13 +0000

    sheesh, wrong one...
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046606 13f79535-47bb-0310-9956-ffa450edef68

--- branches/xml-cocoon2/CVSROOT/build.sh
+++ branches/xml-cocoon2/CVSROOT/build.sh
@@ -2,12 +2,27 @@
 # -----------------------------------------------------------------------------
 # build.sh - Unix Build Script for Apache Cocoon
 #
-# $Id: build.sh,v 1.10.2.11 2000-09-19 00:25:32 stefano Exp $
+# $Id: build.sh,v 1.10.2.12 2000-09-19 00:33:13 stefano Exp $
 # -----------------------------------------------------------------------------
 
-ANT_HOME=.
+# ----- Verify and Set Required Environment Variables -------------------------
+   
+if [ "$ANT_HOME" = "" ] ; then
+  ANT_HOME=.
+fi
+
+if [ "$JAVA_HOME" = "" ] ; then
+  echo You must set JAVA_HOME to point at your Java Development Kit installation
+  exit 1
+fi
+
+# ----- Set Up The Runtime Classpath ------------------------------------------
 
 CP=$JAVA_HOME/lib/tools.jar:$ANT_HOME/lib/ant.jar:./lib/xerces_1_2.jar
+ 
+# ----- Make sure Ant script is executable ------------------------------------
+
+chmod 0755 $ANT_HOME/bin/antRun
 
 # ----- Execute The Requested Build -------------------------------------------
 
