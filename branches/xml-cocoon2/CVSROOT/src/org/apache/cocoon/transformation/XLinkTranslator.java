42be16588d1d02a19f4b599d8e7b2f78fb60056e
Stefano Mazzocchi <stefano@apache.org>
2000-09-25 14:51:56 +0000

    added link translator (used for command line usage only)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046625 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/transformation/XLinkTranslator.java
@@ -0,0 +1,56 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+
+package org.apache.cocoon.transformation;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+
+import java.util.Map;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+import org.xml.sax.EntityResolver;
+
+import org.apache.avalon.utils.Parameters;
+
+import org.apache.cocoon.Cocoon;
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.xml.xlink.XLinkConsumerBridge;
+
+/**
+ * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2000-09-25 14:51:56 $
+ */
+
+public class XLinkTranslator extends XLinkConsumerBridge implements Transformer {
+
+    private Map links;
+    
+    /**
+     * Set the <code>EntityResolver</code>, objectModel <code>Map</code>,
+     * the source and sitemap <code>Parameters</code> used to process the request.
+     */
+    public void setup(EntityResolver resolver, Map objectModel, String src, Parameters par)
+    throws ProcessingException, SAXException, IOException {
+        this.links = (Map) objectModel.get(Cocoon.LINK_TRANSLATED_NAME);
+    }
+    
+    public void simpleLink(String href, String role, String arcrole, String title, String show, String actuate, String uri, String name, String raw, Attributes attr) 
+    throws SAXException {
+        String newhref = (String) this.links.get(href);
+        super.simpleLink((newhref != null) ? newhref : href, role, arcrole, title, show, actuate, uri, name, raw, attr);
+    }
+    
+    public void startLocator(String href, String role, String title, String label, String uri, String name, String raw, Attributes attr)
+    throws SAXException {
+        String newhref = (String) this.links.get(href);
+        super.startLocator((newhref != null) ? newhref : href, role, title, label, uri, name, raw, attr);
+    }
+}
