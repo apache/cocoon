ff3f9fbcb3f7b0737da01b476144bbfbca37cd0e
Giacomo Pati <giacomo@apache.org>
2000-10-08 20:56:38 +0000

    PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046687 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/acting/Action.java
@@ -0,0 +1,54 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+package org.apache.cocoon.acting;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.avalon.Component;
+import org.apache.avalon.ThreadSafe;
+import org.apache.avalon.utils.Parameters;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.EntityResolver;
+
+/**
+ *
+ * @author <a href="mailto:Giacomo.Pati@pwr.ch">Giacomo Pati</a>
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2000-10-08 20:56:38 $
+ */
+public interface Action extends Component, ThreadSafe {
+    /**
+     * Controls the processing against some values of the
+     * <code>Dictionary</code> objectModel and returns a
+     * <code>Map</code> object with values used in subsequent
+     * sitemap substitution patterns.
+     *
+     * NOTE: It is important that <code>Action<code> classes are
+     * written in a thread safe manner.
+     *
+     * @param resolver    The <code>EntityResolver</code> in charge
+     * @param objectModel The <code>Map</code> with object of the
+     *                    calling environment which can be used
+     *                    to select values this controller may need
+     *                    (ie HttpRequest, HttpResponse).
+     * @param parameters  The <code>Parameters</code> for this invocation
+     * @return List       The returned <code>List</code> object with
+     *                    sitemap substitution values which can be used
+     *                    in subsequent elements attributes like src=
+     *                    using a xpath like expression: src="mydir/{1}/foo"
+     *                    If the return value is null the processing inside
+     *                    the <map:act> element of the sitemap will
+     *                    be skipped.
+     * @exception Exception Indicates something is totally wrong
+     */
+    public List act (EntityResolver resolver, Map objectModel, Parameters parameters) throws Exception;
+}
+
+
+
