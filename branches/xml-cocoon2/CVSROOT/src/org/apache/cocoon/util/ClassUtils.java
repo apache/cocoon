2852bb2f397b36856e8290c06abe97686833a995
Giacomo Pati <giacomo@apache.org>
2000-10-08 21:10:51 +0000

    extended with new method to check for implemented interface of a class PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046697 13f79535-47bb-0310-9956-ffa450edef68

--- branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/util/ClassUtils.java
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/util/ClassUtils.java
@@ -18,82 +18,86 @@ import java.io.IOException;
  *
  * @author <a href="mailto:ricardo@apache.org">Ricardo Rocha</a>
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
- * @version CVS $Revision: 1.1.2.4 $ $Date: 2000-09-06 23:22:26 $
+ * @version CVS $Revision: 1.1.2.5 $ $Date: 2000-10-08 21:10:51 $
  */
 public class ClassUtils {
     
-  /**
-   * Create a new instance given a class name
-   *
-   * @param className A class name
-   * @return A new instance
-   * @exception Exception If an instantiation error occurs
-   */
-  public static Object newInstance(String className) throws Exception {
-    return loadClass(className).newInstance();
-  }
-
-  /**
-   * Load a class given its name
-   *
-   * @param className A class name
-   * @return The class pointed to by <code>className</code>
-   * @exception Exception If a loading error occurs
-   */
-  public static Class loadClass(String className) throws Exception {
-    // return getClassLoader().loadClass(className);
-    return Class.forName(className);
-  }
-
-  /**
-   * Return the context classloader
-   *
-   * @return The context classloader.
-   * @exception Exception If a loading error occurs
-   */
-  public static ClassLoader getClassLoader() {
-    // return Thread.currentThread().getContextClassLoader();
-    return ClassUtils.class.getClassLoader();
-  }
-
-  /**
-   * Determine the last modification date for this
-   * class file or its enclosing library
-   *
-   * @param aClass A class whose last modification date is queried
-   * @return The time the given class was last modified
-   * @exception IOException IOError
-   * @exception IllegalArgumentException The class was not loaded from a file
-   * or directory
-   */
-  public static long lastModified(Class aClass)
-    throws IOException, IllegalArgumentException
-  {
-    URL url =
-      aClass.
-      getProtectionDomain().
-      getCodeSource().
-      getLocation();
-
-    if (!url.getProtocol().equals("file")) {
-      throw new IllegalArgumentException("Class was not loaded from a file url");
+    /**
+     * Create a new instance given a class name
+     *
+     * @param className A class name
+     * @return A new instance
+     * @exception Exception If an instantiation error occurs
+     */
+    public static Object newInstance(String className) throws Exception {
+        return loadClass(className).newInstance();
     }
 
-    File directory = new File(url.getFile());
-    if (!directory.isDirectory()) {
-      throw new IllegalArgumentException("Class was not loaded from a directory");
+    /**
+     * Load a class given its name
+     *
+     * @param className A class name
+     * @return The class pointed to by <code>className</code>
+     * @exception Exception If a loading error occurs
+     */
+    public static Class loadClass(String className) throws Exception {
+        // return getClassLoader().loadClass(className);
+        return Class.forName(className);
     }
 
-    String className = aClass.getName();
-    String basename = className.substring(className.lastIndexOf(".") + 1);
+    /**
+     * Return the context classloader
+     *
+     * @return The context classloader.
+     * @exception Exception If a loading error occurs
+     */
+    public static ClassLoader getClassLoader() {
+        // return Thread.currentThread().getContextClassLoader();
+        return ClassUtils.class.getClassLoader();
+    }
 
-    File file = new File(
-      directory.getCanonicalPath() +
-      File.separator +
-      basename +
-      ".class"
-    );
+    /**
+     * Tests if a class implements a given interface
+     *
+     * @return true if class implements given interface.
+     */
+    public static boolean implementsInterface(String className, String iface) throws Exception {
+        Class class1 = loadClass (className);
+        Class class2 = loadClass (iface);
+        if (class2.isAssignableFrom (class1)) {
+            return true;
+        }
+        return false;
+    }
 
-    return file.lastModified();
-  }
+    /**
+     * Determine the last modification date for this
+     * class file or its enclosing library
+     *
+     * @param aClass A class whose last modification date is queried
+     * @return The time the given class was last modified
+     * @exception IOException IOError
+     * @exception IllegalArgumentException The class was not loaded from a file
+     * or directory
+     */
+    public static long lastModified(Class aClass)
+        throws IOException, IllegalArgumentException  {
+        URL url = aClass.getProtectionDomain().getCodeSource().getLocation();
+
+        if (!url.getProtocol().equals("file")) {
+            throw new IllegalArgumentException("Class was not loaded from a file url");
+        }
+
+        File directory = new File(url.getFile());
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException("Class was not loaded from a directory");
+        }
+
+        String className = aClass.getName();
+        String basename = className.substring(className.lastIndexOf(".") + 1);
+
+        File file = new File(directory.getCanonicalPath() + File.separator + basename + ".class");
+
+        return file.lastModified();
+    }
 }
