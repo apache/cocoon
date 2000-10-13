7b7405d00c922442b7fdd23eb14f1cf50a59efa3
Giacomo Pati <giacomo@apache.org>
2000-10-13 04:05:41 +0000

    Classes moved from org/apache/cocoon/sitemap PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046710 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/util/ComponentPool.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.cocoon.util;
+
+import org.apache.avalon.Poolable;
+import org.apache.avalon.ThreadSafe;
+import org.apache.avalon.util.pool.AbstractPool;
+import org.apache.avalon.util.pool.ObjectFactory;
+import org.apache.avalon.util.pool.PoolController;
+
+/**
+ * This is a implementation of <code>Pool</code> for SitemapComponents
+ * that is thread safe.
+ *
+ * @author <a href="mailto:Giacomo.Pati@pwr.ch">Giacomo Pati</a>
+ */
+public class ComponentPool extends AbstractPool implements ThreadSafe {
+
+    public final static int DEFAULT_POOL_SIZE = 16;
+
+    public ComponentPool(final ObjectFactory factory,
+                         final PoolController controller) throws Exception {
+        super(factory, controller, DEFAULT_POOL_SIZE/2, DEFAULT_POOL_SIZE);
+    }
+
+    public ComponentPool(final ObjectFactory factory,
+                         final PoolController controller,
+                         final int initial) throws Exception {
+        super(factory, controller, initial, initial);
+    }
+
+    public ComponentPool(final ObjectFactory factory,
+                         final PoolController controller,
+                         final int initial,
+                         final int maximum) throws Exception {
+        super(factory, controller, initial, maximum);
+    }
+
+    /**
+     * Retrieve an object from pool.
+     *
+     * @return an object from Pool
+     */
+    public final Poolable get() throws Exception {
+        synchronized(m_pool) {
+            return super.get();
+        }
+    }
+
+    /**
+     * Place an object in pool.
+     *
+     * @param poolable the object to be placed in pool
+     */
+    public final void put(final Poolable poolable) {
+        synchronized(m_pool) {
+            super.put(poolable);
+        }
+    }
+}
