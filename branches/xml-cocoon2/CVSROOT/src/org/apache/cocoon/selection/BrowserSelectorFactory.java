cb0a65e59a2a21e3830d25270c5d98d2dd952162
Giacomo Pati <giacomo@apache.org>
2000-10-30 18:40:48 +0000

    Reimplemented the usage of it's configuration. Because of some bugs in the Xercex/Xalan/XSP engines it could not be tested at the moment PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046806 13f79535-47bb-0310-9956-ffa450edef68

--- branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/selection/BrowserSelectorFactory.java
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/selection/BrowserSelectorFactory.java
@@ -26,7 +26,7 @@ import org.apache.cocoon.CodeFactory;
  * @author <a href="mailto:cziegeler@sundn.de">Carsten Ziegeler</a>
  * @author <a href="mailto:Giacomo.Pati@pwr.ch">Giacomo Pati</a>
  * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
- * @version CVS $Revision: 1.1.2.8 $ $Date: 2000-10-25 16:02:18 $
+ * @version CVS $Revision: 1.1.2.9 $ $Date: 2000-10-30 18:40:48 $
 */
 
 
@@ -34,12 +34,38 @@ public class BrowserSelectorFactory implements CodeFactory {
 
     public String generateParameterSource (DocumentFragment conf)
     throws ConfigurationException {
-        return "String";
+        return "String []";
     }
 
     public String generateClassSource (String test, String prefix, DocumentFragment conf)
     throws ConfigurationException {
-        return "\nstatic String " + prefix + "_expr = \"" + test + "\";\n";
+        TreeWalker tw = new TreeWalkerImpl (conf, NodeFilter.SHOW_ALL, null, false);
+        Node node = null;
+        Node nodeattrname  = null;
+        Node nodeattruseragent = null;
+        NamedNodeMap nm = null;
+        int cnt = 0;
+        StringBuffer sb = new StringBuffer();
+        sb.append("static String [] ")
+          .append(prefix)
+          .append("_expr = {");
+        while ((node = tw.nextNode()) != null) {
+            if (node.getNodeName().equals("browser") &&
+                node.getNodeType() == Node.ELEMENT_NODE) {
+                nm = node.getAttributes();
+                if (nm != null) {
+                    nodeattrname = nm.getNamedItem("name");
+                    nodeattruseragent = nm.getNamedItem("useragent");
+                    if (nodeattrname != null && nodeattruseragent != null
+                            && nodeattrname.getNodeValue().equals(test)) {
+                        sb.append(cnt==0 ? "\"" : ",\"")
+                          .append(nodeattruseragent.getNodeValue())
+                          .append("\"");
+                    }
+                }
+            }
+        }
+        return sb.append("};").toString();
     }
 
     public String generateMethodSource (DocumentFragment conf)
@@ -51,8 +77,8 @@ public class BrowserSelectorFactory implements CodeFactory {
         NamedNodeMap nm = null;
 
         StringBuffer sb = new StringBuffer();
-         sb.append("if (pattern != null && objectModel.get(\"request\") != null) {")
-          .append("javax.servlet.http.HttpServletRequest request = (javax.servlet.http.HttpServletRequest) objectModel.get(\"request\");")
+         sb.append("if (pattern != null && objectModel.get(Cocoon.REQUEST_OBJECT) != null) {")
+          .append("javax.servlet.http.HttpServletRequest request = (javax.servlet.http.HttpServletRequest) objectModel.get(Cocoon.REQUEST_OBJECT);")
           .append("String userAgent = request.getHeader(\"User-Agent\");")
           .append("if (userAgent.indexOf(pattern) != -1) return true;");
         return sb.append("} return false;").toString();
