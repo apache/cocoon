59e4de19579ec516e5e57251ea980308e45eb8f0
Berin Loritsch <bloritsch@apache.org>
2000-11-16 18:02:26 +0000

    Added patch for RegExp parenthesis.  Started work on automating the precompiled regexp lists.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046903 13f79535-47bb-0310-9956-ffa450edef68

--- branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/matching/RegexpTargetHostMatcherFactory.java
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/matching/RegexpTargetHostMatcherFactory.java
@@ -21,10 +21,11 @@ import org.w3c.dom.traversal.NodeIterator;
  *
  * @author <a href="mailto:Giacomo.Pati@pwr.ch">Giacomo Pati</a>
  * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
- * @version CVS $Revision: 1.1.2.15 $ $Date: 2000-11-16 18:02:26 $
+ * @author <a href="mailto:paul@luminas.co.uk">Paul Russell</a>
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2000-11-16 18:02:26 $
  */
 
-public class RegexpURIMatcherFactory implements MatcherFactory {
+public class RegexpTargetHostMatcherFactory implements MatcherFactory {
     public String generateParameterSource (NodeIterator conf)
     throws ConfigurationException {
         return "RE";
@@ -39,16 +40,16 @@ public class RegexpURIMatcherFactory implements MatcherFactory {
             String name         = prefix;
             String instructions = name + "PatternInstructions";
             String pat = correctPattern (pattern);
-            sb.append("// Pre-compiled regular expression '")
+            sb.append("\n    // Pre-compiled regular expression '")
               .append(pat).append("'\n")
               .append("    static char[] ");
-            sb.append(instructions).append(" = {");
+            sb.append(instructions).append(" = \n    {");
             REProgram program = r.compile(pat);
             int numColumns = 7;
             char[] p = program.getInstructions();
             for (int j = 0; j < p.length; j++) {
                 if ((j % numColumns) == 0) {
-                    sb.append("\n");
+                    sb.append("\n        ");
                 }
                 String hex = Integer.toHexString(p[j]);
                 while (hex.length() < 4) {
@@ -56,28 +57,12 @@ public class RegexpURIMatcherFactory implements MatcherFactory {
                 }
                 sb.append("0x").append(hex).append(", ");
             }
-            sb.append("\n};\nstatic RE ")
+            sb.append("\n    };")
+              .append("\n    static RE ")
               .append(name)
               .append("_expr = new RE(new REProgram(")
               .append(instructions)
-              .append("));\n")
-              .append("static ArrayList ")
-              .append(name)
-              .append("_list = new ArrayList();\n")
-              .append("static {");
-            // Count number of parens
-            int i = 0;
-            int j = -1;
-            while ((j = pat.indexOf('(', j+1)) != -1) {
-                if (j == 0 || pat.charAt(j-1) != '\\') {
-                    sb.append(name)
-                      .append("_list.add (")
-                      .append(name)
-                      .append("_expr.getParen(")
-                      .append(++i).append("));");
-                }
-            }
-            sb.append("}");
+              .append("));");
             return sb.toString();
         } catch (RESyntaxException rse) {
             throw new ConfigurationException (rse.getMessage());
@@ -88,18 +73,17 @@ public class RegexpURIMatcherFactory implements MatcherFactory {
     throws ConfigurationException {
         StringBuffer sb = new StringBuffer ();
         sb.append("ArrayList list = new ArrayList ();")
-          .append("String uri = ((HttpServletRequest)objectModel.get(Cocoon.REQUEST_OBJECT)).getRequestURI();")
+          .append("String uri = ((HttpServletRequest)objectModel.get(Cocoon.REQUEST_OBJECT)).getHeader(\"Host\");")
           .append("if(uri.startsWith(\"/\")) uri = uri.substring(1);")
           .append("if(pattern.match(uri)) {");
-          /* Handle parenthesised subexpressions. XXX: could be faster if we count
-           * parens *outside* the generated code.
-           * Note: *ONE* based, not zero.
-           */
-          sb.append("int parenCount = pattern.getParenCount();")
-            .append("for (int paren = 1; paren <= parenCount; paren++) {")
-            .append("list.add(pattern.getParen(paren));")
-            .append("}");
-
+        /* Handle parenthesised subexpressions. XXX: could be faster if we count
+         * parens *outside* the generated code.
+         * Note: *ONE* based, not zero.
+         */
+        sb.append("int parenCount = pattern.getParenCount();")
+          .append("for (int paren = 1; paren <= parenCount; paren++) {")
+          .append("list.add(pattern.getParen(paren));")
+          .append("}");
         sb.append("return list; } else { return null; }");
         return sb.toString();
     }
