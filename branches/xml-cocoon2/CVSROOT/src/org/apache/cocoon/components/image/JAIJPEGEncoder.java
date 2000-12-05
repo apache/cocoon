c72102c9459e2fb5e090d3323af95750de3456d3
Berin Loritsch <bloritsch@apache.org>
2000-12-05 00:23:11 +0000

    Slight formatting, and log messages.  Still trying to locate 500 ok error for WebSphere.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1046971 13f79535-47bb-0310-9956-ffa450edef68

--- branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/components/image/JAIJPEGEncoder.java
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/components/image/JAIJPEGEncoder.java
@@ -32,27 +32,54 @@ public class JAIJPEGEncoder implements ImageEncoder, Configurable, ThreadSafe {
     private Logger log = LogKit.getLoggerFor("cocoon");
 
     /** The quality level. The default is 0.75 (high quality) */
-    private float quality;
+    private float quality = -1.0f;
 
+    /**
+     * Configure the JAIJPEGEncoder.  The implementation enforces proper
+     * semantics on Configurable (write once), and as such enforces no
+     * race conditions on <code>quality</code>.
+     */
     public void configure(Configuration conf) throws ConfigurationException {
-        // Using the passed Configuration, generate a far more friendly Parameters object.
-        Parameters p = Parameters.fromConfiguration(conf);
-        quality = p.getParameterAsFloat("quality", 0.75f);
-        log.debug("Quality set to " + quality);
+        if (quality == -1.0f) {
+            // Using the passed Configuration, generate a far more friendly Parameters object.
+            Parameters p = Parameters.fromConfiguration(conf);
+            quality = p.getParameterAsFloat("quality", 0.75f);
+            log.debug("Quality set to " + quality);
+        }
     }
 
     public String getMimeType() {
         return "image/jpeg";
     }
 
+    /**
+     * This method starts the whole shebang.  The JPEG encoder requires
+     * 3 band byte-based images, so this encoder checks to see if the
+     * type is correct.  If the image type is not correct, it creates
+     * a new image with the proper type.
+     *
+     * @param image The <code>BufferedImage</code> we are getting ready
+     *              to serialize.
+     * @param out   The <code>OutputStream</code> we are serializing.
+     */
     public void encode(BufferedImage image, OutputStream out) throws IOException {
         // The JPEG encoder requires 3 band byte-based images, so create a new image
         // TODO: find a faster way of doing this
         int w = image.getWidth();
         int h = image.getHeight();
-        BufferedImage noalpha = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
-        Raster raster = image.getRaster().createChild(0, 0, w, h, 0, 0, new int[] {0, 1, 2});
-        noalpha.setData(raster);
+        BufferedImage noalpha;
+
+        if (image.getType() == BufferedImage.TYPE_3BYTE_BGR) {
+            noalpha = image;
+        } else {
+            noalpha = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
+            Raster raster = image.getRaster().createChild(0, 0, w, h, 0, 0, new int[] {0, 1, 2});
+            noalpha.setData(raster);
+        }
+
+        // Make sure quality is set properly: if configure() has not
+        // been called then too bad.
+        if (quality == -1.0f) quality = 0.75f;
 
         JPEGEncodeParam param = new JPEGEncodeParam();
         param.setQuality(quality);
