f319df25297d04166d47c46c8abc265606b71df3
greenrd <greenrd@unknown>
2000-12-13 15:27:52 +0000

    more ways to set classpath
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1047038 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/CVSROOT/src/org/apache/cocoon/processor/xsp/XSPProcessor.java
+++ trunk/CVSROOT/src/org/apache/cocoon/processor/xsp/XSPProcessor.java
@@ -1,4 +1,4 @@
-/*-- $Id: XSPProcessor.java,v 1.33 2000-12-01 23:36:44 greenrd Exp $ --
+/*-- $Id: XSPProcessor.java,v 1.34 2000-12-13 15:27:52 greenrd Exp $ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
@@ -78,7 +78,7 @@ import org.apache.turbine.services.resources.TurbineResourceService;
  * This class implements the XSP engine.
  *
  * @author <a href="mailto:ricardo@apache.org">Ricardo Rocha</a>
- * @version $Revision: 1.33 $ $Date: 2000-12-01 23:36:44 $
+ * @version $Revision: 1.34 $ $Date: 2000-12-13 15:27:52 $
  */
 public class XSPProcessor extends AbstractActor
   implements Processor, Configurable, Status, Cacheable
@@ -212,13 +212,20 @@ public class XSPProcessor extends AbstractActor
     }
 
     // Languages other than Java may also need to use the classpath to link to Java code
-    // Try to get the classpath from catalina or tomcat first; if that fails use the
-    // standard one.
-    String classpath = (String) 
-      this.servletContext.getAttribute("org.apache.catalina.jsp_classpath");
+    // Try to get the classpath from conf or Catalina or Tomcat or Resin first;
+    // if that all fails, use the standard one.
+    String classpath = (String) conf.get ("localclasspath");
     if (classpath == null) {
       classpath = (String)
-        this.servletContext.getAttribute("org.apache.tomcat.jsp_classpath");
+        this.servletContext.getAttribute("org.apache.catalina.jsp_classpath");
+      if (classpath == null) {
+        classpath = (String)
+          this.servletContext.getAttribute("org.apache.tomcat.jsp_classpath");
+        if (classpath == null) {
+          classpath = (String)
+            this.servletContext.getAttribute ("caucho.class.path");
+        }
+      }
     }
     if (classpath == null) 
       classpath = "";
