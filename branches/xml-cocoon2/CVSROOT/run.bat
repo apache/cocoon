2c178571791e1fec3fbd48e1c6b35fb6b84804ad
Giacomo Pati <giacomo@apache.org>
2000-12-16 14:21:50 +0000

    CLI opertation is now working again. I had to make sure that the work directory is included in the CLASSPATH and that it exists. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1047056 13f79535-47bb-0310-9956-ffa450edef68

--- branches/xml-cocoon2/CVSROOT/run.bat
+++ branches/xml-cocoon2/CVSROOT/run.bat
@@ -2,7 +2,7 @@
 :: -----------------------------------------------------------------------------
 :: run.bat - Win32 Run Script for Apache Cocoon
 ::
-:: $Id: run.bat,v 1.1.2.1 2000-09-25 14:02:32 stefano Exp $
+:: $Id: run.bat,v 1.1.2.2 2000-12-16 14:21:50 giacomo Exp $
 :: -----------------------------------------------------------------------------
 
 :: ----- Verify and Set Required Environment Variables -------------------------
@@ -18,9 +18,13 @@ if not "%COCOON_LIB%" == "" goto gotCocoonLib
 set COCOON_LIB=.\lib
 :gotCocoonLib
 
+if not "%COCOON_WORK%" == "" goto gotCocoonWork
+set COCOON_WORK=.\work
+:gotCocoonWork
+
 :: ----- Set Up The Runtime Classpath ------------------------------------------
 
-set CP=%JAVA_HOME%\lib\tools.jar
+set CP=%JAVA_HOME%\lib\tools.jar;%COCOON_WORK%
 for %%i in (%COCOON_LIB%\*.jar) do call appendcp.bat %%i
 
 :: ----- Run Cocoon ------------------------------------------------------------
--- branches/xml-cocoon2/CVSROOT/run.sh
+++ branches/xml-cocoon2/CVSROOT/run.sh
@@ -2,11 +2,11 @@
 # -----------------------------------------------------------------------------
 # run.sh - Unix Run Script for Apache Cocoon
 #
-# $Id: run.sh,v 1.1.2.1 2000-09-25 14:02:32 stefano Exp $
+# $Id: run.sh,v 1.1.2.2 2000-12-16 14:21:50 giacomo Exp $
 # -----------------------------------------------------------------------------
 
 # ----- Verify and Set Required Environment Variables -------------------------
-   
+
 if [ "$JAVA_HOME" = "" ] ; then
   echo You must set JAVA_HOME to point at your Java Development Kit installation
   exit 1
@@ -18,9 +18,14 @@ if [ "$COCOON_LIB" = "" ] ; then
   COCOON_LIB=./lib
 fi
 
+if [ "$COCOON_WORK" = "" ] ; then
+  COCOON_WORK=./work
+  mkdir $COCOON_WORK
+fi
+
 # ----- Set Up The Runtime Classpath ------------------------------------------
 
-CP=`echo $COCOON_LIB/*.jar | tr ' ' ':'`:$JAVA_HOME/lib/tools.jar:$CLASSPATH
+CP=`echo $COCOON_LIB/*.jar | tr ' ' ':'`:$JAVA_HOME/lib/tools.jar:$COCOON_WORK:$CLASSPATH
 
 # ----- Run Cocoon ------------------------------------------------------------
 
