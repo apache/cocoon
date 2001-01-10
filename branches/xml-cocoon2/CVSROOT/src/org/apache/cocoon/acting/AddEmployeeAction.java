97cc8b33271b9665d8e8f72152038e5ea46bacfd
Berin Loritsch <bloritsch@apache.org>
2001-01-10 17:31:58 +0000

    Cleaned up examples and implemented some real logic to the Action
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1047145 13f79535-47bb-0310-9956-ffa450edef68

--- branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/acting/AddEmployeeAction.java
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/acting/AddEmployeeAction.java
@@ -7,6 +7,10 @@
  *****************************************************************************/
 package org.apache.cocoon.acting;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,9 +18,14 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.ServletContext;
 
 import org.apache.avalon.Component;
+import org.apache.avalon.ComponentSelector;
+import org.apache.avalon.ComponentManagerException;
 import org.apache.avalon.Configuration;
 import org.apache.avalon.ConfigurationException;
 import org.apache.avalon.Parameters;
+
+import org.apache.cocoon.Roles;
+import org.apache.cocoon.components.datasource.DataSourceComponent;
 
 import org.xml.sax.SAXException;
 import org.xml.sax.EntityResolver;
@@ -25,14 +34,22 @@ import org.apache.cocoon.Constants;
 /**
  *
  * @author <a href="mailto:Giacomo.Pati@pwr.ch">Giacomo Pati</a>
- * @version CVS $Revision: 1.1.2.2 $ $Date: 2000-12-30 23:01:01 $
+ * @version CVS $Revision: 1.1.2.3 $ $Date: 2001-01-10 17:31:56 $
  */
 public class AddEmployeeAction extends ComposerAction {
+
+    DataSourceComponent datasource = null;
 
     /**
      * Get the <code>Configuration</code> object for this <code>Component</code>
      */
     public void configure( Configuration configuration) throws ConfigurationException {
+        try {
+            ComponentSelector selector = (ComponentSelector) this.manager.lookup(Roles.DB_CONNECTION);
+            this.datasource = (DataSourceComponent) selector.select(configuration.getChild("use-connection").getValue());
+        } catch (ComponentManagerException cme) {
+            throw new ConfigurationException("Could not get the DataSource Object", cme);
+        }
     }
 
     /**
@@ -41,7 +58,58 @@ public class AddEmployeeAction extends ComposerAction {
      */
     public Map act (EntityResolver resolver, Map objectModel, String src, Parameters par) throws Exception {
         HttpServletRequest req = (HttpServletRequest) objectModel.get(Constants.REQUEST_OBJECT);
-        req.setAttribute("message", "You have added the employee " + req.getParameter("name"));
+        String name = req.getParameter("name");
+        String department = req.getParameter("department");
+
+        if (addEmployee(name, department) == true) {
+            req.setAttribute("message", "You have added the employee " + name);
+        } else {
+            req.setAttribute("message", "You did not add the employee " + name);
+        }
         return null;
+    }
+
+    private boolean addEmployee(String name, String department) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        boolean returnValue = false;
+
+        try {
+            conn = datasource.getConnection();
+            conn.setAutoCommit(false);
+            rs = conn.createStatement().executeQuery("SELECT Max(id) AS maxid FROM employee_table;");
+            int maxid = -1;
+
+            if (rs.next() == true) {
+                maxid = rs.getInt("maxid");
+
+                ps = conn.prepareStatement("INSERT INTO employee_table (id, name, department_id) VALUES (?, ?, ?)");
+                ps.setInt(1, maxid);
+                ps.setString(2, name);
+                ps.setString(3, department);
+
+                returnValue = ps.execute();
+            }
+        } catch (SQLException se) {
+            // returnValue = false;
+        } finally {
+            try {
+                if (returnValue = false) {
+                    conn.rollback();
+                } else {
+                    conn.commit();
+                }
+
+                ps.close();
+                rs.close();
+                rs.getStatement().close();
+                conn.close();
+            } catch (Exception e) {
+                // we should never be here
+            }
+        }
+
+        return returnValue;
     }
 }
