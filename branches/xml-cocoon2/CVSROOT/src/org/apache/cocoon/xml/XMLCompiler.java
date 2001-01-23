e1c628b4a90799c999e1e0582515057a721a6882
Paul Russell <prussell@apache.org>
2001-01-23 17:20:48 +0000

    Added XML Compiler classes in preparation for patches to FileGenerator and XalanTransformer from Carsten Ziegeler.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1047291 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/xml/XMLCompiler.java
@@ -0,0 +1,191 @@
+/**
+ ****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ ****************************************************************************
+ */
+package org.apache.cocoon.xml;
+
+import java.io.*;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
+
+/**
+ * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2001-01-23 17:20:42 $
+ */
+
+public final class XMLCompiler implements ContentHandler {
+
+    private CompiledXMLOutputStream out;
+
+    public void setOutputStream(OutputStream stream) throws IOException {
+        this.out = new CompiledXMLOutputStream(stream);
+    }
+
+    public static final int START_DOCUMENT         = 0;
+    public static final int END_DOCUMENT           = 1;
+    public static final int START_PREFIX_MAPPING   = 2;
+    public static final int END_PREFIX_MAPPING     = 3;
+    public static final int START_ELEMENT          = 4;
+    public static final int END_ELEMENT            = 5;
+    public static final int CHARACTERS             = 6;
+    public static final int IGNORABLE_WHITESPACE   = 7;
+    public static final int PROCESSING_INSTRUCTION = 8;
+
+
+        public void startDocument() throws SAXException {
+            try {
+                out.writeEvent(START_DOCUMENT);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void endDocument() throws SAXException {
+            try {
+                out.writeEvent(END_DOCUMENT);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void startPrefixMapping(java.lang.String prefix, java.lang.String uri) throws SAXException {
+            try {
+                out.writeEvent(START_PREFIX_MAPPING);
+                out.writeString(prefix);
+                out.writeString(uri);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void endPrefixMapping(java.lang.String prefix) throws SAXException {
+            try {
+                out.writeEvent(END_PREFIX_MAPPING);
+                out.writeString(prefix);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void startElement(java.lang.String namespaceURI, java.lang.String localName, java.lang.String qName, Attributes atts) throws SAXException {
+            try {
+                int length = atts.getLength();
+                out.writeEvent(START_ELEMENT);
+                out.writeAttributes(length);
+                for (int i = 0; i < length; i++) {
+                    out.writeString(atts.getURI(i));
+                    out.writeString(atts.getLocalName(i));
+                    out.writeString(atts.getQName(i));
+                    out.writeString(atts.getType(i));
+                    out.writeString(atts.getValue(i));
+                }
+                out.writeString(namespaceURI);
+                out.writeString(localName);
+                out.writeString(qName);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void endElement(java.lang.String namespaceURI, java.lang.String localName, java.lang.String qName) throws SAXException {
+            try {
+                out.writeEvent(END_ELEMENT);
+                out.writeString(namespaceURI);
+                out.writeString(localName);
+                out.writeString(qName);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            try {
+                out.writeEvent(CHARACTERS);
+                out.writeChars(ch, start, length);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+            try {
+                out.writeEvent(IGNORABLE_WHITESPACE);
+                out.writeChars(ch, start, length);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void processingInstruction(java.lang.String target, java.lang.String data) throws SAXException {
+            try {
+                out.writeEvent(PROCESSING_INSTRUCTION);
+                out.writeString(target);
+                out.writeString(data);
+            } catch (Exception e) {
+                throw new SAXException(e);
+            }
+        }
+
+        public void setDocumentLocator(Locator locator) {
+            // ignore.
+        }
+
+        public void skippedEntity(java.lang.String name) throws SAXException {
+            // ignore.
+        }
+    }
+
+    class ErrorHandler implements org.xml.sax.ErrorHandler {
+
+        /** Warning. */
+        public void warning(SAXParseException ex) {
+            System.err.println("[Warning] "+
+                               getLocationString(ex)+": "+
+                               ex.getMessage());
+        }
+
+        /** Error. */
+        public void error(SAXParseException ex) {
+            System.err.println("[Error] "+
+                               getLocationString(ex)+": "+
+                               ex.getMessage());
+        }
+
+        /** Fatal error. */
+        public void fatalError(SAXParseException ex) throws SAXException {
+            System.err.println("[Fatal Error] "+
+                               getLocationString(ex)+": "+
+                               ex.getMessage());
+        }
+
+        /** Returns a string of the location. */
+        private String getLocationString(SAXParseException ex) {
+            StringBuffer str = new StringBuffer();
+
+            String systemId = ex.getSystemId();
+            if (systemId != null) {
+                int index = systemId.lastIndexOf('/');
+                if (index != -1)
+                    systemId = systemId.substring(index + 1);
+                str.append(systemId);
+            }
+            str.append(':');
+            str.append(ex.getLineNumber());
+            str.append(':');
+            str.append(ex.getColumnNumber());
+
+            return str.toString();
+        }
+
+
+}
---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/xml/XMLInterpreter.java
@@ -0,0 +1,160 @@
+/**
+ ****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ ****************************************************************************
+ */
+package org.apache.cocoon.xml;
+
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+import java.util.HashMap;
+
+import org.xml.sax.XMLReader;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.DTDHandler;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
+import org.xml.sax.helpers.AttributesImpl;
+
+/**
+ * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2001-01-23 17:20:45 $
+ */
+
+public final class XMLInterpreter implements XMLReader {
+
+    public static final int START_DOCUMENT         = 0;
+    public static final int END_DOCUMENT           = 1;
+    public static final int START_PREFIX_MAPPING   = 2;
+    public static final int END_PREFIX_MAPPING     = 3;
+    public static final int START_ELEMENT          = 4;
+    public static final int END_ELEMENT            = 5;
+    public static final int CHARACTERS             = 6;
+    public static final int IGNORABLE_WHITESPACE   = 7;
+    public static final int PROCESSING_INSTRUCTION = 8;
+
+    private ContentHandler contentHandler;
+    private DTDHandler dtdHandler;
+    private EntityResolver entityResolver;
+    private ErrorHandler errorHandler;
+
+    private HashMap features = new HashMap();
+    private HashMap properties = new HashMap();
+
+    public ContentHandler getContentHandler() {
+        return this.contentHandler;
+    }
+
+    public DTDHandler getDTDHandler() {
+        return this.dtdHandler;
+    }
+
+    public EntityResolver getEntityResolver() {
+        return this.entityResolver;
+    }
+
+    public ErrorHandler getErrorHandler() {
+        return this.errorHandler;
+    }
+
+    public void setContentHandler(ContentHandler handler) {
+        this.contentHandler = handler;
+    }
+
+    public void setDTDHandler(DTDHandler handler) {
+        this.dtdHandler = handler;
+    }
+
+    public void setEntityResolver(EntityResolver resolver) {
+        this.entityResolver = resolver;
+    }
+
+    public void setErrorHandler(ErrorHandler handler) {
+        this.errorHandler = handler;
+    }
+
+    public boolean getFeature(java.lang.String name) throws SAXNotRecognizedException, SAXNotSupportedException {
+        Object o = this.features.get(name);
+        return ((o != null) && ((Boolean) o).booleanValue());
+    }
+
+    public Object getProperty(java.lang.String name) throws SAXNotRecognizedException, SAXNotSupportedException {
+        return this.properties.get(name);
+    }
+
+    public void setFeature(java.lang.String name, boolean value) throws SAXNotRecognizedException, SAXNotSupportedException {
+        this.features.put(name, new Boolean(value));
+    }
+
+    public void setProperty(java.lang.String name, java.lang.Object value) throws SAXNotRecognizedException, SAXNotSupportedException {
+        this.properties.put(name, value);
+    }
+
+    public void parse(java.lang.String systemId) throws IOException, SAXException {
+        parse(new FileInputStream(systemId));
+    }
+
+    public void parse(InputSource in) throws IOException, SAXException {
+        parse(in.getByteStream());
+    }
+
+    public void parse(InputStream stream) throws IOException, SAXException {
+        CompiledXMLInputStream input = new CompiledXMLInputStream(stream);
+
+        while (true) {
+            switch (input.readEvent()) {
+                case START_DOCUMENT:
+                    contentHandler.startDocument();
+                    break;
+                case END_DOCUMENT:
+                    contentHandler.endDocument();
+                    return;
+                case START_PREFIX_MAPPING:
+                    contentHandler.startPrefixMapping(input.readString(), input.readString());
+                    break;
+                case END_PREFIX_MAPPING:
+                    contentHandler.endPrefixMapping(input.readString());
+                    break;
+                case START_ELEMENT:
+                    int attributes = input.readAttributes();
+                    AttributesImpl atts = new AttributesImpl();
+                    for (int i = 0; i < attributes; i++) {
+                        atts.addAttribute(input.readString(), input.readString(), input.readString(), input.readString(), input.readString());
+                    }
+                    contentHandler.startElement(input.readString(), input.readString(), input.readString(), atts);
+                    break;
+                case END_ELEMENT:
+                    contentHandler.endElement(input.readString(), input.readString(), input.readString());
+                    break;
+                case CHARACTERS:
+                    char[] chars = input.readChars();
+                    int len = chars.length;
+                    while (chars[len-1]==0) len--;
+                    contentHandler.characters(chars, 0, len);
+                    break;
+                case IGNORABLE_WHITESPACE:
+                    char[] spaces = input.readChars();
+                    len = spaces.length;
+                    while (spaces[len-1]==0) len--;
+                    contentHandler.characters(spaces, 0, len);
+                    break;
+                case PROCESSING_INSTRUCTION:
+                    contentHandler.processingInstruction(input.readString(), input.readString());
+                    break;
+                default:
+                    throw new IOException ("parsing error: event not supported.");
+            }
+        }
+    }
+}
