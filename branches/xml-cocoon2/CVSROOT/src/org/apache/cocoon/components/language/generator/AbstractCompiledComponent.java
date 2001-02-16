3bcb625971877268f06ed3503ec5322ae9f30fab
Berin Loritsch <bloritsch@apache.org>
2001-02-16 19:09:04 +0000

    Added this component, I hate when that happens
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1047464 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/components/language/generator/AbstractCompiledComponent.java
@@ -0,0 +1,34 @@
+/*****************************************************************************
+ * Copyright (C) The Apache Software Foundation. All rights reserved.        *
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included  with this distribution in *
+ * the LICENSE file.                                                         *
+ *****************************************************************************/
+package org.apache.cocoon.components.language.generator;
+
+import org.apache.avalon.AbstractLoggable;
+import org.apache.avalon.ComponentManager;
+import org.apache.avalon.util.pool.Pool;
+import org.apache.cocoon.PoolClient;
+
+/**
+ * This interface is the common base of all Compiled Components.  This
+ * includes Sitemaps and XSP Pages
+ *
+ * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
+ * @version CVS $Revision: 1.1.2.1 $ $Date: 2001-02-16 19:09:04 $
+ */
+public abstract class AbstractCompiledComponent extends AbstractLoggable implements PoolClient, CompiledComponent {
+    private Pool pool;
+
+    public void setPool(Pool pool) {
+        if (this.pool == null) {
+           this.pool = pool;
+        }
+    }
+
+    public void recycle() {
+       this.pool.put(this);
+    }
+}
