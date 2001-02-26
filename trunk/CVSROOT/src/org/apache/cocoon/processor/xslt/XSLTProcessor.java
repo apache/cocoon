1f659fca3c3030ce005df9939217709fe8558516
greenrd <greenrd@unknown>
2001-02-26 17:31:38 +0000

    added browser parameter to XSLTProcessor - WARNING - If you use this you MUST set the HTTP Vary header or proxies will not cache correctly
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1047556 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/CVSROOT/src/org/apache/cocoon/processor/xslt/XSLTProcessor.java
+++ trunk/CVSROOT/src/org/apache/cocoon/processor/xslt/XSLTProcessor.java
@@ -1,4 +1,4 @@
-/*-- $Id: XSLTProcessor.java,v 1.28 2001-02-07 19:30:54 greenrd Exp $ --
+/*-- $Id: XSLTProcessor.java,v 1.29 2001-02-26 17:31:38 greenrd Exp $ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
@@ -75,7 +75,7 @@ import org.apache.cocoon.Defaults;
  * This class implements an XSLT processor.
  *
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
- * @version $Revision: 1.28 $ $Date: 2001-02-07 19:30:54 $
+ * @version $Revision: 1.29 $ $Date: 2001-02-26 17:31:38 $
  */
 
 public class XSLTProcessor implements Actor, Processor, Status, Defaults, Cacheable {
@@ -108,6 +108,7 @@ public class XSLTProcessor implements Actor, Processor, Status, Defaults, Cachea
         Hashtable params = this.filterParameters(request);
         params.put ("XSP-ENVIRONMENT", Cocoon.version());
         params.put ("XSP-VERSION", XSPProcessor.version());
+        params.put ("COCOON-BROWSER-ID", browser);
 
         try {
             Object resource = getResource(context, request, response, document, path, browser);
