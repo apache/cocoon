431481bb77cc2c759a77afecbc4e4d7efd2d2d9e
Berin Loritsch <bloritsch@apache.org>
2001-02-26 20:50:48 +0000

    Add the templates for the implementation
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1047561 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/xml-cocoon2/CVSROOT/src/org/apache/cocoon/acting/DatabaseUpdateAction.java
@@ -0,0 +1,24 @@
+package org.apache.cocoon.acting;
+
+import java.util.Map;
+import org.xml.sax.EntityResolver;
+import org.apache.avalon.Parameters;
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2000
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+public class DatabaseUpdateAction extends ComposerAction {
+
+    public DatabaseUpdateAction() {
+    }
+    public Map act(EntityResolver parm1, Map parm2, String parm3, Parameters parm4) throws java.lang.Exception {
+        /**@todo: implement this org.apache.cocoon.acting.AbstractAction abstract method*/
+        return null;
+    }
+}
