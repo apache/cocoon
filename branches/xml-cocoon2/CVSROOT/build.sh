f72a6c00f17005bbe11f4257e7a608e6670dd584
Giacomo Pati <giacomo@apache.org>
2001-03-05 19:56:08 +0000

    Update Ant 1.3 PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1047627 13f79535-47bb-0310-9956-ffa450edef68

--- branches/xml-cocoon2/CVSROOT/build.sh
+++ branches/xml-cocoon2/CVSROOT/build.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 # build.sh - Unix Build Script for Apache Cocoon
 #
-# $Id: build.sh,v 1.10.2.23 2001-02-28 13:26:01 dims Exp $
+# $Id: build.sh,v 1.10.2.24 2001-03-05 19:56:08 giacomo Exp $
 # -----------------------------------------------------------------------------
 
 # ----- Verify and Set Required Environment Variables -------------------------
@@ -18,7 +18,7 @@ fi
 
 # ----- Set Up The Runtime Classpath ------------------------------------------
 
-CP=$JAVA_HOME/lib/tools.jar:$ANT_HOME/lib/ant_1_2.jar:./lib/xerces_1_3_0.jar
+CP=$JAVA_HOME/lib/tools.jar:$ANT_HOME/lib/ant_1_3.jar:./lib/xerces_1_3_0.jar
  
 # ----- Make sure Ant script is executable ------------------------------------
 
