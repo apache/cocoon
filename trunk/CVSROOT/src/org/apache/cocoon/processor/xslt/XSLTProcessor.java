3d15abd46ae1f868dcdb91b09202bbf34fe4128a
balld <balld@unknown>
2001-05-22 23:05:54 +0000

    patch from Pal Wester to fix NPE with iplanet PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/cocoon-1@1048039 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/CVSROOT/src/org/apache/cocoon/processor/xslt/XSLTProcessor.java
+++ trunk/CVSROOT/src/org/apache/cocoon/processor/xslt/XSLTProcessor.java
@@ -1,4 +1,4 @@
-/*-- $Id: XSLTProcessor.java,v 1.32 2001-03-10 13:35:31 greenrd Exp $ --
+/*-- $Id: XSLTProcessor.java,v 1.33 2001-05-22 23:05:54 balld Exp $ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
@@ -75,7 +75,7 @@ import org.apache.cocoon.Defaults;
  * This class implements an XSLT processor.
  *
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
- * @version $Revision: 1.32 $ $Date: 2001-03-10 13:35:31 $
+ * @version $Revision: 1.33 $ $Date: 2001-05-22 23:05:54 $
  */
 
 public class XSLTProcessor implements Actor, Processor, Status, Defaults, Cacheable {
@@ -128,8 +128,8 @@ public class XSLTProcessor implements Actor, Processor, Status, Defaults, Cachea
         if (parameters != null) {
             while (parameters.hasMoreElements()) {
                 String name = (String) parameters.nextElement();
-                if (isValidName (name))
-                    params.put (name, request.getParameter (name));
+                if (isValidName(name))
+                    params.put (name, request.getParameter(name) == null ? "" : request.getParameter(name));
             }
         }
 
