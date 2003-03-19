537a13c76ee5b64003e9baed4eb23c5dc71b802d
Christian Haul <haul@apache.org>
2003-03-19 09:04:40 +0000

    fix missing function name thanks to Michael Gerzabek <michael.gerzabek@at.efp.cc>
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@23769 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/web3/java/org/apache/cocoon/components/web3/impl/DefaultWeb3StreamerImpl.java
+++ src/blocks/web3/java/org/apache/cocoon/components/web3/impl/DefaultWeb3StreamerImpl.java
@@ -67,7 +67,7 @@ import org.xml.sax.ContentHandler;
  *
  * @author  <a href="mailto:mge@efp.co.at>Michael Gerzabek</a>
  * @since 21. Oct. 2002
- * @version CVS $Id: DefaultWeb3StreamerImpl.java,v 1.2 2003/03/16 17:49:09 vgritsenko Exp $
+ * @version CVS $Id: DefaultWeb3StreamerImpl.java,v 1.3 2003/03/19 09:04:40 haul Exp $
  */
 public class DefaultWeb3StreamerImpl implements Web3Streamer, Poolable {
 
@@ -78,7 +78,6 @@ public class DefaultWeb3StreamerImpl implements Web3Streamer, Poolable {
         attributes.clear();
         attributes.addAttribute( Web3.URI, Web3.INCLUDE_NAME_ATTR,  
             Web3.INCLUDE_NAME_ATTR, "CDATA", function.getName().toUpperCase() );
-        attributes.clear();
         contentHandler.startElement( Web3.URI, Web3.INCLUDE_ELEM, 
             Web3.INCLUDE_ELEM, attributes );
         contentHandler.startElement( Web3.URI, Web3.IMPORT_ELEM, 
