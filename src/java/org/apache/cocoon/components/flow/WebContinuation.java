fdaca6489ffc0d1033c0cff12b8704e5a8450f12
Vadim Gritsenko <vgritsenko@apache.org>
2003-03-20 01:04:23 +0000

    reformat code
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@23787 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/flow/WebContinuation.java
+++ src/java/org/apache/cocoon/components/flow/WebContinuation.java
@@ -65,308 +65,289 @@ import org.apache.avalon.framework.logger.AbstractLogEnabled;
  *
  * @author <a href="mailto:ovidiu@cup.hp.com">Ovidiu Predescu</a>
  * @since March 19, 2002
- * @version CVS $Id: WebContinuation.java,v 1.2 2003/03/16 17:49:11 vgritsenko Exp $
+ * @version CVS $Id: WebContinuation.java,v 1.3 2003/03/20 01:04:23 vgritsenko Exp $
  */
 public class WebContinuation extends AbstractLogEnabled
-  implements Comparable
-{
-  /**
-   * The continuation this object represents.
-   */
-  protected Object continuation;
+        implements Comparable {
+    /**
+     * The continuation this object represents.
+     */
+    protected Object continuation;
 
-  /**
-   * The parent <code>WebContinuation</code> from which processing
-   * last started. If null, there is no parent continuation
-   * associated, and this is the first one to be created in a
-   * processing. In this case this <code>WebContinuation</code>
-   * instance becomes the root of the tree maintained by the
-   * <code>ContinuationsManager</code>.
-   *
-   * @see ContinuationsManager
-   */
-  protected WebContinuation parentContinuation;
+    /**
+     * The parent <code>WebContinuation</code> from which processing
+     * last started. If null, there is no parent continuation
+     * associated, and this is the first one to be created in a
+     * processing. In this case this <code>WebContinuation</code>
+     * instance becomes the root of the tree maintained by the
+     * <code>ContinuationsManager</code>.
+     *
+     * @see ContinuationsManager
+     */
+    protected WebContinuation parentContinuation;
 
-  /**
-   * The children continuations. These are continuations created by
-   * resuming the processing from the point stored by
-   * <code>continuation</code>.
-   */
-  protected List children = new ArrayList();
+    /**
+     * The children continuations. These are continuations created by
+     * resuming the processing from the point stored by
+     * <code>continuation</code>.
+     */
+    protected List children = new ArrayList();
 
-  /**
-   * The continuation id used to represent this instance in Web pages.
-   */
-  protected String id;
+    /**
+     * The continuation id used to represent this instance in Web pages.
+     */
+    protected String id;
 
-  /**
-   * A user definable object. This is present for convenience, to
-   * store any information associated with this
-   * <code>WebContinuation</code> a particular implementation might
-   * need.
-   */
-  protected Object userObject;
+    /**
+     * A user definable object. This is present for convenience, to
+     * store any information associated with this
+     * <code>WebContinuation</code> a particular implementation might
+     * need.
+     */
+    protected Object userObject;
 
-  /**
-   * When was this continuation accessed last time. Each time the
-   * continuation is accessed, this time is set to the time of the
-   * access.
-   */
-  protected long lastAccessTime;
+    /**
+     * When was this continuation accessed last time. Each time the
+     * continuation is accessed, this time is set to the time of the
+     * access.
+     */
+    protected long lastAccessTime;
 
-  /**
-   * Indicates how long does this continuation will live (in
-   * seconds). The continuation will be removed once the current time
-   * is bigger than <code>lastAccessTime + timeToLive</code>.
-   */
-  protected int timeToLive;
+    /**
+     * Indicates how long does this continuation will live (in
+     * seconds). The continuation will be removed once the current time
+     * is bigger than <code>lastAccessTime + timeToLive</code>.
+     */
+    protected int timeToLive;
 
-  /**
-   * Create a <code>WebContinuation</code> object. Saves the object in
-   * the hash table of continuations maintained by
-   * <code>manager</code> (this is done as a side effect of obtaining
-   * and identifier from it).
-   *
-   * @param continuation an <code>Object</code> value
-   * @param parentContinuation a <code>WebContinuation</code> value
-   * @param manager a <code>ContinuationsManagerImpl</code> value
-   */
-  public WebContinuation(Object continuation,
-                         WebContinuation parentContinuation,
-                         ContinuationsManagerImpl manager,
-                         int timeToLive)
-  {
-    this.continuation = continuation;
-    this.parentContinuation = parentContinuation;
-    id = manager.generateContinuationId(this);
-    this.updateLastAccessTime();
-    this.timeToLive = timeToLive;
+    /**
+     * Create a <code>WebContinuation</code> object. Saves the object in
+     * the hash table of continuations maintained by
+     * <code>manager</code> (this is done as a side effect of obtaining
+     * and identifier from it).
+     *
+     * @param continuation an <code>Object</code> value
+     * @param parentContinuation a <code>WebContinuation</code> value
+     * @param manager a <code>ContinuationsManagerImpl</code> value
+     */
+    public WebContinuation(Object continuation,
+                           WebContinuation parentContinuation,
+                           ContinuationsManagerImpl manager,
+                           int timeToLive) {
+        this.continuation = continuation;
+        this.parentContinuation = parentContinuation;
+        id = manager.generateContinuationId(this);
+        this.updateLastAccessTime();
+        this.timeToLive = timeToLive;
 
-    if (parentContinuation != null)
-      this.parentContinuation.children.add(this);
-  }
-
-  /**
-   * Return the continuation object.
-   *
-   * @return an <code>Object</code> value
-   */
-  public Object getContinuation()
-  {
-    updateLastAccessTime();
-    return continuation;
-  }
-
-  /**
-   * Return the ancestor continuation situated <code>level</code>s
-   * above the current continuation. The current instance is
-   * considered to be at level 0. The parent continuation of the
-   * receiving instance at level 1, its parent is at level 2 relative
-   * to the receiving instance. If <code>level</code> is bigger than
-   * the depth of the tree, the root of the tree is returned.
-   *
-   * @param level an <code>int</code> value
-   * @return a <code>WebContinuation</code> value
-   */
-  public WebContinuation getContinuation(int level)
-  {
-    if (level <= 0) {
-      updateLastAccessTime();
-      return this;
-    }
-    else if (parentContinuation == null)
-      return this;
-    else
-      return parentContinuation.getContinuation(level - 1);
-  }
-
-  /**
-   * Return the parent <code>WebContinuation</code>. Equivalent with
-   * <code>getContinuation(1)</code>.
-   *
-   * @return a <code>WebContinuation</code> value
-   */
-  public WebContinuation getParentContinuation()
-  {
-    return parentContinuation;
-  }
-
-  /**
-   * Return the children <code>WebContinuation</code> which were
-   * created as a result of resuming the processing from the current
-   * <code>continuation</code>.
-   *
-   * @return a <code>List</code> value
-   */
-  public List getChildren()
-  {
-    return children;
-  }
-
-  /**
-   * Returns the string identifier of this
-   * <code>WebContinuation</code>.
-   *
-   * @return a <code>String</code> value
-   */
-  public String getId()
-  {
-    return id;
-  }
-
-  /**
-   * Returns the last time this
-   * <code>WebContinuation</code> was accessed.
-   *
-   * @return a <code>long</code> value
-   */
-  public long getLastAccessTime()
-  {
-    return lastAccessTime;
-  }
-
-  /**
-   * Returns the the timetolive for this 
-   * <code>WebContinuation</code>.
-   *
-   * @return a <code>long</code> value
-   */
-  public long getTimeToLive()
-  {
-    return this.timeToLive;
-  }
-
-  /**
-   * Sets the user object associated with this instance.
-   *
-   * @param obj an <code>Object</code> value
-   */
-  public void setUserObject(Object obj)
-  {
-    this.userObject = obj;
-  }
-
-  /**
-   * Obtains the user object associated with this instance.
-   *
-   * @return an <code>Object</code> value
-   */
-  public Object getUserObject()
-  {
-    return userObject;
-  }
-
-  /**
-   * Returns the hash code of the associated identifier.
-   *
-   * @return an <code>int</code> value
-   */
-  public int hashCode()
-  {
-    return id.hashCode();
-  }
-
-  /**
-   * True if the identifiers are the same, false otherwise.
-   *
-   * @param another an <code>Object</code> value
-   * @return a <code>boolean</code> value
-   */
-  public boolean equals(Object another)
-  {
-    if (another instanceof WebContinuation)
-      return id.equals(((WebContinuation)another).id);
-    return false;
-  }
-
-  /**
-   * Compares the expiration time of this instance with that of the
-   * WebContinuation passed as argument.
-   *
-   * <p><b>Note:</b> this class has a natural ordering that is
-   * inconsistent with <code>equals</code>.</p>.
-   *
-   * @param other an <code>Object</code> value, which should be a
-   * <code>WebContinuation</code> instance
-   * @return an <code>int</code> value
-   */
-  public int compareTo(Object other)
-  {
-    WebContinuation wk = (WebContinuation)other;
-    return (int)((lastAccessTime + timeToLive)
-                 - (wk.lastAccessTime + wk.timeToLive));
-  }
-
-  /**
-   * Debugging method.
-   *
-   * <p>Assumes the receiving instance as the root of a tree and
-   * displays the tree of continuations.
-   */
-  public void display()
-  {
-    getLogger().debug("\nWK: Tree" + display(0));
-  }
-
-  /**
-   * Debugging method.
-   *
-   * <p>Displays the receiving instance as if it is at the
-   * <code>indent</code> depth in the tree of continuations. Each
-   * level is indented 2 spaces.
-   *
-   * @param depth an <code>int</code> value
-   */
-  protected String display(int depth)
-  {
-    StringBuffer tree = new StringBuffer("\n");
-    for (int i = 0; i < depth; i++) {
-      tree.append("  ");
+        if (parentContinuation != null)
+            this.parentContinuation.children.add(this);
     }
 
-    tree.append("WK: WebContinuation ")
-        .append(id)
-        .append(" ExpireTime [");
-    
-    if ((lastAccessTime + timeToLive) < System.currentTimeMillis()) {
-      tree.append("Expired");
-    }
-    else {
-      tree.append(lastAccessTime + timeToLive);
-    }
-
-    tree.append("]");
-
-    //REVISIT: is this needed for some reason?
-    //System.out.print(spaces); System.out.println("WebContinuation " + id);
-
-    int size = children.size();
-    depth++;
-
-    for (int i = 0; i < size; i++) {
-      tree.append(((WebContinuation)children.get(i)).display(depth));
+    /**
+     * Return the continuation object.
+     *
+     * @return an <code>Object</code> value
+     */
+    public Object getContinuation() {
+        updateLastAccessTime();
+        return continuation;
     }
 
-    return tree.toString();
-  }
+    /**
+     * Return the ancestor continuation situated <code>level</code>s
+     * above the current continuation. The current instance is
+     * considered to be at level 0. The parent continuation of the
+     * receiving instance at level 1, its parent is at level 2 relative
+     * to the receiving instance. If <code>level</code> is bigger than
+     * the depth of the tree, the root of the tree is returned.
+     *
+     * @param level an <code>int</code> value
+     * @return a <code>WebContinuation</code> value
+     */
+    public WebContinuation getContinuation(int level) {
+        if (level <= 0) {
+            updateLastAccessTime();
+            return this;
+        } else if (parentContinuation == null)
+            return this;
+        else
+            return parentContinuation.getContinuation(level - 1);
+    }
 
-  /**
-   * Update the continuation in the 
-   */
-  protected void updateLastAccessTime()
-  {
-    lastAccessTime = new Date().getTime();
-  }
+    /**
+     * Return the parent <code>WebContinuation</code>. Equivalent with
+     * <code>getContinuation(1)</code>.
+     *
+     * @return a <code>WebContinuation</code> value
+     */
+    public WebContinuation getParentContinuation() {
+        return parentContinuation;
+    }
 
-  /**
-   * Determines whether this continuation has expired 
-   *
-   * @return a <code>boolean</code> value
-   */
-  public boolean hasExpired() {
-    long currentTime = System.currentTimeMillis();
-    long expireTime = this.getLastAccessTime() + this.timeToLive;
+    /**
+     * Return the children <code>WebContinuation</code> which were
+     * created as a result of resuming the processing from the current
+     * <code>continuation</code>.
+     *
+     * @return a <code>List</code> value
+     */
+    public List getChildren() {
+        return children;
+    }
 
-    return  (currentTime > expireTime); 
-  }
+    /**
+     * Returns the string identifier of this
+     * <code>WebContinuation</code>.
+     *
+     * @return a <code>String</code> value
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Returns the last time this
+     * <code>WebContinuation</code> was accessed.
+     *
+     * @return a <code>long</code> value
+     */
+    public long getLastAccessTime() {
+        return lastAccessTime;
+    }
+
+    /**
+     * Returns the the timetolive for this
+     * <code>WebContinuation</code>.
+     *
+     * @return a <code>long</code> value
+     */
+    public long getTimeToLive() {
+        return this.timeToLive;
+    }
+
+    /**
+     * Sets the user object associated with this instance.
+     *
+     * @param obj an <code>Object</code> value
+     */
+    public void setUserObject(Object obj) {
+        this.userObject = obj;
+    }
+
+    /**
+     * Obtains the user object associated with this instance.
+     *
+     * @return an <code>Object</code> value
+     */
+    public Object getUserObject() {
+        return userObject;
+    }
+
+    /**
+     * Returns the hash code of the associated identifier.
+     *
+     * @return an <code>int</code> value
+     */
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    /**
+     * True if the identifiers are the same, false otherwise.
+     *
+     * @param another an <code>Object</code> value
+     * @return a <code>boolean</code> value
+     */
+    public boolean equals(Object another) {
+        if (another instanceof WebContinuation)
+            return id.equals(((WebContinuation) another).id);
+        return false;
+    }
+
+    /**
+     * Compares the expiration time of this instance with that of the
+     * WebContinuation passed as argument.
+     *
+     * <p><b>Note:</b> this class has a natural ordering that is
+     * inconsistent with <code>equals</code>.</p>.
+     *
+     * @param other an <code>Object</code> value, which should be a
+     * <code>WebContinuation</code> instance
+     * @return an <code>int</code> value
+     */
+    public int compareTo(Object other) {
+        WebContinuation wk = (WebContinuation) other;
+        return (int) ((lastAccessTime + timeToLive)
+                - (wk.lastAccessTime + wk.timeToLive));
+    }
+
+    /**
+     * Debugging method.
+     *
+     * <p>Assumes the receiving instance as the root of a tree and
+     * displays the tree of continuations.
+     */
+    public void display() {
+        getLogger().debug("\nWK: Tree" + display(0));
+    }
+
+    /**
+     * Debugging method.
+     *
+     * <p>Displays the receiving instance as if it is at the
+     * <code>indent</code> depth in the tree of continuations. Each
+     * level is indented 2 spaces.
+     *
+     * @param depth an <code>int</code> value
+     */
+    protected String display(int depth) {
+        StringBuffer tree = new StringBuffer("\n");
+        for (int i = 0; i < depth; i++) {
+            tree.append("  ");
+        }
+
+        tree.append("WK: WebContinuation ")
+                .append(id)
+                .append(" ExpireTime [");
+
+        if ((lastAccessTime + timeToLive) < System.currentTimeMillis()) {
+            tree.append("Expired");
+        } else {
+            tree.append(lastAccessTime + timeToLive);
+        }
+
+        tree.append("]");
+
+        // REVISIT: is this needed for some reason?
+        // System.out.print(spaces); System.out.println("WebContinuation " + id);
+
+        int size = children.size();
+        depth++;
+
+        for (int i = 0; i < size; i++) {
+            tree.append(((WebContinuation) children.get(i)).display(depth));
+        }
+
+        return tree.toString();
+    }
+
+    /**
+     * Update the continuation in the
+     */
+    protected void updateLastAccessTime() {
+        lastAccessTime = new Date().getTime();
+    }
+
+    /**
+     * Determines whether this continuation has expired
+     *
+     * @return a <code>boolean</code> value
+     */
+    public boolean hasExpired() {
+        long currentTime = System.currentTimeMillis();
+        long expireTime = this.getLastAccessTime() + this.timeToLive;
+
+        return (currentTime > expireTime);
+    }
 }
