3cb9f30dfd6ab6ad62cf5cf30933c298001aacbd
Christopher Oliver <coliver@apache.org>
2003-04-23 20:57:17 +0000

    Transformer interface to JXTemplate functionality
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@24124 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/src/org/apache/cocoon/transformation/JXTemplateTransformer.java
@@ -0,0 +1,16 @@
+package org.apache.cocoon.transformation;
+
+import org.apache.cocoon.generation.JXTemplate;
+
+/**
+ * Transformer adapter for {@link org.apache.cocoon.generation.JXTemplate}
+ */
+
+public class JXTemplateTransformer extends JXTemplate.TransformerAdapter {
+
+    /**
+     * This class is just a placeholder to provide a class name you
+     * can easily reference from your Sitemap. All of its functionality is 
+     * provided by its parent class.
+     */
+}
