534627e92fc632346f4c6dbc682213b1ca4d4a58
Vadim Gritsenko <vgritsenko@apache.org>
2003-04-27 17:57:41 +0000

    Fix bug 10242: Text serializer adds root element when one is missing. Thanks to Peter Hunsberger for patch suggestion.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@29283 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/cocoon/serialization/TextSerializer.java
+++ trunk/src/java/org/apache/cocoon/serialization/TextSerializer.java
@@ -53,27 +53,46 @@ package org.apache.cocoon.serialization;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
 import org.apache.cocoon.CascadingIOException;
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.AttributesImpl;
+
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
 import java.io.OutputStream;
 
-
 /**
+ * Text serializer converts XML into plain text.
+ * It omits all XML tags and writes only character events to the output.
+ * Internally, text serializer uses XML serializer with {@link OutputKeys#METHOD}
+ * set to <code>text</code>.
+ *
+ * <p>Input document must have at least one element - root element - which
+ * should wrap all the text inside it.
+ *
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
- * @version CVS $Id: TextSerializer.java,v 1.1 2003/03/09 00:09:36 pier Exp $
+ * @version CVS $Id: TextSerializer.java,v 1.2 2003/04/27 17:57:41 vgritsenko Exp $
  */
-
 public class TextSerializer extends AbstractTextSerializer {
+
+    /**
+     * Set to true after first XML element
+     */
+    private boolean hasRootElement;
+
+    /**
+     * Set to true after first XML element
+     */
+    private boolean hadNoRootElement;
 
     /**
      * Set the configurations for this serializer.
      */
-    public void configure(Configuration conf)
-    throws ConfigurationException {
-        super.configure( conf );
-        this.format.put(OutputKeys.METHOD,"text");
+    public void configure(Configuration conf) throws ConfigurationException {
+        super.configure(conf);
+        this.format.put(OutputKeys.METHOD, "text");
     }
 
     /**
@@ -94,4 +113,32 @@ public class TextSerializer extends AbstractTextSerializer {
         }
     }
 
+    public void startElement(String uri, String loc, String raw, Attributes a)
+            throws SAXException {
+        this.hasRootElement = true;
+        super.startElement(uri, loc, raw, a);
+    }
+
+    public void characters(char c[], int start, int len)
+            throws SAXException {
+        if (!this.hasRootElement) {
+            this.hasRootElement = this.hadNoRootElement = true;
+            getLogger().warn("Encountered text before root element. Creating <text> wrapper element.");
+            super.startElement("", "text", "text", new AttributesImpl());
+        }
+        super.characters(c, start, len);
+    }
+
+    public void endDocument() throws SAXException {
+        if (this.hadNoRootElement) {
+            super.endElement("", "text", "text");
+        }
+        super.endDocument();
+    }
+
+    public void recycle() {
+        super.recycle();
+        this.hasRootElement = false;
+        this.hadNoRootElement = false;
+    }
 }
