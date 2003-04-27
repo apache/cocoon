5c161ee338d39a3ac6bcda2267f80239a7070aff
Christopher Oliver <coliver@apache.org>
2003-04-27 01:57:05 +0000

    First cut of reimplementation of XMLForm to be in line with W3 candidate recommendation. Renamed to JXForms to emphasize its reliance on Apache JXPath
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@24147 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/src/org/apache/cocoon/transformation/JXFormsTransformer.java
@@ -0,0 +1,16 @@
+package org.apache.cocoon.transformation;
+
+import org.apache.cocoon.generation.JXFormsGenerator;
+
+/**
+ * Transformer adapter for {@link org.apache.cocoon.generation.JXFormsGenerator}
+ */
+
+public class JXFormsTransformer extends JXFormsGenerator.TransformerAdapter {
+
+    /**
+     * This class is just a placeholder to provide a class name you
+     * can easily reference from your Sitemap. All of its functionality is 
+     * provided by its base class.
+     */
+}
