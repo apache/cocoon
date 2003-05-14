1aa5ad1bc4dce837398b6f208a133931faf8a0db
Stephan Michels <stephan@apache.org>
2003-05-14 15:21:44 +0000

    Reducing build time by 18%. Using conditional includes instead of separate targets.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@29418 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/tools/src/anttasks/XConfToolTask.java
+++ trunk/tools/src/anttasks/XConfToolTask.java
@@ -1,13 +1,59 @@
-/*****************************************************************************
- * Copyright (C) The Apache Software Foundation. All rights reserved.        *
- * ------------------------------------------------------------------------- *
- * This software is published under the terms of the Apache Software License *
- * version 1.1, a copy of which has been included  with this distribution in *
- * the LICENSE file.                                                         *
- *****************************************************************************/
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Cocoon" and  "Apache Software Foundation" must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
+ Software Foundation, please see <http://www.apache.org/>.
+
+*/
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.MatchingTask;
 import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -32,115 +78,117 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Add components to the cocoon.xconf.
- * This is an ugly second shot.
+ * Ant task to patch xmlfiles.
  *
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
  * @author <a href="mailto:vgritsenko@apache.org">Vadim Gritsenko</a>
  * @author <a href="mailto:crafterm@fztig938.bank.dresdner.net">Marcus Crafter</a>
  * @author <a href="mailto:ovidiu@cup.hp.com">Ovidiu Predescu</a>
- * @version CVS $Revision: 1.2 $ $Date: 2003/03/11 15:29:13 $
+ * @author <a href="mailto:stephan@apache.org">Stephan Michels</a>
+ * @version CVS $Revision: 1.3 $ $Date: 2003/05/14 15:21:44 $
  */
+public final class XConfToolTask extends MatchingTask {
 
-public final class XConfToolTask extends Task {
+    private File file;
+    private File directory;
+    private File srcdir;
 
-    private String configuration;
-    private String directory;
-    private String extension;
-
-    public void setConfiguration(String configuration) {
-        this.configuration = configuration;
+    /**
+     * Set file, which should be patched.
+     *
+     * @param file File, which should be patched.
+     */
+    public void setFile(File file) {
+        this.file = file;
     }
 
-    public void setDirectory(String directory) {
-        this.directory = directory;
+    /**
+     * Set base directory for the patch files.
+     *
+     * @param srcdir Base directory for the patch files.
+     */
+    public void setSrcdir(File srcdir) {
+        this.srcdir = srcdir;
     }
 
-    public void setExtension(String extension) {
-        this.extension = extension;
-    }
-
+    /**
+     * Execute task.
+     */
     public void execute() throws BuildException {
 
-        if (this.configuration == null) {
-            throw new BuildException("configuration attribute is required", location);
-        }
-        if (this.extension == null) {
-            throw new BuildException("extension attribute is required", location);
-        }
-        if (this.directory == null) {
-            throw new BuildException("directory attribute is required", location);
+        if (this.file==null) {
+            throw new BuildException("file attribute is required",
+                                     location);
         }
 
         try {
             final DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             final Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            final String file = this.project.resolveFile(this.configuration).getCanonicalPath();
 
             // load xml
-            // System.out.println("Reading: " + file);
-            final Document configuration = builder.parse((new File(file)).toURL().toExternalForm());
+            log("Reading: " + this.file, Project.MSG_DEBUG);
+            final Document document = builder.parse(this.file.toURL().toExternalForm());
 
+            if (this.srcdir==null)
+                this.srcdir = project.resolveFile(".");
+
+            DirectoryScanner scanner = getDirectoryScanner(this.srcdir);
+
+            String[] list = scanner.getIncludedFiles();
+
+            boolean hasChanged = false;
             // process recursive
-            if (process(builder, configuration, this.project.resolveFile(this.directory), this.extension)) {
-                // save xml
-                System.out.println("Writing: " + file);
-                transformer.transform(new DOMSource(configuration), new StreamResult(file));
-            } else {
-                // System.out.println("No Changes: " + file);
-            }
-        } catch (TransformerException e) {
-            throw new BuildException("TransformerException: " + e);
-        } catch (SAXException e) {
-            throw new BuildException("SAXException: " + e);
-        } catch (ParserConfigurationException e) {
-            throw new BuildException("ParserConfigurationException: " + e);
-        } catch (IOException ioe) {
-            throw new BuildException("IOException: " + ioe);
-        }
-    }
-
-    /**
-     * Scan recursive
-     */
-    private boolean process(final DocumentBuilder builder,
-                         final Document configuration,
-                         final File   directoryFile,
-                         final String ext)
-    throws IOException, BuildException, ParserConfigurationException, TransformerException, SAXException {
-
-        boolean hasChanged = false;
-        final File[] files = directoryFile.listFiles();
-        if (files != null) {
-            for(int i = 0; i < files.length; i++) {
-                if (files[i].isDirectory()) {
-                    hasChanged |= process(builder, configuration, files[i], ext);
-                } else if (files[i].getName().endsWith("." + ext)) {
-                    String file = files[i].getCanonicalPath();
-                    try {
-                        // Adds configuration snippet from the file to the configuration
-                        hasChanged |= add(configuration, builder.parse((new File(file)).toURL().toExternalForm()), file);
-                    } catch (SAXException e) {
-                        System.out.println("Ignoring: " + file + "\n(not a valid XML)");
-                    }
+            File patchfile;
+            for (int i = 0; i<list.length; i++) {
+                patchfile = new File(this.srcdir, list[i]);
+                try {
+                    // Adds configuration snippet from the file to the configuration
+                    hasChanged |= patch(document,
+                                        builder.parse(patchfile.toURL().toExternalForm()),
+                                        patchfile.toString());
+                } catch (SAXException e) {
+                    log("Ignoring: "+patchfile+"\n(not a valid XML)");
                 }
             }
-        }
 
-        return hasChanged;
+            if (hasChanged) {
+                log("Writing: "+this.file);
+                transformer.transform(new DOMSource(document),
+                                      new StreamResult(this.file));
+            } else {
+                log("No Changes: " + this.file, Project.MSG_DEBUG);
+            }
+        } catch (TransformerException e) {
+            throw new BuildException("TransformerException: "+e);
+        } catch (SAXException e) {
+            throw new BuildException("SAXException: "+e);
+        } catch (ParserConfigurationException e) {
+            throw new BuildException("ParserConfigurationException: "+e);
+        } catch (IOException ioe) {
+            throw new BuildException("IOException: "+ioe);
+        }
     }
 
     /**
-     * Add entry to cocoon.xconf
+     * Patch XML document with a given patch file.
+     *
+     * @param configuration Orginal document
+     * @param component Patch document
+     * @param file Patch file
+     *
+     * @return True, if the document was successfully patched
      */
-    private boolean add(final Document configuration,
-                        final Document component,
-                        String file)
-    throws TransformerException, IOException {
+    private boolean patch(final Document configuration,
+                          final Document component,
+                          String file)
+                          throws TransformerException, IOException {
         // Check to see if Document is an xconf-tool document
         Element elem = component.getDocumentElement();
-        if (!elem.getTagName().equals(extension)) {
-            System.out.println("Skipping non xconf-tool file: " + file);
+
+        String extension = file.lastIndexOf(".")>0?file.substring(file.lastIndexOf(".")+1):"";
+
+        if ( !elem.getTagName().equals(extension)) {
+            log("Skipping non xconf-tool file: "+file);
             return false;
         }
 
@@ -148,30 +196,35 @@ public final class XConfToolTask extends Task {
         String xpath = elem.getAttribute("xpath");
 
         NodeList nodes = XPathAPI.selectNodeList(configuration, xpath);
-        if (nodes.getLength() != 1) {
-            System.out.println("Error in: " + file);
-            throw new IOException("XPath (" + xpath + ") returned not one node, but "
-                    + nodes.getLength() + " nodes");
+
+        if (nodes.getLength()!=1) {
+            log("Error in: "+file);
+            throw new IOException("XPath ("+xpath+
+                                  ") returned not one node, but "+
+                                  nodes.getLength()+" nodes");
         }
         Node root = nodes.item(0);
 
         // Test that 'root' node satisfies 'component' insertion criteria
         String test = component.getDocumentElement().getAttribute("unless");
-        if (test != null && test.length() > 0 &&
-                XPathAPI.selectNodeList(root, test).getLength() != 0) {
-            // System.out.println("Skipping: " + file);
+
+        if ((test!=null) && (test.length()>0) &&
+            (XPathAPI.selectNodeList(root, test).getLength()!=0)) {
+            log("Skipping: " + file, Project.MSG_DEBUG);
             return false;
         } else {
             // Test if component wants us to remove a list of nodes first
             xpath = component.getDocumentElement().getAttribute("remove");
 
             Node remove = null;
-            if (xpath != null && xpath.length() > 0) {
+
+            if ((xpath!=null) && (xpath.length()>0)) {
                 nodes = XPathAPI.selectNodeList(configuration, xpath);
 
-                for (int i = 0, length = nodes.getLength(); i < length; i++) {
+                for (int i = 0, length = nodes.getLength(); i<length; i++) {
                     Node node = nodes.item(i);
                     Node parent = node.getParentNode();
+
                     parent.removeChild(node);
                 }
             }
@@ -179,44 +232,53 @@ public final class XConfToolTask extends Task {
             // Test for an attribute that needs to be added to an element
             String name = component.getDocumentElement().getAttribute("add-attribute");
             String value = component.getDocumentElement().getAttribute("value");
-            if (name != null && name.length() > 0) {
-                if (value == null)
-                    throw new IOException("No attribute value specified for 'add-attribute' " + xpath);
-                if (root instanceof Element)
-                    ((Element)root).setAttribute(name, value);
-            }
 
+            if ((name!=null) && (name.length()>0)) {
+                if (value==null) {
+                    throw new IOException("No attribute value specified for 'add-attribute' "+
+                                          xpath);
+                }
+                if (root instanceof Element) {
+                    ((Element) root).setAttribute(name, value);
+                }
+            }
 
             // Test if 'component' provides desired insertion point
             xpath = component.getDocumentElement().getAttribute("insert-before");
             Node before = null;
-            if (xpath != null && xpath.length() > 0) {
+
+            if ((xpath!=null) && (xpath.length()>0)) {
                 nodes = XPathAPI.selectNodeList(root, xpath);
-                if (nodes.getLength() != 1) {
-                    System.out.println("Error in: " + file);
-                    throw new IOException("XPath (" + xpath + ") returned not one node, but "
-                            + nodes.getLength() + " nodes");
+                if (nodes.getLength()!=1) {
+                    log("Error in: "+file);
+                    throw new IOException("XPath ("+xpath+
+                                          ") returned not one node, but "+
+                                          nodes.getLength()+" nodes");
                 }
                 before = nodes.item(0);
             } else {
                 xpath = component.getDocumentElement().getAttribute("insert-after");
-                if (xpath != null && xpath.length() > 0) {
+                if ((xpath!=null) && (xpath.length()>0)) {
                     nodes = XPathAPI.selectNodeList(root, xpath);
-                    if (nodes.getLength() != 1) {
-                        System.out.println("Error in: " + file);
-                        throw new IOException("XPath (" + xpath + ") returned not one node, but "
-                                + nodes.getLength() + " nodes");
+                    if (nodes.getLength()!=1) {
+                        log("Error in: "+file);
+                        throw new IOException("XPath ("+xpath+
+                                              ") returned not one node, but "+
+                                              nodes.getLength()+" nodes");
                     }
                     before = nodes.item(0).getNextSibling();
                 }
             }
 
             // Add 'component' data into 'root' node
-            System.out.println("Processing: " + file);
+            log("Processing: "+file);
             NodeList componentNodes = component.getDocumentElement().getChildNodes();
-            for (int i = 0; i < componentNodes.getLength(); i++ ){
-                Node node = configuration.importNode(componentNodes.item(i), true);
-                if (before == null) {
+
+            for (int i = 0; i<componentNodes.getLength(); i++) {
+                Node node = configuration.importNode(componentNodes.item(i),
+                                                     true);
+
+                if (before==null) {
                     root.appendChild(node);
                 } else {
                     root.insertBefore(node, before);
