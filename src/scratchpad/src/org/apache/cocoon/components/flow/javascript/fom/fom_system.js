b14178f46178c2a79fdf52790305dbdee49065f1
Christopher Oliver <coliver@apache.org>
2003-06-20 08:28:20 +0000

    Prototype implementation of FOM
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@24730 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/src/org/apache/cocoon/components/flow/javascript/fom/fom_system.js
@@ -0,0 +1,10 @@
+FOM_Cocoon.suicide = new Continuation();
+
+FOM_Cocoon.prototype.sendPageAndWait = function(uri, bizData) {
+    this.sendPage(uri, bizData, new Continuation());
+    FOM_Cocoon.suicide();
+}
+
+FOM_Cocoon.prototype.handleContinuation = function(k) {
+    k(k);
+}
