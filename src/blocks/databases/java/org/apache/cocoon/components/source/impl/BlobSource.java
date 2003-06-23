3c3338d7a09bfc53c8f78b0df499a3ea8661470e
Carsten Ziegeler <cziegeler@apache.org>
2003-06-23 17:53:42 +0000

    Updating to latest excalibur datasource - no more JVM dependency :) Move BlobSource into databases block
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@24760 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/databases/java/org/apache/cocoon/components/source/impl/BlobSource.java
+++ src/blocks/databases/java/org/apache/cocoon/components/source/impl/BlobSource.java
@@ -99,7 +99,7 @@ import org.apache.excalibur.source.SourceValidity;
  *
  * @author <a href="mailto:sylvain@apache.org">Sylvain Wallez</a>
  * @author <a href="mailto:stephan@apache.org">Stephan Michels</a>
- * @version CVS $Id: BlobSource.java,v 1.2 2003/03/16 18:03:54 vgritsenko Exp $
+ * @version CVS $Id: BlobSource.java,v 1.1 2003/06/23 17:53:42 cziegeler Exp $
  */
 public class BlobSource extends AbstractLogEnabled implements Source, Composable {
 
