d9a865dc0d7585c843e15a9a55eaa22c356a9c18
Bruno Dumon <bruno@apache.org>
2003-06-26 09:09:59 +0000

    initial commit
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@24792 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/blocks/woody/java/org/apache/cocoon/woody/datatype/validationruleimpl/Mod10ValidationRule.java
@@ -0,0 +1,88 @@
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Cocoon" and  "Apache Software Foundation" must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
+ Software Foundation, please see <http://www.apache.org/>.
+
+*/
+package org.apache.cocoon.woody.datatype.validationruleimpl;
+
+import org.apache.cocoon.woody.datatype.ValidationError;
+import org.outerj.expression.ExpressionContext;
+
+/**
+ * Performs the so called "mod 10" algorithm to check the validity of credit card numbers
+ * such as VISA.
+ *
+ * <p>In addition to this, the credit card number can be further validated by its length
+ * and prefix, but those properties are depended on the credit card type and such validation
+ * is not performed by this validation rule.
+ */
+public class Mod10ValidationRule extends AbstractValidationRule {
+    public ValidationError validate(Object value, ExpressionContext expressionContext) {
+        String numberToCheck = (String)value;
+        int nulOffset = '0';
+        int sum = 0;
+        for (int i = 1; i <= numberToCheck.length(); i++) {
+            int currentDigit = numberToCheck.charAt(numberToCheck.length() - i) - nulOffset;
+            if ((i % 2) == 0) {
+                currentDigit *= 2;
+                currentDigit = currentDigit > 9 ? currentDigit - 9 : currentDigit;
+                sum += currentDigit;
+            } else {
+                sum += currentDigit;
+            }
+        }
+        if(!((sum % 10) == 0))
+            return hasFailMessage() ? getFailMessage() : new ValidationError("validation.mod10");
+        else
+            return null;
+    }
+
+    public boolean supportsType(Class clazz, boolean arrayType) {
+        return String.class.isAssignableFrom(clazz) && !arrayType;
+    }
+}
