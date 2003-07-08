f27d698e3f8a2b332d487d4c9b1a90e4f390e8e9
Christopher Oliver <coliver@apache.org>
2003-07-08 05:46:55 +0000

    Formatted code and fixed typo
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@24973 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/linotype/samples/flow.js
+++ src/blocks/linotype/samples/flow.js
@@ -18,46 +18,46 @@ function main(action) {
     var args = new Array(arguments.length - 1);
     for (var i = 1; i < arguments.length; i++) {
         args[i-1] = arguments[i];
-    }    	
+    }            
 
-	if ((userid == undefined) || (userid == "")) {
-		login(action, args);
-	}
-		
-	invoke(action, args);
+    if ((userid == undefined) || (userid == "")) {
+        login(action, args);
+    }
+                
+    invoke(action, args);
 }
 
 /*
  * If the user is not yet authorized, than authentication takes place
  */
 function login(action, args) {
-	var name = "";
-	var password = "";
-	var userError = "";
-	var passError = "";
+    var name = "";
+    var password = "";
+    var userError = "";
+    var passError = "";
 
-	while (true) {
-		sendPageAndWait("screen/login", { username : name, userError : userError, passError : passError});
+    while (true) {
+        sendPageAndWait("screen/login", { username : name, userError : userError, passError : passError});
 
-		name = cocoon.request.getParameter("username");
-		password = cocoon.request.getParameter("password");
-		
-		if (users.isValidName(name)) {
-		    if (users.isValidPassword(name,password)) {
-		    	userid = name;
-		    	username = users.getFullName(name);
-				break;
-			} else {
-				userError = "";
-				passError = "Password doesn't match";
-			}
-		} else {
-			userError = "User not found";
-			passError = "";
-		}
-	}
-	
-	cocoon.createSession();
+        name = cocoon.request.getParameter("username");
+        password = cocoon.request.getParameter("password");
+                
+        if (users.isValidName(name)) {
+            if (users.isValidPassword(name,password)) {
+                userid = name;
+                username = users.getFullName(name);
+                break;
+            } else {
+                userError = "";
+                passError = "Password doesn't match";
+            }
+        } else {
+            userError = "User not found";
+            passError = "";
+        }
+    }
+        
+    cocoon.createSession();
 }
 
 /*
@@ -70,10 +70,10 @@ function login(action, args) {
 function invoke(action, args) {
     func = this[action];
     if (func != undefined) {
-    	func.apply(this,args);
-   	} else {
-  		sendPage("screen/" + action, {"user" : username});
-  	}
+        func.apply(this,args);
+    } else {
+        sendPage("screen/" + action, {"user" : username});
+    }
 }
 
 // ----------------------------- actions ----------------------------------
@@ -83,52 +83,50 @@ function invoke(action, args) {
  * that the user has logged out and should be further considered authenticated. 
  */
 function logout() {
-	userid = "";
-	sendPage("screen/logout");
+    userid = "";
+    sendPage("screen/logout");
 }
    
 /*
  * The edit action performs the editing subflow.
  */
 function edit(id,type,subpage) {
-   	var repository = home + "repository/" + type + "/";
+    var repository = home + "repository/" + type + "/";
     
-	if (id == "template") {
-		id = repo.getID(repository);
-		repo.copy(repository + "template", repository + id);
-		redirect("../" + id + "/");
-	} else if ((subpage != undefined) && (subpage != "")) {
-   		sendPage("edit/" + type + "/" + id + "/" + subpage,{});
-	} else {
-		var document = repository + id;
+    if (id == "template") {
+        id = repo.getID(repository);
+        repo.copy(repository + "template", repository + id);
+        redirect("../" + id + "/");
+    } else if ((subpage != undefined) && (subpage != "")) {
+        sendPage("edit/" + type + "/" + id + "/" + subpage,{});
+    } else {
+        var document = repository + id;
 
-		while (true) {
-			var versions = repo.getVersions(document);
-	    	sendPageAndWait("edit/" + type + "/" + id + "/", 
-	    		{ 
-	    			userid : userid, 
-	    			username : username, 
-	    			versions : versions,
-	    			innerHTML : cocoon.request.getParameter("innerHTML") 
-	    		}
-	    	);
-	        var action = cocoon.request.getParameter("action");
-	       	if (action == "delete") {
-	        	repo.remove(document);
-	        	break;
-	       	} else if (action == "restore") {
-	       		var version = cocoon.request.getParameter("version");
-	        	repo.revertFrom(document,version);
-	       	} else {
-	       		var output = repo.getOutputStream(document);
-		        process("samples/linotype/action/save-" + type,{},output);
-		        output.close();
-		        repo.save(cocoon.request, document);
-		    	if (action == "finish") break;
-			}	   	
-		}
+        while (true) {
+            var versions = repo.getVersions(document);
+            sendPageAndWait("edit/" + type + "/" + id + "/", { 
+                userid : userid, 
+                username : username, 
+                versions : versions,
+                innerHTML : cocoon.request.getParameter("innerHTML") 
+            });
+            var action = cocoon.request.getParameter("action");
+            if (action == "delete") {
+                repo.remove(document);
+                break;
+            } else if (action == "restore") {
+                var version = cocoon.request.getParameter("version");
+                repo.revertFrom(document,version);
+            } else {
+                var output = repo.getOutputStream(document);
+                process("samples/linotype/action/save-" + type,{},output);
+                output.close();
+                repo.save(cocoon.request, document);
+                if (action == "finish") break;
+            }                   
+        }
 
-		redirect("../../../" + type);
-	}
+        redirect("../../../" + type);
+    }
 }
 
