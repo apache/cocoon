3063e949d5249dd2a4b66a50f35a1a7baf6d47c5
Christopher Oliver <coliver@apache.org>
2003-07-08 05:48:53 +0000

    Moving JXTemplate* and FOM to core
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@29694 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/src/java/org/apache/cocoon/transformation/JXTemplateTransformer.java
@@ -0,0 +1,16 @@
+package org.apache.cocoon.transformation;
+
+import org.apache.cocoon.generation.JXTemplateGenerator;
+
+/**
+ * Transformer adapter for {@link org.apache.cocoon.generation.JXTemplateGenerator}
+ */
+
+public class JXTemplateTransformer extends JXTemplateGenerator.TransformerAdapter {
+
+    /**
+     * This class is just a placeholder to provide a class name you
+     * can easily reference from your Sitemap. All of its functionality is 
+     * provided by its parent class.
+     */
+}
