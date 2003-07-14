9aea573819f8e6afea83e8b426580b9ab37d83b6
Carsten Ziegeler <cziegeler@apache.org>
2003-07-14 10:12:14 +0000

    Unify code base
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@25070 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/portal/java/org/apache/cocoon/portal/event/aspect/impl/LinkEventAspect.java
+++ src/blocks/portal/java/org/apache/cocoon/portal/event/aspect/impl/LinkEventAspect.java
@@ -50,128 +50,51 @@
 */
 package org.apache.cocoon.portal.event.aspect.impl;
 
-import java.util.StringTokenizer;
-
-import org.apache.avalon.framework.component.ComponentException;
-import org.apache.avalon.framework.component.ComponentManager;
-import org.apache.avalon.framework.component.Composable;
-import org.apache.avalon.framework.logger.AbstractLogEnabled;
-import org.apache.avalon.framework.thread.ThreadSafe;
-import org.apache.cocoon.environment.ObjectModelHelper;
-import org.apache.cocoon.environment.Request;
-import org.apache.cocoon.portal.PortalService;
 import org.apache.cocoon.portal.event.Event;
 import org.apache.cocoon.portal.event.Publisher;
-import org.apache.cocoon.portal.event.aspect.EventAspect;
-import org.apache.cocoon.portal.event.aspect.EventAspectContext;
 import org.apache.cocoon.portal.event.impl.ChangeAspectDataEvent;
 import org.apache.cocoon.portal.layout.Layout;
 import org.apache.cocoon.portal.layout.impl.LinkLayout;
-import org.apache.cocoon.portal.profile.ProfileManager;
 
 /**
  *
  * @author <a href="mailto:juergen.seitz@basf-it-services.com">J&uuml;rgen Seitz</a>
  * 
- * @version CVS $Id: LinkEventAspect.java,v 1.1 2003/07/10 13:17:03 cziegeler Exp $
+ * @version CVS $Id: LinkEventAspect.java,v 1.2 2003/07/14 10:12:14 cziegeler Exp $
  */
-public class LinkEventAspect
-    extends AbstractLogEnabled
-    implements EventAspect, ThreadSafe, Composable {
+public class LinkEventAspect extends AbstractContentEventAspect {
 
-    protected ComponentManager manager;
-
-    /* (non-Javadoc)
-     * @see org.apache.cocoon.portal.event.aspect.EventAspect#process(org.apache.cocoon.portal.event.aspect.EventAspectContext, org.apache.cocoon.portal.PortalService)
-     */
-    public void process(EventAspectContext context, PortalService service) {
+    protected String getRequestParameterName() {
         // TODO - make this configurable
-        final String requestParameterName = "link";
-        final Request request = ObjectModelHelper.getRequest(context.getObjectModel());
-        String[] values = request.getParameterValues(requestParameterName);
-        if (values != null) {
-            final Publisher publisher = context.getEventPublisher();
-            for (int i = 0; i < values.length; i++) {
-                final String value = values[i];
-                Event e = null;
-                try {
-                    e = context.getEventConverter().decode(value);
-                    if (null != e) {
-                        publisher.publish(e);
-                    }
-                } catch (Exception ignore) {
-                }
-                if (e == null) {
-                    // Use '|' character as delimiter between targetGroup, targetId, contentGroup and contentId
-                    StringTokenizer tokenizer = new StringTokenizer(value, "|");
-                    int tokenNumber = 0;
-                    int tokenCount = tokenizer.countTokens();
-					// if only 3 params are in the String
-					if (tokenCount == 3)
-						tokenNumber = tokenNumber + 1;
+        return "link";
+    }
 
-					String targetGroup = null;
-					String targetId = null;
-					String contentGroup = null;
-					String contentId = null;
-					
-                    while (tokenizer.hasMoreTokens())
-                    {
-                        	
-                    	switch (tokenNumber)
-                        {
-                            case 0 :
-								targetGroup = tokenizer.nextToken();
-                                break;
-							case 1 :
-								targetId = tokenizer.nextToken();
-								break;
-							case 2 :
-								contentGroup = tokenizer.nextToken();
-								break;
-							case 3 :
-								contentId = tokenizer.nextToken();
-								break;
-                        }
-                        
-						tokenNumber = tokenNumber + 1;
-                    } // while
-                    
-					if (tokenCount > 0) {                                            
-                        ProfileManager profileManager = null;
-                        try {
-                            profileManager = (ProfileManager)this.manager.lookup(ProfileManager.ROLE);
-							Layout layout = profileManager.getPortalLayout(targetGroup, targetId );
-                            if ( layout != null ) {
-                            	if (layout instanceof LinkLayout){
-									LinkLayout linkLayout = (LinkLayout)layout;
-									e = new ChangeAspectDataEvent(linkLayout, "link-layout-key", contentGroup);
-									publisher.publish(e);	
-                                    e = new ChangeAspectDataEvent(linkLayout, "link-layout-id", contentId);
-                                    publisher.publish(e);   
-                            	} else {
-									this.getLogger().warn("the configured layout: " + layout.getName() + " is not a linkLayout.");
-                            	}								
-                            }
-                        } catch (ComponentException ignore) {
-                        } finally {
-                            this.manager.release( profileManager );
-                        }
-                    } else {
-                    	this.getLogger().warn("data for LinkEvent is not set correctly");
-                    }
-                }
-            }
-        }
-        // and invoke next one
-        context.invokeNext(service);
+    protected int getRequiredValueCount() {
+        return 4;
     }
 
     /* (non-Javadoc)
-     * @see org.apache.avalon.framework.component.Composable#compose(org.apache.avalon.framework.component.ComponentManager)
+     * @see org.apache.cocoon.portal.event.aspect.impl.AbstractContentEventAspect#publish(org.apache.cocoon.portal.event.Publisher, org.apache.cocoon.portal.layout.Layout, java.lang.String[])
      */
-    public void compose(ComponentManager manager) throws ComponentException {
-        this.manager = manager;
+    protected void publish(Publisher publisher,
+                             Layout layout,
+                              String[] values) {
+        if (layout instanceof LinkLayout) {
+            LinkLayout linkLayout = (LinkLayout) layout;
+            Event e = new ChangeAspectDataEvent(linkLayout,
+                                                "link-layout-key",
+                                                values[2]);
+            publisher.publish(e);
+            e = new ChangeAspectDataEvent(linkLayout,
+                                          "link-layout-id",
+                                          values[3]);
+            publisher.publish(e);
+        } else {
+            this.getLogger().warn(
+                "the configured layout: "
+                    + layout.getName()
+                    + " is not a LinkLayout.");
+        }
     }
 
 }
