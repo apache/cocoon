8c2c025bdd2069799bc6c82e8b843436d534f5cb
Geoffrey Alexander Howard <ghoward@apache.org>
2003-07-21 01:54:37 +0000

    undo simplification accidentally left from debugging
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@25152 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/eventcache/samples/event.js
+++ src/blocks/eventcache/samples/event.js
@@ -2,7 +2,7 @@ var role = Packages.org.apache.cocoon.caching.Cache.ROLE;
 
 function cacheEvent() {
 	var cache = cocoon.getComponent(role);
-	var event = new Packages.org.apache.cocoon.caching.validity.NamedEvent("one");
+	var event = new Packages.org.apache.cocoon.caching.validity.NamedEvent(cocoon.request.event);
 	var rand = Math.random() * 10000000000000000000;
 	cache.processEvent(event);
 	cocoon.releaseComponent(cache);
