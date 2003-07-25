286bb9b6918ba9519370e0bbe989132f762b2f4e
Bruno Dumon <bruno@apache.org>
2003-07-25 16:18:22 +0000

    check that supplied object is not null
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@25208 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/woody/java/org/apache/cocoon/woody/binding/JXPathBindingBase.java
+++ src/blocks/woody/java/org/apache/cocoon/woody/binding/JXPathBindingBase.java
@@ -80,6 +80,8 @@ public abstract class JXPathBindingBase implements Binding, LogEnabled {
      * the new overloaded version of this method.
      */
     public final void loadFormFromModel(Widget frmModel, Object objModel) {
+        if (objModel == null)
+            throw new NullPointerException("null object passed to loadFormFromModel() method");
         JXPathContext jxpc;
         if (!(objModel instanceof JXPathContext)) {
             jxpc = JXPathContext.newContext(objModel);
@@ -102,6 +104,8 @@ public abstract class JXPathBindingBase implements Binding, LogEnabled {
      * the new overloaded version of this method.
      */
     public void saveFormToModel(Widget frmModel, Object objModel) throws BindingException {
+        if (objModel == null)
+            throw new NullPointerException("null object passed to saveFormToModel() method");
         JXPathContext jxpc;
         if (!(objModel instanceof JXPathContext)) {
             jxpc = JXPathContext.newContext(objModel);
