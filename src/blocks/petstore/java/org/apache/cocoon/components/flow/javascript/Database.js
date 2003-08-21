aff7ee9943d1817d3b75def9db027dfd26a3307b
Christopher Oliver <coliver@apache.org>
2003-08-21 01:07:46 +0000

    Update to use FOM
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@25512 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/petstore/java/org/apache/cocoon/components/flow/javascript/Database.js
+++ src/blocks/petstore/java/org/apache/cocoon/components/flow/javascript/Database.js
@@ -1,5 +1,5 @@
 //
-// CVS $Id: Database.js,v 1.1 2003/07/12 19:09:39 coliver Exp $
+// CVS $Id: Database.js,v 1.2 2003/08/21 01:07:46 coliver Exp $
 //
 // Prototype Database API
 //
@@ -10,12 +10,12 @@ defineClass("org.apache.cocoon.components.flow.javascript.ScriptableConnection")
 defineClass("org.apache.cocoon.components.flow.javascript.ScriptableResult");
 
 Database.getConnection = function(selectorValue) {
-    var selector = cocoon.componentManager.lookup(Packages.org.apache.avalon.excalibur.datasource.DataSourceComponent.ROLE + "Selector");
+    var selector = cocoon.getComponent(Packages.org.apache.avalon.excalibur.datasource.DataSourceComponent.ROLE + "Selector");
     try {
 	var ds = selector.select(selectorValue);
 	return new Database(ds.getConnection());
     } finally {
-	cocoon.componentManager.release(selector);
+	cocoon.releaseComponent(selector);
     }
 }
 
