8ffda4a1bc8ab7dc6c246acc34276479fb96e788
Poetz Reinhard <reinhard@apache.org>
2003-09-08 22:58:29 +0000

    - added calls of cocoon.sendPageAndWait() and woody.send() - added interception definitions for stopExecution() and continueExecution()
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@25715 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/scratchpad/samples/intercepted-flow/aspects2.js
+++ src/blocks/scratchpad/samples/intercepted-flow/aspects2.js
@@ -2,5 +2,7 @@ function interception* {
     before(): {
         cocoon.log.info( "before:interception*" ); 
     }
-     
+    stopExecution(): {
+        cocoon.log.info( "stopExecution:interception*" );          
+    }      
 }
