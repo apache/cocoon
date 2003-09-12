87602198278e5e62e1fadcba9e38b395be1d42d2
Poetz Reinhard <reinhard@apache.org>
2003-09-12 14:48:49 +0000

    - extend example showing reflecting the latest changes (return statements follow after() interceptions)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@25747 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/scratchpad/samples/intercepted-flow/basescript.js
+++ src/blocks/scratchpad/samples/intercepted-flow/basescript.js
@@ -5,12 +5,17 @@ cocoon.apply( "aspects2.js" );
 function interceptionTest() {
   var uri = "page/info";
   cocoon.log.error ( "interceptionTest - baseScript" );
-  callAnotherFunction();
+  var x = callAnotherFunction();
   cocoon.sendPageAndWait(uri, { });
 }
 
 function callAnotherFunction() {
-  cocoon.log.error ( "callAnotherFunction() - baseScript" );       
+  var x = 1;
+  cocoon.log.error ( "callAnotherFunction() - baseScript" );     
+  if( x == 2 ) {
+    return 72;
+  }
+  return 27;
 }
 
 function testSendPageAndWait() { 
