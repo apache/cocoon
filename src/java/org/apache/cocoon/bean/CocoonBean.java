adeeee636a7e0829dbd043756f667c17ef89ba57
Joerg Heinicke <joerg@apache.org>
2003-09-22 22:51:02 +0000

    removed unused local variable
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@25800 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/bean/CocoonBean.java
+++ src/java/org/apache/cocoon/bean/CocoonBean.java
@@ -86,7 +86,7 @@ import java.util.List;
  * @author <a href="mailto:nicolaken@apache.org">Nicola Ken Barozzi</a>
  * @author <a href="mailto:vgritsenko@apache.org">Vadim Gritsenko</a>
  * @author <a href="mailto:uv@upaya.co.uk">Upayavira</a>
- * @version CVS $Id: CocoonBean.java,v 1.29 2003/09/19 12:48:37 upayavira Exp $
+ * @version CVS $Id: CocoonBean.java,v 1.30 2003/09/22 22:51:02 joerg Exp $
  */
 public class CocoonBean extends CocoonWrapper {
 
@@ -686,7 +686,6 @@ public class CocoonBean extends CocoonWrapper {
         if (includeLinkExtensions == null) {
             return true;
         } else {
-            String extension = target.getExtension();
             return includeLinkExtensions.contains(target.getExtension());
         }
     }
