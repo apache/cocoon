697bb57a8af17b0295c61daba7d6bd8ea93510fc
Antonio Gallardo Rivera <antonio@apache.org>
2003-10-09 18:30:53 +0000

    Updating OJB samples.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@25952 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/ojb/java/org/apache/cocoon/ojb/samples/EmployeeImpl.java
+++ src/blocks/ojb/java/org/apache/cocoon/ojb/samples/EmployeeImpl.java
@@ -6,7 +6,12 @@
  */
 package org.apache.cocoon.ojb.samples;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.jdo.Extent;
 import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 import javax.jdo.Transaction;
 
 import org.apache.cocoon.ojb.jdo.components.JdoPMF;
@@ -22,11 +27,45 @@ public class EmployeeImpl {
 	
 	public EmployeeImpl(){}
 	
-	public Employee loadEmployee() {
-		return emp;
+	
+	public Employee load(int id, JdoPMF pmf) {
+		String filter = "employee.id ==" + id;
+		/* 1. Get the PersistenceManager */
+		PersistenceManager persistenceManager = pmf.getPersistenceManager();
+		Extent extent = persistenceManager.getExtent(Employee.class, true);
+		Query query = persistenceManager.newQuery(extent, filter);
+		query.setOrdering("employee.id ascending");
+		Collection result = (Collection)query.execute();
+		Iterator iter = result.iterator();
+		while(iter.hasNext()) {
+			Employee e = (Employee) iter.next();
+			if (id == e.getId())
+				return e;
+		}
+		return null;
 	}
 	
-	public void saveEmployee(Employee e, JdoPMF pmf) {
+//	public Employee load(int id, JdoPMF pmf) {
+//		
+//		/* 1. Get the PersistenceManager */
+//		PersistenceManager persistenceManager = pmf.getPersistenceManager();
+//		
+//		Employee e = new Employee();
+//		e.setId(id);
+//		PersistenceBroker broker = PersistenceBrokerFactory.defaultPersistenceBroker();
+//		Identity oid = new Identity(e, broker);
+//		
+//		Employee toBeReturned = new Employee();
+//		//	2. start transaction
+//		persistenceManager.currentTransaction().begin();
+//		// 3. Get the Object based on the primary key
+//		toBeReturned = (Employee) persistenceManager.getObjectById(oid, false);
+//		// 4. End transaction
+//		persistenceManager.currentTransaction().commit();
+//		return toBeReturned;
+//	}
+	
+	public void save(Employee e, JdoPMF pmf) {
 		// Setting up the Bean 
 		emp = e;
 		/* 1. Get the PersistenceManager */
--- src/blocks/ojb/samples/woody/employee.js
+++ src/blocks/ojb/samples/woody/employee.js
@@ -2,21 +2,23 @@ cocoon.load("resource://org/apache/cocoon/woody/flow/javascript/woody2.js");
 
 function employeeform_jdo(form) {
     // Create a empty Bean
+
+   	var factory = cocoon.getComponent(Packages.org.apache.cocoon.ojb.jdo.components.JdoPMF.ROLE);
     var bean = new Packages.org.apache.cocoon.ojb.samples.Employee();
+    var ojbEmployee = Packages.org.apache.cocoon.ojb.samples.EmployeeImpl();
 	// Fill some initial data to the bean
-    bean.setId("35");
-    bean.setDepartmentId("2");
-    bean.setName("Bernardo Robelo");
+    // Load bean based on the given PrimaryKey
+    bean = ojbEmployee.load(1, factory);
+
     // Load the Bean to the form
     form.load(bean);
     // Let woody handle the form
     form.showForm("jdo/woody/employee-form-display");
     // Update the Bean based on user input
 	form.save(bean);
-	var factory = cocoon.getComponent(Packages.org.apache.cocoon.ojb.jdo.components.JdoPMF.ROLE);
-    // Get persistent Manager
-	var dbManage = Packages.org.apache.cocoon.ojb.samples.EmployeeImpl();
-	dbManage.saveEmployee(bean, factory);
+
+    // Save Bean in Database
+	ojbEmployee.save(bean, factory);
 	cocoon.releaseComponent(factory);
     cocoon.request.setAttribute("employeeform", form.getWidget());
     cocoon.sendPage("jdo/woody/employee-form-success");
