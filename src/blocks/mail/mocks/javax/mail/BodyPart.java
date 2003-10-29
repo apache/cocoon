a41d6056f8216ba507673e3e32a168e12a020b13
Anthony Collen <tony@apache.org>
2003-10-29 16:08:58 +0000

    minor fix for a mock
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@26170 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/mail/mocks/javax/mail/BodyPart.java
+++ src/blocks/mail/mocks/javax/mail/BodyPart.java
@@ -8,7 +8,7 @@ package javax.mail;
  */
 public abstract class BodyPart implements Part {
 
-    public void setContent(String a, String b) {
+    public void setContent(Object a, String b) {
         throw new NoSuchMethodError("This is a mock object");
     }
 
