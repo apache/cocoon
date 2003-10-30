12f322dddc4cb04f1fa764e15b71f3bb31fac3ce
Unico Hommes <unico@apache.org>
2003-10-30 21:57:47 +0000

    typos and license notice
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@26192 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/repository/java/org/apache/cocoon/components/repository/SourceRepository.java
+++ src/blocks/repository/java/org/apache/cocoon/components/repository/SourceRepository.java
@@ -1,4 +1,53 @@
-/* Created on Oct 18, 2003 7:00:43 PM by unico */
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Cocoon" and  "Apache Software Foundation" must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
+ Software Foundation, please see <http://www.apache.org/>.
+
+*/
 package org.apache.cocoon.components.repository;
 
 import java.io.IOException;
@@ -6,7 +55,7 @@ import java.io.IOException;
 import org.apache.excalibur.source.SourceException;
 
 /**
- * A stateless utitlity service intended to be use by flowscripts to help
+ * A stateless utitlity service intended to be used by flowscripts to help
  * them with persistent operations on sources.
  * 
  * <p>
@@ -38,7 +87,7 @@ public interface SourceRepository {
     public static final int STATUS_NOT_FOUND = 404;
 
     /**
-     * Status NOT_ALLOWED (<code>495</code>).
+     * Status NOT_ALLOWED (<code>405</code>).
      */
     public static final int STATUS_NOT_ALLOWED = 405;
     
