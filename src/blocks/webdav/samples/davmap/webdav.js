87fad840b258ff9ba7431911e1d8bfc82cc643f2
Unico Hommes <unico@apache.org>
2003-10-30 14:39:39 +0000

    refactorings: simplify PROPFIND, implement PUT using pipeline "T-ing", add directory browsing, add MKCOL method, add DELETE method
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@26180 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/webdav/samples/davmap/webdav.js
+++ src/blocks/webdav/samples/davmap/webdav.js
@@ -1,5 +1,53 @@
+
+importPackage(Packages.org.apache.cocoon.components.repository);
+var repository = cocoon.getComponent(SourceRepository.ROLE);
+
 function selectMethod() {
-    var page = cocoon.parameters["page"];
-    var method = cocoon.request.getMethod();
-    cocoon.sendPage(method+"/"+page, {});
+  var page = cocoon.parameters["page"];
+  var method = cocoon.request.getMethod();
+  cocoon.sendPage(method+"/"+page, null);
 }
+
+function sendStatus() {
+  var status  = cocoon.parameters["status"];
+  var msg = cocoon.parameters["message"];
+  cocoon.sendPage("status/" + status, {message:msg});
+}
+
+function put() {
+  var src  = cocoon.parameters["src"];
+  var dest = cocoon.parameters["dest"];
+  try {
+    var status = repository.save(src,dest);
+    cocoon.sendPage("status/" + status, {message:""});
+  }
+  catch (e) {
+    cocoon.log.error(e);
+    cocoon.sendPage("status/500",{message:"unknown error"});
+  }
+}
+
+function remove() {
+  var location = cocoon.parameters["location"];
+  try {
+    var status = repository.remove(location);
+    cocoon.sendPage("status/" + status, {message:""});
+  }
+  catch (e) {
+    cocoon.log.error(e);
+    cocoon.sendPage("status/500",{message:"unknown error"});
+  }
+}
+
+function mkcol() {
+  var location = cocoon.parameters["location"];
+  try {
+    var status = repository.makeCollection(location);
+    cocoon.sendPage("status/" + status, {message:""});
+  }
+  catch (e) {
+    cocoon.log.error(e);
+    cocoon.sendPage("status/500",{message:"unknown error"});
+  }
+}
+
