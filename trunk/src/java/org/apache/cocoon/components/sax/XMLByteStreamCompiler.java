ba4a7e32d10d881d42a02149211dc1aa11a203a5
Joerg Heinicke <joerg@apache.org>
2003-11-07 14:33:40 +0000

    XMLByteStreamCompiler now handles also text nodes longer than 32 k (thanks to Simon Mieth)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@30323 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/cocoon/components/sax/XMLByteStreamCompiler.java
+++ trunk/src/java/org/apache/cocoon/components/sax/XMLByteStreamCompiler.java
@@ -63,7 +63,7 @@ import java.util.HashMap;
  *
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
- * @version CVS $Id: XMLByteStreamCompiler.java,v 1.4 2003/10/23 08:37:44 cziegeler Exp $
+ * @version CVS $Id: XMLByteStreamCompiler.java,v 1.5 2003/11/07 14:33:40 joerg Exp $
  * 
  * @avalon.component
  * @avalon.service type="XMLSerializer"
@@ -317,8 +317,14 @@ implements XMLSerializer, Resettable {
             }
         }
 
-        if (utflen > 0x00007FFF)
-            throw new SAXException("UTFDataFormatException: String cannot be longer than 32k.");
+        if (utflen > 0x00007FFF) {
+            // handling "UTFDataFormatException: String cannot be longer than 32k."
+            int split = length / 2;
+            writeChars(ch, start, length - split);
+            writeEvent(CHARACTERS);
+            writeChars(ch, start + length - split, split);
+            return;
+        }
 
         byte[] bytearr = new byte[utflen+2];
         bytearr[count++] = (byte) ((utflen >>> 8) & 0xFF);
