6e3c269f5bc07099d3cfc16ef8c005a2b37837e4
Unico Hommes <unico@apache.org>
2003-11-16 14:30:01 +0000

    merge with AbstractProcessingNodeBuilder
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@30338 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/java/org/apache/cocoon/components/treeprocessor/AbstractProcessingNode.java
+++ trunk/src/java/org/apache/cocoon/components/treeprocessor/AbstractProcessingNode.java
@@ -50,39 +50,116 @@
 */
 package org.apache.cocoon.components.treeprocessor;
 
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.cocoon.Constants;
+import org.apache.cocoon.components.treeprocessor.variables.VariableResolverFactory;
 import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.sitemap.PatternException;
+import org.apache.cocoon.xml.LocationAugmentationPipe;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  *
  * @author <a href="mailto:sylvain@apache.org">Sylvain Wallez</a>
- * @version CVS $Id: AbstractProcessingNode.java,v 1.1 2003/03/09 00:09:15 pier Exp $
+ * @author <a href="mailto:unico@apache.org">Unico Hommes</a>
+ * @version CVS $Id: AbstractProcessingNode.java,v 1.2 2003/11/16 14:30:01 unico Exp $
  */
+public abstract class AbstractProcessingNode extends AbstractLogEnabled 
+implements ProcessingNode, Serviceable, Configurable {
 
-public abstract class AbstractProcessingNode extends AbstractLogEnabled implements ProcessingNode {
-
-    protected String location = "unknown location";
-
+    private static final String PARAMETER_ELEMENT = "parameter";
+    private static final String PARAMETER_NAME_ATTR = "name";
+    private static final String PARAMETER_VALUE_ATTR = "value";
+    
+    private String m_location;
+    protected Map m_parameters;
+    
+    protected ServiceManager m_manager;
+    
+    public AbstractProcessingNode() {
+    }
+    
+    public void configure(final Configuration config) throws ConfigurationException {
+        m_location = getConfigLocation(config);
+        if (hasParameters()) {
+            setParameters(config);
+        }
+    }
+    
+    public void service(final ServiceManager manager) throws ServiceException {
+        m_manager = manager;
+    }
+    
+    /**
+     * Get the location of this node.
+     */
+    public final String getLocation() {
+        return m_location;
+    }
+        
+    /**
+     * Parametrizable ProcessingNodes can overide this method to
+     * have resolvable parameters set at configuration time.
+     * 
+     * @return  whether this processing node is parametrizable.
+     */
+    protected boolean hasParameters() {
+        return false;
+    }
+    
+    /**
+     * Set &lt;xxx:parameter&gt; elements as a <code>Map</code> of </code>ListOfMapResolver</code>s,
+     * that can be turned into parameters using <code>ListOfMapResolver.buildParameters()</code>.
+     *
+     * @return the Map of ListOfMapResolver, or <code>null</code> if there are no parameters.
+     */
+    private final void setParameters(Configuration config) throws ConfigurationException {
+        final Configuration[] children = config.getChildren(PARAMETER_ELEMENT);
+        if (children.length == 0) {
+            return;
+        }
+        m_parameters = new HashMap();
+        for (int i = 0; i < children.length; i++) {
+            Configuration child = children[i];
+            String name = child.getAttribute(PARAMETER_NAME_ATTR);
+            String value = child.getAttribute(PARAMETER_VALUE_ATTR);
+            try {
+                m_parameters.put(
+                    VariableResolverFactory.getResolver(name, m_manager),
+                    VariableResolverFactory.getResolver(value, m_manager));
+            } catch(PatternException pe) {
+                String msg = "Invalid pattern '" + value + "' at " + getConfigLocation(child);
+                throw new ConfigurationException(msg, pe);
+            }
+        }
+    }
+    
+    /**
+     * Get the location information that is encoded as a location attribute
+     * on the current configuration element.
+     * 
+     * @param config  the configuration element to read the location from.
+     * @return  the location if the location attribute exists, else <code>Unknown</code>.
+     */
+    protected final String getConfigLocation(Configuration config) {
+        return config.getAttribute(LocationAugmentationPipe.LOCATION_ATTR,
+            LocationAugmentationPipe.UNKNOWN_LOCATION);
+    }
+    
     /**
      * Get the <code>SourceResolver</code> in an object model.
      */
     protected static final SourceResolver getSourceResolver(Map objectModel) {
-        return (SourceResolver)objectModel.get(OBJECT_SOURCE_RESOLVER);
+        return (SourceResolver) objectModel.get(OBJECT_SOURCE_RESOLVER);
     }
 
-    /**
-     * Get the location of this node.
-     */
-    public String getLocation() {
-        return this.location;
-    }
-
-    /**
-     * Set the location of this node.
-     */
-    public void setLocation(String location) {
-        this.location = location;
-    }
 }
