bd929ff763f8feebce0c0e8805283a9a9fcf7728
Unico Hommes <unico@apache.org>
2003-11-16 14:20:38 +0000

    pipe for inserting location information into the SAX stream in the form of a location attribute on every element
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@30332 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/src/java/org/apache/cocoon/xml/LocationAugmentationPipe.java
@@ -0,0 +1,110 @@
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Cocoon" and  "Apache Software Foundation" must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
+ Software Foundation, please see <http://www.apache.org/>.
+
+*/
+package org.apache.cocoon.xml;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+/**
+ * A pipe that augments each element in the XML stream 
+ * with a location attribute that holds its Locator information.
+ * 
+ * @author <a href="mailto:unico@apache.org">Unico Hommes</a>
+ */
+public class LocationAugmentationPipe extends AbstractXMLPipe implements XMLPipe {
+
+    private static final String URI = "";
+    
+    public static final String LOCATION_ATTR = "location";
+    public static final String UNKNOWN_LOCATION = "Unknown";
+    
+    private Locator m_locator;
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#setDocumentLocator(org.xml.sax.Locator)
+     */
+    public void setDocumentLocator(Locator locator) {
+        m_locator = locator;
+        super.setDocumentLocator(locator);
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
+     */
+    public void startElement(String uri, String loc, String raw, Attributes a)
+        throws SAXException {
+        
+        String location = getLocationString();
+        AttributesImpl atts = new AttributesImpl(a);
+        atts.addAttribute(URI,LOCATION_ATTR,LOCATION_ATTR,AttributeTypes.CDATA,location);
+        
+        super.startElement(uri, loc, raw, atts);
+    }
+    
+    /**
+     * Returns a string showing the current system ID, line number and column number.
+     *
+     * @return a <code>String</code> value
+     */
+    private String getLocationString() {
+        if(m_locator == null) {
+            return UNKNOWN_LOCATION;
+        } else {
+            final int columnNumber = m_locator.getColumnNumber();
+            return
+                m_locator.getSystemId() + ":"
+                + m_locator.getLineNumber()
+                + (columnNumber >= 0 ? (":" + columnNumber) : "");
+        }
+    }
+}
