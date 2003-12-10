be53ecd20f9128072f3bc34b9acd49230ca3bea2
Bernhard Huber <huber@apache.org>
2003-12-10 18:52:39 +0000

    initial import
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@26668 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/cocoon/transformation/CIncludeTransformerTestCase.java
@@ -0,0 +1,184 @@
+/*
+ 
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+ 
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+ 
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+ 
+ 4. The names "Apache Cocoon" and  "Apache Software Foundation" must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+ 
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+ 
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
+ Software Foundation, please see <http://www.apache.org/>.
+ 
+ */
+
+package org.apache.cocoon.transformation;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.Processor;
+import org.apache.cocoon.SitemapComponentTestCase;
+import org.apache.cocoon.components.CocoonComponentManager;
+import org.apache.cocoon.environment.mock.MockEnvironment;
+
+/**
+ * A simple testcase for FilterTransformer.
+ *
+ * @author <a href="mailto:stephan@apache.org">Stephan Michels </a>
+ * @version CVS $Id: CIncludeTransformerTestCase.java,v 1.1 2003/12/10 18:52:39 huber Exp $
+ */
+public class CIncludeTransformerTestCase extends SitemapComponentTestCase {
+    
+    /** Create new testcase
+     * @param name of testase
+     */
+    public CIncludeTransformerTestCase(String name) {
+        super(name);
+    }
+    
+    /**
+     * Run this test suite from commandline
+     *
+     * @param args commandline arguments (ignored)
+     */
+    public static void main( String[] args ) {
+        TestRunner.run(suite());
+    }
+    
+    /** Create a test suite.
+     * This test suite contains all test cases of this class.
+     * @return the Test object containing all test cases.
+     */
+    public static Test suite() {
+        TestSuite suite = new TestSuite(CIncludeTransformerTestCase.class);
+        return suite;
+    }
+    
+    /** Testcase for cinclude simple include
+     *
+     * @throws Exception iff ComponentManager enterEnvironment fails
+     */
+    public void testCInclude1() throws Exception {
+        getLogger().debug("testCInclude1");
+        
+        Parameters parameters = new Parameters();
+        parameters.setParameter( "support-caching", "false" );
+        
+        String input = "resource://org/apache/cocoon/transformation/cinclude-input-1.xml";
+        String result = "resource://org/apache/cocoon/transformation/cinclude-result-1.xml";
+        String src =  null;
+        
+        // enter & leave environment, as a manager is looked up using
+        // the processing context stack
+        MockEnvironment env = new
+        MockEnvironment(null);
+        Processor processor = (Processor)this.manager.lookup(Processor.ROLE);
+        
+        CocoonComponentManager.enterEnvironment(
+        env, this.manager, processor);
+        
+        assertEqual( load(result),
+        transform("cinclude", src, parameters, load(input)));
+        
+        CocoonComponentManager.leaveEnvironment();
+    }
+    
+    /**
+     * Testcase for cinclude specifying element for wrapping included content
+     *
+     * @throws Exception iff ComponentManager enterEnvironment fails
+     */
+    public void testCInclude2() throws Exception {
+        getLogger().debug("testCInclude2");
+        
+        Parameters parameters = new Parameters();
+        parameters.setParameter( "support-caching", "false" );
+        
+        String input = "resource://org/apache/cocoon/transformation/cinclude-input-2.xml";
+        String result = "resource://org/apache/cocoon/transformation/cinclude-result-2.xml";
+        String src =  null;
+        
+        // enter & leave environment, as a manager is looked up using
+        // the processing context stack
+        MockEnvironment env = new
+        MockEnvironment(null);
+        Processor processor = (Processor)this.manager.lookup(Processor.ROLE);
+        
+        CocoonComponentManager.enterEnvironment(
+        env, this.manager, processor);
+        
+        assertEqual( load(result),
+        transform("cinclude", src, parameters, load(input)));
+        
+        CocoonComponentManager.leaveEnvironment();
+    }
+    
+    /**
+     * Testcase for cinclude specifying select attribute, selection elements from the included document
+     *
+     * @throws Exception iff ComponentManager enterEnvironment fails
+     */
+    public void testCInclude3() throws Exception {
+        getLogger().debug("testCInclude3");
+        
+        Parameters parameters = new Parameters();
+        parameters.setParameter( "support-caching", "false" );
+        
+        String input = "resource://org/apache/cocoon/transformation/cinclude-input-3.xml";
+        String result = "resource://org/apache/cocoon/transformation/cinclude-result-3.xml";
+        String src =  null;
+        
+        // enter & leave environment, as a manager is looked up using
+        // the processing context stack
+        MockEnvironment env = new
+        MockEnvironment(null);
+        Processor processor = (Processor)this.manager.lookup(Processor.ROLE);
+        
+        CocoonComponentManager.enterEnvironment(
+        env, this.manager, processor);
+        
+        assertEqual( load(result),
+        transform("cinclude", src, parameters, load(input)));
+        
+        CocoonComponentManager.leaveEnvironment();
+    }
+}
