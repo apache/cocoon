66c00689b25c3903a57664c57ed48dd88522fa6e
Christopher Oliver <coliver@apache.org>
2004-01-28 05:46:21 +0000

    Added createPageLocal() and createWebContinuation()
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@27118 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/flow/javascript/fom/fom_system.js
+++ src/java/org/apache/cocoon/components/flow/javascript/fom/fom_system.js
@@ -1,12 +1,14 @@
 FOM_Cocoon.suicide = new Continuation();
 
-FOM_Cocoon.prototype.sendPageAndWait = function(uri, bizData, fun) {
-    this.sendPage(uri, bizData, new Continuation());
+FOM_Cocoon.prototype.sendPageAndWait = function(uri, bizData, fun, ttl) {
+    this.sendPage(uri, bizData,
+                  new FOM_WebContinuation(new Continuation(), 
+                                          this.continuation, ttl));
     if (fun) {
-      if (!(fun instanceof Function)) {
-        throw "Expected a function instead of: " + fun;
-      }
-      fun();
+        if (!(fun instanceof Function)) {
+            throw "Expected a function instead of: " + fun;
+        }
+        fun();
     }
     FOM_Cocoon.suicide();
 }
@@ -14,3 +16,12 @@ FOM_Cocoon.prototype.sendPageAndWait = function(uri, bizData, fun) {
 FOM_Cocoon.prototype.handleContinuation = function(k, wk) {
     k(wk);
 }
+
+FOM_Cocoon.prototype.createWebContinuation = function(ttl) {
+   var wk = this.makeWebContinuation(new Continuation(), ttl);
+   wk.setBookmark(true);
+   return wk;
+}
+
+
+
