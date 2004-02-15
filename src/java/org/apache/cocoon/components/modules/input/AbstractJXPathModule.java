8386a16a19280e857cfe552b7bd6c915a848ac73
Christian Haul <haul@apache.org>
2004-02-15 19:12:44 +0000

    move common code of AbstractJXPathModule and JXPathMetaModule to helper class handle empty configuration similar to no configuration cache JXPath expressions in XMLFileModule (will move to TransientStore later) add new module for switch like cases
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@27368 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/modules/input/AbstractJXPathModule.java
+++ src/java/org/apache/cocoon/components/modules/input/AbstractJXPathModule.java
@@ -51,14 +51,11 @@
 
 package org.apache.cocoon.components.modules.input;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.commons.jxpath.*;
-
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * JXPathModule allows to access properties of any object in generic
@@ -91,22 +88,13 @@ import java.util.Map;
  *
  * @author <a href="mailto:kpiroumian@apache.org">Konstantin Piroumian</a>
  * @author <a href="mailto:haul@apache.org">Christian Haul</a>
- * @version CVS $Id: AbstractJXPathModule.java,v 1.2 2003/03/16 17:49:12 vgritsenko Exp $
+ * @version CVS $Id: AbstractJXPathModule.java,v 1.3 2004/02/15 19:12:44 haul Exp $
  */
 public abstract class AbstractJXPathModule extends AbstractInputModule {
 
-    /**
-     * Contains all globally registered extension classes and
-     * packages. Thus the lookup and loading of globally registered
-     * extensions is done only once.
-     *
-     */
-    protected FunctionLibrary library = null;
+    protected JXPathHelperConfiguration configuration = null;
 
-    /** set lenient mode for jxpath (i.e. throw an exception on
-     * unsupported attributes) ? 
-     */
-    protected boolean lenient = true;
+    private static final boolean lenient = true; 
 
     /** override attribute name */
     protected String parameter = null;
@@ -120,103 +108,7 @@ public abstract class AbstractJXPathModule extends AbstractInputModule {
      */
     public void configure(Configuration config) throws ConfigurationException {
 
-        // JXPathMetaModule starts copying here
-        // please keep both in sync.
-
-        this.lenient = config.getChild("lenient").getValueAsBoolean(this.lenient);
-        this.library = new FunctionLibrary();
-        getFunctions(this.library, config);
-        getPackages(this.library, config);
-    }
-
-
-
-    /**
-     * Register all extension functions listed in the configuration
-     * through <code>&lt;function name="fully.qualified.Class"
-     * prefix="prefix"/&gt;</code> in the given FunctionLibrary.
-     *
-     * @param lib a <code>FunctionLibrary</code> value
-     * @param conf a <code>Configuration</code> value
-     */
-    protected void getFunctions(FunctionLibrary lib, Configuration conf) {
-
-        Configuration[] children = conf.getChildren("function");
-        int i = children.length;
-        while (i-- >0) {
-            String clazzName = children[i].getAttribute("name",null);
-            String prefix = children[i].getAttribute("prefix",null);
-            if (clazzName != null && prefix != null) {
-                try {
-                    Class clazz = Class.forName(clazzName);
-                    if (getLogger().isDebugEnabled())
-                        getLogger().debug("adding Class "+clazzName+" to functions");
-                    lib.addFunctions(new ClassFunctions(clazz, prefix));
-                } catch (ClassNotFoundException cnf) {
-                    if (getLogger().isWarnEnabled())
-                        getLogger().warn("Class not found: "+clazzName);
-                }
-            } else {
-                if (getLogger().isWarnEnabled())
-                    getLogger().warn("Class name or prefix null: "+clazzName+" / "+prefix);
-            }
-        }
-    }
-
-
-    /**
-     * Register all extension packages listed in the configuration
-     * through <code>&lt;package name="fully.qualified.package"
-     * prefix="prefix"/&gt;</code> in the given FunctionLibrary.
-     *
-     * @param lib a <code>FunctionLibrary</code> value
-     * @param conf a <code>Configuration</code> value
-     */
-    protected void getPackages(FunctionLibrary lib, Configuration conf)  {
-
-        Configuration[] children = conf.getChildren("package");
-        int i = children.length;
-        while (i-- >0) {
-            String packageName = children[i].getAttribute("name",null);
-            String prefix = children[i].getAttribute("prefix",null);
-            if (packageName != null && prefix != null) {
-                if (getLogger().isDebugEnabled())
-                    getLogger().debug("adding Package "+packageName+" to functions");
-                lib.addFunctions(new PackageFunctions(packageName, prefix));
-            } else {
-                if (getLogger().isWarnEnabled())
-                    getLogger().warn("Package name or prefix null: "+packageName+" / "+prefix);
-            }
-        }
-    }
-
-
-    /**
-     * Actually add global functions and packages as well as those
-     * listed in the configuration object.
-     *
-     * @param context a <code>JXPathContext</code> value
-     * @param conf a <code>Configuration</code> value holding local
-     * packages and functions.
-     */
-    protected void setupExtensions(JXPathContext context, Configuration conf) {
-        
-        FunctionLibrary localLib = null;
-
-        if (conf != null) {
-            if (getLogger().isDebugEnabled())
-                getLogger().debug("adding local Classes and Packages to functions");
-            localLib = new FunctionLibrary();
-            localLib.addFunctions(this.library);
-            getPackages(localLib, conf);
-            getFunctions(localLib, conf);
-        } else {
-            if (getLogger().isDebugEnabled())
-                getLogger().debug("no local Classes or Packages");
-            localLib = this.library;
-        }
-        
-        context.setFunctions(localLib);
+        this.configuration = JXPathHelper.setup(config, lenient); 
     }
 
 
@@ -224,75 +116,31 @@ public abstract class AbstractJXPathModule extends AbstractInputModule {
         throws ConfigurationException {
 
         Object contextObj = getContextObject(modeConf, objectModel);
-        if (contextObj == null) return null;
         if (modeConf != null) { 
             name = modeConf.getChild("parameter").getValue(this.parameter != null ? this.parameter : name); 
         }
-        try {
-            JXPathContext jxContext = JXPathContext.newContext(contextObj);
-            setupExtensions(jxContext, modeConf);
-            if (this.lenient) jxContext.setLenient(true); // return null insted of exception on non existing property
-            Object obj = jxContext.getValue(name);
-            return obj;
-        } catch (Exception e) {
-            throw new ConfigurationException(
-                "Module does not support <" + name + ">" + "attribute.",
-                e
-            );
-        }
+        return JXPathHelper.getAttribute(name, modeConf, this.configuration, contextObj);
     }
+
 
     public Iterator getAttributeNames(Configuration modeConf, Map objectModel)
         throws ConfigurationException {
 
         Object contextObj = getContextObject(modeConf, objectModel);
-        if (contextObj == null) return null;
-        try {
-            JXPathBeanInfo info = JXPathIntrospector.getBeanInfo(
-                contextObj.getClass());
-            java.beans.PropertyDescriptor[] properties = info.getPropertyDescriptors();
-            java.util.List names = new java.util.LinkedList();
-            for (int i = 0; i < properties.length; i++) {
-                names.add(properties[i].getName());
-            }
-            return names.listIterator();
-        } catch (Exception e) {
-            throw new ConfigurationException(
-                "Error retrieving attribute names for class: "
-                + contextObj.getClass(),
-                e
-            );
-        }
-
+        return JXPathHelper.getAttributeNames(this.configuration, contextObj);
     }
+
 
     public Object[] getAttributeValues(String name, Configuration modeConf, Map objectModel)
         throws ConfigurationException {
 
         Object contextObj = getContextObject(modeConf, objectModel);
-        if (contextObj == null) return null;
         if (modeConf != null) { 
             name = modeConf.getChild("parameter").getValue(this.parameter != null ? this.parameter : name); 
         }
-        try {
-            JXPathContext jxContext = JXPathContext.newContext(contextObj);
-            List values = new LinkedList();
-            setupExtensions(jxContext, modeConf);
-            if (this.lenient) jxContext.setLenient(true); // return null insted of exception on non existing property
-            Iterator i = jxContext.iterate(name);
-            while (i.hasNext()) {
-                values.add(i.next());
-            }
-            Object[] obj = values.toArray();
-            if (obj.length == 0) obj = null;
-            return obj;
-        } catch (Exception e) {
-            throw new ConfigurationException(
-                "Module does not support <" + name + ">" + "attribute.",
-                e
-            );
-        }
+        return JXPathHelper.getAttributeValues(name, modeConf, this.configuration, contextObj);
     }
+
 
     /**
      * Returns the object which should be used as JXPath context.
