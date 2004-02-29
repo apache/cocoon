3b517af3ebc8c20e2115f41c63425ac97f7fdc21
Vadim Gritsenko <vgritsenko@apache.org>
2004-02-29 00:59:15 +0000

    Add String() function
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@27459 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/blocks/woody/java/org/apache/cocoon/woody/expression/StringFunction.java
@@ -0,0 +1,78 @@
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Cocoon" and  "Apache Software Foundation" must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
+ Software Foundation, please see <http://www.apache.org/>.
+
+*/
+package org.apache.cocoon.woody.expression;
+
+import org.outerj.expression.AbstractExpression;
+import org.outerj.expression.ExpressionContext;
+import org.outerj.expression.ExpressionException;
+import org.outerj.expression.Expression;
+
+/**
+ * Converts argument to the string. Useful when concatenating non-string
+ * values, such as numbers.
+ *
+ * @author <a href="http://cocoon.apache.org/">The Apache Cocoon Team</a>
+ * @version CVS $Id: StringFunction.java,v 1.1 2004/02/29 00:59:15 vgritsenko Exp $
+ */
+public class StringFunction extends AbstractExpression {
+
+    public Object evaluate(ExpressionContext context) throws ExpressionException {
+        return String.valueOf(((Expression)arguments.get(0)).evaluate(context));
+    }
+
+    public Class getResultType() {
+        return String.class;
+    }
+
+    public String getDescription() {
+        return "String function";
+    }
+}
