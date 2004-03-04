d85b2fa253cbf82db672d1f06d82ae5f41c5a2cf
Joerg Heinicke <joerg@apache.org>
2004-03-04 22:40:08 +0000

    avoid additional files in the root, move .bat into tools/bin
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@27542 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 rem ----------------------------------------------------------------------------
 rem build.bat - Win32 Build Script for Apache Cocoon
 rem
-rem $Id: build.bat,v 1.6 2004/03/04 07:53:55 antonio Exp $
+rem $Id: build.bat,v 1.7 2004/03/04 22:40:07 joerg Exp $
 rem ----------------------------------------------------------------------------
 
 rem ----- Verify and Set Required Environment Variables ------------------------
@@ -10,7 +10,7 @@ rem ----- Verify and Set Required Environment Variables ------------------------
 rem ----- Ignore system CLASSPATH variable
 set OLD_CLASSPATH=%CLASSPATH%
 set CLASSPATH=
-for %%i in (.\lib\endorsed\*.jar) do call appendcp.bat %%i
+for %%i in (.\lib\endorsed\*.jar) do call .\tools\bin\appendcp.bat %%i
 echo Using classpath: "%CLASSPATH%"
 
 rem ----- Use Java in JAVA_HOME if JAVA_HOME is set.
