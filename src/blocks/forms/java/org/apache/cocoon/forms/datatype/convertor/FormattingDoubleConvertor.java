1f8bcd3b9720580a730a1d5c81184fca567e7fd5
Bruno Dumon <bruno@apache.org>
2004-03-18 11:45:55 +0000

    Added a double datatype
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@27790 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/blocks/forms/java/org/apache/cocoon/forms/datatype/convertor/FormattingDoubleConvertor.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.forms.datatype.convertor;
+
+import org.outerj.i18n.DecimalFormat;
+
+import java.util.Locale;
+import java.text.ParseException;
+
+/**
+ * A Convertor for {@link Double}s backed by the
+ * {@link java.text.DecimalFormat DecimalFormat} class.
+ *
+ * <p>This class is mostly the same as the {@link FormattingDecimalConvertor},
+ * so see there for more information.
+ *
+ * @version CVS $Id: FormattingDoubleConvertor.java,v 1.1 2004/03/18 11:45:55 bruno Exp $
+ */
+public class FormattingDoubleConvertor extends FormattingDecimalConvertor {
+
+    public FormattingDoubleConvertor() {
+        super();
+    }
+
+    public Object convertFromString(String value, Locale locale, Convertor.FormatCache formatCache) {
+        DecimalFormat decimalFormat = getDecimalFormat(locale, formatCache);
+        try {
+            Number decimalValue = decimalFormat.parse(value);
+            if (decimalValue instanceof Double)
+                return decimalValue;
+            else
+                return new Double(decimalValue.doubleValue());
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    public Class getTypeClass() {
+        return Double.class;
+    }
+}
---
+++ src/blocks/forms/java/org/apache/cocoon/forms/datatype/convertor/PlainDoubleConvertor.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.forms.datatype.convertor;
+
+import java.util.Locale;
+
+/**
+ * Convertor for java.lang.Doubles that does not do any (Locale-dependent)
+ * formatting. It simply uses String.valueOf() and Long.parseLong().
+ *
+ * @version CVS $Id: PlainDoubleConvertor.java,v 1.1 2004/03/18 11:45:55 bruno Exp $
+ */
+public class PlainDoubleConvertor implements Convertor {
+    public Object convertFromString(String value, Locale locale, Convertor.FormatCache formatCache) {
+        try {
+            return new Double(Double.parseDouble(value));
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public String convertToString(Object value, Locale locale, Convertor.FormatCache formatCache) {
+        return String.valueOf(value);
+    }
+
+    public Class getTypeClass() {
+        return Double.class;
+    }
+}
