b687a22940ec8456e1045f45eeaf276c45285f9c
Outerthought - Bruno Dumon <bruno@apache.org>
2004-03-18 13:56:09 +0000

    Added methods to create a form instance or definition based on a DOM tree instead of a Source.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@27792 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/forms/java/org/apache/cocoon/forms/FormManager.java
+++ src/blocks/forms/java/org/apache/cocoon/forms/FormManager.java
@@ -16,16 +16,45 @@
 package org.apache.cocoon.forms;
 
 import org.apache.cocoon.forms.formmodel.Form;
+import org.apache.cocoon.forms.formmodel.FormDefinition;
 import org.apache.excalibur.source.Source;
+import org.w3c.dom.Element;
 
 /**
  * Work interface for the component that can create {@link Form}s.
  * 
- * @version $Id: FormManager.java,v 1.1 2004/03/09 10:34:12 reinhard Exp $
+ * @version $Id: FormManager.java,v 1.2 2004/03/18 13:56:09 bruno Exp $
  */
 public interface FormManager {
     
     String ROLE = FormManager.class.getName();
 
+    /**
+     * Creates a form instance based on the XML form definition
+     * that can be read from the specified source.
+     *
+     * <p>The form definition will be cached, so that future form instances
+     * can be creted quickly.
+     */
     Form createForm(Source source) throws Exception;
+
+    /**
+     * Creates a form instance based on the XML form definition that is
+     * supplied as a DOM tree.
+     *
+     * <p>The specified element must be a fd:form element.
+     *
+     * <p>The Form Definition will not be cached.
+     */
+    Form createForm(Element formElement) throws Exception;
+
+    /**
+     * Creates a form definition based on the XML form definition that is
+     * supplied as a DOM tree.
+     *
+     * <p>The specified element must be a fd:form element.
+
+     * <p>The Form Definition will not be cached.
+     */
+    FormDefinition createFormDefinition(Element formElement) throws Exception;
 }
