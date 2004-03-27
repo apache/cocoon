213e7d67a44d8be685e261032e87e2fd5a228484
Unico Hommes <unico@apache.org>
2004-03-27 15:41:50 +0000

    pass additional mimetype hint
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@27894 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/repository/java/org/apache/cocoon/generation/XPathTraversableGenerator.java
+++ src/blocks/repository/java/org/apache/cocoon/generation/XPathTraversableGenerator.java
@@ -93,7 +93,7 @@ import org.xml.sax.helpers.AttributesImpl;
  *
  * @author <a href="mailto:gianugo@apache.org">Gianugo Rabellino</a>
  * @author <a href="mailto:d.madama@pro-netics.com">Daniele Madama</a>
- * @version CVS $Id: XPathTraversableGenerator.java,v 1.6 2004/03/24 12:26:54 unico Exp $
+ * @version CVS $Id: XPathTraversableGenerator.java,v 1.7 2004/03/27 15:41:50 unico Exp $
  */
 public class XPathTraversableGenerator extends TraversableGenerator {
 
@@ -214,7 +214,7 @@ public class XPathTraversableGenerator extends TraversableGenerator {
     protected void performXPathQuery(TraversableSource in) throws SAXException {
         doc = null;
         try {
-            doc = SourceUtil.toDOM(this.manager, in);
+            doc = SourceUtil.toDOM(this.manager, "text/xml", in);
         } catch (SAXException se) {
              this.getLogger().error("Warning:" + in.getName()
               + " is not a valid XML document. Ignoring");
