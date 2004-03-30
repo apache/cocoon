c1faa5bfbe0147d654715fb0ffae753e53405f6a
Pier Fumagalli <pier@apache.org>
2004-03-30 16:19:06 +0000

    Adding methods to manage reload of Instances in the Deployer.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@30639 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/kernel/org/apache/cocoon/kernel/deployment/Deployer.java
+++ trunk/src/kernel/org/apache/cocoon/kernel/deployment/Deployer.java
@@ -18,6 +18,7 @@
  * ========================================================================== */
 package org.apache.cocoon.kernel.deployment;
 
+import java.util.Iterator;
 import org.apache.cocoon.kernel.identification.Identifier;
 import org.apache.cocoon.kernel.archival.Library;
 
@@ -27,7 +28,7 @@ import org.apache.cocoon.kernel.archival.Library;
  *
  * @author <a href="mailto:pier@apache.org">Pier Fumagalli</a>
  * @author <a href="http://www.vnunet.com/">VNU Business Publications</a>
- * @version 1.0 (CVS $Revision: 1.1 $)
+ * @version 1.0 (CVS $Revision: 1.2 $)
  */
 public interface Deployer {
 
@@ -39,6 +40,11 @@ public interface Deployer {
      * Implemented interface blocks must be available for successful deployment,
      * but those will never be &quot;instantiated&quot; (like in Java &trade;
      * it is impossible to create new instances of interface classes).</p>
+     *
+     * <p>The returned {@link Instance} will not be deployed until the
+     * {@link #deploy(Instance)} method is called, but it will be returned by
+     * the {@link #instances()} method and its {@link Instance#deployed()}
+     * will return <b>false</b>.</p>
      *
      * @param identifier the {@link Identifier} of the block to deploy.
      * @param name an optional (possibly unique) {@link String} instance name.
@@ -58,4 +64,51 @@ public interface Deployer {
      */
     public void deploy(Instance instance)
     throws DeploymentException;
+
+    /**
+     * <p>Replace the specified block {@link Instance} with another one.</p>
+     *
+     * <p>This method will replace a deployed {@link Instance} with another,
+     * rewiring all wired blocks to the new {@link Instance}.</p>
+     *
+     * <p>The old {@link Instance} will not be destroyed, but still returned
+     * by the {@link #instances()} method, but its {@link Instance#deployed()}
+     * will return <b>false</b>.</p>
+     *
+     * @param old the old {@link Instance} instance to replace.
+     * @param instance the {@link Instance} replacing the old {@link Instance}.
+     * @throws DeploymentException if the specified {@link Instance} could
+     *                             not be replaced for any reason.
+     */
+    public void replace(Instance old, Instance instance)
+    throws DeploymentException;
+
+    /**
+     * <p>Destroy the specified deployed block {@link Instance}.</p>
+     *
+     * <p>If the specified {@link Instance} is deployed <b>and</b> is required
+     * by any other block (there is an active wiring), this method will throw
+     * a {@link DeploymentException}.</p>
+     *
+     * <p>If this {@link Instance} has been replaced by another or has not been
+     * wired to any other block, all the eventual wirings to it will be cut and
+     * it will be destroyed.</p>
+     *
+     * @param instance the {@link Instance} instance to deploy.
+     * @throws DeploymentException if the specified {@link Instance} could
+     *                             not be deployed for any reason.
+     */
+    public void destroy(Instance instance)
+    throws DeploymentException;
+
+    /**
+     * <p>Return an {@link Iterator} over all {@link Instance}s known by this
+     * {@link Deployer}.</p>
+     *
+     * <p>This method will return all instances known by this {@link Deployer},
+     * whether they have been simply installed, deployed, or have been replaced
+     * by other instances. To check if an {@link Instance} is actively deployed
+     * the {@link Instance#deployed()} method can be called.</p>
+     */
+    public Iterator instances();
 }
