e8e107c8621352291cff62fd2433605a37542760
Carsten Ziegeler <cziegeler@apache.org>
2004-03-31 11:24:48 +0000

    Use the same license formatting
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@30645 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/kernel/org/apache/cocoon/kernel/deployment/Instance.java
+++ trunk/src/kernel/org/apache/cocoon/kernel/deployment/Instance.java
@@ -1,21 +1,18 @@
-/* ========================================================================== *
- *                                                                            *
- * Copyright 2004 The Apache Software Foundation.                             *
- *                                                                            *
- * Licensed  under the Apache License,  Version 2.0 (the "License");  you may *
- * not use this file except in compliance with the License.  You may obtain a *
- * copy of the License at                                                     *
- *                                                                            *
- *     http://www.apache.org/licenses/LICENSE-2.0                             *
- *                                                                            *
- * Unless  required  by  applicable law or  agreed  to in  writing,  software *
- * distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT *
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.           *
- *                                                                            *
- * See  the  License for  the  specific language  governing  permissions  and *
- * limitations under the License.                                             *
- *                                                                            *
- * ========================================================================== */
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.cocoon.kernel.deployment;
 
 import java.util.Iterator;
@@ -28,7 +25,7 @@ import org.apache.cocoon.kernel.configuration.Parameters;
  *
  * @author <a href="mailto:pier@apache.org">Pier Fumagalli</a>
  * @author <a href="http://www.vnunet.com/">VNU Business Publications</a>
- * @version 1.0 (CVS $Revision: 1.1 $)
+ * @version 1.0 (CVS $Revision: 1.2 $)
  */
 public interface Instance extends Configurable {
 
