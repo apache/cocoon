32b71e79a477c161014402e9747c92fd2bcb1a77
Guido Casper <gcasper@apache.org>
2004-04-02 14:53:53 +0000

    repository component for WebDAV
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@27987 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/blocks/webdav/java/org/apache/cocoon/components/repository/impl/WebDAVRepositoryTransactionHelper.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.components.repository.impl;
+
+import java.io.IOException;
+
+import org.apache.avalon.framework.activity.Disposable;
+import org.apache.avalon.framework.component.Component;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.cocoon.components.repository.helpers.CredentialsToken;
+import org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper;
+import org.apache.cocoon.components.webdav.WebDAVUtil;
+import org.apache.commons.httpclient.HttpException;
+
+/**
+ * A transaction an locking helper class
+ * intended to be used by flowscripts or corresponding wrapper components.
+ */
+public class WebDAVRepositoryTransactionHelper extends AbstractLogEnabled
+implements RepositoryTransactionHelper, Serviceable, Disposable, Component {
+    
+    /* The ServiceManager */
+    private ServiceManager manager;
+
+    /* The repository component */
+    private WebDAVRepository repo;
+
+    /* The credentials to be used against the WebDAV repository */
+    private CredentialsToken credentials;
+
+    /* (non-Javadoc)
+     * @see org.apache.avalon.framework.service.Serviceable#service(org.apache.avalon.framework.service.ServiceManager)
+     */
+    public void service(ServiceManager manager) throws ServiceException {
+        this.manager = manager;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.avalon.framework.activity.Disposable#dispose()
+     */
+    public void dispose() {
+        this.manager = null;
+    }
+
+    /**
+     * create a WebDAVRepositoryTransactionHelper
+     * 
+     * @param credentials  the user credentials to be used against the WebDAV repository.
+     * @param repo  a reference to the WebDAVRepository object.
+     */
+    public WebDAVRepositoryTransactionHelper(CredentialsToken credentials, WebDAVRepository repo) {
+        this.credentials = credentials;
+        this.repo = repo;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper#beginTran()
+     */
+    public boolean beginTran() {
+        //may be implemented via DeltaV activities?
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper#commitTran()
+     */
+    public boolean commitTran() {
+        //may be implemented via DeltaV activities?
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper#rollbackTran()
+     */
+    public boolean rollbackTran() {
+        //may be implemented via DeltaV activities?
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper#lock(java.lang.String)
+     */
+    public boolean lock(String uri) {
+
+        try {
+            WebDAVUtil.getWebdavResource(this.repo.getAbsoluteURI(uri)).lockMethod();
+            return true;
+
+        } catch (HttpException he) {
+            this.getLogger().error("HTTP Error locking " + uri, he);
+        } catch (IOException ioe) {
+            this.getLogger().error("IO Error locking " + uri, ioe);
+        }
+
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper#lock(java.lang.String, int)
+     */
+    public boolean lock(String uri, int timeout) {
+
+        try {
+            WebDAVUtil.getWebdavResource(this.repo.getAbsoluteURI(uri))
+                      .lockMethod(this.credentials.getPrincipal().getName(), timeout);
+            return true;
+
+        } catch (HttpException he) {
+            this.getLogger().error("HTTP Error locking " + uri, he);
+        } catch (IOException ioe) {
+            this.getLogger().error("IO Error locking " + uri, ioe);
+        }
+
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper#unlock(java.lang.String)
+     */
+    public boolean unlock(String uri) {
+
+        try {
+            WebDAVUtil.getWebdavResource(this.repo.getAbsoluteURI(uri)).unlockMethod();
+            return true;
+
+        } catch (HttpException he) {
+            this.getLogger().error("HTTP Error unlocking " + uri, he);
+        } catch (IOException ioe) {
+            this.getLogger().error("IO Error unlocking " + uri, ioe);
+        }
+
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper#supportsTransactions()
+     */
+    public boolean supportsTransactions() {
+        //may be implemeted via DeltaV activities? --> make configurable
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryTransactionHelper#supportsLocking()
+     */
+    public boolean supportsLocking() {
+        return true;
+    }
+
+}
---
+++ src/blocks/webdav/java/org/apache/cocoon/components/repository/impl/WebDAVRepositoryVersioningHelper.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.components.repository.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.avalon.framework.activity.Disposable;
+import org.apache.avalon.framework.component.Component;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.cocoon.components.repository.helpers.CredentialsToken;
+import org.apache.cocoon.components.repository.helpers.RepositoryVersioningHelper;
+import org.apache.cocoon.components.webdav.WebDAVUtil;
+import org.apache.commons.httpclient.HttpException;
+
+/**
+ * A versioning helper class
+ * intended to be used by flowscripts or corresponding wrapper components.
+ */
+public class WebDAVRepositoryVersioningHelper extends AbstractLogEnabled
+implements RepositoryVersioningHelper, Serviceable, Disposable, Component {
+    
+    /* The ServiceManager */
+    private ServiceManager manager;
+
+    /* The repository component */
+    private WebDAVRepository repo;
+
+    /* The credentials to be used against the WebDAV repository */
+    private CredentialsToken credentials;
+
+    /* (non-Javadoc)
+     * @see org.apache.avalon.framework.service.Serviceable#service(org.apache.avalon.framework.service.ServiceManager)
+     */
+    public void service(ServiceManager manager) throws ServiceException {
+        this.manager = manager;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.avalon.framework.activity.Disposable#dispose()
+     */
+    public void dispose() {
+        this.manager = null;
+    }
+
+    /**
+     * create a WebDAVRepositoryVersioningHelper
+     * 
+     * @param credentials  the user credentials to be used against the WebDAV repository.
+     * @param repo  a reference to the WebDAVRepository object.
+     */
+    public WebDAVRepositoryVersioningHelper (CredentialsToken credentials, WebDAVRepository repo) {
+        this.credentials = credentials;
+        this.repo = repo;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryVersioningHelper#checkout(java.lang.String)
+     */
+    public boolean checkout(String uri) {
+
+        try {
+            WebDAVUtil.getWebdavResource(this.repo.getAbsoluteURI(uri)).checkoutMethod();
+            return true;
+
+        } catch (HttpException he) {
+            this.getLogger().error("HTTP Error checking out " + uri, he);
+        } catch (IOException ioe) {
+            this.getLogger().error("IO Error checking out " + uri, ioe);
+        }
+
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryVersioningHelper#checkin(java.lang.String)
+     */
+    public boolean checkin(String uri) {
+
+        try {
+            WebDAVUtil.getWebdavResource(this.repo.getAbsoluteURI(uri)).checkinMethod();
+            return true;
+
+        } catch (HttpException he) {
+            this.getLogger().error("HTTP Error checking in " + uri, he);
+        } catch (IOException ioe) {
+            this.getLogger().error("IO Error checking in " + uri, ioe);
+        }
+
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryVersioningHelper#uncheckout(java.lang.String)
+     */
+    public boolean uncheckout(String uri) {
+
+        try {
+            WebDAVUtil.getWebdavResource(this.repo.getAbsoluteURI(uri)).uncheckoutMethod();
+            return true;
+
+        } catch (HttpException he) {
+            this.getLogger().error("HTTP Error while uncheckout " + uri, he);
+        } catch (IOException ioe) {
+            this.getLogger().error("IO Error while uncheckout " + uri, ioe);
+        }
+
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryVersioningHelper#isVersioned(java.lang.String)
+     */
+    public boolean isVersioned(String uri) {
+        //not yet implemented        
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryVersioningHelper#setVersioned(java.lang.String, boolean)
+     */
+    public boolean setVersioned(String uri, boolean versioned) {
+        //not yet implemented
+        throw new UnsupportedOperationException();
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.components.repository.helpers.RepositoryVersioningHelper#getVersions(java.lang.String)
+     */
+    public List getVersions(String uri) {
+        //not yet implemented
+        throw new UnsupportedOperationException();
+    }
+
+}
