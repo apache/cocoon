54fa2e7f07f8f3bc66eea93a518da5098caf0a7a
Joerg Heinick <joerg@apache.org>
2004-05-05 21:03:30 +0000

    fixing "bad variable names" errors as suggested by Lars Huttar (http://marc.theaimsgroup.com/?l=xml-cocoon-users&m=108363787604460&w=4)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@28331 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ else
 fi
 
 # ----- Ignore system CLASSPATH variable
-OLD_CLASSPATH=$CLASSPATH
+OLD_CLASSPATH="$CLASSPATH"
 unset CLASSPATH
 CLASSPATH="`echo lib/endorsed/*.jar | tr ' ' $S`"
 export CLASSPATH
@@ -39,11 +39,11 @@ export ANT_OPTS="-Xms32M -Xmx512M -Djava.endorsed.dirs=lib/endorsed"
 "$ANT_HOME/bin/ant" -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
 
 # ----- Restore ANT_HOME and ANT_OPTS
-export ANT_HOME=$OLD_ANT_HOME
+export ANT_HOME="$OLD_ANT_HOME"
 unset OLD_ANT_HOME
-export ANT_OPTS=$OLD_ANT_OPTS
+export ANT_OPTS="$OLD_ANT_OPTS"
 unset OLD_ANT_OPTS
 
 # ----- Restore CLASSPATH
-export CLASSPATH=$OLD_CLASSPATH
+export CLASSPATH="$OLD_CLASSPATH"
 unset OLD_CLASSPATH
