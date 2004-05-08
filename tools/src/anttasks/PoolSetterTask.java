3632a261ff8f644c01bb05f07eb0a5d8e021721e
David Crossley <crossley@apache.org>
2004-05-08 02:54:45 +0000

    Fix inconsistent whitespace.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@28376 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/anttasks/PoolSetterTask.java
+++ tools/src/anttasks/PoolSetterTask.java
@@ -1,16 +1,15 @@
-/* 
- * Copyright 2003-2004 The Apache Software Foundation
- * Licensed  under the  Apache License,  Version 2.0  (the "License");
- * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at 
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed  under the  License is distributed on an "AS IS" BASIS,
- * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
- * implied.
- * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -34,7 +33,7 @@ import org.xml.sax.SAXException;
  *
  * @since 2.1.5
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
- * @version CVS $Revision: 1.3 $ $Date: 2004/05/01 16:12:05 $
+ * @version CVS $Revision: 1.4 $ $Date: 2004/05/08 02:54:45 $
  */
 public final class PoolSetterTask extends Task {
 
