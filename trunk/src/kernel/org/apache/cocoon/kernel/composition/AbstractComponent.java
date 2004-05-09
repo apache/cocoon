5a447a7df7609e5b601873333249878bdf4b3d63
Pier Fumagalli <pier@apache.org>
2004-05-09 12:08:32 +0000

    Eclipse default templates fix (again - sorry).
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/new-kernel@30692 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/kernel/org/apache/cocoon/kernel/composition/AbstractComponent.java
+++ trunk/src/kernel/org/apache/cocoon/kernel/composition/AbstractComponent.java
@@ -1,16 +1,28 @@
-/* ========================================================================== *
- * Copyright (C) 1996-2004 VNU Business Publications LTD. All rights reserved *
- * ========================================================================== */
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.cocoon.kernel.composition;
 
 import org.apache.cocoon.kernel.resolution.Resolver;
 
 
 /**
- * <p></p> 
+ * <p>An abstract implementation of the {@link Component} interface.</p> 
  *
  * @author <a href="mailto:pier@apache.org">Pier Fumagalli</a>
- * @version 1.0 (CVS $Revision: 1.1 $)
+ * @version 1.0 (CVS $Revision: 1.2 $)
  */
 public class AbstractComponent implements Component {
  
