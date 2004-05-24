6f631ad0017008b6630a0e19a71402f7d0580911
Carsten Ziegeler <cziegeler@apache.org>
2004-05-24 13:31:30 +0000

    Make LinkTranslator cacheable (from 2.2 repo)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@28491 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/sitemap/LinkTranslator.java
+++ src/java/org/apache/cocoon/sitemap/LinkTranslator.java
@@ -18,9 +18,12 @@ package org.apache.cocoon.sitemap;
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.Constants;
 import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.caching.CacheableProcessingComponent;
 import org.apache.cocoon.environment.SourceResolver;
 import org.apache.cocoon.transformation.Transformer;
 import org.apache.cocoon.xml.xlink.ExtendedXLinkPipe;
+import org.apache.excalibur.source.SourceValidity;
+import org.apache.excalibur.source.impl.validity.NOPValidity;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -30,9 +33,9 @@ import java.util.Map;
 
 /**
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
- * @version CVS $Id: LinkTranslator.java,v 1.6 2004/03/05 13:02:58 bdelacretaz Exp $
+ * @version CVS $Id: LinkTranslator.java,v 1.7 2004/05/24 13:31:30 cziegeler Exp $
  */
-public class LinkTranslator extends ExtendedXLinkPipe implements Transformer {
+public class LinkTranslator extends ExtendedXLinkPipe implements Transformer, CacheableProcessingComponent {
     
     private Map links;
 
@@ -43,6 +46,26 @@ public class LinkTranslator extends ExtendedXLinkPipe implements Transformer {
     public void setup(SourceResolver resolver, Map objectModel, String src, Parameters par) 
     throws ProcessingException, SAXException, IOException {
         this.links = (Map)objectModel.get(Constants.LINK_OBJECT);
+    }
+
+    /**
+     * Generate the unique key.
+     * This key must be unique inside the space of this component.
+     *
+     * @return The generated key hashes the src
+     */
+    public java.io.Serializable getKey() {
+        return "1";
+    }
+
+    /**
+     * Generate the validity object.
+     *
+     * @return The generated validity object or <code>null</code> if the
+     *         component is currently not cacheable.
+     */
+    public SourceValidity getValidity() {
+        return NOPValidity.SHARED_INSTANCE;
     }
 
     public void simpleLink(String href, String role, String arcrole, 
