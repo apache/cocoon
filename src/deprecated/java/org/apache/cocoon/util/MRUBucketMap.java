fd15d81beeaa504946e3cc47ba130c5c9854d09d
Carsten Ziegeler <cziegeler@apache.org>
2004-05-27 13:52:20 +0000

    Deprecated bucket map - it's not used at all
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@28534 13f79535-47bb-0310-9956-ffa450edef68

--- src/deprecated/java/org/apache/cocoon/util/MRUBucketMap.java
+++ src/deprecated/java/org/apache/cocoon/util/MRUBucketMap.java
@@ -28,67 +28,59 @@ import java.util.Iterator;
  * <br />
  * MRUBucketMap is based on the Avalon's BucketMap.
  *
+ * @deprecated Use a corresponding map from commons-collections
  * @author  <a href="mailto:vgritsenko@apache.org">Vadim Gritsenko</a>
- * @version CVS $Id: MRUBucketMap.java,v 1.4 2004/03/05 13:03:00 bdelacretaz Exp $
+ * @version CVS $Id: MRUBucketMap.java,v 1.1 2004/05/27 13:52:20 cziegeler Exp $
  */
-public final class MRUBucketMap implements Map
-{
+public final class MRUBucketMap implements Map {
+    
     private static final int DEFAULT_BUCKETS = 255;
-    private final Node[] m_buckets;
-    private final Object[] m_locks;
-    private final Node m_header = new Node();
-    private int m_size = 0;
+    private final Node[] buckets;
+    private final Object[] locks;
+    private final Node header = new Node();
+    private int size;
 
     /**
      * Creates map with default number of buckets.
      */
-    public MRUBucketMap()
-    {
+    public MRUBucketMap() {
         this( DEFAULT_BUCKETS );
     }
 
     /**
      * Creates map with specified number of buckets.
      */
-    public MRUBucketMap( int numBuckets )
-    {
+    public MRUBucketMap( int numBuckets ) {
         int size = Math.max( 17, numBuckets );
 
         // Ensure that bucketSize is never a power of 2 (to ensure maximal distribution)
-        if( size % 2 == 0 )
-        {
+        if( size % 2 == 0 ) {
             size--;
         }
 
-        m_buckets = new Node[ size ];
-        m_locks = new Object[ size ];
+        this.buckets = new Node[ size ];
+        this.locks = new Object[ size ];
 
-        for( int i = 0; i < size; i++ )
-        {
-            m_locks[ i ] = new Object();
+        for( int i = 0; i < size; i++ ) {
+            this.locks[ i ] = new Object();
         }
 
-        m_header.mru_next = m_header.mru_previous = m_header;
+        this.header.mru_next = this.header.mru_previous = this.header;
     }
 
-    private final int getHash( Object key )
-    {
-        final int hash = key.hashCode() % m_buckets.length;
+    private final int getHash( Object key ) {
+        final int hash = key.hashCode() % this.buckets.length;
         return ( hash < 0 ) ? hash * -1 : hash;
     }
 
-    public Set keySet()
-    {
+    public Set keySet() {
         Set keySet = new HashSet();
 
-        for( int i = 0; i < m_buckets.length; i++ )
-        {
-            synchronized( m_locks[ i ] )
-            {
-                Node n = m_buckets[ i ];
+        for( int i = 0; i < this.buckets.length; i++ ) {
+            synchronized( this.locks[ i ] ) {
+                Node n = this.buckets[ i ];
 
-                while( n != null )
-                {
+                while( n != null ) {
                     keySet.add( n.key );
                     n = n.next;
                 }
@@ -101,18 +93,15 @@ public final class MRUBucketMap implements Map
     /**
      * Returns the current number of key, value pairs.
      */
-    public int size()
-    {
-        return m_size;
+    public int size() {
+        return this.size;
     }
 
     /**
      * Put a reference in the Map.
      */
-    public Object put( final Object key, final Object value )
-    {
-        if( null == key || null == value )
-        {
+    public Object put( final Object key, final Object value ) {
+        if( null == key || null == value ) {
             return null;
         }
 
@@ -122,28 +111,22 @@ public final class MRUBucketMap implements Map
 
         int hash = getHash( key );
 
-        synchronized( m_locks[ hash ] )
-        {
-            Node n = m_buckets[ hash ];
-            if( n == null )
-            {
+        synchronized( this.locks[ hash ] ) {
+            Node n = this.buckets[ hash ];
+            if( n == null ) {
                 node = new Node();
                 node.key = key;
                 node.value = value;
-                m_buckets[ hash ] = node;
+                this.buckets[ hash ] = node;
                 isNew = 1;
-            }
-            else
-            {
+            } else {
                 // Set n to the last node in the linked list.  Check each key along the way
                 //  If the key is found, then change the value of that node and return
                 //  the old value.
-                for( Node next = n; next != null; next = next.next )
-                {
+                for( Node next = n; next != null; next = next.next ) {
                     n = next;
 
-                    if( n.key.equals( key ) )
-                    {
+                    if( n.key.equals( key ) ) {
                         oldValue = n.value;
                         n.value = value;
                         break;
@@ -165,28 +148,25 @@ public final class MRUBucketMap implements Map
             }
         }
 
-        synchronized ( m_header )
-        {
+        synchronized ( this.header ) {
             if (isNew == 0) {
                 // Remove
                 node.mru_previous.mru_next = node.mru_next;
                 node.mru_next.mru_previous = node.mru_previous;
             }
             // Move node to the head.
-            node.mru_previous = m_header;
-            node.mru_next = m_header.mru_next;
+            node.mru_previous = this.header;
+            node.mru_next = this.header.mru_next;
             node.mru_previous.mru_next = node;
             node.mru_next.mru_previous = node;
-            m_size += isNew;
+            this.size += isNew;
         }
 
         return oldValue;
     }
 
-    public Object get( final Object key )
-    {
-        if( null == key )
-        {
+    public Object get( final Object key ) {
+        if( null == key ) {
             return null;
         }
 
@@ -194,14 +174,11 @@ public final class MRUBucketMap implements Map
 
         int hash = getHash( key );
 
-        synchronized( m_locks[ hash ] )
-        {
-            n = m_buckets[ hash ];
+        synchronized( this.locks[ hash ] ) {
+            n = this.buckets[ hash ];
 
-            while( n != null )
-            {
-                if( n.key.equals( key ) )
-                {
+            while( n != null ) {
+                if( n.key.equals( key ) ) {
                     break;
                 }
 
@@ -210,13 +187,13 @@ public final class MRUBucketMap implements Map
         }
 
         if( n != null ) {
-            synchronized( m_header ) {
+            synchronized( this.header ) {
                 // Remove
                 n.mru_previous.mru_next = n.mru_next;
                 n.mru_next.mru_previous = n.mru_previous;
                 // Add first
-                n.mru_previous = m_header;
-                n.mru_next = m_header.mru_next;
+                n.mru_previous = this.header;
+                n.mru_next = this.header.mru_next;
                 n.mru_previous.mru_next = n;
                 n.mru_next.mru_previous = n;
             }
@@ -226,23 +203,18 @@ public final class MRUBucketMap implements Map
         return null;
     }
 
-    public boolean containsKey( final Object key )
-    {
-        if( null == key )
-        {
+    public boolean containsKey( final Object key ) {
+        if( null == key ) {
             return false;
         }
 
         int hash = getHash( key );
 
-        synchronized( m_locks[ hash ] )
-        {
-            Node n = m_buckets[ hash ];
+        synchronized( this.locks[ hash ] ) {
+            Node n = this.buckets[ hash ];
 
-            while( n != null )
-            {
-                if( n.key.equals( key ) )
-                {
+            while( n != null ) {
+                if( n.key.equals( key ) ) {
                     return true;
                 }
 
@@ -253,19 +225,14 @@ public final class MRUBucketMap implements Map
         return false;
     }
 
-    public boolean containsValue( final Object value )
-    {
-        if( null == value )
-        {
+    public boolean containsValue( final Object value ) {
+        if( null == value ) {
             return false;
         }
 
-        synchronized( m_header )
-        {
-            for( Node n = m_header.mru_next; n != m_header; n = n.mru_next )
-            {
-                if( n.value.equals( value ) )
-                {
+        synchronized( this.header ) {
+            for( Node n = this.header.mru_next; n != this.header; n = n.mru_next ) {
+                if( n.value.equals( value ) ) {
                     return true;
                 }
             }
@@ -274,14 +241,11 @@ public final class MRUBucketMap implements Map
         return false;
     }
 
-    public Collection values()
-    {
+    public Collection values() {
         Set valueSet = new HashSet();
 
-        synchronized( m_header )
-        {
-            for( Node n = m_header.mru_next; n != m_header; n = n.mru_next )
-            {
+        synchronized( this.header ) {
+            for( Node n = this.header.mru_next; n != this.header; n = n.mru_next ) {
                 valueSet.add( n.value );
             }
         }
@@ -289,14 +253,11 @@ public final class MRUBucketMap implements Map
         return valueSet;
     }
 
-    public Set entrySet()
-    {
+    public Set entrySet() {
         Set entrySet = new HashSet();
 
-        synchronized( m_header )
-        {
-            for( Node n = m_header.mru_next; n != m_header; n = n.mru_next )
-            {
+        synchronized( this.header ) {
+            for( Node n = this.header.mru_next; n != this.header; n = n.mru_next ) {
                 entrySet.add( n );
             }
         }
@@ -307,21 +268,17 @@ public final class MRUBucketMap implements Map
     /**
      * Add all the contents of one Map into this one.
      */
-    public void putAll( Map other )
-    {
+    public void putAll( Map other ) {
         Iterator i = other.keySet().iterator();
 
-        while( i.hasNext() )
-        {
+        while( i.hasNext() ) {
             Object key = i.next();
             put( key, other.get( key ) );
         }
     }
 
-    public Object remove( Object key )
-    {
-        if( null == key )
-        {
+    public Object remove( Object key ) {
+        if( null == key ) {
             return null;
         }
 
@@ -329,23 +286,17 @@ public final class MRUBucketMap implements Map
 
         int hash = getHash( key );
 
-        synchronized( m_locks[ hash ] )
-        {
-            n = m_buckets[ hash ];
+        synchronized( this.locks[ hash ] ) {
+            n = this.buckets[ hash ];
             Node prev = null;
 
-            while( n != null )
-            {
-                if( n.key.equals( key ) )
-                {
+            while( n != null ) {
+                if( n.key.equals( key ) ) {
                     // Remove this node from the linked list of nodes.
-                    if( null == prev )
-                    {
+                    if( null == prev ) {
                         // This node was the head, set the next node to be the new head.
-                        m_buckets[ hash ] = n.next;
-                    }
-                    else
-                    {
+                        this.buckets[ hash ] = n.next;
+                    } else {
                         // Set the next node of the previous node to be the node after this one.
                         prev.next = n.next;
                     }
@@ -359,11 +310,11 @@ public final class MRUBucketMap implements Map
         }
 
         if (n != null) {
-            synchronized(m_header) {
+            synchronized(this.header) {
                 // Remove
                 n.mru_previous.mru_next = n.mru_next;
                 n.mru_next.mru_previous = n.mru_previous;
-                m_size--;
+                this.size--;
             }
 
             return n.value;
@@ -372,28 +323,24 @@ public final class MRUBucketMap implements Map
         return null;
     }
 
-    public final boolean isEmpty()
-    {
-        return m_size == 0;
+    public final boolean isEmpty() {
+        return this.size == 0;
     }
 
-    public final void clear()
-    {
-        synchronized ( m_header ) {
-            for( int i = 0; i < m_buckets.length; i++ )
-            {
-                m_buckets[ i ] = null;
+    public final void clear() {
+        synchronized ( this.header ) {
+            for( int i = 0; i < this.buckets.length; i++ ) {
+                this.buckets[ i ] = null;
             }
 
-            m_header.mru_next = m_header.mru_previous = m_header;
-            m_size = 0;
+            this.header.mru_next = this.header.mru_previous = this.header;
+            this.size = 0;
         }
     }
 
-    public Map.Entry removeLast()
-    {
-        Node node = m_header.mru_previous;
-        if (node == m_header) {
+    public Map.Entry removeLast() {
+        Node node = this.header.mru_previous;
+        if (node == this.header) {
             throw new NoSuchElementException("MRUBucketMap is empty");
         }
 
@@ -403,8 +350,8 @@ public final class MRUBucketMap implements Map
         return node;
     }
 
-    private static final class Node implements Map.Entry
-    {
+    private static final class Node implements Map.Entry {
+        
         protected Object key;
         protected Object value;
         protected Node next;
@@ -412,40 +359,18 @@ public final class MRUBucketMap implements Map
         protected Node mru_previous;
         protected Node mru_next;
 
-        public Object getKey()
-        {
-            return key;
+        public Object getKey() {
+            return this.key;
         }
 
-        public Object getValue()
-        {
-            return value;
+        public Object getValue() {
+            return this.value;
         }
 
-        public Object setValue( Object val )
-        {
-            Object retVal = value;
-            value = val;
+        public Object setValue( Object val ) {
+            Object retVal = this.value;
+            this.value = val;
             return retVal;
-        }
-    }
-
-
-    static class X {
-        String x;
-        public X (String s) {
-            x = s;
-        }
-        public int hashCode() {
-            return 1;
-        }
-
-        public boolean equals(Object obj) {
-            return this == obj;
-        }
-
-        public String toString() {
-            return x;
         }
     }
 }
