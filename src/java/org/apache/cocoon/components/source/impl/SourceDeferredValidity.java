a8e80c43bea160b9d9cf7543bc76a22ed8cf569a
Vadim Gritsenko <vgritsenko@apache.org>
2004-06-16 14:29:31 +0000

    zap tabs
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@28641 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/source/impl/SourceDeferredValidity.java
+++ src/java/org/apache/cocoon/components/source/impl/SourceDeferredValidity.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,21 +24,20 @@ import org.apache.excalibur.source.impl.validity.DeferredValidity;
  * by invoking a pipeline.
  *
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
- * @version CVS $Id: SourceDeferredValidity.java,v 1.2 2004/03/05 13:02:50 bdelacretaz Exp $
+ * @version CVS $Id: SourceDeferredValidity.java,v 1.3 2004/06/16 14:29:31 vgritsenko Exp $
  */
 public final class SourceDeferredValidity implements DeferredValidity {
 
     private Source source;
-    
+
     public SourceDeferredValidity(Source source) {
         this.source = source;
     }
-    
-	/**
-	 * @see org.apache.excalibur.source.impl.validity.DeferredValidity#getValidity()
-	 */
-	public SourceValidity getValidity() {
-		return this.source.getValidity();
-	}
 
+    /**
+     * @see org.apache.excalibur.source.impl.validity.DeferredValidity#getValidity()
+     */
+    public SourceValidity getValidity() {
+        return this.source.getValidity();
+    }
 }
