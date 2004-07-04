9feaa2c9eba52882518615e8f066e93f6a08c401
Bernhard Huber <huber@apache.org>
2004-07-04 18:01:39 +0000

    junit test for augment, and encode url transformers PR: Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: PR: CVS:   If this change addresses a PR in the problem report tracking CVS:   database, then enter the PR number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, such as NCSA, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to Apache by someone else; i.e., CVS:   they sent us a patch or a new module, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@28771 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/cocoon/transformation/AugmentTransformerTestCase.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cocoon.transformation;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.Processor;
+import org.apache.cocoon.SitemapComponentTestCase;
+import org.apache.cocoon.components.container.ComponentManagerWrapper;
+import org.apache.cocoon.environment.internal.EnvironmentHelper;
+import org.apache.cocoon.environment.mock.MockEnvironment;
+import org.w3c.dom.Document;
+
+
+/**
+ * A simple testcase for AugmentTransformer.
+ *
+ * @author <a href="mailto:huber@apache.org">Bernhard Huber</a>
+ * @version CVS $Id: AugmentTransformerTestCase.java,v 1.1 2004/07/04 18:01:39 huber Exp $
+ */
+public class AugmentTransformerTestCase extends SitemapComponentTestCase {
+    
+    /** Create new testcase
+     * @param name of testase
+     */
+    public AugmentTransformerTestCase(String name) {
+        super(name);
+    }
+    
+    /**
+     * Run this test suite from commandline
+     *
+     * @param args commandline arguments (ignored)
+     */
+    public static void main( String[] args ) {
+        TestRunner.run(suite());
+    }
+    
+    /** Create a test suite.
+     * This test suite contains all test cases of this class.
+     * @return the Test object containing all test cases.
+     */
+    public static Test suite() {
+        TestSuite suite = new TestSuite(AugmentTransformerTestCase.class);
+        return suite;
+    }
+    
+    /** Testcase for augment transformation
+     *
+     * @throws Exception iff ComponentManager enterEnvironment fails
+     */
+    public void testAugment1() throws Exception {
+        getLogger().debug("testAugment1");
+        
+        Parameters parameters = new Parameters();
+        parameters.setParameter( "mount", "portal1/sect1/" );
+        
+        String input = "resource://org/apache/cocoon/transformation/augment-input-1.xml";
+        String result = "resource://org/apache/cocoon/transformation/augment-result-1.xml";
+        String src =  null;
+        
+        // enter & leave environment, as a manager is looked up using
+        // the processing context stack
+        MockEnvironment env = new MockEnvironment();
+        Processor processor = (Processor)this.manager.lookup(Processor.ROLE);
+        
+        EnvironmentHelper.enterProcessor(processor, new ComponentManagerWrapper(this.manager), env);
+        
+        Document resultDocument = load(result);
+        Document inputDocument = load(input);
+        Document transformDocument = transform("augment", src, parameters, inputDocument );
+        
+        printDocs( resultDocument, inputDocument, transformDocument );
+        
+        assertIdentical( resultDocument, transformDocument );
+        
+        EnvironmentHelper.leaveProcessor();
+    }
+    
+    /**
+     * print documents to System.out
+     *
+     * @param resultDocument the expected result document
+     * @param inputDocument the input document
+     * @param transformDocument  the transformed input document
+     */
+    protected void printDocs( Document resultDocument, Document inputDocument, Document transformDocument ) {
+        System.out.println( "resultDocument" );
+        this.print( resultDocument );
+        System.out.println( "inputDocument" );
+        this.print( inputDocument );
+        System.out.println( "transformDocument" );
+        this.print( transformDocument );
+    }
+}
