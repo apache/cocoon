c0a3c1cc5e359804953608ec2609cbe90623ce6c
Vadim Gritsenko <vgritsenko@apache.org>
2004-07-29 12:35:59 +0000

    Sync with 2.2
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@30920 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #  Copyright 1999-2004 The Apache Software Foundation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#
+
 chmod u+x ./tools/bin/antRun
 chmod u+x ./tools/bin/ant
 
@@ -32,18 +33,21 @@ export CLASSPATH
 
 # ----- Use Ant shipped with Cocoon. Ignore installed in the system Ant
 OLD_ANT_HOME="$ANT_HOME"
-export ANT_HOME=tools
+ANT_HOME=tools
 OLD_ANT_OPTS="$ANT_OPTS"
-export ANT_OPTS="-Xms32M -Xmx512M -Djava.endorsed.dirs=lib/endorsed"
+ANT_OPTS="-Xms32M -Xmx512M -Djava.endorsed.dirs=lib/endorsed"
+export ANT_HOME ANT_OPTS
 
 "$ANT_HOME/bin/ant" -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
 
 # ----- Restore ANT_HOME and ANT_OPTS
-export ANT_HOME="$OLD_ANT_HOME"
+ANT_HOME="$OLD_ANT_HOME"
+ANT_OPTS="$OLD_ANT_OPTS"
+export ANT_HOME ANT_OPTS
 unset OLD_ANT_HOME
-export ANT_OPTS="$OLD_ANT_OPTS"
 unset OLD_ANT_OPTS
 
 # ----- Restore CLASSPATH
-export CLASSPATH="$OLD_CLASSPATH"
+CLASSPATH="$OLD_CLASSPATH"
+export CLASSPATH
 unset OLD_CLASSPATH
