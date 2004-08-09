850bcf8643b60c734e51fc381baa227f5d6e72ce
Vadim Gritsenko <vgritsenko@apache.org>
2004-08-09 14:23:02 +0000

    Bug #30046. Check session attribute "locale" and cookie "locale" in I18nMatcher. Code cleanup.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@36113 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/matching/I18nMatcher.java
+++ src/java/org/apache/cocoon/matching/I18nMatcher.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,14 +31,17 @@ import org.apache.avalon.framework.service.Serviceable;
 import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.Session;
+import org.apache.cocoon.environment.Cookie;
 import org.apache.cocoon.sitemap.PatternException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.excalibur.source.Source;
 import org.apache.excalibur.source.SourceResolver;
 
 /**
- * A matcher that locates and identifies to the pipeline a source document to be used as 
+ * A matcher that locates and identifies to the pipeline a source document to be used as
  * the content for an i18n site, based upon a locale provided in a range of ways.
+ *
  * <h1>Configuration</h1>
  * <p>
  * A sample configuration (given in the &lt;matchers&gt; section of the sitemap) is given
@@ -46,27 +49,28 @@ import org.apache.excalibur.source.SourceResolver;
  * </p>
  * <pre>
  *  &lt;map:matcher name="i18n" src="org.apache.cocoon.matching.I18nMatcher"&gt;
- *    &lt;request-param-name&gt;lang&lt;/request-param-name&gt;
+ *    &lt;locale-attribute&gt;locale&lt;/locale-attribute&gt;
  *    &lt;use-locale&gt;true&lt;/use-locale&gt;
  *    &lt;use-locales&gt;true&lt;/use-locales&gt;
  *    &lt;default-locale lang="pt" country="BR"/&gt;
  *    &lt;test-blank-locale&gt;true&lt;/test-blank-locale&gt;
- *  &lt;/map:matcher&gt;  
+ *  &lt;/map:matcher&gt;
  * </pre>
  * <p>
  * Within this configuration, it is possible to:
  *   <ul>
- *     <li>Specify the request parameter that is to be used as a locale, if present</li>
+ *     <li>Specify the name of request parameter / session attribute / cookie
+ *     that is to be used as a locale (defaults to <code>locale</code>)</li>
  *     <li>Specify whether the primary locale provided by the user agent is to be used</li>
  *     <li>Specify whether each locale provided by the user agent should be tested in turn</li>
  *     <li>Specify the default locale to be used when none matches any of the previous</li>
- *     <li>Specify whether a file should be looked for without a locale in its filename, e.g. 
- *         after looking for index.en.html, try index.html.</li>
+ *     <li>Specify whether a file should be looked for without a locale in
+ *     its filename or filepath, e.g. after looking for index.en.html, try index.html.</li>
  *   </ul>
  * </p>
+ *
  * <h1>Usage</h1>
- * <p>This matcher will be used in a pipeline like so:
- * </p>
+ * <p>This matcher will be used in a pipeline like so:</p>
  * <pre>
  *   &lt;map:match pattern="*.html"&gt;
  *     &lt;map:match type="i18n" pattern="xml/{1}.*.xml"&gt;
@@ -77,23 +81,25 @@ import org.apache.excalibur.source.SourceResolver;
  * </pre>
  *
  * <h1>Locale Identification</h1>
- * <p>
- * A source will be looked for using each of the following as a source for locale. Where the 
- * full locale (language, country, variant) doesn't match, it will fall back to first language 
+ * <p>A source will be looked for using each of the following as a source for locale. Where the
+ * full locale (language, country, variant) doesn't match, it will fall back to first language
  * and country, and then just language, before moving on to the next locale.
  * <ul>
  *   <li>Locale provided as a request parameter</li>
- *   <li>Locale provided using a &lt;map:parameter name="locale" value="{1}"/&gt; style parameter
- *       within the &lt;map:match&gt; node</li>
- *   <li>Locales provided by the user agent</li>
+ *   <li>Locale provided as a session attribute</li>
+ *   <li>Locale provided as a cookie</li>
+ *   <li>Locale provided using a map parameter<br>
+ *   (&lt;map:parameter name="locale" value="{1}"/&gt; style parameter within
+ *   the &lt;map:match&gt; node)</li>
+ *   <li>Locale(s) provided by the user agent or server default</li>
  *   <li>The default locale specified in the matcher's configuration</li>
- *   <li>Resources with no defined locale</li>
+ *   <li>Resources with no defined locale (blank locale)</li>
  * </ul>
  * </p>
+ *
  * <h1>Sitemap Variables</h1>
- * <p>
- * Once a matching locale has been found, the following sitemap variables will be available to 
- * sitemap elements contained within the matcher:
+ * <p>Once a matching locale has been found, the following sitemap variables
+ * will be available to sitemap elements contained within the matcher:
  * <ul>
  *   <li>{source}: The URI of the source that matched</li>
  *   <li>{locale}: The locale that matched that resource</li>
@@ -103,26 +109,18 @@ import org.apache.excalibur.source.SourceResolver;
  *   <li>{variant}: The variant of the matching resource</li>
  * </ul>
  * </p>
- *    
+ *
  * @author <a href="mailto:uv@upaya.co.uk">Upayavira</a>
- * @version CVS $Id: I18nMatcher.java,v 1.4 2004/07/14 06:06:08 antonio Exp $
+ * @version CVS $Id$
  */
 public class I18nMatcher extends AbstractLogEnabled implements Matcher, ThreadSafe, Serviceable, Configurable {
-
-    private ServiceManager manager;
-    private SourceResolver resolver;
-    private String requestParameterName;
-    private boolean useLocale;
-    private boolean useLocales;
-    private Locale defaultLocale;
-    private boolean testBlankLocale;
 
     private static final boolean DEFAULT_USE_LOCALE = true;
     private static final boolean DEFAULT_USE_LOCALES = true;
     private static final String DEFAULT_DEFAULT_LANG = "en";
     private static final String DEFAULT_DEFAULT_COUNTRY = "US";
     private static final String DEFAULT_DEFAULT_VARIANT = null;
-    private static final String DEFAULT_REQUEST_PARAM_NAME = null;
+    private static final String DEFAULT_LOCALE_ATTRIBUTE = "locale";
     private static final boolean DEFAULT_TEST_BLANK_LOCALE = true;
 
     private static final String MAP_LOCALE = "locale";
@@ -132,40 +130,37 @@ public class I18nMatcher extends AbstractLogEnabled implements Matcher, ThreadSa
     private static final String MAP_LANGUAGE ="language";
     private static final String MAP_VARIANT = "variant";
 
+    private ServiceManager manager;
+    private SourceResolver resolver;
+    private String localeAttribute;
+    private boolean useLocale;
+    private boolean useLocales;
+    private Locale defaultLocale;
+    private boolean testBlankLocale;
+
     public void service(ServiceManager manager) throws ServiceException {
         this.manager = manager;
         this.resolver = (SourceResolver)this.manager.lookup(SourceResolver.ROLE);
     }
 
     public void configure(Configuration config) {
-        Configuration child;
-        child = config.getChild("use-locale");
-        useLocale = child == null ? DEFAULT_USE_LOCALE : child.getValueAsBoolean(DEFAULT_USE_LOCALE);
-        
-        child = config.getChild("use-locales");
-        useLocales = child == null ? DEFAULT_USE_LOCALES : child.getValueAsBoolean(DEFAULT_USE_LOCALES);
-        
-        child = config.getChild("default-locale");
-        if (child == null) {
-            defaultLocale = getLocale(DEFAULT_DEFAULT_LANG, DEFAULT_DEFAULT_COUNTRY, DEFAULT_DEFAULT_VARIANT);   
-        } else {
-            defaultLocale = getLocale(child.getAttribute("lang", DEFAULT_DEFAULT_LANG),
-                                      child.getAttribute("country", DEFAULT_DEFAULT_COUNTRY),
-                                      child.getAttribute("variant", DEFAULT_DEFAULT_VARIANT));
-        }
+        this.useLocale = config.getChild("use-locale").getValueAsBoolean(DEFAULT_USE_LOCALE);
+        this.useLocales = config.getChild("use-locales").getValueAsBoolean(DEFAULT_USE_LOCALES);
 
-        child = config.getChild("request-param-name");
-        requestParameterName = child == null ? DEFAULT_REQUEST_PARAM_NAME : child.getValue(DEFAULT_REQUEST_PARAM_NAME);
+        Configuration child = config.getChild("default-locale");
+        this.defaultLocale = getLocale(child.getAttribute("lang", DEFAULT_DEFAULT_LANG),
+                                       child.getAttribute("country", DEFAULT_DEFAULT_COUNTRY),
+                                       child.getAttribute("variant", DEFAULT_DEFAULT_VARIANT));
 
-        child = config.getChild("test-blank-locale");
-        testBlankLocale = child == null ? DEFAULT_TEST_BLANK_LOCALE : child.getValueAsBoolean(DEFAULT_TEST_BLANK_LOCALE);
+        this.localeAttribute = config.getChild("locale-attribute").getValue(DEFAULT_LOCALE_ATTRIBUTE);
+        this.testBlankLocale = config.getChild("test-blank-locale").getValueAsBoolean(DEFAULT_TEST_BLANK_LOCALE);
     }
 
     private Locale getLocale(String lang, String country, String variant) {
         if (lang == null) {
             return null;
         }
-            
+
         if (country == null) {
             return new Locale(lang, "");
         }
@@ -180,25 +175,52 @@ public class I18nMatcher extends AbstractLogEnabled implements Matcher, ThreadSa
     public Map match(String pattern, Map objectModel, Parameters parameters) throws PatternException {
         Map map = new HashMap();
         Request request = ObjectModelHelper.getRequest(objectModel);
-        Locale locale = request.getLocale();
-        Enumeration locales = request.getLocales();
-        String requestParameter = request.getParameter(requestParameterName);
-        String sitemapParameter = parameters.getParameter("locale", null);
 
-        if (requestParameter != null && isValidResource(pattern, new Locale(requestParameter, ""), map)) {
+        // 1. Request parameter 'locale'
+        String locale = request.getParameter(localeAttribute);
+        if (locale != null && isValidResource(pattern, new Locale(locale, ""), map)) {
              return map;
         }
 
-        if (sitemapParameter != null && isValidResource(pattern, new Locale(sitemapParameter, ""), map)) {
-              return map;
-        }
-
-        if (useLocale && !useLocales && isValidResource(pattern, locale, map)) {
+        // 2. Session attribute 'locale'
+        Session session = request.getSession(false);
+        if (session != null &&
+            ((locale = (String) session.getAttribute(localeAttribute)) != null) &&
+                isValidResource(pattern, new Locale(locale, ""), map)) {
             return map;
         }
 
+        // 3. First matching cookie parameter 'locale' within each cookie sent
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie cookie = cookies[i];
+                if (cookie.getName().equals(localeAttribute)) {
+                    if (isValidResource(pattern, new Locale(locale, ""), map)) {
+                        return map;
+                    }
+                    break;
+                }
+            }
+        }
+
+        // 4. Sitemap parameter "locale"
+        locale = parameters.getParameter("locale", null);
+        if (locale != null && isValidResource(pattern, new Locale(locale, ""), map)) {
+              return map;
+        }
+
+        // 5. Locale setting of the requesting browser or server default
+        if (useLocale && !useLocales) {
+            Locale l = request.getLocale();
+            if(isValidResource(pattern, l, map)) {
+                return map;
+            }
+        }
+
         if (useLocales) {
-            for (; locales.hasMoreElements();) {
+            Enumeration locales = request.getLocales();
+            while (locales.hasMoreElements()) {
                 Locale l = (Locale)locales.nextElement();
                 if (isValidResource(pattern, l, map)) {
                     return map;
@@ -206,57 +228,63 @@ public class I18nMatcher extends AbstractLogEnabled implements Matcher, ThreadSa
             }
         }
 
+        // 6. Default
         if (defaultLocale != null && isValidResource(pattern, defaultLocale, map)) {
             return map;
         }
 
+        // 7. Blank
         if (testBlankLocale && isValidResource(pattern, null, map)) {
-            return map;   
+            return map;
         }
         return null;
-    }    
+    }
 
     private boolean isValidResource(String pattern, Locale locale, Map map) {
         Locale testLocale;
 
         if (locale == null) {
-            return isValidResource(pattern, null, null, map);   
+            return isValidResource(pattern, null, null, map);
         }
 
         testLocale = locale;
         if (isValidResource(pattern, locale, testLocale.toString(), map)) {
-            return true;        
+            return true;
         }
 
-        testLocale = new Locale(locale.getLanguage(), locale.getCountry()); 
+        testLocale = new Locale(locale.getLanguage(), locale.getCountry());
         if (isValidResource(pattern, locale, testLocale.toString(), map)) {
-            return true;        
+            return true;
         }
 
-        testLocale = new Locale(locale.getLanguage(), ""); 
+        testLocale = new Locale(locale.getLanguage(), "");
         if (isValidResource(pattern, locale, testLocale.toString(), map)) {
-            return true;        
+            return true;
         }
 
         return false;
     }
 
     private boolean isValidResource(String pattern, Locale locale, String localeString, Map map) {
-        Source source;
-
         String url;
-        if (localeString!=null) {
+        if (localeString != null) {
             url = StringUtils.replace(pattern, "*", localeString);
         } else {
+            // If same character found before and after the '*', leave only one.
             int starPos = pattern.indexOf("*");
-            if (starPos< pattern.length()-1 && starPos > 1 && 
-                    pattern.charAt(starPos-1) == pattern.charAt(starPos+1)) {
-                url = pattern.substring(0,starPos-1) + pattern.substring(starPos+1);
+            if (starPos < pattern.length() - 1 && starPos > 1 &&
+                    pattern.charAt(starPos - 1) == pattern.charAt(starPos + 1)) {
+                url = pattern.substring(0, starPos - 1) + pattern.substring(starPos + 1);
             } else {
                 url = StringUtils.replace(pattern, "*", "");
             }
+
+            // Blank locale - empty string
+            localeString = "";
         }
+
         boolean result = false;
+        Source source = null;
         try {
             source = resolver.resolveURI(url);
             if (source.exists()) {
@@ -270,10 +298,14 @@ public class I18nMatcher extends AbstractLogEnabled implements Matcher, ThreadSa
                 }
                 result = true;
             }
-            resolver.release(source);
         } catch (IOException e) {
-            return false;
+            // result is false
+        } finally {
+            if (source != null) {
+                resolver.release(source);
+            }
         }
-    return result;
+
+        return result;
     }
 }
