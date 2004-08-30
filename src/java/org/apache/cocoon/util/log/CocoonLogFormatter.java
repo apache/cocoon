f4b1d0f2a38cf04b9c2bf471aa34aec182f96146
Vadim Gritsenko <vgritsenko@apache.org>
2004-08-30 12:23:09 +0000

    no change
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@37195 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/util/log/CocoonLogFormatter.java
+++ src/java/org/apache/cocoon/util/log/CocoonLogFormatter.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,7 +42,7 @@ import org.apache.log.Logger;
  * </ul>
  *
  * @author <a href="mailto:sylvain@apache.org">Sylvain Wallez</a>
- * @version CVS $Id: CocoonLogFormatter.java,v 1.4 2004/03/28 10:27:20 antonio Exp $
+ * @version CVS $Id$
  */
 public class CocoonLogFormatter extends ExtensiblePatternFormatter
 {
