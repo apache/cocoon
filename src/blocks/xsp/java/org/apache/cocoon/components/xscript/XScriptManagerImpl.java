50ddc813c57021b8db103304145575cf25caf103
Vadim Gritsenko <vgritsenko@apache.org>
2004-09-09 01:07:53 +0000

    Implement dispose; clean up context on dispose
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@43554 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/xsp/java/org/apache/cocoon/components/xscript/XScriptManagerImpl.java
+++ src/blocks/xsp/java/org/apache/cocoon/components/xscript/XScriptManagerImpl.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,7 @@
  */
 package org.apache.cocoon.components.xscript;
 
+import org.apache.avalon.framework.activity.Disposable;
 import org.apache.avalon.framework.component.Component;
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.parameters.ParameterException;
@@ -39,24 +40,24 @@ import java.util.Map;
  * The actual implementation of the <code>XScriptManager</code> interface.
  *
  * @author <a href="mailto:ovidiu@cup.hp.com">Ovidiu Predescu</a>
- * @version CVS $Id: XScriptManagerImpl.java,v 1.1 2004/03/10 12:58:08 stephan Exp $
+ * @version CVS $Id$
  * @since August  4, 2001
  */
 public class XScriptManagerImpl
         extends AbstractLogEnabled
-        implements XScriptManager, Serviceable, Component, Parameterizable, Contextualizable, ThreadSafe
+        implements XScriptManager, Serviceable, Component, Parameterizable, Contextualizable, ThreadSafe, Disposable
 {
     public static final String CONTEXT = "org.apache.cocoon.components.xscript.scope";
 
     /**
      * The <code>ServiceManager</code> instance.
      */
-    protected ServiceManager manager = null;
+    protected ServiceManager manager;
 
     /**
      * The <code>Context</code> instance.
      */
-    protected Context context = null;
+    protected Context context;
 
 
     public void contextualize(org.apache.avalon.framework.context.Context context)
@@ -88,6 +89,12 @@ public class XScriptManagerImpl
             String resourceString = params.getParameter(names[i]);
             XScriptObject resource = new XScriptObjectFromURL(this, resourceString);
             s.put(names[i], resource);
+        }
+    }
+
+    public void dispose() {
+        if (context != null) {
+            context.removeAttribute(CONTEXT);
         }
     }
 
