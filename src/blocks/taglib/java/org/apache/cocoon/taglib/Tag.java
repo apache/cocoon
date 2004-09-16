bb7472ba455f68555a1eafc84ce9dd97477e7050
Vadim Gritsenko <vgritsenko@apache.org>
2004-09-16 20:08:06 +0000

    Taglib block: Initial implementation of BodyTag.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@46207 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/taglib/java/org/apache/cocoon/taglib/Tag.java
+++ src/blocks/taglib/java/org/apache/cocoon/taglib/Tag.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,7 +33,7 @@ import org.xml.sax.SAXException;
  * @see org.apache.cocoon.transformation.TagTransformer
  * 
  * @author <a href="mailto:volker.schmitt@basf-it-services.com">Volker Schmitt</a>
- * @version CVS $Id: Tag.java,v 1.3 2004/03/05 13:02:24 bdelacretaz Exp $
+ * @version CVS $Id$
  */
 public interface Tag extends Component {
     
@@ -53,7 +53,7 @@ public interface Tag extends Component {
 
     /**
      * Continue evaluating the page.
-     * Valid return value for doEndTag().
+     * Valid return value for doEndTag.
      */
     int EVAL_PAGE = 2;
 
