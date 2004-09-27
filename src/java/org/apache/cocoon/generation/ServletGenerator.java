b6d61f5cc275ab63bac040c1fa8f55b977e32183
Vadim Gritsenko <vgritsenko@apache.org>
2004-09-27 16:07:01 +0000

    javadoc, spaces
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@47301 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/generation/ServletGenerator.java
+++ src/java/org/apache/cocoon/generation/ServletGenerator.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,17 +34,11 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- * @author CZiegeler
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
-/**
  *
  * @author <a href="mailto:Giacomo.Pati@pwr.ch">Giacomo Pati</a>
- * @version CVS $Id: ServletGenerator.java,v 1.4 2004/05/24 12:37:52 cziegeler Exp $
+ * @version CVS $Id$
  */
-public abstract class ServletGenerator extends AbstractGenerator 
+public abstract class ServletGenerator extends AbstractGenerator
 implements Composable, Disposable {
 
     protected Request request;
@@ -64,7 +58,7 @@ implements Composable, Disposable {
     public void dispose() {
         this.manager = null;
     }
-    
+
     /* (non-Javadoc)
      * @see org.apache.cocoon.sitemap.SitemapModelComponent#setup(org.apache.cocoon.environment.SourceResolver, java.util.Map, java.lang.String, org.apache.avalon.framework.parameters.Parameters)
      */
--- src/java/org/apache/cocoon/serialization/Serializer.java
+++ src/java/org/apache/cocoon/serialization/Serializer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,10 +19,12 @@ import org.apache.cocoon.sitemap.SitemapOutputComponent;
 import org.apache.cocoon.xml.XMLConsumer;
 
 /**
+ * A serializer is the last point of a pipeline. It "serializes" XML
+ * arriving as SAX events into any binary format.
  *
  * @author <a href="mailto:pier@apache.org">Pierpaolo Fumagalli</a>
  *         (Apache Software Foundation)
- * @version CVS $Id: Serializer.java,v 1.2 2004/03/05 13:02:58 bdelacretaz Exp $
+ * @version CVS $Id$
  */
 public interface Serializer extends XMLConsumer, SitemapOutputComponent {
 
