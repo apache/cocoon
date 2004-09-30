3a907f2a939d370cdcb98ca30acea31eda402c29
Vadim Gritsenko <vgritsenko@apache.org>
2004-09-30 03:15:55 +0000

    fix fireJob
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@47533 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/cron/java/org/apache/cocoon/components/cron/JobScheduler.java
+++ src/blocks/cron/java/org/apache/cocoon/components/cron/JobScheduler.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,12 +22,11 @@ import java.util.NoSuchElementException;
 import org.apache.avalon.framework.CascadingException;
 import org.apache.avalon.framework.parameters.Parameters;
 
-
 /**
  * This component schedules jobs.
  *
  * @author <a href="mailto:giacomo@apache.org">Giacomo Pati</a>
- * @version CVS $Id: JobScheduler.java,v 1.7 2004/03/08 13:43:42 unico Exp $
+ * @version CVS $Id$
  *
  * @since 2.1.1
  */
