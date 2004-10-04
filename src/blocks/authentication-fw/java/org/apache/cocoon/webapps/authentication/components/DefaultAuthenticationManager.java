9a25bfaf4f555ead5a56a4b202d8fb2172a4a21c
Vadim Gritsenko <vgritsenko@apache.org>
2004-10-04 19:19:19 +0000

    tab hunt
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@53739 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/authentication-fw/java/org/apache/cocoon/webapps/authentication/components/DefaultAuthenticationManager.java
+++ src/blocks/authentication-fw/java/org/apache/cocoon/webapps/authentication/components/DefaultAuthenticationManager.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,42 +65,42 @@ import org.xml.sax.SAXException;
  * This is the basis authentication component.
  *
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
- * @version CVS $Id: DefaultAuthenticationManager.java,v 1.25 2004/03/19 14:16:55 cziegeler Exp $
+ * @version CVS $Id$
 */
 public class DefaultAuthenticationManager
-extends AbstractLogEnabled
-implements AuthenticationManager, 
-            SitemapConfigurable, 
-            Serviceable, 
-            Disposable, 
-            ThreadSafe, 
-            Contextualizable,
-            Component {
+        extends AbstractLogEnabled
+        implements AuthenticationManager,
+                   SitemapConfigurable,
+                   Serviceable,
+                   Disposable,
+                   ThreadSafe,
+                   Contextualizable,
+                   Component {
 
     /** The name of the session attribute storing the user status */
     public final static String SESSION_ATTRIBUTE_USER_STATUS = DefaultAuthenticationManager.class.getName() + "/UserStatus";
 
     /** The manager for the authentication handlers */
     protected SitemapConfigurationHolder holder;
-    
+
     /** The Service Manager */
     protected ServiceManager manager;
-    
+
     /** The Source Resolver */
     protected SourceResolver resolver;
-    
+
     /** The context */
     protected Context context;
-    
+
     /** Instantiated authenticators */
     protected Map authenticators = new HashMap();
-    
+
     /** The xpath processor */
     protected XPathProcessor xpathProcessor;
 
     /** This is the key used to store the current request state in the request object */
     private static final String REQUEST_STATE_KEY = RequestState.class.getName();
-    
+
     /**
      * Set the sitemap configuration containing the handlers
      */
@@ -112,12 +112,12 @@ implements AuthenticationManager,
     /**
      * Get the handler configuration for the current sitemap
      */
-    private Map getHandlerConfigurations() 
+    private Map getHandlerConfigurations()
     throws ProcessingException {
         Map configs = (Map) this.holder.getPreparedConfiguration();
         if ( null == configs ) {
-            try {       
-                configs = DefaultHandlerManager.prepareHandlerConfiguration(ContextHelper.getObjectModel(this.context), 
+            try {
+                configs = DefaultHandlerManager.prepareHandlerConfiguration(ContextHelper.getObjectModel(this.context),
                                                                             this.holder);
             } catch (ConfigurationException ce) {
                 throw new ProcessingException("Configuration error.", ce);
@@ -131,24 +131,24 @@ implements AuthenticationManager,
      * @param name The handler name
      * @return The configuration or null.
      */
-    private HandlerConfiguration getHandlerConfiguration(String name) 
-    throws ProcessingException {   
+    private HandlerConfiguration getHandlerConfiguration(String name)
+    throws ProcessingException {
         final Map configs = this.getHandlerConfigurations();
         HandlerConfiguration c = null;
         if ( configs != null) {
-            c = (HandlerConfiguration)configs.get( name ); 
+            c = (HandlerConfiguration)configs.get( name );
         }
         return c;
     }
-    
+
     private Request getRequest() {
         return ContextHelper.getRequest(this.context);
     }
-    
-    private Session getSession(boolean create) {        
+
+    private Session getSession(boolean create) {
         return this.getRequest().getSession(create);
     }
-    
+
     private UserState getUserState() {
         final Session session = this.getSession( false );
         UserState status = null;
@@ -167,7 +167,7 @@ implements AuthenticationManager,
         }
         return status;
     }
-    
+
     private UserHandler getUserHandler(String name) {
         final UserState status = this.getUserState();
         if ( status != null ) {
@@ -175,17 +175,17 @@ implements AuthenticationManager,
         }
         return null;
     }
-    
+
     private void updateUserState() {
         final Session session = this.getSession(true);
         Object status = session.getAttribute(SESSION_ATTRIBUTE_USER_STATUS);
         session.setAttribute(SESSION_ATTRIBUTE_USER_STATUS, status);
     }
-    
-	/* (non-Javadoc)
-	 * @see org.apache.cocoon.webapps.authentication.components.Manager#authenticate(java.lang.String, java.lang.String, org.apache.excalibur.source.SourceParameters)
-	 */
-	public UserHandler login(String handlerName,
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.webapps.authentication.components.Manager#authenticate(java.lang.String, java.lang.String, org.apache.excalibur.source.SourceParameters)
+     */
+    public UserHandler login(String handlerName,
                              String applicationName,
                              SourceParameters parameters)
     throws ProcessingException {
@@ -198,7 +198,7 @@ implements AuthenticationManager,
         if ( handler != null ) {
             throw new ProcessingException("User is already authenticated using handler: " + handlerName);
         }
-        
+
         Authenticator authenticator = this.lookupAuthenticator( config );
         try {
             Authenticator.AuthenticationResult result = authenticator.authenticate( config, parameters );
@@ -213,7 +213,7 @@ implements AuthenticationManager,
                 try {
                     contextManager = (ContextManager) this.manager.lookup( ContextManager.ROLE );
                     SessionContext temp = contextManager.getContext( SessionConstants.TEMPORARY_CONTEXT );
-                    
+
                     final DocumentFragment fragment = result.result.createDocumentFragment();
                     final Node root = result.result.getDocumentElement();
                     root.normalize();
@@ -236,25 +236,25 @@ implements AuthenticationManager,
                 } finally {
                     this.manager.release( contextManager );
                 }
-                
+
             }
-            
+
         } finally {
             this.releaseAuthenticator( authenticator, config );
         }
-        
+
         if ( handler != null ) {
             // create UserStatus
             final UserState status = this.createUserState();
-        
-            status.addHandler( handler );        
+
+            status.addHandler( handler );
             this.updateUserState();
-        
+
             // update RequestState
             RequestState state = new RequestState( handler, applicationName);
             this.setState( state );
             state.initialize( this.resolver );
-            
+
             // And now load applications
             Iterator applications = handler.getHandlerConfiguration().getApplications().values().iterator();
 
@@ -265,11 +265,11 @@ implements AuthenticationManager,
                 }
             }
         }
-        
- 		return handler;
-	}
 
-	/**
+        return handler;
+    }
+
+    /**
      * Release the used authenticator
      */
     protected void releaseAuthenticator(Authenticator authenticator, HandlerConfiguration config) {
@@ -277,9 +277,9 @@ implements AuthenticationManager,
     }
 
     /**
-     * The authenticator used to authenticate a user 
+     * The authenticator used to authenticate a user
      */
-    protected Authenticator lookupAuthenticator(HandlerConfiguration config) 
+    protected Authenticator lookupAuthenticator(HandlerConfiguration config)
     throws ProcessingException {
         final String name = config.getAuthenticatorClassName();
         Authenticator authenticator = (Authenticator) this.authenticators.get(name);
@@ -294,7 +294,7 @@ implements AuthenticationManager,
                         ContainerUtil.service( authenticator, this.manager );
                         ContainerUtil.initialize( authenticator );
                         this.authenticators.put(name, authenticator);
-                        
+
                     } catch (Exception e ) {
                         throw new ProcessingException("Unable to initialize authenticator from class " + name, e);
                     }
@@ -305,12 +305,12 @@ implements AuthenticationManager,
     }
 
     /* (non-Javadoc)
-	 * @see org.apache.cocoon.webapps.authentication.components.Manager#checkAuthentication(org.apache.cocoon.environment.Redirector, java.lang.String, java.lang.String)
-	 */
-	public boolean checkAuthentication(Redirector redirector,
-                                        String handlerName,
-                                        String applicationName)
-	throws IOException, ProcessingException {
+     * @see org.apache.cocoon.webapps.authentication.components.Manager#checkAuthentication(org.apache.cocoon.environment.Redirector, java.lang.String, java.lang.String)
+     */
+    public boolean checkAuthentication(Redirector redirector,
+                                       String handlerName,
+                                       String applicationName)
+    throws IOException, ProcessingException {
         HandlerConfiguration config = this.getHandlerConfiguration( handlerName );
         if ( config == null ) {
             throw new ProcessingException("Unknown handler to check: " + handlerName);
@@ -328,7 +328,7 @@ implements AuthenticationManager,
                 if (request.getQueryString() != null) {
                     resource += '?' + request.getQueryString();
                 }
-    
+
                 parameters.setSingleParameterValue("resource", resource);
                 final String redirectURI = config.getRedirectURI();
                 redirector.globalRedirect(false, SourceUtil.appendParameters(redirectURI, parameters));
@@ -339,9 +339,9 @@ implements AuthenticationManager,
             this.setState( state );
             state.initialize( this.resolver );
         }
-        
-		return authenticated;
-	}
+
+        return authenticated;
+    }
 
     public String getForwardingURI(String handlerName) throws ProcessingException {
         HandlerConfiguration config = this.getHandlerConfiguration( handlerName );
@@ -352,24 +352,24 @@ implements AuthenticationManager,
         if (request.getQueryString() != null) {
             resource += '?' + request.getQueryString();
         }
-    
+
         parameters.setSingleParameterValue("resource", resource);
         final String redirectURI = config.getRedirectURI();
         return SourceUtil.appendParameters(redirectURI, parameters);
     }
-    
-	/* (non-Javadoc)
-	 * @see org.apache.cocoon.webapps.authentication.components.Manager#isAuthenticated(java.lang.String)
-	 */
-	public UserHandler isAuthenticated(String handlerName)
+
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.webapps.authentication.components.Manager#isAuthenticated(java.lang.String)
+     */
+    public UserHandler isAuthenticated(String handlerName)
     throws ProcessingException {
         return this.getUserHandler( handlerName  );
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see org.apache.cocoon.webapps.authentication.components.Manager#logout(java.lang.String, java.lang.String)
-	 */
-	public void logout(String handlerName, int mode) 
+    /* (non-Javadoc)
+     * @see org.apache.cocoon.webapps.authentication.components.Manager#logout(java.lang.String, java.lang.String)
+     */
+    public void logout(String handlerName, int mode)
     throws ProcessingException {
         HandlerConfiguration config = this.getHandlerConfiguration( handlerName );
         if ( config == null ) {
@@ -379,18 +379,18 @@ implements AuthenticationManager,
         UserHandler handler = this.getUserHandler( handlerName );
         // we don't throw an exception if we are already logged out!
         if ( handler != null ) {
-            
+
             // Do we save something on logout?
             /*
-            
-            if ( config.saveOnLogout() 
+
+            if ( config.saveOnLogout()
                  && config.getSaveResource() != null) {
                 final AuthenticationContext authContext = handler.getContext();
                 try {
                     // This might not work, because of the missing state
-                    authContext.saveXML("/authentication", 
-                                    null, 
-                                    ContextHelper.getObjectModel(this.context), 
+                    authContext.saveXML("/authentication",
+                                    null,
+                                    ContextHelper.getObjectModel(this.context),
                                     this.resolver, this.manager);
                 } catch (Exception ignore) {
                     // we don't want to stop the logout process
@@ -414,7 +414,7 @@ implements AuthenticationManager,
             } catch (Exception ignore) {
                 // we really ignore any exception!
             }
-            
+
             List applicationContexts = handler.getApplicationContexts();
             if ( applicationContexts != null ) {
                 ContextManager contextManager = null;
@@ -433,7 +433,7 @@ implements AuthenticationManager,
                     this.manager.release( contextManager);
                 }
             }
-    
+
             UserState status = this.getUserState();
             status.removeHandler( handlerName );
             this.updateUserState();
@@ -442,45 +442,44 @@ implements AuthenticationManager,
             SessionManager sessionManager = null;
             try {
                 sessionManager = (SessionManager)this.manager.lookup( SessionManager.ROLE );
-            
+
                 if ( mode == AuthenticationConstants.LOGOUT_MODE_IMMEDIATELY ) {
                     sessionManager.terminateSession(true);
                 } else if ( mode == AuthenticationConstants.LOGOUT_MODE_IF_UNUSED ) {
                     if ( !status.hasHandler()) {
-                        sessionManager.terminateSession( false ); 
+                        sessionManager.terminateSession( false );
                     }
-                 
+
                 } else if ( mode == AuthenticationConstants.LOGOUT_MODE_IF_NOT_AUTHENTICATED) {
                     if ( !status.hasHandler()) {
-                        sessionManager.terminateSession( true ); 
+                        sessionManager.terminateSession( true );
                     }
                 } else {
                     throw new ProcessingException("Unknown logout mode: " + mode);
                 }
-            
+
             } catch (ServiceException se) {
                 throw new ProcessingException("Unable to lookup session manager.", se);
             } finally {
                 this.manager.release( sessionManager );
             }
         }
-        
-	}
+    }
 
-	/**
+    /**
      * Serviceable
-	 */
-	public void service(ServiceManager manager) 
+     */
+    public void service(ServiceManager manager)
     throws ServiceException {
         this.manager = manager;
         this.resolver = (SourceResolver) this.manager.lookup(SourceResolver.ROLE);
         this.xpathProcessor = (XPathProcessor)this.manager.lookup(XPathProcessor.ROLE);
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see org.apache.avalon.framework.activity.Disposable#dispose()
-	 */
-	public void dispose() {
+    /* (non-Javadoc)
+     * @see org.apache.avalon.framework.activity.Disposable#dispose()
+     */
+    public void dispose() {
         Iterator iter = this.authenticators.values().iterator();
         while ( iter.hasNext() ) {
             final Authenticator authenticator = (Authenticator) iter.next();
@@ -493,7 +492,7 @@ implements AuthenticationManager,
             this.xpathProcessor = null;
             this.manager = null;
         }
-	}
+    }
 
     /**
      * Get the current state of authentication
@@ -506,7 +505,7 @@ implements AuthenticationManager,
         final Request req = ContextHelper.getRequest(context);
         return (RequestState)req.getAttribute( REQUEST_STATE_KEY);
     }
-    
+
     /* (non-Javadoc)
      * @see org.apache.avalon.framework.context.Contextualizable#contextualize(org.apache.avalon.framework.context.Context)
      */
@@ -522,7 +521,7 @@ implements AuthenticationManager,
             req.removeAttribute( REQUEST_STATE_KEY );
         }
     }
-    
+
     /**
      * Create Application Context.
      * This context is destroyed when the user logs out of the handler
@@ -533,7 +532,7 @@ implements AuthenticationManager,
     throws ProcessingException {
         RequestState state = this.getState();
         UserHandler handler = state.getHandler();
-        
+
         SessionContext context = null;
 
         if ( handler != null ) {
@@ -559,7 +558,4 @@ implements AuthenticationManager,
 
         return context;
     }
-
 }
-
-
