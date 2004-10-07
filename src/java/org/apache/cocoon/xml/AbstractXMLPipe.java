fadd3417862449f1f3ee2ca346fb01db2a948815
Vadim Gritsenko <vgritsenko@apache.org>
2004-10-07 14:26:29 +0000

    javadoc changes mostly
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@53979 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/xml/AbstractXMLPipe.java
+++ src/java/org/apache/cocoon/xml/AbstractXMLPipe.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,11 +24,10 @@ import org.xml.sax.SAXException;
  * handlers and lexical handlers.
  *
  * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
- * @version CVS $Id: AbstractXMLPipe.java,v 1.2 2004/03/05 13:03:01 bdelacretaz Exp $
+ * @version CVS $Id$
  */
-public abstract class AbstractXMLPipe
-extends AbstractXMLProducer
-implements XMLPipe {
+public abstract class AbstractXMLPipe extends AbstractXMLProducer
+                                      implements XMLPipe {
 
     /**
      * Receive an object for locating the origin of SAX document events.
