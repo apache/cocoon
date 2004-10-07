283ebb4c4ee5db75a4ee6d71f7d0f31e2edea805
Vadim Gritsenko <vgritsenko@apache.org>
2004-10-07 14:26:29 +0000

    javadoc changes mostly
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@53979 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/xml/XMLFragment.java
+++ src/java/org/apache/cocoon/xml/XMLFragment.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,25 +20,18 @@ import org.w3c.dom.Node;
 /**
  * This interface must be implemented by classes willing
  * to provide an XML representation of their current state.
- * <br/>
- * This interface exists in both Cocoon 1 and Cocoon 2 and to ensure
- * a minimal compatibility between the two versions.
- * <br/>
- * Cocoon 2 only objects can implement the SAX-only <code>XMLizable</code>
- * interface.
+ *
+ * <p>This interface exists in both Cocoon 1 and Cocoon 2 and to ensure
+ * a minimal compatibility between the two versions.</p>
+ *
+ * <p>Cocoon 2 only objects can implement the SAX-only <code>XMLizable</code>
+ * interface.</p>
  *
  * @author <a href="mailto:sylvain.wallez@anyware-tech.com">Sylvain Wallez</a>
  * @author <a href="mailto:ricardo@apache.org">Ricardo Rocha</a> for the original XObject class
- * @version CVS $Id: XMLFragment.java,v 1.2 2004/03/05 13:03:01 bdelacretaz Exp $
+ * @version CVS $Id$
  */
-
 public interface XMLFragment extends org.apache.excalibur.xml.sax.XMLizable {
-// Now inherited from XMLizable
-//    /**
-//     * Generates SAX events representing the object's state
-//     * for the given content handler.
-//     */
-//    void toSAX(ContentHandler handler) throws SAXException;
 
     /**
      * Appends children representing the object's state to the given node.
