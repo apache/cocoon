99aae9be72b0a1d262d4d1a72f37bad55c9e0a2e
Vadim Gritsenko <vgritsenko@apache.org>
2004-10-07 14:25:45 +0000

    javadoc changes mostly
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@53978 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/xml/AbstractDOMFragment.java
+++ src/java/org/apache/cocoon/xml/AbstractDOMFragment.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,31 +26,29 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 /**
- * Abstract implementation of {@link XMLFragment} for objects that are more easily represented
- * as a DOM.
- * <br/>
- * The toSAX() method is implemented by streaming (using a <code>DOMStreamer</code>)
- * the results of <code>toDOM()</code> that must be implemented by concrete subclasses.
+ * Abstract implementation of {@link XMLFragment} for objects that are more
+ * easily represented as a DOM.
+ *
+ * <p>The {@link #toSAX} method is implemented by streaming (using a
+ * {@link DOMStreamer}) the results of the {@link #toDOM} that must be
+ * implemented by concrete subclasses.</p>
  *
  * @author <a href="mailto:sylvain.wallez@anyware-tech.com">Sylvain Wallez</a>
- * @version CVS $Id: AbstractDOMFragment.java,v 1.3 2004/03/05 13:03:01 bdelacretaz Exp $
+ * @version CVS $Id$
  */
-
 public abstract class AbstractDOMFragment implements XMLFragment {
 
     /**
      * Generates SAX events representing the object's state by serializing the
      * result of <code>toDOM()</code>.
      */
-
     public void toSAX(ContentHandler handler) throws SAXException {
         // The ComponentManager is unknown here : use JAXP to create a document
         DocumentBuilder builder;
         try {
             builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        }
-        catch (ParserConfigurationException pce) {
-            throw new SAXException("Couldn't get a DocumentBuilder", pce);
+        } catch (ParserConfigurationException e) {
+            throw new SAXException("Couldn't get a DocumentBuilder", e);
         }
 
         Document doc = builder.newDocument();
@@ -62,15 +60,13 @@ public abstract class AbstractDOMFragment implements XMLFragment {
         // Build the DOM representation of this object
         try {
             toDOM(df);
-        }
-        catch(Exception e) {
+        } catch(Exception e) {
             throw new SAXException("Exception while converting object to DOM", e);
         }
 
         // Stream the document fragment
         handler.startDocument();
-        DOMStreamer streamer = new DOMStreamer(handler);
-        streamer.stream(df);
+        new DOMStreamer(handler).stream(df);
         handler.endDocument();
     }
 }
--- src/java/org/apache/cocoon/xml/AbstractXMLProducer.java
+++ src/java/org/apache/cocoon/xml/AbstractXMLProducer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,11 +26,10 @@ import org.xml.sax.ext.LexicalHandler;
  *
  * @author <a href="mailto:pier@apache.org">Pierpaolo Fumagalli</a>
  *         (Apache Software Foundation)
- * @version CVS $Id: AbstractXMLProducer.java,v 1.3 2004/03/18 18:33:13 joerg Exp $
+ * @version CVS $Id$
  */
-public abstract class AbstractXMLProducer
-extends AbstractLogEnabled
-implements XMLProducer, Recyclable {
+public abstract class AbstractXMLProducer extends AbstractLogEnabled
+                                          implements XMLProducer, Recyclable {
 
     /** The <code>XMLConsumer</code> receiving SAX events. */
     protected XMLConsumer xmlConsumer;
