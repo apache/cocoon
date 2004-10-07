573acf92ea0f00a526a69646e4762463075397d5
Vadim Gritsenko <vgritsenko@apache.org>
2004-10-07 14:21:36 +0000

    cleanup
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@53976 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/taglib/java/org/apache/cocoon/taglib/core/SourceTag.java
+++ src/blocks/taglib/java/org/apache/cocoon/taglib/core/SourceTag.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,11 +26,10 @@ import org.xml.sax.SAXException;
 
 /**
  * @author <a href="mailto:volker.schmitt@basf-it-services.com">Volker Schmitt</a>
- * @version CVS $Id: SourceTag.java,v 1.4 2004/03/05 13:02:24 bdelacretaz Exp $
+ * @version CVS $Id$
  */
 public class SourceTag extends XMLProducerTagSupport {
     private String src;
-    private EmbeddedXMLPipe embeddedXMLPipe = new EmbeddedXMLPipe(null);
 
     public void setSrc(String src) {
         this.src = src;
@@ -39,12 +38,14 @@ public class SourceTag extends XMLProducerTagSupport {
     /*
      * @see Tag#doEndTag(String, String, String)
      */
-    public int doEndTag(String namespaceURI, String localName, String qName) throws SAXException {
+    public int doEndTag(String namespaceURI, String localName, String qName)
+    throws SAXException {
         Source source = null;
         try {
-            embeddedXMLPipe.setConsumer(this.xmlConsumer);
             source = resolver.resolveURI(src);
-            SourceUtil.toSAX(source, this.embeddedXMLPipe);
+            SourceUtil.toSAX(source, new EmbeddedXMLPipe(this.xmlConsumer));
+        } catch (SAXException e) {
+            throw e;
         } catch (Exception e) {
             if (e instanceof ProcessingException) {
                 ProcessingException pe = (ProcessingException) e;
@@ -54,9 +55,9 @@ public class SourceTag extends XMLProducerTagSupport {
             }
             throw new SAXException(e.getMessage(), e);
         } finally {
-            embeddedXMLPipe.setConsumer(null);
-            if (source != null)
+            if (source != null) {
                 resolver.release(source);
+            }
         }
         return EVAL_PAGE;
     }
