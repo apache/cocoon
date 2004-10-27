27ff1d6e2e77de010087f26939feed2b0759f4d1
Vadim Gritsenko <vgritsenko@apache.org>
2004-10-27 20:25:23 +0000

    Make sure work directory present in context before initializing logger.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@55730 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/scratchpad/java/org/apache/cocoon/ant/CocoonTask.java
+++ src/blocks/scratchpad/java/org/apache/cocoon/ant/CocoonTask.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -620,8 +620,6 @@ public class CocoonTask extends Task {
                         this.logKitLoggerManager = new LogKitLoggerManager(Hierarchy.getDefaultHierarchy());
 
                         final DefaultContext subcontext = new DefaultContext(this.ctx);
-                        File contextDir = (File) this.ctx.get("context-root");
-                        subcontext.put("context-root", contextDir);
                         this.logKitLoggerManager.contextualize(subcontext);
                         this.logKitLoggerManager.configure(logKitConf);
                         logger = this.logKitLoggerManager.getLoggerForCategory(logKitLogCategory);
