0fdd0241d25d13a92ade5bfa2ddeb88bdf9bf9a8
Unico Hommes <unico@apache.org>
2004-11-11 16:24:43 +0000

    port changes to CachingSource from trunk
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@57444 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/scratchpad/java/org/apache/cocoon/components/source/impl/UpdateTarget.java
+++ src/blocks/scratchpad/java/org/apache/cocoon/components/source/impl/UpdateTarget.java
@@ -15,6 +15,7 @@
  */
 package org.apache.cocoon.components.source.impl;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
@@ -23,12 +24,8 @@ import org.apache.avalon.framework.service.ServiceException;
 import org.apache.avalon.framework.service.ServiceManager;
 import org.apache.avalon.framework.service.Serviceable;
 import org.apache.cocoon.caching.Cache;
-import org.apache.cocoon.caching.IdentifierCacheKey;
 import org.apache.cocoon.components.cron.ConfigurableCronJob;
-import org.apache.excalibur.source.Source;
 import org.apache.excalibur.source.SourceResolver;
-import org.apache.excalibur.source.SourceValidity;
-import org.apache.excalibur.source.impl.validity.ExpiresValidity;
 
 /**
  * A target updating a cache entry.
@@ -48,17 +45,13 @@ import org.apache.excalibur.source.impl.validity.ExpiresValidity;
  *  The time in seconds the cached content is valid
  * </li>
  * <li>
- *  <code>fail-safe (boolean)</code>
- *  Whether to invalidate the cached response when updating it failed.
- * </li>
- * <li>
- *  <code>cache-key (SimpleCacheKey)</code>: 
+ *  <code>cache-name (String)</code>: 
  *  The key used to cache the content
  * </li>
  * </ul>
  *  
  * @since 2.1.1
- * @version CVS $Id: UpdateTarget.java,v 1.6 2004/04/15 08:05:56 cziegeler Exp $
+ * @version CVS $Id$
  */
 public class UpdateTarget extends AbstractLogEnabled
 implements Serviceable, ConfigurableCronJob {
@@ -71,10 +64,7 @@ implements Serviceable, ConfigurableCronJob {
     private String uri;
     private String cacheRole;
     private int expires;
-    private boolean failSafe;
-    
-    // the key under which to store the CachedResponse in the Cache
-    private IdentifierCacheKey cacheKey;
+    private String cacheName;
     
         
     // ---------------------------------------------------- Lifecycle
@@ -96,14 +86,13 @@ implements Serviceable, ConfigurableCronJob {
     public void setup(Parameters pars, Map objects) {
         this.uri = pars.getParameter("uri", null);
         this.cacheRole = pars.getParameter("cache-role", Cache.ROLE);
-        this.expires = pars.getParameterAsInteger("cache-expires", 60);
-        this.failSafe = pars.getParameterAsBoolean("fail-safe", true);
-        this.cacheKey = (IdentifierCacheKey) objects.get("cache-key");
+        this.expires = pars.getParameterAsInteger("cache-expires", 0);
+        this.cacheName = pars.getParameter("cache-name", null);
     }
     
     
     // ---------------------------------------------------- CronJob implementation
-    
+
     /* (non-Javadoc)
      * @see org.apache.avalon.cornerstone.services.scheduler.Target#targetTriggered(java.lang.String)
      */
@@ -112,78 +101,36 @@ implements Serviceable, ConfigurableCronJob {
             if (this.getLogger().isDebugEnabled()) {
                 this.getLogger().debug("Refreshing " + this.uri);
             }
-            
-            Source source = null;
             Cache cache = null;
+            CachingSource source = null;
             try {
-                
-                cache = (Cache) this.manager.lookup(this.cacheRole);
-                source = this.resolver.resolveURI(this.uri);
-                
-                // check if the source is really expired and invalid
-                CachedSourceResponse response = (CachedSourceResponse) cache.get(this.cacheKey);
-                if (response != null) {
-                    final SourceValidity sourceValidity = response.getValidityObjects()[1];
-                    if (CachingSource.isValid(sourceValidity, source)) {
-                        if (getLogger().isDebugEnabled()) {
-                            getLogger().debug("Cached response is still valid " +                                "for source " + this.uri + ".");
-                        }
-                        response.getValidityObjects()[0] = new ExpiresValidity(this.expires * 1000);
-                        return;
-                    }
+                cache = (Cache) manager.lookup(cacheRole);
+                source = CachingSourceFactory.newCachingSource(
+                                               this.resolver.resolveURI(this.uri),
+                                               "cached",
+                                               "cached:" + uri,
+                                               expires,
+                                               cacheName,
+                                               true,
+                                               cache,
+                                               getLogger(),
+                                               this.manager);
+                                               
+                source.refresh();
+            }
+            catch (IOException e) {
+                getLogger().error("Error refreshing source", e);
+            }
+            catch (ServiceException e) {
+                getLogger().error("Error refreshing source", e);
+            }
+            finally {
+                if (cache != null) {
+                    manager.release(cache);
                 }
-                
-                if (source.exists()) {
-                    
-                    // what is in the cached response?
-                    byte[] binary = null;
-                    byte[] xml = null;
-                    if (response != null) {
-                        binary = response.getBinaryResponse();
-                        xml = response.getXMLResponse();
-                    }
-                    
-                    // create a new cached response
-                    final ExpiresValidity cacheValidity = new ExpiresValidity(this.expires * 1000);
-                    final SourceValidity sourceValidity = source.getValidity();
-                    response = new CachedSourceResponse(new SourceValidity[] {cacheValidity, sourceValidity});
-                    
-                    // only create objects that have previously been used
-                    if (binary != null) {
-                        binary = CachingSource.readBinaryResponse(source);
-                        response.setBinaryResponse(binary);
-                    }
-                    if (xml != null) {
-                        xml = CachingSource.readXMLResponse(source, binary, this.manager);
-                        response.setXMLResponse(xml);
-                    }
-                    // meta info is always set
-                    response.setExtra(CachingSource.readMeta(source));
-                    cache.store(this.cacheKey, response);
+                if (source != null) {
+                    this.resolver.release(source);
                 }
-                else if (response != null) {
-                    // FIXME: There is a potential problem when the parent
-                    // source has not yet been updated thus listing this
-                    // source still as one of its children. We'll have to remove 
-                    // the parent's cached response here too.
-                    if (getLogger().isDebugEnabled()) {
-                        getLogger().debug("Source " + this.uri + " no longer exists." +                            " Throwing out cached response.");
-                    }
-                    cache.remove(this.cacheKey);
-                }
-            } catch (Exception e) {
-                if (!failSafe) {
-                    // the content expires, so remove it
-                    cache.remove(cacheKey);
-                    getLogger().warn("Exception during updating of source " + this.uri, e);
-                }
-                else {
-                    getLogger().warn("Updating of source " + this.uri + " failed. " +
-                        "Cached response (if any) will be stale.", e);
-                }
-            } finally {
-                this.resolver.release(source);
-                this.manager.release(cache);
             }
         }
     }
