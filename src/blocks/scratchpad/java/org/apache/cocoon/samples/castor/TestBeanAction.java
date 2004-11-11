5c26a6176947e1367c3e072b3cadbbe8adb02e7a
Vadim Gritsenko <vgritsenko@apache.org>
2004-11-11 15:15:58 +0000

    Scratchpad block: Restored CastorTransformer support for context beans,      restored bean lookup logic, fixed sample.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@57441 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/scratchpad/java/org/apache/cocoon/samples/castor/TestBeanAction.java
+++ src/blocks/scratchpad/java/org/apache/cocoon/samples/castor/TestBeanAction.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,28 +29,28 @@ import java.util.Map;
 
 /**
  *
- * @version CVS $Id: TestBeanAction.java,v 1.3 2004/03/05 10:07:26 bdelacretaz Exp $
+ * @version CVS $Id$
  */
 public class TestBeanAction extends AbstractAction {
 
-  public TestBeanAction() {
-  }
+    public TestBeanAction() {
+    }
 
-  public Map act(Redirector redirector, SourceResolver resolver, Map objectModel, String src, Parameters param) throws java.lang.Exception {
-      Request request = ObjectModelHelper.getRequest(objectModel);
-      Context context = ObjectModelHelper.getContext(objectModel);
-      if(context != null)
-        request.setAttribute("Wale",new  TestBean("Wale in the big sea","context"));
+    public Map act(Redirector redirector, SourceResolver resolver, Map objectModel, String src, Parameters param)
+    throws Exception {
+        Request request = ObjectModelHelper.getRequest(objectModel);
+        Context context = ObjectModelHelper.getContext(objectModel);
+        if (context != null) {
+            context.setAttribute("Wale",new  TestBean("Wale in the big sea", "context"));
+        }
 
-      Session session =request.getSession(true);
-      session.setAttribute("Mouse",new  TestBean("Liveing in the session","session"));
-      objectModel.put("Lion", new TestBean("Lion:walking on the sitemap","sitemap") );
-      request.setAttribute("Hamster",new TestBean("Hamster:Wer hat nach mir gefragt","request")  );
-      session.setAttribute("Elefant",new  TestBean("Elefant:from Africa","session"));
-      request.setAttribute("Elefant",new  TestBean("Elefant:from India","request"));
+        Session session =request.getSession(true);
+        session.setAttribute("Mouse",new  TestBean("Liveing in the session","session"));
+        objectModel.put("Lion", new TestBean("Lion:walking on the sitemap","sitemap") );
+        request.setAttribute("Hamster",new TestBean("Hamster:Wer hat nach mir gefragt","request")  );
+        session.setAttribute("Elefant",new  TestBean("Elefant:from Africa","session"));
+        request.setAttribute("Elefant",new  TestBean("Elefant:from India","request"));
 
-
-      return objectModel;
-
-  }
+        return objectModel;
+    }
 }
