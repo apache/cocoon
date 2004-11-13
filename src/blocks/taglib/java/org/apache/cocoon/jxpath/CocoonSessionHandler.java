4965069e74a4d679a2d34955fbc29e50616a753b
Vadim Gritsenko <vgritsenko@apache.org>
2004-11-13 22:58:29 +0000

    whitespaces
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@65528 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/taglib/java/org/apache/cocoon/jxpath/CocoonSessionHandler.java
+++ src/blocks/taglib/java/org/apache/cocoon/jxpath/CocoonSessionHandler.java
@@ -1,19 +1,18 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.cocoon.jxpath;
 
 import java.util.Enumeration;
@@ -31,16 +30,16 @@ import org.apache.commons.jxpath.DynamicPropertyHandler;
  */
 public class CocoonSessionHandler implements DynamicPropertyHandler {
 
-    public String[] getPropertyNames(Object session){
-        Enumeration e = ((Session)session).getAttributeNames();
-        return (String [])EnumerationUtils.toList(e).toArray();
+    public String[] getPropertyNames(Object session) {
+        final Enumeration e = ((Session) session).getAttributeNames();
+        return (String[]) EnumerationUtils.toList(e).toArray();
     }
 
-    public Object getProperty(Object session, String property){
-        return ((Session)session).getAttribute(property);
+    public Object getProperty(Object session, String property) {
+        return ((Session) session).getAttribute(property);
     }
 
-    public void setProperty(Object session, String property, Object value){
-        ((Session)session).setAttribute(property, value);
+    public void setProperty(Object session, String property, Object value) {
+        ((Session) session).setAttribute(property, value);
     }
 }
