24b9a2575eede651fa5f2f0d6eb22f4d1d592d7d
Vadim Gritsenko <vgritsenko@apache.org>
2004-11-13 23:00:29 +0000

    whitespaces
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@65531 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/taglib/java/org/apache/cocoon/jxpath/CocoonContextHandler.java
+++ src/blocks/taglib/java/org/apache/cocoon/jxpath/CocoonContextHandler.java
@@ -1,19 +1,18 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.cocoon.jxpath;
 
 import java.util.Enumeration;
@@ -31,16 +30,16 @@ import org.apache.commons.jxpath.DynamicPropertyHandler;
  */
 public class CocoonContextHandler implements DynamicPropertyHandler {
 
-    public String[] getPropertyNames(Object context){
-        Enumeration e = ((Context)context).getAttributeNames();
-        return (String [])EnumerationUtils.toList(e).toArray();
+    public String[] getPropertyNames(Object context) {
+        final Enumeration e = ((Context) context).getAttributeNames();
+        return (String[]) EnumerationUtils.toList(e).toArray();
     }
 
-    public Object getProperty(Object context, String property){
-        return ((Context)context).getAttribute(property);
+    public Object getProperty(Object context, String property) {
+        return ((Context) context).getAttribute(property);
     }
 
-    public void setProperty(Object context, String property, Object value){
-        ((Context)context).setAttribute(property, value);
+    public void setProperty(Object context, String property, Object value) {
+        ((Context) context).setAttribute(property, value);
     }
 }
