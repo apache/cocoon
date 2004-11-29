51f4ae2ae287ddb2276165c8e4c66e195974c8ee
Antonio Gallardo Rivera <antonio@apache.org>
2004-11-29 05:24:35 +0000

    Fixing typo ;-)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@106887 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/resources/scripts/prettycontent.js
+++ src/webapp/resources/scripts/prettycontent.js
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-function xml2html.toggle(event) {
+function xml2htmlToggle(event) {
 
     var mark;
     if (event.srcElement) {
