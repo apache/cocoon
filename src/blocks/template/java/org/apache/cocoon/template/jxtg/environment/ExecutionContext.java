8503951dd3a23a11a6cb85aba0d5d1a31716516c
Par Daniel Fagerstrohm <danielf@apache.org>
2004-12-12 17:28:46 +0000

    Refactored JXTemplate by gathering script run time dependencies to ExecutionContext. Factored out the execution engine to Invoker.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@111658 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/blocks/template/java/org/apache/cocoon/template/jxtg/environment/ExecutionContext.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.template.jxtg.environment;
+
+import java.util.Map;
+
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.cocoon.template.jxtg.expression.MyJexlContext;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.Variables;
+
+public class ExecutionContext {
+    private MyJexlContext jexlContext;
+    private JXPathContext jxpathContext;
+    private Variables variables;
+    private Map definitions;
+    private Map cache;
+    private ServiceManager manager;
+
+    public ExecutionContext(MyJexlContext jexlContext, JXPathContext jxpathContext,
+                            Variables variables, Map definitions, Map cache,
+                            ServiceManager manager) {
+        this.jexlContext = jexlContext;
+        this.jxpathContext = jxpathContext;
+        this.variables = variables;
+        this.definitions = definitions;
+        this.cache = cache;
+        this.manager = manager;
+    }
+
+    public MyJexlContext getJexlContext() {
+        return this.jexlContext;
+    }
+
+    public JXPathContext getJXPathContext() {
+        return this.jxpathContext;
+    }
+
+    public Variables getVariables() {
+        return this.variables;
+    }
+
+    public Map getDefinitions() {
+        return this.definitions;
+    }
+
+    public Map getCache() {
+        return this.cache;
+    }
+
+    public ServiceManager getServiceManager() {
+        return this.manager;
+    }
+
+    public ExecutionContext getChildContext(MyJexlContext jexlContext,
+                                            JXPathContext jxpathContext) {
+        return new ExecutionContext(jexlContext, jxpathContext,
+                                    this.variables, this.definitions, this.cache,
+                                    this.manager);
+    }
+}
