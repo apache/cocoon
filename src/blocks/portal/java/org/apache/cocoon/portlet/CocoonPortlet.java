1e49b1116c3c77d5d85ce40ad251cf90e1ee80ad
Vadim Gritsenko <vgritsenko@apache.org>
2004-12-13 14:16:40 +0000

    uri must not start with '/'
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@111715 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/portal/java/org/apache/cocoon/portlet/CocoonPortlet.java
+++ src/blocks/portal/java/org/apache/cocoon/portlet/CocoonPortlet.java
@@ -1087,6 +1087,9 @@ public class CocoonPortlet extends GenericPortlet {
 
         Environment env;
         try {
+            if (uri.charAt(0) == '/') {
+                uri = uri.substring(1);
+            }
             env = getEnvironment(servletPath, pathInfo, uri, request, res);
         } catch (Exception e) {
             if (getLogger().isErrorEnabled()) {
@@ -1239,6 +1242,9 @@ public class CocoonPortlet extends GenericPortlet {
 
         Environment env;
         try {
+            if (uri.charAt(0) == '/') {
+                uri = uri.substring(1);
+            }
             env = getEnvironment(servletPath, pathInfo, uri, request, res);
         } catch (Exception e) {
             if (getLogger().isErrorEnabled()) {
