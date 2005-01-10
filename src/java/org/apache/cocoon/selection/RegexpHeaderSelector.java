599afec186c86b05b5cdfab899d53e308439bf38
Vadim Gritsenko <vgritsenko@apache.org>
2005-01-10 13:32:27 +0000

    svn:eol-style native
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@124799 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/selection/RegexpHeaderSelector.java
+++ src/java/org/apache/cocoon/selection/RegexpHeaderSelector.java
@@ -49,7 +49,7 @@ import org.apache.cocoon.environment.ObjectModelHelper;
  * also be specified as a <code>&lt;map:parameter&nbsp;.../&gt;</code> inside the
  * pipeline itself.</p>
  * 
- * @version CVS $Id: RegexpHeaderSelector.java 36086 2004-08-08 14:27:52Z ugo $
+ * @version CVS $Id$
  */
 public class RegexpHeaderSelector extends AbstractRegexpSelector {
 
