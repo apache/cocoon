d4d37a3791f894cda0fb9b4cb0f22a1158bd58e4
Ralph Goers <rgoers@apache.org>
2005-01-19 00:39:55 +0000

    Encode PortletURL
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@125575 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/portal/java/org/apache/cocoon/portal/pluto/PortletURLProviderImpl.java
+++ src/blocks/portal/java/org/apache/cocoon/portal/pluto/PortletURLProviderImpl.java
@@ -252,7 +252,7 @@ public class PortletURLProviderImpl
                 this.generatedURL = this.linkService.getLinkURI(l, secure);
             }
         }
-        return this.generatedURL;
+        return linkService.encodeURL(this.generatedURL);
     }
 
     /* (non-Javadoc)
