5d867721cb333f6474cf5140c5337885dad90380
Carsten Ziegeler <cziegeler@apache.org>
2005-01-24 14:18:52 +0000

    Improved layout
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@126285 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/portal/samples/skins/basic/styles/borderless-window.xsl
+++ src/blocks/portal/samples/skins/basic/styles/borderless-window.xsl
@@ -26,127 +26,92 @@
         <xsl:value-of select="@bgColor" />
     </xsl:when>
     <xsl:otherwise>
-      <xsl:text>#294563</xsl:text>
+      <xsl:text>#CCCCCC</xsl:text>
     </xsl:otherwise>
   </xsl:choose>
 </xsl:variable>
 
-  <table cellSpacing="0" cellpadding="0" width="100%" border="0"><tbody>
-    <tr vAlign="top" height="20">
-      <td width="5" style="background-color:#294563;" valign="top">
-        <img src="images/window-top-left.gif" border="0" width="5" height="5" alt="top-left"/>
-      </td>
-      <td bgColor="{$bgColor}" align="left" valign="middle">
-        <font color="#ffffff" face="Arial" size="2"><b>
-          <xsl:choose>
-            <xsl:when test="@title">
-              <xsl:value-of select="@title"/>
-            </xsl:when>
-            <xsl:otherwise>
-              <xsl:value-of select="title"/>
-            </xsl:otherwise>
-          </xsl:choose>	
-        </b></font>
-       </td>
-      <td align="right" valign="middle" bgColor="{$bgColor}">
-        <table cellSpacing="0" cellpadding="0" border="0"><tbody><tr>
+<table border="0" cellSpacing="0" cellpadding="0" width="100%">
+	<tr vAlign="top">
+		<td bgColor="{$bgColor}" valign="middle">
+			<font>
+				<xsl:attribute name="color">#ffffff</xsl:attribute>
+				<xsl:attribute name="face">Arial</xsl:attribute>
+				<xsl:attribute name="size">2</xsl:attribute>	
+				<xsl:choose>
+				    <xsl:when test="@title">
+					    <b><xsl:value-of select="@title"/></b>
+					</xsl:when>
+				    <xsl:otherwise>
+					    <b><xsl:value-of select="title"/></b>
+					</xsl:otherwise>
+				</xsl:choose>	
+			</font>
+	    </td>
+		<td align="right" bgColor="{$bgColor}">
 			<xsl:if test="basket-add-link">
-            <td width="15">
 				<a href="{basket-add-link}">
 	  			    <img src="images/basket.gif" border="0" alt="Add Link"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="basket-add-content">
-            <td width="15">
 				<a href="{basket-add-content}">
 	  			    <img src="images/basket.gif" border="0" alt="Add Content"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="edit-uri">
-            <td width="15">
 				<a href="{edit-uri}">
 	  			    <img src="images/edit.gif" border="0" alt="Edit"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="help-uri">
-            <td width="15">
 				<a href="{help-uri}">
 	  			    <img src="images/help.gif" border="0" alt="Help"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="view-uri">
-            <td width="15">
 				<a href="{view-uri}">
 	  			    <img src="images/view.gif" border="0" alt="View"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="fullscreen-uri and not(maximize-uri)">
-            <td width="15">
 				<a href="{fullscreen-uri}">
 	  			    <img src="images/customize.gif" border="0" alt="Full Screen"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="maxpage-uri">
-            <td width="15">
 				<a href="{maxpage-uri}">
 	  			    <img src="images/show.gif" border="0" alt="Max Page"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="minpage-uri">
-            <td width="15">
 				<a href="{minpage-uri}">
 	  			    <img src="images/show.gif" border="0" alt="Min Page"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="maximize-uri">
-            <td width="15">
 				<a href="{maximize-uri}">
 	  			    <img src="images/maximize.gif" border="0" alt="Maximize"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="minimize-uri">
-            <td width="15">
 				<a href="{minimize-uri}">
 	  			    <img src="images/minimize.gif" border="0" alt="Minimize"/>
 	  			</a>
-            </td>
 			</xsl:if>
 			<xsl:if test="remove-uri">
-            <td width="15">
 				<a href="{remove-uri}">
 	  			    <img src="images/delete.gif" border="0" alt="Delete"/>
 	  			</a>
-            </td>
 			</xsl:if>
-        </tr></tbody></table>
-       </td>
-       <td width="5" style="background-color:#294563;" valign="top">
-         <img src="images/window-top-right.gif" border="0" width="5" height="5" alt="top-right"/>
-       </td>
-       <td width="1"/>
-     </tr>
-     <tr>
-		<td width="5" style="background-image:url(images/window-border-left.gif);"><img src="images/space.gif" border="0" width="5" height="5" alt="space"/></td>
+		</td>
+	</tr>
+	<tr>
 		<td colSpan="2">
                <xsl:apply-templates select="content"/>
 		</td>
-		<td width="6" colSpan="2" style="background-image:url(images/window-border-right.gif);"><img src="images/space.gif" border="0" width="5" height="5" alt="space"/></td>
 	</tr>
-	<tr>
-		<td width="5"><img src="images/window-bottom-left.gif" border="0" width="5" height="5" alt="top-left"/></td>
-		<td colspan="2" style="background-image:url(images/window-border-bottom.gif);"></td>
-		<td width="5"><img src="images/window-bottom-right.gif" border="0" width="5" height="5" alt="top-left"/></td>
-	</tr>
-  </tbody></table>
+</table>
 </xsl:template>
 
 <xsl:template match="content">
---
+++ src/core/java/org/apache/cocoon/core/container/handler/NewPoolableComponentHandler.java
@@ -0,0 +1,313 @@
+/* 
+ * Copyright 2002-2005 The Apache Software Foundation
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.core.container.handler;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.cocoon.components.ComponentInfo;
+import org.apache.cocoon.core.container.ComponentFactory;
+
+/**
+ * The PoolableComponentHandler to make sure that poolable components are initialized
+ * destroyed and pooled correctly.
+ * <p>
+ * Components which implement Poolable may be configured to be pooled using the following
+ *  example configuration.  This example assumes that the user component class MyComp
+ *  implements Poolable.
+ * <p>
+ * Configuration Example:
+ * <pre>
+ *   &lt;my-comp pool-max="8"/&gt;
+ * </pre>
+ * <p>
+ * Roles Example:
+ * <pre>
+ *   &lt;role name="com.mypkg.MyComponent"
+ *         shorthand="my-comp"
+ *         default-class="com.mypkg.DefaultMyComponent"/&gt;
+ * </pre>
+ * <p>
+ * Configuration Attributes:
+ * <ul>
+ * <li>The <code>pool-max</code> attribute is used to set the maximum number of components which
+ *  will be pooled. (Defaults to "8") If additional instances are required, they're created,
+ *  but not pooled.</li>
+ * </ul>
+ *
+ * @version CVS $Id$
+ */
+public class NewPoolableComponentHandler
+extends AbstractFactoryHandler {
+    
+    /** The default max size of the pool */
+    public static final int DEFAULT_MAX_POOL_SIZE = 8;
+
+    /**
+     * Object used to synchronize access to the get and put methods
+     */
+    protected final Object semaphore = new Object();
+
+    /**
+     * The maximum size of the pool.
+     */
+    private final int max;
+
+    /**
+     * List of the Poolable instances which are available for use.
+     */
+    private LinkedList ready;
+
+    /**
+     * Store the size of the ready list to optimize operations which require this value.
+     */
+    private int readySize;
+
+    /**
+     * Total number of Poolable instances in the pool
+     */
+    private int size;
+    
+    /**
+     * Create a PoolableComponentHandler which manages a pool of Components
+     *  created by the specified factory object.
+     *
+     * @param factory The factory object which is responsible for creating the components
+     *                managed by the ComponentHandler.
+     * @param config The configuration to use to configure the pool.
+     */
+    public NewPoolableComponentHandler( final ComponentInfo info,
+                                     final Logger logger,
+                                     final ComponentFactory factory,
+                                     final Configuration config )
+    throws Exception {
+        super(info, logger, factory);
+
+        final int poolMax = config.getAttributeAsInteger( "pool-max", DEFAULT_MAX_POOL_SIZE );
+        this.max = ( poolMax <= 0 ? Integer.MAX_VALUE : poolMax );
+
+        // Create the pool lists.
+        this.ready = new LinkedList();
+    }
+
+    /**
+     * Dispose of the ComponentHandler and any associated Pools and Factories.
+     */
+    public void dispose() {
+        super.dispose();
+
+        // Any Poolables in the m_ready list need to be disposed of
+        synchronized( this.semaphore ) {
+            // Remove objects in the ready list.
+            for( Iterator iter = this.ready.iterator(); iter.hasNext(); ) {
+                Object poolable = iter.next();
+                iter.remove();
+                this.readySize--;
+                this.permanentlyRemovePoolable( poolable );
+            }
+
+            if( ( this.size > 0 ) && this.logger.isDebugEnabled() ) {
+                this.logger.debug( "There were " + this.size
+                                   + " outstanding objects when the pool was disposed." );
+            }
+        }
+    }
+    
+    /**
+     * Permanently removes a poolable from the pool's active list and
+     *  destroys it so that it will not ever be reused.
+     * <p>
+     * This method is only called by threads that have m_semaphore locked.
+     */
+    protected void permanentlyRemovePoolable( Object poolable ) {
+        this.size--;
+        this.decommission( poolable );
+    }
+
+    /**
+     * Gets a Poolable from the pool.  If there is room in the pool, a new Poolable will be
+     *  created.  Depending on the parameters to the constructor, the method may block or throw
+     *  an exception if a Poolable is not available on the pool.
+     *
+     * @return Always returns a Poolable.  Contract requires that put must always be called with
+     *  the Poolable returned.
+     * @throws Exception An exception may be thrown as described above or if there is an exception
+     *  thrown by the ObjectFactory's newInstance() method.
+     */
+    protected Object doGet() throws Exception {
+        Object poolable;
+        synchronized( this.semaphore ) {
+            // Look for a Poolable at the end of the m_ready list
+            if( this.readySize > 0 ){
+                // A poolable is ready and waiting in the pool
+                poolable = this.ready.removeLast();
+                this.readySize--;
+            } else {
+                // Create a new poolable.  May throw an exception if the poolable can not be
+                //  instantiated.
+                poolable = this.factory.newInstance();
+                this.size++;
+
+                if( this.logger.isDebugEnabled() ) {
+                    this.logger.debug( "Created a new " + poolable.getClass().getName()
+                                       + " from the object factory." );
+                }
+            }
+        }
+
+        this.factory.exitingPool(poolable);
+        
+        if( this.logger.isDebugEnabled() ) {
+            this.logger.debug( "Got a " + poolable.getClass().getName() + " from the pool." );
+        }
+
+        return createProxy(poolable);
+    }
+
+    /**
+     * Returns a poolable to the pool 
+     *
+     * @param poolable Poolable to return to the pool.
+     */
+    protected void doPut( final Object poolable ) {
+        try {
+            this.factory.enteringPool(poolable);
+        } catch (Exception ignore) {
+            this.logger.warn("Exception during putting component back into the pool.", ignore);
+        }
+
+        synchronized( this.semaphore ) {
+            if( this.size <= this.max ) {
+                if( this.disposed ) {
+                    // The pool has already been disposed.
+                    if( this.logger.isDebugEnabled() ) {
+                        this.logger.debug( "Put called for a " + poolable.getClass().getName()
+                                           + " after the pool was disposed." );
+                    }
+
+                    this.permanentlyRemovePoolable( poolable );
+                } else {
+                    // There is room in the pool to keep this poolable.
+                    if( this.logger.isDebugEnabled() ) {
+                        this.logger.debug( "Put a " + poolable.getClass().getName()
+                                           + " back into the pool." );
+                    }
+
+                    this.ready.addLast( poolable );
+                    this.readySize++;
+
+                }
+            } else {
+                // More Poolables were created than can be held in the pool, so remove.
+                if( this.logger.isDebugEnabled() ) {
+                    this.logger.debug( "No room to put a " + poolable.getClass().getName()
+                                       + " back into the pool, so remove it." );
+                }
+
+                this.permanentlyRemovePoolable( poolable );
+            }
+        }
+    }
+    
+    protected void doInitialize() {
+        // nothing to do here
+    }
+    
+    protected Object createProxy(Object component) {
+        Proxy.newProxyInstance(component.getClass().getClassLoader(), 
+                               this.guessWorkInterfaces(component.getClass()), 
+                               new ProxyHandler(component));
+        return component;
+    }
+
+    /**
+     * Get a list of interfaces to proxy by scanning through
+     * all interfaces a class implements and skipping invalid interfaces
+     * (as defined in {@link #INVALID_INTERFACES}).
+     *
+     * @param clazz the class
+     * @return the list of interfaces to proxy
+     */
+    protected Class[] guessWorkInterfaces( final Class clazz ) {
+        final HashSet workInterfaces = new HashSet();
+
+        // Get *all* interfaces
+        this.guessWorkInterfaces( clazz, workInterfaces );
+
+        return (Class[]) workInterfaces.toArray( new Class[workInterfaces.size()] );
+    }
+
+    /**
+     * Get a list of interfaces to proxy by scanning through
+     * all interfaces a class implements.
+     *
+     * @param clazz           the class
+     * @param workInterfaces  the set of current work interfaces
+     */
+    private void guessWorkInterfaces( final Class clazz,
+                                      final Set workInterfaces ) {
+        if ( null != clazz ) {
+            this.addInterfaces( clazz.getInterfaces(), workInterfaces );
+
+            this.guessWorkInterfaces( clazz.getSuperclass(), workInterfaces );
+        }
+    }
+
+    /**
+     * Get a list of interfaces to proxy by scanning through
+     * all interfaces a class implements.
+     *
+     * @param interfaces      the array of interfaces
+     * @param workInterfaces  the set of current work interfaces
+     */
+    private void addInterfaces( final Class[] interfaces,
+                                final Set workInterfaces ) {
+        for ( int i = 0; i < interfaces.length; i++ ) {
+            workInterfaces.add( interfaces[i] );
+            this.addInterfaces(interfaces[i].getInterfaces(), workInterfaces);
+        }
+    }
+
+    protected static final class ProxyHandler implements InvocationHandler {
+        
+        private final Object component;
+        
+        public ProxyHandler(Object component) {
+            this.component = component;
+        }
+        
+        /* (non-Javadoc)
+         * @see java.lang.reflect.InvocationHandler#invoke(java.lang.Object, java.lang.reflect.Method, java.lang.Object[])
+         */
+        public Object invoke(Object proxy, Method method, Object[] args)
+        throws Throwable {
+            try {
+                return method.invoke(component, args);
+            } catch (InvocationTargetException ite) {
+                throw ite.getTargetException();
+            }
+        }
+    }
+}
