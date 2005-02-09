0d229683c209e68abb76becd7682bcb498347799
Carsten Ziegeler <cziegeler@apache.org>
2005-02-09 18:05:23 +0000

    Remove dependency to xsp and fix samples
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@153096 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/chaperon/samples/misc/wiki.js
+++ src/blocks/chaperon/samples/misc/wiki.js
@@ -1,5 +1,5 @@
 /*
-* Copyright 1999-2004 The Apache Software Foundation
+* Copyright 1999-2005 The Apache Software Foundation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -13,10 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-importPackage(Packages.org.apache.excalibur.source);
-//importPackage(Packages.java.io);
-
-var resolver = cocoon.getComponent(SourceResolver.ROLE);
+var resolver = cocoon.getComponent(Packages.org.apache.excalibur.source.SourceResolver.ROLE);
 
 function view() {
 
@@ -136,3 +133,11 @@ function exists(page)  {
     return exists;
 }
 
+function mathexp() {
+    var value;
+    if ( cocoon.request.getParameter("text") != null )
+        value = cocoon.request.getParameter("text");
+    else
+        value = "3*a-5*6/7 + ( b-c)*5- b";
+    cocoon.sendPage("mathexp-view.html", {"value" : value});
+}
