cebd57262b187ecc7049550762965ccbfc213532
Vadim Gritsenko <vgritsenko@apache.org>
2005-02-17 17:13:51 +0000

    merge changes: separate ojb connection factory config out of ojb access components (pb/jdo/odmg). cleanup code. integrate ojb logging with cocoon logging.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@154170 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/ojb/java/org/apache/cocoon/ojb/odmg/components/OdmgImplementation.java
+++ src/blocks/ojb/java/org/apache/cocoon/ojb/odmg/components/OdmgImplementation.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2002,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,9 @@
  */
 package org.apache.cocoon.ojb.odmg.components;
 
+
 import org.odmg.Implementation;
 import org.odmg.ODMGException;
-
 
 /**
  * Interface of the ODMG Implementation. It is used to get the Implementation Object to interact with ODMG using OJB
@@ -38,7 +38,7 @@ public interface OdmgImplementation {
      * @throws ODMGException DOCUMENT ME!
      */
     Implementation getInstance()
-        throws ODMGException;
+    throws ODMGException;
 
     /**
      * Get a ODMG Instance with a specific connection definition.
@@ -50,7 +50,7 @@ public interface OdmgImplementation {
      * @throws ODMGException DOCUMENT ME!
      */
     Implementation getInstance(String connection)
-        throws ODMGException;
+    throws ODMGException;
 
     /**
      * Get a ODMG Instance with a specific connection definition and a Database operation mode.
@@ -63,7 +63,7 @@ public interface OdmgImplementation {
      * @throws ODMGException DOCUMENT ME!
      */
     Implementation getInstance(String connection, int mode)
-        throws ODMGException;
+    throws ODMGException;
 
     /**
      * Get a ODMG Instance with a default connection definition and a Database operation mode.
@@ -75,5 +75,5 @@ public interface OdmgImplementation {
      * @throws ODMGException DOCUMENT ME!
      */
     Implementation getInstance(int mode)
-        throws ODMGException;
+    throws ODMGException;
 }
