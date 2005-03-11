4e392528bef42372bd7a8ba3c9af4d76666cccdf
Vadim Gritsenko <vgritsenko@apache.org>
2005-03-11 20:13:26 +0000

    SimpleParentProcessingNode: fix logic in hasChildren method. Whitespaces, Javadoc, cleanup.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@157144 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/treeprocessor/sitemap/AggregateNodeBuilder.java
+++ src/java/org/apache/cocoon/components/treeprocessor/sitemap/AggregateNodeBuilder.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ * Copyright 1999-2005 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,13 +31,11 @@ import org.apache.cocoon.components.treeprocessor.ProcessingNode;
 import org.apache.cocoon.components.treeprocessor.variables.VariableResolverFactory;
 
 /**
- *
  * @author <a href="mailto:sylvain@apache.org">Sylvain Wallez</a>
- * @version CVS $Id: AggregateNodeBuilder.java,v 1.3 2004/03/05 13:02:51 bdelacretaz Exp $
+ * @version $Id$
  */
-
 public class AggregateNodeBuilder extends AbstractProcessingNodeBuilder
-  implements LinkedProcessingNodeBuilder {
+                                  implements LinkedProcessingNodeBuilder {
 
     /** The views for the aggregate element */
     private Collection views;
@@ -56,21 +54,21 @@ public class AggregateNodeBuilder extends AbstractProcessingNodeBuilder
         this.treeBuilder.setupNode(this.node, config);
 
         this.views = ((SitemapLanguage)this.treeBuilder).getViewsForStatement("", "", config);
-        
+
         // Bug #7196 : ensure this.views is never null (see continuation of fix below)
         if (this.views == null) {
             this.views = new HashSet();
         }
-        
+
         // The sitemap builder
         SitemapLanguage sitemap = (SitemapLanguage)this.treeBuilder;
 
         // All parts of the aggregate
         List allParts = new ArrayList();
-   
+
         // For each view that a part matches, the list of all parts that match it
         Map viewParts = new HashMap();
-        
+
         Configuration[] childConfigs = config.getChildren();
         for (int i = 0; i < childConfigs.length; i++) {
             Configuration childConfig = childConfigs[i];
@@ -90,22 +88,22 @@ public class AggregateNodeBuilder extends AbstractProcessingNodeBuilder
                 VariableResolverFactory.getResolver(childConfig.getAttribute("prefix", ""), this.manager),
                 VariableResolverFactory.getResolver(childConfig.getAttribute("strip-root", "false"), this.manager)
             );
-            
+
             allParts.add(currentPart);
-            
+
             // Get the views for this part
             Collection viewsForPart = sitemap.getViewsForStatement("", "", childConfig);
-            
+
             // Associate this part to all the views it belongs to
             if (viewsForPart != null) {
-                
+
                 // Bug #7196 : add part view to aggregate views
                 this.views.addAll(viewsForPart);
-                
+
                 Iterator iter = viewsForPart.iterator();
                 while(iter.hasNext()) {
                     String currentView = (String)iter.next();
-                    
+
                     // Get collection of parts for current view
                     Collection currentViewParts = (Collection)viewParts.get(currentView);
                     if (currentViewParts == null) {
@@ -113,7 +111,7 @@ public class AggregateNodeBuilder extends AbstractProcessingNodeBuilder
                         currentViewParts = new ArrayList();
                         viewParts.put(currentView, currentViewParts);
                     }
-                    
+
                     // Add the current part to the parts list of the view
                     currentViewParts.add(currentPart);
                 }
@@ -128,11 +126,11 @@ public class AggregateNodeBuilder extends AbstractProcessingNodeBuilder
         // Now convert all Collections to Array for faster traversal
         AggregateNode.Part[] allPartsArray = (AggregateNode.Part[])allParts.toArray(
             new AggregateNode.Part[allParts.size()]);
-            
+
         Iterator iter = viewParts.entrySet().iterator();
         while(iter.hasNext()) {
             Map.Entry entry = (Map.Entry)iter.next();
-            
+
             // Get collection of parts for this entry
             Collection coll = (Collection)entry.getValue();
 
@@ -152,7 +150,7 @@ public class AggregateNodeBuilder extends AbstractProcessingNodeBuilder
 
         // Give the AggregateNode a Node for each view
         SitemapLanguage sitemap = (SitemapLanguage)this.treeBuilder;
-        
+
         this.node.setViewNodes(sitemap.getViewNodes(this.views));
     }
 }
