def10dfa3cb56c1489de57ac2a2cd8f024d4e1f2
Vadim Gritsenko <vgritsenko@apache.org>
2005-03-14 17:23:40 +0000

    SVN Id, Copyright year.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@157439 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/bean/CocoonWrapper.java
+++ src/java/org/apache/cocoon/bean/CocoonWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2005 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,7 +67,7 @@ import org.xml.sax.ContentHandler;
  * @author <a href="mailto:nicolaken@apache.org">Nicola Ken Barozzi</a>
  * @author <a href="mailto:vgritsenko@apache.org">Vadim Gritsenko</a>
  * @author <a href="mailto:uv@upaya.co.uk">Upayavira</a>
- * @version CVS $Id$
+ * @version $Id$
  */
 public class CocoonWrapper {
 
