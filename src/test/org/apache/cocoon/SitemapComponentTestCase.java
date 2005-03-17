5c0f1889122c8c15e14706d03768ef113b74eb63
Par Daniel Fagerstrohm <danielf@apache.org>
2005-03-17 08:01:44 +0000

    Fix to make tests runable again. Don't know if it the right way to initaliize settings though.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@157912 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/cocoon/SitemapComponentTestCase.java
+++ src/test/org/apache/cocoon/SitemapComponentTestCase.java
@@ -40,6 +40,8 @@ import org.apache.cocoon.components.flow.AbstractInterpreter;
 import org.apache.cocoon.components.flow.FlowHelper;
 import org.apache.cocoon.components.flow.Interpreter;
 import org.apache.cocoon.components.source.SourceResolverAdapter;
+import org.apache.cocoon.configuration.Settings;
+import org.apache.cocoon.core.Core;
 import org.apache.cocoon.core.container.CoreServiceManager;
 import org.apache.cocoon.core.container.StandaloneServiceSelector;
 import org.apache.cocoon.environment.ObjectModelHelper;
@@ -106,6 +108,7 @@ public abstract class SitemapComponentTestCase extends CocoonTestCase {
         context.put(ContextHelper.CONTEXT_REQUEST_OBJECT, request);
         context.put(ContextHelper.CONTEXT_RESPONSE_OBJECT, response);
         context.put(ContextHelper.CONTEXT_OBJECT_MODEL, objectmodel);
+        context.put(Core.CONTEXT_SETTINGS, new Settings());
     }
 
     /* (non-Javadoc)
