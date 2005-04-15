b3677490140dce8373f08fe5e2972c5f43a6285e
Vadim Gritsenko <vgritsenko@apache.org>
2005-04-15 15:54:05 +0000

    Merge NonStaticClassLoaderManager into ClassLoaderManagerImpl, deprecate NonStaticClassLoaderManager. See also http://marc.theaimsgroup.com/?t=104524385000003&r=1. Add Collection constructor to RepositoryClassLoader. Reformat code (4 space indent). Add dispose() to ClassLoaderManagerImpl.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@161488 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/classloader/ClassLoaderManagerImpl.java
+++ src/java/org/apache/cocoon/components/classloader/ClassLoaderManagerImpl.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ * Copyright 1999-2005 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,7 @@
  */
 package org.apache.cocoon.components.classloader;
 
+import org.apache.avalon.framework.activity.Disposable;
 import org.apache.avalon.framework.thread.ThreadSafe;
 
 import java.io.File;
@@ -22,65 +23,68 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Vector;
 
 /**
  * A singleton-like implementation of <code>ClassLoaderManager</code>
  *
  * @author <a href="mailto:ricardo@apache.org">Ricardo Rocha</a>
- * @version CVS $Id$
+ * @version $Id$
  */
-public class ClassLoaderManagerImpl implements ClassLoaderManager, ThreadSafe {
-  /**
-   * The single class loader instance
-   */
-  private static RepositoryClassLoader instance = null;
+public class ClassLoaderManagerImpl implements ClassLoaderManager, ThreadSafe, Disposable {
 
-  protected final static Set fileSet = Collections.synchronizedSet(new HashSet());
+    /**
+     * Set of class directories
+     */
+    protected final Set fileSet = Collections.synchronizedSet(new HashSet());
 
-  /**
-   * A constructor that ensures only a single class loader instance exists
-   *
-   */
-  public ClassLoaderManagerImpl() {
-    if (instance == null) {
-      this.reinstantiate();
+    /**
+     * The class loader instance
+     */
+    private RepositoryClassLoader instance;
+
+    /**
+     * A constructor that ensures only a single class loader instance exists
+     */
+    public ClassLoaderManagerImpl() {
+        reinstantiate();
     }
-  }
 
-  /**
-   * Add a directory to the proxied class loader
-   *
-   * @param directoryName The repository name
-   * @exception IOException If the directory is invalid
-   */
-  public void addDirectory(File directoryName) throws IOException {
-    if ( ! ClassLoaderManagerImpl.fileSet.contains(directoryName)) {
-        ClassLoaderManagerImpl.fileSet.add(directoryName);
-        ClassLoaderManagerImpl.instance.addDirectory(directoryName);
+    public void dispose() {
+        this.fileSet.clear();
+        reinstantiate();
     }
-  }
 
-  /**
-   * Load a class through the proxied class loader
-   *
-   * @param className The name of the class to be loaded
-   * @return The loaded class
-   * @exception ClassNotFoundException If the class is not found
-   */
-  public Class loadClass(String className) throws ClassNotFoundException {
-    return ClassLoaderManagerImpl.instance.loadClass(className);
-  }
-
-  /**
-   * Reinstantiate the proxied class loader to allow for class reloading
-   *
-   */
-  public void reinstantiate() {
-    if ( ClassLoaderManagerImpl.fileSet.isEmpty()) {
-      ClassLoaderManagerImpl.instance = new RepositoryClassLoader();
-    } else {
-      ClassLoaderManagerImpl.instance = new RepositoryClassLoader(new Vector(ClassLoaderManagerImpl.fileSet));
+    /**
+     * Add a directory to the proxied class loader
+     *
+     * @param directoryName The repository name
+     * @throws IOException If the directory is invalid
+     */
+    public void addDirectory(File directoryName) throws IOException {
+        if (this.fileSet.add(directoryName)) {
+            this.instance.addDirectory(directoryName);
+        }
     }
-  }
+
+    /**
+     * Load a class through the proxied class loader
+     *
+     * @param className The name of the class to be loaded
+     * @return The loaded class
+     * @throws ClassNotFoundException If the class is not found
+     */
+    public Class loadClass(String className) throws ClassNotFoundException {
+        return this.instance.loadClass(className);
+    }
+
+    /**
+     * Reinstantiate the proxied class loader to allow for class reloading
+     */
+    public void reinstantiate() {
+        if (this.fileSet.isEmpty()) {
+            this.instance = new RepositoryClassLoader();
+        } else {
+            this.instance = new RepositoryClassLoader(this.fileSet);
+        }
+    }
 }
