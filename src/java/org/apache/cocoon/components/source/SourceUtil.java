8e4bf2892a10e3c762978cc5fa61a3b3a704ea98
Vadim Gritsenko <vgritsenko@apache.org>
2005-04-29 18:13:40 +0000

    handle SourceException
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@165322 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/source/SourceUtil.java
+++ src/java/org/apache/cocoon/components/source/SourceUtil.java
@@ -96,9 +96,13 @@ public final class SourceUtil {
             source.toSAX(handler);
         } catch (SAXException e) {
             // Unwrap ProcessingException, IOException, and extreme cases of SAXExceptions.
+            // Handle SourceException.
             // See also handleSAXException
             final Exception cause = e.getException();
             if (cause != null) {
+                if (cause instanceof SourceException) {
+                    handle((SourceException) cause);
+                }
                 if (cause instanceof ProcessingException) {
                     throw (ProcessingException) cause;
                 }
@@ -447,6 +451,11 @@ public final class SourceUtil {
         final Exception cause = e.getException();
         if (cause != null) {
             // Unwrap ProcessingException, IOException, and extreme cases of SAXExceptions.
+            // Handle SourceException.
+            // See also toSax(XMLizable, ContentHandler
+            if (cause instanceof SourceException) {
+                handle((SourceException) cause);
+            }
             if (cause instanceof ProcessingException) {
                 throw (ProcessingException) cause;
             }
