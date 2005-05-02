54d016aec67e11a27b3a066fcd0cbd9aee954c6b
Carsten Ziegeler <cziegeler@apache.org>
2005-05-02 14:21:28 +0000

    Add missing ThreadSafe
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@165639 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/source/CocoonSourceResolver.java
+++ src/java/org/apache/cocoon/components/source/CocoonSourceResolver.java
@@ -31,6 +31,7 @@ import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.service.ServiceException;
 import org.apache.avalon.framework.service.ServiceManager;
 import org.apache.avalon.framework.service.Serviceable;
+import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.Processor;
 import org.apache.cocoon.environment.SourceResolver;
 import org.apache.cocoon.environment.internal.EnvironmentHelper;
@@ -50,7 +51,7 @@ import org.apache.excalibur.source.URIAbsolutizer;
 */
 public class CocoonSourceResolver 
 extends AbstractLogEnabled
-implements SourceResolver, Contextualizable, Serviceable, Disposable {
+implements SourceResolver, Contextualizable, Serviceable, Disposable, ThreadSafe {
 
     /** A (optional) custom source resolver */
     protected org.apache.excalibur.source.SourceResolver customResolver;
@@ -104,78 +105,77 @@ implements SourceResolver, Contextualizable, Serviceable, Disposable {
         }
         if ( this.customResolver != null ) {
             return this.customResolver.resolveURI(location, baseURI, parameters);
+        }
+        if( this.getLogger().isDebugEnabled() ) {
+            this.getLogger().debug( "Resolving '" + location + "' with base '" + baseURI + "' in context '" + this.baseURL + "'" );
+        }
+        if( location == null ) {
+            throw new MalformedURLException( "Invalid System ID" );
+        }
+        if( null != baseURI && org.apache.excalibur.source.SourceUtil.indexOfSchemeColon(baseURI) == -1 ) {
+            throw new MalformedURLException( "BaseURI is not valid, it must contain a protocol: " + baseURI );
+        }
+
+        if( baseURI == null ) {
+            baseURI = this.baseURL.toExternalForm();
+        }
+
+        String systemID = location;
+        // special handling for windows file paths
+        if( location.length() > 1 && location.charAt( 1 ) == ':' ) {
+            systemID = "file:/" + location;
+        } else if( location.length() > 2 && location.charAt(0) == '/' && location.charAt(2) == ':' ) {
+            systemID = "file:" + location;
+        }
+
+        // determine protocol (scheme): first try to get the one of the systemID, if that fails, take the one of the baseURI
+        String protocol;
+        int protocolPos = org.apache.excalibur.source.SourceUtil.indexOfSchemeColon(systemID);
+        if( protocolPos != -1 ) {
+            protocol = systemID.substring( 0, protocolPos );
         } else {
-            if( this.getLogger().isDebugEnabled() ) {
-                this.getLogger().debug( "Resolving '" + location + "' with base '" + baseURI + "' in context '" + this.baseURL + "'" );
-            }
-            if( location == null ) {
-                throw new MalformedURLException( "Invalid System ID" );
-            }
-            if( null != baseURI && org.apache.excalibur.source.SourceUtil.indexOfSchemeColon(baseURI) == -1 ) {
-                throw new MalformedURLException( "BaseURI is not valid, it must contain a protocol: " + baseURI );
-            }
-
-            if( baseURI == null ) {
-                baseURI = this.baseURL.toExternalForm();
-            }
-
-            String systemID = location;
-            // special handling for windows file paths
-            if( location.length() > 1 && location.charAt( 1 ) == ':' ) {
-                systemID = "file:/" + location;
-            } else if( location.length() > 2 && location.charAt(0) == '/' && location.charAt(2) == ':' ) {
-                systemID = "file:" + location;
-            }
-
-            // determine protocol (scheme): first try to get the one of the systemID, if that fails, take the one of the baseURI
-            String protocol;
-            int protocolPos = org.apache.excalibur.source.SourceUtil.indexOfSchemeColon(systemID);
+            protocolPos = org.apache.excalibur.source.SourceUtil.indexOfSchemeColon(baseURI);
             if( protocolPos != -1 ) {
-                protocol = systemID.substring( 0, protocolPos );
+                protocol = baseURI.substring( 0, protocolPos );
             } else {
-                protocolPos = org.apache.excalibur.source.SourceUtil.indexOfSchemeColon(baseURI);
-                if( protocolPos != -1 ) {
-                    protocol = baseURI.substring( 0, protocolPos );
-                } else {
-                    protocol = "*";
-                }
+                protocol = "*";
             }
+        }
 
-            final ServiceManager m = this.getServiceManager();
+        final ServiceManager m = this.getServiceManager();
 
-            Source source = null;
-            // search for a SourceFactory implementing the protocol
-            SourceFactory factory = null;
+        Source source = null;
+        // search for a SourceFactory implementing the protocol
+        SourceFactory factory = null;
+        try {
+            factory = this.getSourceFactory( m, protocol );
+            systemID = this.absolutize( factory, baseURI, systemID );
+            if( getLogger().isDebugEnabled() ) {
+                getLogger().debug( "Resolved to systemID : " + systemID );
+            }
+            source = factory.getSource( systemID, parameters );
+        } catch( final ServiceException ce ) {
+            // no selector available, use fallback
+        } finally {
+            m.release( factory );
+        }
+
+        if( null == source ) {
             try {
-                factory = this.getSourceFactory( m, protocol );
+                factory = this.getSourceFactory( m, "*");
                 systemID = this.absolutize( factory, baseURI, systemID );
                 if( getLogger().isDebugEnabled() ) {
                     getLogger().debug( "Resolved to systemID : " + systemID );
                 }
                 source = factory.getSource( systemID, parameters );
-            } catch( final ServiceException ce ) {
-                // no selector available, use fallback
+            } catch (ServiceException se ) {
+                throw new SourceException( "Unable to select source factory for " + systemID, se );
             } finally {
-                m.release( factory );
+                m.release(factory);
             }
-
-            if( null == source ) {
-                try {
-                    factory = this.getSourceFactory( m, "*");
-                    systemID = this.absolutize( factory, baseURI, systemID );
-                    if( getLogger().isDebugEnabled() ) {
-                        getLogger().debug( "Resolved to systemID : " + systemID );
-                    }
-                    source = factory.getSource( systemID, parameters );
-                } catch (ServiceException se ) {
-                    throw new SourceException( "Unable to select source factory for " + systemID, se );
-                } finally {
-                    m.release(factory);
-                }
-            }
-
-            return source;
         }
+
+        return source;
     }
 
     /**
