85ab188477d5791cbb10bb0831c021b5c3e6bab4
Vadim Gritsenko <vgritsenko@apache.org>
2005-05-19 16:07:55 +0000

    remove (long unused) WidgetReplacingPipe
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@170952 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/forms/java/org/apache/cocoon/forms/transformation/FormsTemplateTransformer.java
+++ src/blocks/forms/java/org/apache/cocoon/forms/transformation/FormsTemplateTransformer.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ * Copyright 1999-2005 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,8 +27,8 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- * See description of {@link WidgetReplacingPipe}.
- * 
+ * See description of {@link EffectWidgetReplacingPipe}.
+ *
  * @version $Id$
  */
 public class FormsTemplateTransformer extends EffectWidgetReplacingPipe implements Transformer {
