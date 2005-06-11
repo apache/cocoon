36709438b528c3cccd71ac71aac8a2e52c479d8a
Carsten Ziegeler <cziegeler@apache.org>
2005-06-11 17:15:03 +0000

    Allow only one flow node per sitemap
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@190154 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/treeprocessor/TreeBuilder.java
+++ src/java/org/apache/cocoon/components/treeprocessor/TreeBuilder.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ * Copyright 1999-2005 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -49,8 +49,10 @@ public interface TreeBuilder extends Component {
      * Register a <code>ProcessingNode</code> under a given name.
      * For example, <code>ResourceNodeBuilder</code> stores here the <code>ProcessingNode</code>s
      * it produces for use by sitemap pipelines. This allows to turn the tree into a graph.
+     * If a node with the name is already registed, the process fails!
+     * @return If the node could be registered, <code>true</code> is returned; otherwise false.
      */
-    void registerNode(String name, ProcessingNode node);
+    boolean registerNode(String name, ProcessingNode node);
 
     /**
      * @throws IllegalStateException
