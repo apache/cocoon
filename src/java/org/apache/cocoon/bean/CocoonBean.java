b36c2cad9c6d0249220651d65f4820d10d7fcf04
Carsten Ziegeler <cziegeler@apache.org>
2005-08-06 17:01:28 +0000

    Add build info to CLI output
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@230553 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/bean/CocoonBean.java
+++ src/java/org/apache/cocoon/bean/CocoonBean.java
@@ -740,6 +740,7 @@ public class CocoonBean extends CocoonWrapper {
         msg.append("------------------------------------------------------------------------ ").append(lSep);
         msg.append(Constants.NAME).append(" ").append(Constants.VERSION).append(lSep);
         msg.append("Copyright (c) ").append(Constants.YEAR).append(" Apache Software Foundation. All rights reserved.").append(lSep);
+        msg.append("Build: ").append(Constants.BUILD_INFO).append(lSep);
         msg.append("------------------------------------------------------------------------ ").append(lSep).append(lSep);
         return msg.toString();
     }
