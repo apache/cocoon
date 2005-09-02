63a597dc8646105868bfd85e72f9f201de7f256f
Carsten Ziegeler <cziegeler@apache.org>
2005-09-02 08:53:34 +0000

    Fix class cast exception
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@267163 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/portal/java/org/apache/cocoon/portal/pluto/servlet/ServletRequestImpl.java
+++ src/blocks/portal/java/org/apache/cocoon/portal/pluto/servlet/ServletRequestImpl.java
@@ -70,144 +70,112 @@ public class ServletRequestImpl implements HttpServletRequest {
         return new ServletRequestImpl(request, provider, window);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getAuthType()
      */
     public String getAuthType() {
         return this.request.getAuthType();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getContextPath()
      */
     public String getContextPath() {
         return this.request.getContextPath();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getCookies()
      */
     public Cookie[] getCookies() {
         return this.request.getCookies();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getDateHeader(java.lang.String)
      */
     public long getDateHeader(String arg0) {
         return this.request.getDateHeader(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getHeader(java.lang.String)
      */
     public String getHeader(String arg0) {
         return this.request.getHeader(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getHeaderNames()
      */
     public Enumeration getHeaderNames() {
         return this.request.getHeaderNames();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getHeaders(java.lang.String)
      */
     public Enumeration getHeaders(String arg0) {
         return this.request.getHeaders(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getIntHeader(java.lang.String)
      */
     public int getIntHeader(String arg0) {
         return this.request.getIntHeader(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getMethod()
      */
     public String getMethod() {
         return this.request.getMethod();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getPathInfo()
      */
     public String getPathInfo() {
         return this.request.getPathInfo();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getPathTranslated()
      */
     public String getPathTranslated() {
         return this.request.getPathTranslated();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getQueryString()
      */
     public String getQueryString() {
         return null;
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getRemoteUser()
      */
     public String getRemoteUser() {
         return this.request.getRemoteUser();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getRequestedSessionId()
      */
     public String getRequestedSessionId() {
         return this.request.getRequestedSessionId();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getRequestURI()
      */
     public String getRequestURI() {
         return this.request.getRequestURI();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getRequestURL()
      */
     public StringBuffer getRequestURL() {
@@ -215,252 +183,196 @@ public class ServletRequestImpl implements HttpServletRequest {
         return null;
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getServletPath()
      */
     public String getServletPath() {
         return this.request.getServletPath();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getSession()
      */
     public HttpSession getSession() {
         return this.request.getSession();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getSession(boolean)
      */
     public HttpSession getSession(boolean arg0) {
         return this.request.getSession(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#getUserPrincipal()
      */
     public Principal getUserPrincipal() {
         return this.request.getUserPrincipal();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#isRequestedSessionIdFromCookie()
      */
     public boolean isRequestedSessionIdFromCookie() {
         return this.request.isRequestedSessionIdFromCookie();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#isRequestedSessionIdFromUrl()
      */
     public boolean isRequestedSessionIdFromUrl() {
         return this.request.isRequestedSessionIdFromURL();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#isRequestedSessionIdFromURL()
      */
     public boolean isRequestedSessionIdFromURL() {
         return this.request.isRequestedSessionIdFromURL();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#isRequestedSessionIdValid()
      */
     public boolean isRequestedSessionIdValid() {
         return this.request.isRequestedSessionIdValid();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.http.HttpServletRequest#isUserInRole(java.lang.String)
      */
     public boolean isUserInRole(String arg0) {
         return this.request.isUserInRole(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getAttribute(java.lang.String)
      */
     public Object getAttribute(String arg0) {
         return this.request.getAttribute(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getAttributeNames()
      */
     public Enumeration getAttributeNames() {
         return this.request.getAttributeNames();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getCharacterEncoding()
      */
     public String getCharacterEncoding() {
         return this.request.getCharacterEncoding();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getContentLength()
      */
     public int getContentLength() {
         return this.request.getContentLength();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getInputStream()
      */
     public ServletInputStream getInputStream() throws IOException {
         return this.request.getInputStream();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getLocale()
      */
     public Locale getLocale() {
         return this.request.getLocale();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getLocales()
      */
     public Enumeration getLocales() {
         return this.request.getLocales();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getProtocol()
      */
     public String getProtocol() {
         return this.request.getProtocol();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getReader()
      */
     public BufferedReader getReader() throws IOException {
         return this.request.getReader();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getRealPath(java.lang.String)
      */
     public String getRealPath(String arg0) {
         return this.request.getRealPath(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getRemoteAddr()
      */
     public String getRemoteAddr() {
         return this.request.getRemoteAddr();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getRemoteHost()
      */
     public String getRemoteHost() {
         return this.request.getRemoteHost();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getRequestDispatcher(java.lang.String)
      */
     public RequestDispatcher getRequestDispatcher(String arg0) {
         return this.request.getRequestDispatcher(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getScheme()
      */
     public String getScheme() {
         return this.request.getScheme();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getServerName()
      */
     public String getServerName() {
         return this.request.getServerName();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getServerPort()
      */
     public int getServerPort() {
         return this.request.getServerPort();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#isSecure()
      */
     public boolean isSecure() {
         return this.request.isSecure();
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#removeAttribute(java.lang.String)
      */
     public void removeAttribute(String arg0) {
         this.request.removeAttribute(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#setAttribute(java.lang.String,
      *      java.lang.Object)
      */
@@ -468,9 +380,7 @@ public class ServletRequestImpl implements HttpServletRequest {
         this.request.setAttribute(arg0, arg1);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#setCharacterEncoding(java.lang.String)
      */
     public void setCharacterEncoding(String arg0)
@@ -478,9 +388,7 @@ public class ServletRequestImpl implements HttpServletRequest {
         //this.request.setCharacterEncoding(arg0);
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getContentType()
      */
     public String getContentType() {
@@ -491,19 +399,19 @@ public class ServletRequestImpl implements HttpServletRequest {
         return contentType;
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getParameter(java.lang.String)
      */
     public String getParameter(String name) {
-        return (String) this.getParameterMap().get(name);
+        final String[] values = (String[])this.getParameterMap().get(name);
+        if ( values != null && values.length > 0 ) {
+            return values[0];
+        }
+        return null;
 
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getParameterMap()
      */
     public Map getParameterMap() {
@@ -563,18 +471,14 @@ public class ServletRequestImpl implements HttpServletRequest {
         return this.portletParameterMap;
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getParameterNames()
      */
     public Enumeration getParameterNames() {
         return Collections.enumeration(this.getParameterMap().keySet());
     }
 
-    /*
-     * (non-Javadoc)
-     *
+    /**
      * @see javax.servlet.ServletRequest#getParameterValues(java.lang.String)
      */
     public String[] getParameterValues(String name) {
