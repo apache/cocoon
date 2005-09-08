a45f141128878b00df1d01d46fbb1b150a0d1d9a
Berin Loritsch <bloritsch@apache.org>
2005-09-08 17:37:58 +0000

    bolster the sitemap component contract information
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@279591 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/sitemap/SitemapModelComponent.java
+++ src/java/org/apache/cocoon/sitemap/SitemapModelComponent.java
@@ -24,9 +24,20 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- * This interface marks a component as a sitemap component that is used
- * in the processing phase, like a generator, a transformer, a serializer
- * or a reader.
+ * The SitemapModelComponent identifies the contract between the Sitemap and
+ * your pipeline components that create or transform information.  The types
+ * of components that fit within this umbrella are your Generators,
+ * Transformers, and your Readers.  It is very important to note that all
+ * components impementing this interface must be pooled or created on demand.
+ * This is due to the separation between the setup and the execution.  If you
+ * don't ensure every instance of the component is unique within a pipeline,
+ * or accross pipelines, then the setup process will start killing all the
+ * other setups and you will end up with serious race conditions.  It's not
+ * that they need synchronized keywords applied to the methods, its that the
+ * methods have to be called in a certain order.  This is by design.  If you
+ * really think about it, due to the SAX infrastructure we would still need to
+ * keep them synchronized because the order of SAX events affects the validity
+ * of your XML document.
  * 
  * @author <a href="mailto:pier@apache.org">Pierpaolo Fumagalli</a>
  *         (Apache Software Foundation)
@@ -34,8 +45,19 @@ import java.util.Map;
  */
 public interface SitemapModelComponent {
     /**
-     * Set the <code>SourceResolver</code>, objectModel <code>Map</code>,
-     * the source and sitemap <code>Parameters</code> used to process the request.
+     * The Sitemap will call the setup() method to prepare the component for
+     * use.  This is where you start the process of getting your information
+     * ready to generate your results.  See {@link org.apache.cocoon.environment.ObjectModelHelper} for help with the <code>objectModel</code>.
+     *
+     * @param resolver     The <code>SourceResolver</code> to find resources within your context.
+     * @param objectModel  A <code>java.util.Map</code> that contains the request and session information.
+     * @param src          The value of the "src" attribute in the sitemap.
+     * @param par          The sitemap parameters passed into your component.
+     *
+     * @throws SAXException if there is a problem reading a SAX stream.
+     * @throws IOException  if there is a problem reading files.
+     * @throws ResourceNotFoundException if the resource to be rendered cannot be found.
+     * @throws ProcessingException if there is any other unexpected problem.
      */
     void setup(SourceResolver resolver, Map objectModel, String src, Parameters par) 
     throws ProcessingException, SAXException, IOException;
