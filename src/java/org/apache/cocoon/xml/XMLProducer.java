127617f001ad0373773ca40c45687e623711b0da
Berin Loritsch <bloritsch@apache.org>
2005-09-08 17:12:20 +0000

    update the docs on the three core contracts
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@279586 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/xml/XMLProducer.java
+++ src/java/org/apache/cocoon/xml/XMLProducer.java
@@ -18,9 +18,25 @@ package org.apache.cocoon.xml;
 /**
  * This interfaces identifies classes that produce XML data, sending SAX
  * events to the configured <code>XMLConsumer</code>.
- * <br>
- * It's beyond the scope of this interface to specify a way in which the XML
- * data production is started.
+ * <p>
+ * The XMLProducer is comprised of only one method to give the component the
+ * next element of the pipeline.  Cocoon calls the <code>setConsumer()</code>
+ * method with the reference to the next XMLConsumer in the pipeline.  The
+ * approach allows the XMLProducer to call the different SAX related methods on
+ * the XMLConsumer without knowing ahead of time what that consumer will be.
+ * The design is very simple and very powerful in that it allows Cocoon to
+ * daisy chain several components in any order and then execute the pipeline.
+ * </p>
+ * <p>
+ * Any producer can be paired with any consumer and we have a pipeline.  The
+ * core design is very powerful and allows the end user to mix and match
+ * sitemap components as they see fit.  Cocoon will always call
+ * <code>setConsumer()</code> on every XMLProducer in a pipeline or it will
+ * throw an exception saying that the pipeline is invalid (i.e. there is no
+ * serializer for the pipeline).  The only contract that the XMLProducer has to
+ * worry about is that it must always make calls to the XMLConsumer passed in
+ * through the <code>setConsumer()</code> method.
+ * </p>
  *
  * @author <a href="mailto:pier@apache.org">Pierpaolo Fumagalli</a>
  *         (Apache Software Foundation)
@@ -30,7 +46,8 @@ public interface XMLProducer {
 
     /**
      * Set the <code>XMLConsumer</code> that will receive XML data.
+     *
+     * @param consumer  The XMLConsumer target for SAX events.
      */
     void setConsumer(XMLConsumer consumer);
-
 }
