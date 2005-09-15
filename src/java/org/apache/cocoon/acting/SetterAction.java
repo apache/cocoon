fd27629a30b827ad0670d04a289964ee168d0d93
Carsten Ziegeler <cziegeler@apache.org>
2005-09-15 08:24:23 +0000

    Add missing ThreadSafe
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@289177 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/acting/SetterAction.java
+++ src/java/org/apache/cocoon/acting/SetterAction.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2005 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,7 @@ import java.util.Map;
 import org.apache.avalon.framework.parameters.ParameterException;
 import org.apache.avalon.framework.parameters.Parameterizable;
 import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.cocoon.environment.Redirector;
 import org.apache.cocoon.environment.SourceResolver;
@@ -35,7 +36,7 @@ import org.apache.cocoon.environment.SourceResolver;
  */
 public class SetterAction
     extends AbstractAction
-    implements Parameterizable {
+    implements Parameterizable, ThreadSafe {
 
     public static final int MODE_OBJECT_MODEL = 1;
     public static final int MODE_REQUEST_ATTR = 2;
