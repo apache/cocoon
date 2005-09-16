40588cda2cda61869fdb5b52e7860e8b955e917f
Carsten Ziegeler <cziegeler@apache.org>
2005-09-16 07:40:04 +0000

    Fix CopletModule.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@289415 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/portal/java/org/apache/cocoon/portal/components/modules/input/CopletModule.java
+++ src/blocks/portal/java/org/apache/cocoon/portal/components/modules/input/CopletModule.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ * Copyright 1999-2005 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -88,16 +88,9 @@ extends AbstractModule {
                 return copletId;
             }
             JXPathContext jxpathContext = JXPathContext.newContext(portalService.getComponentManager().getProfileManager().getCopletInstanceData(copletId));
-            Object value = jxpathContext.getValue(name);
-                
-            if (value == null) {
-                return null;
-            }
-                
-            return value.toString();
-            
+            return jxpathContext.getValue(name);            
         } catch (ServiceException e) {
-            throw new ConfigurationException("ServiceException ", e);
+            throw new ConfigurationException("Unable to lookup portal service.", e);
         } finally {
             this.manager.release(portalService);
         }
