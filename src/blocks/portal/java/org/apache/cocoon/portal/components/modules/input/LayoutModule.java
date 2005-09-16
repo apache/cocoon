3f7fff686c55ccb11b6846e94f27c3fbb9166454
Carsten Ziegeler <cziegeler@apache.org>
2005-09-16 07:40:04 +0000

    Fix CopletModule.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@289415 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/portal/java/org/apache/cocoon/portal/components/modules/input/LayoutModule.java
+++ src/blocks/portal/java/org/apache/cocoon/portal/components/modules/input/LayoutModule.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ * Copyright 1999-2005 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -68,13 +68,11 @@ extends AbstractModule {
                 final JXPathContext jxpathContext = JXPathContext.newContext(layout);
                 value = jxpathContext.getValue(path);
             }
-            return value;
-            
+            return value;            
         } catch (ServiceException e) {
-            throw new ConfigurationException("ServiceException ", e);
+            throw new ConfigurationException("Unable to lookup portal service.", e);
         } finally {
             this.manager.release(portalService);
         }
     }
-
 }
