fa6bf0946c5eb674a5e024f037e49541bf151a64
Carsten Ziegeler <cziegeler@apache.org>
2005-09-27 18:05:39 +0000

    Move all pluto related stuff into the pluto package Clean up portal configuration Remove tabs in ProjectPathModule
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@292009 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/modules/input/ProjectPathModule.java
+++ src/java/org/apache/cocoon/components/modules/input/ProjectPathModule.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,77 +65,101 @@ import java.util.Vector;
  * </p>
  *
  */
-public class ProjectPathModule extends AbstractInputModule implements Configurable, ThreadSafe {
+public class ProjectPathModule
+    extends AbstractInputModule
+    implements Configurable, ThreadSafe {
 
-		protected static String PROJECT_PARAM_NAME = "uri-prefix";
-		protected static String PROJECT_PARAM_DEFAULT = "/";
+    protected static String PROJECT_PARAM_NAME = "uri-prefix";
+    protected static String PROJECT_PARAM_DEFAULT = "/";
 
-		protected String projectBase;
-		
-    final  static Vector returnNames;
+    protected String projectBase;
+	
+    final static Vector returnNames;
     static {
-			Vector tmp = new Vector();
-			tmp.add("relative");
-			tmp.add("path");
-			tmp.add("folder");
-			returnNames = tmp;
+        Vector tmp = new Vector();
+        tmp.add("relative");
+        tmp.add("path");
+        tmp.add("folder");
+        returnNames = tmp;
     }
 
+    /**
+     * @see org.apache.avalon.framework.configuration.Configurable#configure(org.apache.avalon.framework.configuration.Configuration)
+     */
     public void configure(Configuration conf) throws ConfigurationException {
-			this.projectBase = conf.getChild(PROJECT_PARAM_NAME).getValue();
-			if (getLogger().isDebugEnabled()) getLogger().debug("Configuration supplied: " + this.projectBase);
-			if (this.projectBase == null) {
-       	this.projectBase = PROJECT_PARAM_DEFAULT;
-				if (getLogger().isWarnEnabled()) getLogger().warn("No configuration supplied, using default: " + PROJECT_PARAM_DEFAULT);
-       }
-			if (this.projectBase.equals("")) {
-       	this.projectBase = PROJECT_PARAM_DEFAULT;
-				if (getLogger().isWarnEnabled()) getLogger().warn("Empty configuration supplied, using default: " + PROJECT_PARAM_DEFAULT);
-      }
+        this.projectBase = conf.getChild(PROJECT_PARAM_NAME).getValue();
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("Configuration supplied: " + this.projectBase);
+        }
+        if (this.projectBase == null) {
+       	    this.projectBase = PROJECT_PARAM_DEFAULT;
+            if (getLogger().isWarnEnabled()) {
+                getLogger().warn("No configuration supplied, using default: " + PROJECT_PARAM_DEFAULT);
+            }
+        }
+        if (this.projectBase.equals("")) {
+            this.projectBase = PROJECT_PARAM_DEFAULT;
+            if (getLogger().isWarnEnabled()) {
+                getLogger().warn("Empty configuration supplied, using default: " + PROJECT_PARAM_DEFAULT);
+            }
+        }
     }
 
-    public Object getAttribute(String name, Configuration modeConf, Map objectModel) throws ConfigurationException {
-			String uri = ObjectModelHelper.getRequest(objectModel).getServletPath();
-			StringBuffer result = new StringBuffer(uri.length());
-			int baseIndex = uri.indexOf(this.projectBase);
-			if (baseIndex != -1) {
-				uri = uri.substring(baseIndex + this.projectBase.length());
-			} else {
-				throw new ConfigurationException( "No project-base path found in URI");
-			}
-			try {
-				// provide a relative path back to the project
-				if (name.startsWith("relative")) {
-					int nextIndex = 0;
-	        while ((nextIndex = uri.indexOf('/', nextIndex) + 1) > 0) {
-            result.append("../");
-	        }
-				} else if (name.startsWith("path")) { // provide the full path from the project
-					result.append("/");
-					result.append(uri);
-				} else if (name.startsWith("folder")) { // provide the folder path from the project
-					result.append("/");
-					result.append(uri.substring(0,uri.lastIndexOf("/") + 1));
-				} else {
-					if (getLogger().isWarnEnabled()) getLogger().warn("Invalid verb: " + name);
-				}
-				return result;
-			} catch( final Exception mue ) {
-				throw new ConfigurationException( "Problems resolving project path.", mue);
-			}
+    /**
+     * @see org.apache.cocoon.components.modules.input.InputModule#getAttribute(java.lang.String, org.apache.avalon.framework.configuration.Configuration, java.util.Map)
+     */
+    public Object getAttribute(String name, Configuration modeConf, Map objectModel)
+    throws ConfigurationException {
+        String uri = ObjectModelHelper.getRequest(objectModel).getServletPath();
+        StringBuffer result = new StringBuffer(uri.length());
+        int baseIndex = uri.indexOf(this.projectBase);
+        if (baseIndex != -1) {
+            uri = uri.substring(baseIndex + this.projectBase.length());
+        } else {
+            throw new ConfigurationException( "No project-base path found in URI");
+        }
+        try {
+            // provide a relative path back to the project
+            if (name.startsWith("relative")) {
+                int nextIndex = 0;
+                while ((nextIndex = uri.indexOf('/', nextIndex) + 1) > 0) {
+                    result.append("../");
+                }
+            } else if (name.startsWith("path")) {
+                // provide the full path from the project
+                result.append("/");
+                result.append(uri);
+            } else if (name.startsWith("folder")) {
+                // provide the folder path from the project
+                result.append("/");
+                result.append(uri.substring(0,uri.lastIndexOf("/") + 1));
+            } else {
+                if (getLogger().isWarnEnabled()) {
+                    getLogger().warn("Invalid verb: " + name);
+                }
+            }
+            return result;
+        } catch( final Exception mue ) {
+            throw new ConfigurationException( "Problems resolving project path.", mue);
+        }
     }
 
-    public Iterator getAttributeNames( Configuration modeConf, Map objectModel ) throws ConfigurationException {
-			return ProjectPathModule.returnNames.iterator();
+    /**
+     * @see org.apache.cocoon.components.modules.input.InputModule#getAttributeNames(org.apache.avalon.framework.configuration.Configuration, java.util.Map)
+     */
+    public Iterator getAttributeNames( Configuration modeConf, Map objectModel )
+    throws ConfigurationException {
+        return ProjectPathModule.returnNames.iterator();
     }
 
-
+    /**
+     * @see org.apache.cocoon.components.modules.input.InputModule#getAttributeValues(java.lang.String, org.apache.avalon.framework.configuration.Configuration, java.util.Map)
+     */
     public Object[] getAttributeValues( String name, Configuration modeConf, Map objectModel )
-        throws ConfigurationException {
+    throws ConfigurationException {
+        List values = new LinkedList();
+        values.add( this.getAttribute(name, modeConf, objectModel) );
 
-			List values = new LinkedList();
-			values.add( this.getAttribute(name, modeConf, objectModel) );
-
-			return values.toArray();
+        return values.toArray();
     }
 }
