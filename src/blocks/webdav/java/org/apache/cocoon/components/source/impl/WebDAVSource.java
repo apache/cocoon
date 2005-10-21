b0b6c8db4a36117e06fb82bd6c10ca244357ab0c
Joerg Heinick <joerg@apache.org>
2005-10-21 21:00:52 +0000

    improved WebDAVSource error message (bug 36371, thanks to Jean-Baptiste Quenot)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@327579 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/webdav/java/org/apache/cocoon/components/source/impl/WebDAVSource.java
+++ src/blocks/webdav/java/org/apache/cocoon/components/source/impl/WebDAVSource.java
@@ -225,8 +225,8 @@ public class WebDAVSource extends AbstractLogEnabled
             if (e.getReasonCode() == HttpStatus.SC_NOT_FOUND) {
                 throw new SourceNotFoundException("Not found: " + getSecureURI(), e);
             }
-            final String msg = "Could not initialize webdav resource. Server responded "
-                + e.getReasonCode() + " (" + e.getReason() + ") - " + e.getMessage();
+            final String msg = "Could not initialize webdav resource at " + getSecureURI()
+                + ". Server responded " + e.getReasonCode() + " (" + e.getReason() + ") - " + e.getMessage();
             throw new SourceException(msg, e);
        } catch (IOException e) {
             throw new SourceException("Could not initialize webdav resource", e);
