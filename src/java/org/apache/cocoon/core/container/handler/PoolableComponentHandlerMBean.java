a8d7cfc844916d76b13fbbec6d9a2f423a1f1d86
Giacomo Pati <giacomo@apache.org>
2005-12-24 13:30:48 +0000

    added MBeans
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@358932 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/cocoon/core/container/handler/PoolableComponentHandlerMBean.java
@@ -0,0 +1,93 @@
+/* 
+ * Copyright 2002-2005 The Apache Software Foundation
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.core.container.handler;
+
+
+import org.mortbay.util.jmx.ModelMBeanImpl;
+
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.modelmbean.InvalidTargetObjectTypeException;
+
+/**
+ * The ThreadSafeComponentHandlerMBean adds JMX managability for ThreadSafeComponentHandler.
+ *
+ * @version $Id: ThreadSafeComponentHandler.java 312637 2005-10-10 13:00:42Z cziegeler $
+ * @since 2.2
+ */
+public class PoolableComponentHandlerMBean
+extends ModelMBeanImpl {
+    
+    private PoolableComponentHandler handler;
+    
+    protected void defineManagedResource() {
+        super.defineManagedResource();
+        defineAttribute("interfaces", false, true);
+        defineAttribute("defaultMaxPoolSize", false, true);
+        defineAttribute("maxPoolSize", false, true);
+        defineAttribute("readyPoolSize", false, true);
+        defineAttribute("totalPoolSize", false, true);
+    }
+    /**
+     * Construction of PoolableComponentHandlerMBean
+     *
+     * @param handler The managed PoolableComponentHandler instance
+     */
+    public PoolableComponentHandlerMBean(final PoolableComponentHandler handler)
+        throws MBeanException, InstanceNotFoundException {
+        super( handler );
+        this.handler = handler;
+        try
+        {
+            super.setManagedResource( handler, "objectReference" );
+        }
+        catch( final InvalidTargetObjectTypeException e )
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    public String[] getInterfaces()
+    {
+        final String [] ifaces = new String[this.handler.interfaces.length];
+        for(int i = 0; i < ifaces.length; i++) {
+            ifaces[i] = this.handler.interfaces[i].getName();
+        }
+        return ifaces;
+    }
+
+    public int getDefaultMaxPoolSize()
+    {
+        return handler.DEFAULT_MAX_POOL_SIZE;
+    }
+
+    public int getMaxPoolSize()
+    {
+        return handler.max;
+    }
+
+    public int getReadyPoolSize()
+    {
+        return handler.readySize;
+    }
+
+    public int getTotalPoolSize()
+    {
+        return handler.size;
+    }
+
+}
