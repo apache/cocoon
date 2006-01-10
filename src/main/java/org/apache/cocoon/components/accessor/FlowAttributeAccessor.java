57620afe46f774d98217be6300e62610cac7b8a1
Par Daniel Fagerstrohm <danielf@apache.org>
2006-01-10 19:14:52 +0000

    Flattening the template block, part 9
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/cocoon-template/cocoon-template-impl@367742 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/cocoon/components/accessor/FlowAttributeAccessor.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 1999-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.components.accessor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.cocoon.components.flow.FlowHelper;
+import org.apache.cocoon.environment.TemplateObjectModelHelper;
+
+/**
+ * @version SVN $Id$
+ */
+public class FlowAttributeAccessor extends ObjectModelAccessor {
+
+    public Object getObject() {
+        Map map = new HashMap();
+	Object contextObject = FlowHelper.getContextObject(getObjectModel());
+
+        if (contextObject instanceof Map) {
+            map.putAll((Map)contextObject);
+        } else if (contextObject != null) {
+            TemplateObjectModelHelper.fillContext(contextObject, map);
+        }
+        
+        return map;
+    }
+}
