e4c99976f53dc0c6f0e9f9239cdc1b69b7b2e0ea
Giacomo Pati <giacomo@apache.org>
2006-01-12 19:12:37 +0000

    move deployer to trunk
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@368442 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cocoon-deployer/src/test/java/org/apache/cocoon/deployer/BlockDeployerTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.deployer;
+
+import java.io.File;
+import java.io.FileReader;
+
+import org.apache.cocoon.deployer.applicationserver.CocoonServer22;
+import org.apache.cocoon.deployer.generated.deploy.x10.Deploy;
+import org.apache.cocoon.deployer.logger.ConsoleLogger;
+import org.apache.cocoon.deployer.resolver.NullVariableResolver;
+import org.easymock.MockControl;
+
+public class BlockDeployerTest extends AbstractDeployerTestCase {
+
+	public void testDeploy() throws Exception {
+		BlockDeployer blockDeployer = new BlockDeployer(
+				this.getArtifactProviderInstance(),
+				new NullVariableResolver(),
+				new ConsoleLogger());
+		blockDeployer.deploy(
+			(Deploy) Deploy.unmarshal(new FileReader(this.getMockArtefact("validDeploy-04/deploy.xml"))));
+		System.out.println("x");
+		// assertions: ...
+		
+	}
+	
+	private ArtifactProvider getArtifactProviderInstance() {
+		MockControl aProviderCtrl = MockControl.createControl(ArtifactProvider.class);
+		ArtifactProvider aProvider = (ArtifactProvider) aProviderCtrl.getMock();
+		
+		// server
+		aProvider.getArtifact(CocoonServer22.COCOON_22_SERVER_ARTIFACT);
+		File vanillaCocoonServerArtifact = this.getMockArtefact("validVanillaCocoon22App/appRoot.zip");
+		assertTrue(vanillaCocoonServerArtifact.exists());		
+		aProviderCtrl.setReturnValue(vanillaCocoonServerArtifact);
+		
+		aProvider.getArtifact("mvn:validBlock-03:validBlock-03:1.0");
+		aProviderCtrl.setReturnValue(this.getMockArtefact("validBlock-03/valid-block-1.0.jar"));
+		
+		aProvider.getArtifact("mvn:validBlock-04:validBlock-04:1.0");
+		aProviderCtrl.setReturnValue(this.getMockArtefact("validBlock-04/valid-block-1.0.jar"));		
+		
+		aProvider.getArtifact("mvn:anyblock:anyblock-05:1.0");
+		aProviderCtrl.setReturnValue(this.getMockArtefact("validBlock-05/valid-block-1.0.jar"));
+		
+		aProvider.getArtifact("mvn:anyblock:anyblock-06:1.0");
+		aProviderCtrl.setReturnValue(this.getMockArtefact("validBlock-06/valid-block-1.0.jar"));		
+		
+		aProvider.getArtifact("mvn:anyblock:anyblock-07:1.0");
+		aProviderCtrl.setReturnValue(this.getMockArtefact("validBlock-07/valid-block-1.0.jar"));			
+		
+		aProviderCtrl.replay();
+		return aProvider;
+	}	
+	
+	public void testAutoWiringResolving() {
+
+	}
+	
+}
---
+++ cocoon-deployer/src/test/java/org/apache/cocoon/deployer/block/BlockFactoryTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.deployer.block;
+
+import java.io.File;
+
+import org.apache.cocoon.deployer.AbstractDeployerTestCase;
+
+public class BlockFactoryTest extends AbstractDeployerTestCase {
+	
+	private static final String VALID_BLOCK_01_JAR = "validBlock-01/valid-block-1.0.jar";
+	private static final String VALID_DEPLOY_01 = "validDeploy-01/deploy.xml";
+
+	/**
+	 * Test if the passed block is a file and not a directory
+	 */
+	public void testBlockIsFile() throws Exception {
+		File blockAsFile;
+		
+		// valid deployment descriptor
+		org.apache.cocoon.deployer.generated.deploy.x10.Block blockDeployDesc = 
+			this.getDeploy(VALID_DEPLOY_01).getBlock(0);
+		
+		
+		// valid block
+		blockAsFile = this.getMockArtefact(VALID_BLOCK_01_JAR);
+		BlockFactory.createBinaryBlock(blockAsFile, blockDeployDesc);
+		
+		// block exists
+		try {
+			blockAsFile = this.getMockArtefact("validBlock-01/valid-block-1.0.ja__");
+			BlockFactory.createBinaryBlock(blockAsFile, blockDeployDesc);
+			fail("block must exist");
+		} catch(IllegalArgumentException iae) {
+			// expected
+		}
+		
+		// block not null
+		try {
+			BlockFactory.createBinaryBlock(null, null);
+			fail("null block argument mustn't be allowed");
+		} catch(IllegalArgumentException iae) {
+			// expected
+		}	
+		
+		// deploydesc not null
+		blockAsFile = this.getMockArtefact(VALID_BLOCK_01_JAR);		
+		try {
+			BlockFactory.createBinaryBlock(blockAsFile, null);
+			fail("a block descriptor MUST be provided");
+		} catch(IllegalArgumentException iae) {
+			// excpected
+		}
+		
+	}
+	
+	public void testBlockCreation() throws Exception {
+		File blockAsFile = this.getMockArtefact(VALID_BLOCK_01_JAR);
+		BinaryBlock block = BlockFactory.createBinaryBlock(blockAsFile, this.getDeploy(VALID_DEPLOY_01).getBlock(0));
+		assertNotNull(block);
+		assertNotNull(block.getBlockDescriptor());
+		Object o = block.getBlockDescriptor();
+		org.apache.cocoon.deployer.generated.block.x10.Block b = (org.apache.cocoon.deployer.generated.block.x10.Block) o;
+		assertEquals(b.getId(), "http://cocoon.apache.org/blocks/anyblock/1.0");
+
+		assertNotNull(block.getInputStream());
+		assertEquals( Block.BLOCK_NS_10, block.getNamespace());
+		assertEquals("http://cocoon.apache.org/blocks/anyblock/1.0", block.getId());
+	}
+
+}
