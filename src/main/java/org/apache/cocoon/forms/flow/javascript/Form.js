15448689e08e415840ef2e21cbc43b2701874a23
Jean-Baptiste Quenot <jbq@apache.org>
2006-01-20 14:49:52 +0000

    * Fix COCOON-1720 (Form.js overwrites the CFormsInstance attribute) by moving   the assignment of viewdata[CFormsInstance] above the sendPage() * Add the sendForm() function, same as showForm() but without creating a continuation * Add the sendFormAndWait() function, alias for showForm()
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/cocoon-forms/cocoon-forms-impl@370831 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/cocoon/forms/flow/javascript/Form.js
+++ src/main/java/org/apache/cocoon/forms/flow/javascript/Form.js
@@ -105,25 +105,41 @@ Form.prototype.lookupWidget = function(path) {
  * @parameter ttl the time to live of the continuation used to display the form
  */
 Form.prototype.showForm = function(uri, viewdata, ttl) {
+    this.sendFormAndWait(uri, viewdata, ttl)
+}
 
-    if (viewdata == undefined) viewdata = new Object();
+Form.prototype.sendForm = function(uri, viewdata) {
+    this.buildViewData(viewdata)
+    cocoon.sendPage(uri, viewdata);
+
+    // Clean up after sending the page
+    if (this.cleanupHook) {
+        this.cleanupHook(this);
+    }
+
+    FOM_Cocoon.suicide();
+}
+
+Form.prototype.buildViewData = function(viewdata) {
     viewdata[Packages.org.apache.cocoon.forms.transformation.FormsPipelineConfig.CFORMSKEY] = this.form;
 
     if (this.locale == null)
         this.locale = java.util.Locale.getDefault();
     viewdata["locale"] = this.locale;
+}
 
+Form.prototype.sendFormAndWait = function(uri, viewdata, ttl) {
     var finished = false;
 
     var comingBack = false;
     var bookmark = cocoon.createWebContinuation(ttl);
-    
+
     // Attach the form to the continuation so that we can access by just knowing the continuation id
     bookmark.setAttribute("form", this.form);
 
     if (comingBack) {
         // We come back to the bookmark: process the form
-        
+
         if (finished && cocoon.request.getParameter("cocoon-ajax-continue") != null) {
             // A request with this parameter is sent by the client upon receiving the indication
             // that Ajax interaction on the form is finished (see below).
@@ -131,10 +147,11 @@ Form.prototype.showForm = function(uri, viewdata, ttl) {
             // faulty or hacked request. It's set to false, this will simply redisplay the form.
             return bookmark;
         }
-        
-	    if (this.restoreHook) {
-	        this.restoreHook(this);
-	    }
+
+        if (this.restoreHook) {
+            this.restoreHook(this);
+        }
+
         var formContext = new Packages.org.apache.cocoon.forms.FormContext(cocoon.request, this.locale);
 
         // Prematurely add the viewdata as in the object model so that event listeners can use it 	 
@@ -144,12 +161,13 @@ Form.prototype.showForm = function(uri, viewdata, ttl) {
         org.apache.cocoon.components.flow.FlowHelper.setContextObject(objectModel, viewdata); 	 
 
         finished = this.form.process(formContext);
+
         if (finished) {
             this.isValid = this.form.isValid();
             var widget = this.form.getSubmitWidget();
             // Can be null on "normal" submit
             this.submitId = widget == null ? null : widget.getId();
-            
+
             if (cocoon.request.getParameter("cocoon-ajax") != null) {
                 // Ask the client to issue a new request reloading the whole page.
                 // As we have nothing special to send back, so a header should be just what we need...
@@ -161,6 +179,7 @@ Form.prototype.showForm = function(uri, viewdata, ttl) {
                 // in the sitemap for just sending constant response, which isn't nice.
                 cocoon.sendStatus(200);
                 var httpResponse = objectModel.get(org.apache.cocoon.environment.http.HttpEnvironment.HTTP_RESPONSE_OBJECT);
+
                 if (httpResponse) {
                     httpResponse.setContentType("text/xml");
                     var text = "<?xml version='1.0'?><bu:document xmlns:bu='" +
@@ -175,18 +194,20 @@ Form.prototype.showForm = function(uri, viewdata, ttl) {
 
                 FOM_Cocoon.suicide();
             }
-            
+
             return bookmark;
         }
     }
+
     comingBack = true;
+    this.buildViewData(viewdata)
     cocoon.sendPage(uri, viewdata, bookmark);
-    
+
     // Clean up after sending the page
     if (this.cleanupHook) {
         this.cleanupHook(this);
     }
-    
+
     FOM_Cocoon.suicide();
 }
 
@@ -202,6 +223,7 @@ Form.prototype.createBinding = function(bindingURI) {
     var bindingManager = null;
     var source = null;
     var resolver = null;
+
     try {
         bindingManager = cocoon.getComponent(Packages.org.apache.cocoon.forms.binding.BindingManager.ROLE);
         resolver = cocoon.getComponent(Packages.org.apache.cocoon.environment.SourceResolver.ROLE);
@@ -218,12 +240,14 @@ Form.prototype.createBinding = function(bindingURI) {
 Form.prototype.load = function(object) {
     if (this.binding == null)
         throw new Error("Binding not configured for this form.");
+
     this.binding.loadFormFromModel(this.form, object);
 }
 
 Form.prototype.save = function(object) {
     if (this.binding == null)
         throw new Error("Binding not configured for this form.");
+
     this.binding.saveFormToModel(this.form, object);
 }
 
@@ -242,12 +266,14 @@ Form.prototype.removeAttribute = function(name) {
 Form.prototype.getXML = function() {
     if (this.xmlAdapter == null)
         this.xmlAdapter = new Packages.org.apache.cocoon.forms.util.XMLAdapter(this.form);
+
     return this.xmlAdapter;
 }
 
 Form.prototype.loadXML = function(uri) {
     var source = null;
     var resolver = null;
+
     try {
         resolver = cocoon.getComponent(Packages.org.apache.cocoon.environment.SourceResolver.ROLE);
         source = resolver.resolveURI(uri);
@@ -255,6 +281,7 @@ Form.prototype.loadXML = function(uri) {
     } finally {
         if (source != null)
             resolver.release(source);
+
         cocoon.releaseComponent(resolver);
     }
 }
@@ -286,7 +313,9 @@ Form.prototype.saveXML = function(uri) {
     } finally {
         if (source != null)
             resolver.release(source);
+
         cocoon.releaseComponent(resolver);
+
         if (outputStream != null) {
             try {
                 outputStream.flush();
@@ -313,18 +342,22 @@ function handleForm() {
     var form = new Form(def);
     // set the binding on the form if there is one
     var bindingURI = cocoon.parameters["bindingURI"];
+
     if (bindingURI != null) {
         form.createBinding(bindingURI);
     }
+
     // get the function to call to handle the form
     var funcName = cocoon.parameters["function"];
     var func = this[funcName];
+
     // check the function exists
     if (!func) {
         throw "Function \"" + funcName + "\" is not defined.";
     } else if (!(func instanceof Function)) {
         throw "\"" + funcName + "\" is not a function.";
     }
+
     // call the function
     func.apply(this, [form]);
 }
