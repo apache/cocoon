3228caaa57fc4173ea29ec5d62add07f294d1a11
Par Daniel Fagerstrohm <danielf@apache.org>
2006-01-22 21:51:22 +0000

    Factored out the creation of the blocks service manager from BlockManager and prepared to make the choice of service manager configurable. The SitemapServlet creates an own Avalon Context from a Core object that it gets from its parent service manager and its ServletContext, in this way it doesn't need to be Contextualizable anymore. The setup of the block servlet manager (now in ECMBlockServiceManager), is changed so that there is no need to mark one of the component managers as core anymore. Each block has an own Core object.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@371399 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cocoon-blocks-fw/cocoon-blocks-fw-impl/src/main/java/org/apache/cocoon/container/ECMBlockServiceManager.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright 1999-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.container;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+
+import org.apache.avalon.framework.activity.Initializable;
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.configuration.DefaultConfiguration;
+import org.apache.avalon.framework.context.Context;
+import org.apache.avalon.framework.logger.LogEnabled;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.cocoon.blocks.BlockConstants;
+import org.apache.cocoon.blocks.util.CoreUtil;
+import org.apache.cocoon.components.LifecycleHelper;
+import org.apache.cocoon.components.container.CocoonServiceManager;
+import org.apache.cocoon.components.source.impl.ContextSourceFactory;
+import org.apache.cocoon.core.Core;
+import org.apache.cocoon.core.Settings;
+import org.apache.cocoon.core.container.CoreServiceManager;
+import org.apache.cocoon.core.container.RoleManager;
+import org.apache.cocoon.core.container.RoleManagerOwner;
+import org.apache.cocoon.core.source.SimpleSourceResolver;
+import org.apache.excalibur.source.SourceFactory;
+import org.apache.excalibur.source.impl.ResourceSourceFactory;
+import org.apache.excalibur.source.impl.URLSourceFactory;
+
+/**
+ * @version $Id$
+ */
+public class ECMBlockServiceManager
+implements ServiceManager, LogEnabled, Configurable, Serviceable, Initializable, RoleManagerOwner {
+
+    ServletConfig servletConfig;
+    
+    Logger logger;
+    Configuration configuration;
+    ServiceManager parentServiceManager;
+    ServiceManager serviceManager;
+    
+    // Wiring
+
+    public void setServletConfig(ServletConfig servletConfig) {
+        this.servletConfig = servletConfig;        
+    }
+
+    // Life cycle methods
+    
+    public void enableLogging(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void configure(Configuration configuration) throws ConfigurationException {
+        this.configuration = configuration; 
+    }
+
+    public void service(ServiceManager parentServiceManager) throws ServiceException {
+        this.parentServiceManager = parentServiceManager;
+    }
+
+    public void initialize() throws Exception {
+        ServletContext servletContext = this.servletConfig.getServletContext();
+
+        Settings settings = CoreUtil.createSettings(this.servletConfig);
+        Context newContext =
+            CoreUtil.createContext(this.servletConfig, settings, BlockConstants.BLOCK_CONF); 
+        String contextURL = CoreUtil.getContextURL(servletContext, BlockConstants.BLOCK_CONF);
+        String confLocation = contextURL + "::";
+
+        CocoonServiceManager coreServicemanager =
+            new CocoonServiceManager(this.parentServiceManager);
+        LifecycleHelper.setupComponent(
+                this.serviceManager,
+                this.getLogger(),
+                newContext,
+                null,
+                null,
+                false);
+        
+        // The Core object is needed for the Sitemap and various other Cocoon components
+        Core core = new Core(settings, newContext);
+        coreServicemanager.addInstance(Core.ROLE, core);
+        
+        // For the first block that is setup, source factories need to be setup before
+        // starting to configure service managers that includes other configuration files
+        // TODO This is really complex and should be simplified
+        String resourceSourceSelector = SourceFactory.ROLE + "/resource";
+        if (!this.parentServiceManager.hasService(resourceSourceSelector)) {
+            ResourceSourceFactory resourceFactory = new ResourceSourceFactory();
+            resourceFactory.enableLogging(this.getLogger());
+            coreServicemanager.addInstance(resourceSourceSelector, resourceFactory);
+        }
+        String urlSourceSelector = SourceFactory.ROLE + "/*";
+        if (!this.parentServiceManager.hasService(urlSourceSelector)) {
+            URLSourceFactory urlFactory = new URLSourceFactory();
+            urlFactory.enableLogging(getLogger());
+            coreServicemanager.addInstance(urlSourceSelector, urlFactory);
+        }
+        String contextSourceSelector = SourceFactory.ROLE + "/context";
+        if (!this.parentServiceManager.hasService(contextSourceSelector)) {
+            SimpleSourceResolver sourceResolver = new SimpleSourceResolver();
+            LifecycleHelper.setupComponent(
+                    sourceResolver,
+                    this.getLogger(),
+                    newContext,
+                    null,
+                    null);
+            ContextSourceFactory contextFactory = new ContextSourceFactory();
+            LifecycleHelper.setupComponent(
+                    contextFactory,
+                    this.getLogger(),
+                    newContext,
+                    new SimpleSourceResolver.SimpleServiceManager(sourceResolver),
+                    null);
+            coreServicemanager.addInstance(contextSourceSelector, contextFactory);
+        }
+        
+        coreServicemanager.initialize();
+        this.serviceManager = coreServicemanager;
+
+        // The source resolver must be defined in this service
+        // manager, otherwise the root path will be the one from the
+        // parent manager, we add a resolver to get it right. If the
+        // components section contain includes the CoreComponentManager
+        // use the location of the configuration an the parent SourceResolver
+        // for resolving the include.
+        DefaultConfiguration sourceManagerConf =
+            new DefaultConfiguration("components", confLocation);
+        
+        // FIXME: Need a local role manager as it is not inherited through the InterBlockServiceManager 
+        DefaultConfiguration roleInclude =
+            new DefaultConfiguration("include");
+        roleInclude.setAttribute("src", "resource://org/apache/cocoon/cocoon.roles");
+        sourceManagerConf.addChild(roleInclude);
+        DefaultConfiguration resolverConf =
+            new DefaultConfiguration("source-resolver");
+        sourceManagerConf.addChild(resolverConf);
+        ServiceManager sourceResolverSM =
+            new CoreServiceManager(this.serviceManager);
+        LifecycleHelper.setupComponent(
+                sourceResolverSM,
+                this.getLogger(),
+                newContext,
+                null,
+                sourceManagerConf);
+        
+        this.serviceManager =
+            sourceResolverSM;
+        
+        // Create a service manager with the exposed components of the block
+        if (this.configuration != null) {
+            DefaultConfiguration componentConf =
+                new DefaultConfiguration("components", confLocation);
+            componentConf.addAll(this.configuration);
+            this.serviceManager = new CocoonServiceManager(this.serviceManager);
+            LifecycleHelper.setupComponent(this.serviceManager,
+                    this.getLogger(),
+                    newContext,
+                    null,
+                    componentConf);
+        }
+    }
+    
+    // service manager methods
+    public Object lookup(String role) throws ServiceException {
+        return this.serviceManager.lookup(role);
+    }
+
+    public boolean hasService(String role) {
+        return this.serviceManager.hasService(role);
+    }
+
+    public void release(Object component) {
+        this.serviceManager.release(component);
+    }
+
+    // logger
+
+    public RoleManager getRoleManager() {
+        return ((RoleManagerOwner)this.serviceManager).getRoleManager();
+    }
+
+    private Logger getLogger() {
+        return this.logger;
+    }
+}
