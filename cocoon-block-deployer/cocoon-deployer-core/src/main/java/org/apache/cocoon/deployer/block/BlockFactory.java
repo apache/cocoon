f62feaad4126651ac181ad881dfd68b538c271ea
Reinhard Pötz <reinhard@apache.org>
2006-02-10 08:38:19 +0000

    COCOON-1759
    The deployer now supports relative locations and translates
    them at deployment into paths that are relative to the Cocoon
    install directory
    
     - added a second block type: LocalBlock
       This type only has a location and at deployment the 
       deployer does not installit
     - relative path calculation is covered by unit tests
    
    Note: The block-fw needs some changes as it currently resolves all
    block locations relative to the servlet context and if a
    block is outside this results in an exception as 
    servletContext.resolve("../foo") returns null
    
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@376592 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-block-deployer/cocoon-deployer-core/src/main/java/org/apache/cocoon/deployer/block/BlockFactory.java
+++ cocoon-block-deployer/cocoon-deployer-core/src/main/java/org/apache/cocoon/deployer/block/BlockFactory.java
@@ -18,11 +18,14 @@ package org.apache.cocoon.deployer.block;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 import org.apache.cocoon.deployer.DeploymentException;
 import org.apache.cocoon.deployer.block.impl.Block10;
+import org.apache.cocoon.deployer.block.impl.LocalBlock10;
 import org.apache.cocoon.deployer.generated.block.x10.Block;
 import org.apache.cocoon.deployer.util.XMLUtils;
 import org.apache.cocoon.deployer.util.ZipUtils;
@@ -50,10 +53,12 @@ public class BlockFactory {
 			Unmarshaller unmarshaller = new Unmarshaller(Block.class);
 			// unmarshaller.setDebug(true);
 			blockDescriptor = (Block) unmarshaller.unmarshal(new InputStreamReader(ZipUtils.getBlockDescriptorIs(blockArchive)));
+			
 			block.setBlockDescriptor(blockDescriptor);
 			block.setInputStream(new FileInputStream(blockArchive));
 			block.setNameSpace(XMLUtils.getDocumentNamespace(ZipUtils.getBlockDescriptorIs(blockArchive)));
 			block.setId(blockDescriptor.getId());
+		
 		} catch (FileNotFoundException e) {
 			throw new DeploymentException("The block archive file can't be found.");
 		} catch (IOException e) {
@@ -79,6 +84,103 @@ public class BlockFactory {
 		deployBlock.setUrn(block.getBlockDescriptor().getId());
 		
 		return block;
+	}
+
+	public static LocalBlock createLocalBlock(org.apache.cocoon.deployer.generated.deploy.x10.Block installBlock, 
+			String cocoonTargetUrl) {
+		
+		Validate.notNull(installBlock, "You have to pass a block deployment configuration.");	
+		
+		LocalBlock10 localBlock = new LocalBlock10();
+		localBlock.setDeployDescriptor(installBlock);
+		
+		// get and set the descriptor
+		Block blockDescriptor = null;
+		
+		try {
+			String location = installBlock.getLocation();
+			Validate.notNull(location, "The location (attribute 'location' of the element 'block') of the block mustn't be null");
+			
+			File blockDescriptorFile = new File(location + File.separator + 
+			    org.apache.cocoon.deployer.block.Block.BLOCK_DESCRIPTOR_LOCATION);
+			
+			System.out.println("blockDescriptorFile: " + blockDescriptorFile.getAbsolutePath());
+			
+			if(!blockDescriptorFile.exists()) {
+			    throw new DeploymentException("The block descriptor file can't be found: " + 
+			        blockDescriptorFile.getAbsolutePath());
+			}
+			
+			Unmarshaller unmarshaller = new Unmarshaller(Block.class);
+			// unmarshaller.setDebug(true);			
+			blockDescriptor = (Block) unmarshaller.unmarshal(new FileReader(blockDescriptorFile));
+			
+			localBlock.setBlockDescriptor(blockDescriptor);
+			localBlock.setId(blockDescriptor.getId());
+			localBlock.setNameSpace(XMLUtils.getDocumentNamespace(new FileInputStream(blockDescriptorFile)));
+			
+			// calculate the location relative to the server root directory in the case that the
+			// location is relative itself
+			String blockPath = new File(location).getCanonicalPath();
+			String cocoonPath = new File(cocoonTargetUrl).getCanonicalPath();
+			localBlock.setBaseDirectory(createRelativeLocation(cocoonPath, blockPath));
+
+		} catch (MarshalException e) {
+			throw new DeploymentException("The block descriptor can't be read correctly.");
+		} catch (ValidationException e) {
+			throw new DeploymentException("The block descriptor can't be read correctly because of validation problems (XML schema).");
+		} catch (FileNotFoundException e) {
+			throw new DeploymentException("The block descriptor file can't be found.");
+		} catch (SAXException e) {
+			throw new DeploymentException("The block descriptor can't be read correctly.");
+		} catch (IOException e) {
+			throw new DeploymentException("The block descriptor can't be read correctly.");
+		}		
+
+		return localBlock;
+	}
+	
+	protected static String createRelativeLocation(final String cocoonLocation, final String blockLocation) {
+
+		String cleanedCocoonLocation = cocoonLocation.replaceAll("\\\\", "/");
+		String cleanedBlockLocation = blockLocation.replaceAll("\\\\", "/");		
+		String commonBaseDir = "";
+		
+		for(int i = 0; i < cleanedBlockLocation.length(); i++) {
+			String reducedBlockLocation = cleanedBlockLocation.substring(0, cleanedBlockLocation.length() - i);
+			if(cleanedCocoonLocation.indexOf(reducedBlockLocation) > -1) {
+				commonBaseDir = reducedBlockLocation.substring(0, reducedBlockLocation.lastIndexOf("/") + 1);
+				break;
+			}
+		}
+		
+		String relativeCocoonLocation = cleanedCocoonLocation.substring(commonBaseDir.length());
+		relativeCocoonLocation = relativeCocoonLocation.endsWith("/") ? relativeCocoonLocation : relativeCocoonLocation + "/";
+		
+		String relativeBlockLocation = cleanedBlockLocation.substring(commonBaseDir.length());
+		relativeBlockLocation = relativeBlockLocation.endsWith("/") ? relativeBlockLocation : relativeBlockLocation + "/";
+		
+		// if there is no common basedir, the relativeBlockLocation has to be absolute
+		if(commonBaseDir.equals("")) {
+			return relativeBlockLocation;
+		}
+		return relativizePath(relativeCocoonLocation) + relativeBlockLocation;
+	}
+	
+	/**
+	 * Use it to get ../../ out of e.g. x/y/
+	 * 
+	 * @param path The path to be relativized
+	 * @return the relativzed path
+	 */
+	protected static String relativizePath(final String path) {
+		StringTokenizer tokenizer = new StringTokenizer(path, "/");
+		StringBuffer relativizedPathSb = new StringBuffer();
+		while(tokenizer.hasMoreElements()) {
+			tokenizer.nextElement();
+			relativizedPathSb.append("../");
+		}
+		return relativizedPathSb.toString();
 	}
 	
 }
