80efff92ec895e4b7781637fddaa6474274deb50
Par Daniel Fagerstrohm <danielf@apache.org>
2006-02-27 22:23:34 +0000

    Preparing BlockContext for setter injection.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/cocoon-blocks-fw/cocoon-blocks-fw-impl@381466 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/cocoon/blocks/BlockContext.java
+++ src/main/java/org/apache/cocoon/blocks/BlockContext.java
@@ -47,10 +47,6 @@ public class BlockContext extends ServletContextWrapper {
     private Dictionary connections;
     private Dictionary properties;
     
-    public BlockContext(ServletContext parentContext) {
-        super(parentContext);
-    }
-
     /*
      * (non-Javadoc)
      * 
