b6b5b340c6cc50fda0c0af8d8d4d56c9eeebad3e
Carsten Ziegeler <cziegeler@apache.org>
2006-03-03 13:48:51 +0000

    Clean up method names and variable names Add support for beanpostprocessor beans
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@382811 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-core/src/main/java/org/apache/cocoon/core/container/spring/NameForAliasAware.java
+++ cocoon-core/src/main/java/org/apache/cocoon/core/container/spring/NameForAliasAware.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2005 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,11 +19,12 @@ package org.apache.cocoon.core.container.spring;
  * $Id$
  */
 public interface NameForAliasAware {
+
     /**
-     * Give the real component name given a alias name, return the given name if
+     * Give the real component name given an alias name, return the given name if
      * its not an alias.
-     * @param alias
-     * @return
+     * @param alias The alias for the component.
+     * @return The real component name.
      */
-    public String getNameForAlias(String alias);
+    String getNameForAlias(String alias);
 }
