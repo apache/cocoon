1de4fffaa4e473666899d7f8aab4fd6729c5c091
Bruno Dumon <bruno@apache.org>
2006-03-11 10:56:01 +0000

    Correction of commit "Sharing template block with 2.1: step 1": re-adding imports removed by accident.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/cocoon-template/cocoon-template-impl@385056 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/cocoon/template/instruction/FormatDate.java
+++ src/main/java/org/apache/cocoon/template/instruction/FormatDate.java
@@ -26,6 +26,7 @@ import org.apache.cocoon.template.environment.ErrorHolder;
 import org.apache.cocoon.template.environment.ExecutionContext;
 import org.apache.cocoon.template.environment.ParsingContext;
 import org.apache.cocoon.template.expression.JXTExpression;
+import org.apache.cocoon.template.expression.StringTemplateParser;
 import org.apache.cocoon.template.script.event.Event;
 import org.apache.cocoon.template.script.event.StartElement;
 import org.apache.cocoon.xml.XMLConsumer;
