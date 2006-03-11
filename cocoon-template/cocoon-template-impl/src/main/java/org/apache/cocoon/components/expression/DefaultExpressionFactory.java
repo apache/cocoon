876d5125e3fc1ee2fbca5eaefe266aad859030da
Outerthought - Bruno Dumon <bruno@apache.org>
2006-03-11 10:36:51 +0000

    Sharing template block with 2.1: step 2: make ExpressionFactory an interface
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@385052 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cocoon-template/cocoon-template-impl/src/main/java/org/apache/cocoon/components/expression/DefaultExpressionFactory.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 1999-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.components.expression;
+
+import org.apache.avalon.framework.activity.Disposable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.ServiceSelector;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.avalon.framework.thread.ThreadSafe;
+
+/**
+ * @version SVN $Id$
+ */
+public class DefaultExpressionFactory extends AbstractLogEnabled implements Disposable, Serviceable, ThreadSafe, ExpressionFactory {
+    public static String DEFAULT_EXPRESSION_LANGUAGE = "default";
+
+    /** The component manager */
+    protected ServiceManager manager;
+
+    /** The Expression compiler selector */
+    protected ServiceSelector compilerSelector;
+
+    public void service(final ServiceManager manager) throws ServiceException {
+        this.manager = manager;
+
+        this.compilerSelector = (ServiceSelector) this.manager.lookup(ExpressionCompiler.ROLE + "Selector");
+    }
+
+    public void dispose() {
+        if (null != this.manager) {
+            this.manager.release(this.compilerSelector);
+            this.compilerSelector = null;
+        }
+    }
+
+    public Expression getExpression(String language, String expression) throws ExpressionException {
+
+        Expression expressionImpl = null;
+        ExpressionCompiler compiler = null;
+        try {
+            compiler = (ExpressionCompiler) this.compilerSelector.select(language);
+            expressionImpl = compiler.compile(language, expression);
+        } catch (final ServiceException ce) {
+            throw new ExpressionException("Can't find a compiler for " + language);
+        } finally {
+            this.compilerSelector.release(compiler);
+        }
+        return expressionImpl;
+    }
+
+    public Expression getExpression(String expression) throws ExpressionException {
+        String language = DEFAULT_EXPRESSION_LANGUAGE;
+        int end = expression.indexOf(':');
+        if (end != -1) {
+            language = expression.substring(0, end);
+            expression = expression.substring(end + 1);
+        }
+        return getExpression(language, expression);
+    }
+}
