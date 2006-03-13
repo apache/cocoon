35f3e8fbdfa8fbc8bb0691d84e70a00bc524795d
Sylvain Wallez <sylvain@apache.org>
2006-03-13 10:18:24 +0000

    open FOM call context needed for suggestion lists
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@385493 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-core/src/main/java/org/apache/cocoon/components/flow/javascript/fom/FOM_Cocoon.java
+++ cocoon-core/src/main/java/org/apache/cocoon/components/flow/javascript/fom/FOM_Cocoon.java
@@ -206,7 +206,7 @@ public class FOM_Cocoon extends ScriptableObject {
         defineClass(scope, PageLocalImpl.class);
     }
 
-    void pushCallContext(FOM_JavaScriptInterpreter interp,
+    public void pushCallContext(FOM_JavaScriptInterpreter interp,
                          Redirector redirector,
                          ServiceManager manager,
                          Context avalonContext,
@@ -229,7 +229,7 @@ public class FOM_Cocoon extends ScriptableObject {
                                            logger, lastContinuation);
     }
 
-    void popCallContext() {
+    public void popCallContext() {
         // Clear the scope attribute
         FOM_JavaScriptFlowHelper.setFOM_FlowScope(this.getObjectModel(), null);
 
