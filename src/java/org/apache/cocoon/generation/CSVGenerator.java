6255002db44f4fc35b3734f1f4356f95e30a776e
David Crossley <crossley@apache.org>
2006-03-23 06:59:06 +0000

    Fix mangled copyright header.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@388072 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/generation/CSVGenerator.java
+++ src/java/org/apache/cocoon/generation/CSVGenerator.java
@@ -1,15 +1,18 @@
-/* =============================================================================== *
- * Copyright (C) 1999-2004, The Apache Software Foundation.   All rights reserved. *
- *                                                                                 *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not use *
- * this file except in  compliance with the License.  You may obtain a copy of the *
- * License at <http://www.apache.org/licenses/LICENSE-2.0>.                        *
- *                                                                                 *
- * Unless required by applicable law or agreed to in writing, software distributed *
- * under the License  is distributed on an  "AS IS"  BASIS,  WITHOUT WARRANTIES OR *
- * CONDITIONS  OF ANY KIND,  either express  or implied.  See the License  for the *
- * specific language governing permissions and limitations under the License.      *
- * =============================================================================== */
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.cocoon.generation;
 
 import java.io.BufferedReader;
@@ -95,8 +98,6 @@ import org.xml.sax.helpers.AttributesImpl;
  * different in cases.</p>
  *
  * @author <a href="mailto:pier@apache.org">Pier Fumagalli</a>
- * @author Copyright &copy; 2000-2004 <a href="http://www.apache.org/">The Apache
- *         Software Foundation</a>. All rights reserved.
  */
 public class CSVGenerator extends FileGenerator {
 
