6c04ea21eac3d20f76537b37c6ff77e6fa139e40
Poetz Reinhard <reinhard@apache.org>
2006-03-30 08:52:03 +0000

    convert blocks from javaflow-lucene
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@390035 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blocks/cocoon-jsp/cocoon-jsp-impl/src/main/java/org/apache/cocoon/components/jsp/JSPEngine.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.components.jsp;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * A component for loading and running Servlets and JSPs.
+ *
+ * @author <a href="mailto:dims@yahoo.com">Davanum Srinivas</a>
+ * @version $Id$
+ */
+public interface JSPEngine {
+
+    public static final String ROLE = JSPEngine.class.getName();
+    
+    /**
+     * Execute the Servlet/JSP and return the output.
+     * Output of the JSPEngine <b>must</b> be in UTF8 encoding.
+     * 
+     * @exception IOException
+     * @exception ServletException
+     * @exception Exception
+     */
+    public byte[] executeJSP(String url,
+                             HttpServletRequest request,
+                             HttpServletResponse response,
+                             ServletContext context)
+        throws IOException, ServletException, Exception;
+}
