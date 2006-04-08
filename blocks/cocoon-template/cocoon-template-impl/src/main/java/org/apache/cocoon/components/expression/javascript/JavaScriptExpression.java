49beff88d8692315aef88fff3e337d091df565b7
Carsten Ziegeler <cziegeler@apache.org>
2006-04-08 19:07:58 +0000

    Moving jcr and template block
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@392586 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blocks/cocoon-template/cocoon-template-impl/src/main/java/org/apache/cocoon/components/expression/javascript/JavaScriptExpression.java
@@ -0,0 +1,118 @@
+/*
+ * $Id$
+ *
+ * Created on 2005-10-17
+ *
+ * Copyright (c) 2005, MobileBox sp. z o.o.
+ * All rights reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.apache.cocoon.components.expression.javascript;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.io.StringReader;
+
+import org.apache.cocoon.components.expression.AbstractExpression;
+import org.apache.cocoon.components.expression.ExpressionContext;
+import org.apache.cocoon.components.expression.ExpressionException;
+import org.apache.cocoon.components.expression.jexl.JSIntrospector;
+import org.apache.cocoon.components.flow.FlowHelper;
+import org.apache.cocoon.environment.TemplateObjectModelHelper;
+import org.apache.commons.jexl.util.introspection.Info;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Script;
+import org.mozilla.javascript.Scriptable;
+
+public class JavaScriptExpression extends AbstractExpression {
+    private Script script;
+    private JSIntrospector introspector;
+
+    public JavaScriptExpression(String language, String expression) {
+        super(language, expression);
+        compile();
+    }
+
+    private void compile() {
+        Context ctx = Context.enter();
+        try {
+            // Note: used compileReader instead of compileString to work with the older Rhino in C2.1
+            this.script = ctx.compileReader(TemplateObjectModelHelper.getScope(), new StringReader(getExpression()), "", 1, null);
+        } catch (Exception e) {
+            // Note: this catch block is only needed for the Rhino in C2.1 where the older
+            //       Rhino does not throw RuntimeExceptions
+            if (e instanceof RuntimeException)
+                throw (RuntimeException)e;
+            else
+                throw new RuntimeException(e);
+        } finally {
+            Context.exit();
+        }
+    }
+
+    public Object evaluate(ExpressionContext context) throws ExpressionException {
+        Context ctx = Context.enter();
+        try {
+            Scriptable scope = ctx.newObject(TemplateObjectModelHelper.getScope());
+            // Populate the scope
+            Iterator iter = context.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry entry = (Map.Entry) iter.next();
+                String key = (String) entry.getKey();
+                Object value = entry.getValue();
+                scope.put(key, scope, Context.toObject(value, scope));
+            }
+
+            Object result = this.script.exec(ctx, scope);
+            return FlowHelper.unwrap(result);
+        } catch (Exception e) {
+            // Note: this catch block is only needed for the Rhino in C2.1 where the older
+            //       Rhino does not throw RuntimeExceptions
+            if (e instanceof RuntimeException)
+                throw (RuntimeException)e;
+            else
+                throw new RuntimeException(e);
+        } finally {
+            Context.exit();
+        }
+    }
+
+    public Iterator iterate(ExpressionContext context) throws ExpressionException {
+        Object result = evaluate(context);
+        if (result == null)
+            return EMPTY_ITER;
+
+        if (this.introspector == null)
+            introspector = new JSIntrospector();
+
+        Iterator iter = null;
+        try {
+            iter = introspector.getIterator(result, new Info("Unknown", 0, 0));
+        } catch (Exception e) {
+            throw new ExpressionException("Couldn't get an iterator from expression " + getExpression(), e);
+        }
+
+        if (iter == null)
+            iter = EMPTY_ITER;
+        return iter;
+    }
+
+    public void assign(ExpressionContext context, Object value) throws ExpressionException {
+        throw new UnsupportedOperationException("assignment not implemented for javascript expressions");
+    }
+
+    public Object getNode(ExpressionContext context) throws ExpressionException {
+        return evaluate(context);
+    }
+}
---
+++ blocks/cocoon-template/cocoon-template-impl/src/main/java/org/apache/cocoon/template/expression/DefaultStringTemplateParser.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 1999-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.template.expression;
+
+import java.io.Reader;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @version SVN $Id: DefaultStringTemplateParser.java 325973 2005-10-17
+ *          19:59:39Z lgawron $
+ */
+public class DefaultStringTemplateParser extends AbstractStringTemplateParser {
+    public List parseSubstitutions(Reader in) throws Exception {
+        LinkedList substitutions = new LinkedList();
+        StringBuffer buf = new StringBuffer();
+        buf.setLength(0);
+        int ch;
+        boolean inExpr = false;
+        top: while ((ch = in.read()) != -1) {
+            // column++;
+            char c = (char) ch;
+            processChar: while (true) {
+                if (inExpr) {
+                    if (c == '\\') {
+                        ch = in.read();
+                        buf.append(ch == -1 ? '\\' : (char) ch);
+                    } else if (c == '}') {
+                        String str = buf.toString();
+                        substitutions.add(compile(str));
+                        buf.setLength(0);
+                        inExpr = false;
+                    } else {
+                        buf.append(c);
+                    }
+                } else if (c == '{') {
+                    ch = in.read();
+                    if (ch != '{') {
+                        inExpr = true;
+                        if (buf.length() > 0) {
+                            substitutions.add(new Literal(buf.toString()));
+                            buf.setLength(0);
+                        }
+                        buf.append((char) ch);
+                        continue top;
+                    }
+                    buf.append(c);
+                    if (ch != -1) {
+                        c = (char) ch;
+                        continue processChar;
+                    }
+                } else {
+                    buf.append(c);
+                }
+                break;
+            }
+        }
+        if (inExpr)
+            throw new Exception("Unterminated {");
+
+        if (buf.length() > 0)
+            substitutions.add(new Literal(buf.toString()));
+        return substitutions;
+    }
+
+}
