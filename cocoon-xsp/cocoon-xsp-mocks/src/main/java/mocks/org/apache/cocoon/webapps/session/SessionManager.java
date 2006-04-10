cafef109b5d4164254c2c7373c2a2ab410a44c6e
Carsten Ziegeler <cziegeler@apache.org>
2006-04-10 17:34:36 +0000

    Fix poms
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/blocks@392997 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-xsp/cocoon-xsp-mocks/src/main/java/mocks/org/apache/cocoon/webapps/session/SessionManager.java
+++
@@ -1,9 +0,0 @@
-package org.apache.cocoon.webapps.session;
-
-import org.w3c.dom.DocumentFragment;
-
-
-public interface SessionManager {
-    String ROLE = SessionManager.class.getClass().getName();
-    DocumentFragment getContextFragment (String s1, String s2);
-}
