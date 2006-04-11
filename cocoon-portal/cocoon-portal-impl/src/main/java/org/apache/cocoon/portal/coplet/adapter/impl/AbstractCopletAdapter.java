66372cc626751d481e8ac059f4f9c6a2a603ccdc
Carsten Ziegeler <cziegeler@apache.org>
2006-04-11 20:07:12 +0000

    Unify coplet adapter implementation and add preloading aspect
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/blocks@393302 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-portal/cocoon-portal-impl/src/main/java/org/apache/cocoon/portal/coplet/adapter/impl/AbstractCopletAdapter.java
+++ cocoon-portal/cocoon-portal-impl/src/main/java/org/apache/cocoon/portal/coplet/adapter/impl/AbstractCopletAdapter.java
@@ -15,25 +15,44 @@
  */
 package org.apache.cocoon.portal.coplet.adapter.impl;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
 import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.caching.Cache;
+import org.apache.cocoon.caching.CachedResponse;
+import org.apache.cocoon.components.sax.XMLByteStreamCompiler;
+import org.apache.cocoon.components.sax.XMLByteStreamInterpreter;
 import org.apache.cocoon.components.thread.RunnableManager;
 import org.apache.cocoon.environment.CocoonRunnable;
+import org.apache.cocoon.portal.PortalService;
 import org.apache.cocoon.portal.coplet.CopletData;
 import org.apache.cocoon.portal.coplet.CopletInstanceData;
+import org.apache.cocoon.portal.coplet.CopletInstanceDataFeatures;
 import org.apache.cocoon.portal.coplet.adapter.CopletAdapter;
+import org.apache.cocoon.portal.event.CopletInstanceEvent;
+import org.apache.cocoon.portal.event.Receiver;
 import org.apache.cocoon.portal.impl.AbstractComponent;
+import org.apache.cocoon.util.NetUtils;
 import org.apache.cocoon.xml.SaxBuffer;
 import org.apache.cocoon.xml.XMLUtils;
+import org.apache.excalibur.source.SourceValidity;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.ext.LexicalHandler;
 
 import EDU.oswego.cs.dl.util.concurrent.CountDown;
 
 /**
- * This is the abstract base adapter to use pipelines as coplets
+ * This is the abstract base adapter which could be used by any adapter
+ * implementation. It already provides several base features which can
+ * be used by sub classes.
  *
  * <h2>Configuration</h2>
  * <table><tbody>
@@ -62,7 +81,63 @@ import EDU.oswego.cs.dl.util.concurrent.CountDown;
  */
 public abstract class AbstractCopletAdapter 
     extends AbstractComponent
-    implements CopletAdapter {
+    implements CopletAdapter, Receiver {
+
+    /** The configuration name for buffering. */
+    public static final String CONFIGURATION_BUFFERING = "buffer";
+
+    /** The configuration name for timeout. */    
+    public static final String CONFIGURATION_TIMEOUT = "timeout";
+
+    /** The configuration name for enabling/disabling the cache. */
+    public static final String CONFIGURATION_ENABLE_CACHING = "cache-enabled";
+
+    /** The configuration name for using the global cache. */
+    public static final String CONFIGURATION_CACHE_GLOBAL= "cache-global";
+
+    /** The configuration name for querying instance attributes to generate the key
+     * for the global cache. */
+    public static final String CONFIGURATION_CACHE_GLOBAL_USE_ATTRIBUTES= "cache-global-use-attributes";
+
+    /** The configuration name for ignoring all sizing events to clear the cache. */
+    public static final String CONFIGURATION_IGNORE_SIZING_EVENTS = "ignore-sizing-events";
+
+    /** The configuration name for ignoring just min/normal sizing events to clear the cache. */
+    public static final String CONFIGURATION_IGNORE_SIMPLE_SIZING_EVENTS = "ignore-simple-sizing-events";
+
+    /** The temporary attribute name for the storing the cached coplet content. */
+    public static final String CACHE = "cacheData";
+
+    /** This temporary attribute can be set on the instance to not cache the current response. */
+    public static final String DO_NOT_CACHE = "doNotCache";
+    
+    /** The cache to use for global caching. */
+    protected Cache cache;
+
+    /** The runnable manager for starting background tasks. */
+    protected RunnableManager runnableManager;
+
+    /**
+     * @see org.apache.avalon.framework.service.Serviceable#service(org.apache.avalon.framework.service.ServiceManager)
+     */
+    public void service(ServiceManager manager) throws ServiceException {
+        super.service(manager);
+        this.cache = (Cache)this.manager.lookup(Cache.ROLE);
+        this.runnableManager = (RunnableManager)this.manager.lookup( RunnableManager.ROLE );
+    }
+
+    /**
+     * @see org.apache.avalon.framework.activity.Disposable#dispose()
+     */
+    public void dispose() {
+        if ( this.manager != null ) {
+            this.manager.release(this.cache);
+            this.cache = null;
+            this.manager.release(this.runnableManager);
+            this.runnableManager = null;
+        }
+        super.dispose();
+    }
 
     /**
      * Get a configuration value
@@ -95,10 +170,10 @@ public abstract class AbstractCopletAdapter
     }
 
     /**
-     * Implement this and not toSAX()
+     * Implement this and not toSAX().
      */
     public abstract void streamContent(CopletInstanceData coplet, 
-                                         ContentHandler contentHandler)
+                                       ContentHandler contentHandler)
     throws SAXException; 
 
     /**
@@ -112,8 +187,8 @@ public abstract class AbstractCopletAdapter
     public void toSAX(CopletInstanceData coplet, ContentHandler contentHandler)
     throws SAXException {
         final long startTime = System.currentTimeMillis();
-        Boolean bool = (Boolean) this.getConfiguration( coplet, "buffer" );
-        Integer timeout = (Integer) this.getConfiguration( coplet, "timeout");
+        Boolean bool = (Boolean) this.getConfiguration( coplet, CONFIGURATION_BUFFERING );
+        Integer timeout = (Integer) this.getConfiguration( coplet, CONFIGURATION_TIMEOUT);
         if ( timeout != null ) {
             // if timeout is set we have to buffer!
             bool = Boolean.TRUE;
@@ -127,13 +202,8 @@ public abstract class AbstractCopletAdapter
 
                 if ( timeout != null ) {
                     final int milli = timeout.intValue() * 1000;
-                    LoaderThread loader = new LoaderThread(this, coplet, buffer);
-                    final RunnableManager runnableManager = (RunnableManager)this.manager.lookup( RunnableManager.ROLE );
-                    try {
-                        runnableManager.execute( new CocoonRunnable(loader) );
-                    } finally {
-                        this.manager.release( runnableManager );
-                    }
+                    final LoaderThread loader = new LoaderThread(this, coplet, buffer);
+                    this.runnableManager.execute( new CocoonRunnable(loader) );
                     try {
                         read = loader.join( milli );
                     } catch (InterruptedException ignore) {
@@ -144,7 +214,7 @@ public abstract class AbstractCopletAdapter
                         this.getLogger().warn("Unable to get content of coplet: " + coplet.getId(), error);                        
                     }
                 } else {
-                    this.streamContent( coplet, buffer );
+                    this.streamContentAndCache( coplet, buffer );
                     read = true;
                 }
             } catch (Exception exception ) {
@@ -168,12 +238,80 @@ public abstract class AbstractCopletAdapter
                 }
             }
         } else {
-            this.streamContent( coplet, contentHandler );
+            this.streamContentAndCache( coplet, contentHandler );
         }
         if ( this.getLogger().isInfoEnabled() ) {
             final long msecs = System.currentTimeMillis() - startTime;
             this.getLogger().info("Streamed coplet " + coplet.getCopletData().getId() +
                                   " (instance " + coplet.getId() + ") in " + msecs + "ms.");
+        }
+    }
+
+    /**
+     * This method does the caching (if enabled).
+     */
+    public void streamContentAndCache( final CopletInstanceData coplet,
+                                       final ContentHandler contentHandler)
+    throws SAXException {
+        // Is caching enabled?
+        boolean cachingEnabled = ((Boolean)this.getConfiguration(coplet, CONFIGURATION_ENABLE_CACHING, Boolean.TRUE)).booleanValue();
+        // do we cache globally?
+        boolean cacheGlobal = ((Boolean)this.getConfiguration(coplet, CONFIGURATION_CACHE_GLOBAL, Boolean.FALSE)).booleanValue();
+
+        Object data = null;
+        // If caching is enabed and the cache is still valid, then use the cache
+        if (cachingEnabled) {
+            if ( cacheGlobal ) {
+                final String key = this.getCacheKey(coplet);
+                CachedResponse response = this.cache.get(key);
+                if (response != null ) {
+                    data = response.getResponse();
+                }
+            } else {
+                data = coplet.getTemporaryAttribute(CACHE);
+            }
+        }
+        if (data == null) {
+            // if caching is permanently or temporary disabled, flush the cache and invoke coplet
+            if ( !cachingEnabled || coplet.getTemporaryAttribute(DO_NOT_CACHE) != null ) {
+                coplet.removeTemporaryAttribute(DO_NOT_CACHE);
+                if ( cacheGlobal ) {
+                    final String key = this.getCacheKey(coplet);
+                    this.cache.remove(key); 
+                } else {
+                    coplet.removeTemporaryAttribute(CACHE);
+                }
+                this.streamContent(coplet, contentHandler);                
+            } else {
+
+                XMLByteStreamCompiler bc = new XMLByteStreamCompiler();
+
+                this.streamContent(coplet, bc);
+                data = bc.getSAXFragment();
+                if (coplet.removeTemporaryAttribute(DO_NOT_CACHE) == null) {
+                    if ( cacheGlobal ) {
+                        CachedResponse response = new CachedResponse((SourceValidity[])null, (byte[])data);
+                        try {
+                            final String key = this.getCacheKey(coplet);
+                            this.cache.store(key, response);
+                        } catch (ProcessingException pe) {
+                            // we ignore this
+                            this.getLogger().warn("Exception during storing response into cache.", pe);
+                        }
+                    } else {
+                        coplet.setTemporaryAttribute(CACHE, data);
+                    }
+                }
+            }
+        }
+        // and now stream the data
+        if ( data != null ) {
+            XMLByteStreamInterpreter bi = new XMLByteStreamInterpreter();
+            bi.setContentHandler(contentHandler);
+            if ( contentHandler instanceof LexicalHandler ) {
+                bi.setLexicalHandler((LexicalHandler)contentHandler);
+            }
+            bi.deserialize(data);
         }
     }
 
@@ -227,6 +365,105 @@ public abstract class AbstractCopletAdapter
     throws SAXException {
         return false;
     }
+    /**
+     * This adapter listens for CopletInstanceEvents. Each event sets the cache invalid.
+     * @see org.apache.cocoon.portal.event.Receiver
+     */
+    public void inform(CopletInstanceEvent event, PortalService service) {
+        final CopletInstanceData coplet = event.getTarget();
+
+        // do we ignore SizingEvents
+        boolean ignoreSizing = ((Boolean)this.getConfiguration(coplet, CONFIGURATION_IGNORE_SIZING_EVENTS, Boolean.TRUE)).booleanValue();
+
+        if ( !ignoreSizing || !CopletInstanceDataFeatures.isSizingEvent(event)) {
+            boolean cleanupCache = true;
+            boolean ignoreSimpleSizing = ((Boolean)this.getConfiguration(coplet, CONFIGURATION_IGNORE_SIMPLE_SIZING_EVENTS, Boolean.FALSE)).booleanValue();
+            if ( ignoreSimpleSizing && CopletInstanceDataFeatures.isSizingEvent(event) ) {
+                int newSize = CopletInstanceDataFeatures.getSize(event);
+                int oldSize = coplet.getSize();
+                if (  (oldSize == CopletInstanceData.SIZE_NORMAL || oldSize == CopletInstanceData.SIZE_MINIMIZED )
+                   && (newSize == CopletInstanceData.SIZE_NORMAL || newSize == CopletInstanceData.SIZE_MINIMIZED )) {
+                    cleanupCache = false;
+                }
+            }
+            if ( cleanupCache ) {
+                // do we cache globally?
+                boolean cacheGlobal = ((Boolean)this.getConfiguration(coplet, CONFIGURATION_CACHE_GLOBAL, Boolean.FALSE)).booleanValue();
+                boolean cacheGlobalUseAttributes = ((Boolean)this.getConfiguration(coplet, CONFIGURATION_CACHE_GLOBAL_USE_ATTRIBUTES, Boolean.FALSE)).booleanValue();
+                if ( cacheGlobal ) {
+                    if ( !cacheGlobalUseAttributes ) {
+                        final String key = this.getCacheKey(coplet);
+                        this.cache.remove(key);
+                    }
+                } else {
+                    coplet.removeTemporaryAttribute(CACHE);
+                }
+            }
+        }
+    }
+
+    /**
+     * Build the key for the global cache.
+     */
+    protected String getCacheKey(CopletInstanceData coplet) {
+        final Boolean useAttributes = (Boolean)this.getConfiguration(coplet,
+                                                            CONFIGURATION_CACHE_GLOBAL_USE_ATTRIBUTES,
+                                                            Boolean.FALSE);
+        if ( !useAttributes.booleanValue() ) {
+            return "coplet:" + coplet.getCopletData().getId();
+        }
+        final StringBuffer buffer = new StringBuffer("coplet:");
+        buffer.append(coplet.getCopletData().getId());
+        boolean hasParams = false;
+        // first add attributes:
+        // sort the keys
+        List keyList = new ArrayList(coplet.getAttributes().keySet());
+        Collections.sort(keyList);
+        Iterator i = keyList.iterator();
+        while ( i.hasNext() ) {
+            final Object name = i.next();
+            final Object value = coplet.getAttribute(name.toString());
+            if ( hasParams ) {
+                buffer.append('&');
+            } else {
+                buffer.append('?');
+                hasParams = true;
+            }
+            buffer.append(name.toString());
+            buffer.append('=');
+            if ( value != null ) {
+                try {
+                    buffer.append(NetUtils.encode(value.toString(), "utf-8"));
+                } catch (UnsupportedEncodingException ignore) {
+                    // we ignore this
+                }
+            }
+        }
+        // second add temporary attributes
+        keyList = new ArrayList(coplet.getTemporaryAttributes().keySet());
+        Collections.sort(keyList);
+        i = keyList.iterator();
+        while ( i.hasNext() ) {
+            final Object name = i.next();
+            final Object value = coplet.getTemporaryAttribute(name.toString());
+            if ( hasParams ) {
+                buffer.append('&');
+            } else {
+                buffer.append('?');
+                hasParams = true;
+            }
+            buffer.append(name.toString());
+            buffer.append('=');
+            if ( value != null ) {
+                try {
+                    buffer.append(NetUtils.encode(value.toString(), "utf-8"));
+                } catch (UnsupportedEncodingException ignore) {
+                    // we ignore this
+                }
+            }
+        }
+        return buffer.toString();            
+    }
 }
 
 final class LoaderThread implements Runnable {
@@ -248,7 +485,7 @@ final class LoaderThread implements Runnable {
 
     public void run() {
         try {
-            adapter.streamContent( this.coplet, this.handler );
+            adapter.streamContentAndCache( this.coplet, this.handler );
         } catch (Exception local) {
             this.exception = local;
         } finally {
