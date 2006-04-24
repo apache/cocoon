a4139ad594e126ea15d6ca4daccd908eecb0470c
Reinhard Pötz <reinhard@apache.org>
2006-04-24 12:16:42 +0000

    merged with cocoon-maven-eclipse-plugin - can be deleted now
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@396528 13f79535-47bb-0310-9956-ffa450edef68

--- tools/cocoon-maven-eclipse-plugin1/src/test/projects/project-3/src/test/java/DummyClass.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
- * @version $Id: DummyClass.java 240143 2005-08-25 21:28:30Z kenney $
- */
-public class DummyClass
-{
-}
