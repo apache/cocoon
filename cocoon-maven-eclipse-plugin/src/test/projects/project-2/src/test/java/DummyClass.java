c41263d3d65382b3e9d0af3d4bbc06afaeb71642
Reinhard Pötz <reinhard@apache.org>
2006-04-24 10:24:53 +0000

    Create a branch of the Maven Eclipse plugin to work on PDE support. The goal is to work on a patch that hopefully gets accepted by the Maven guys - in the meantime we can use the branched version to create Eclipse project files.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/tools@396502 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cocoon-maven-eclipse-plugin/src/test/projects/project-2/src/test/java/DummyClass.java
@@ -0,0 +1,7 @@
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id: DummyClass.java 169837 2005-05-12 15:40:51Z brett $
+ */
+public class DummyClass
+{
+}
