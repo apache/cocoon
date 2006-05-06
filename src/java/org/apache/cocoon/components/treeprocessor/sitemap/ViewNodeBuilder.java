f1f614221987cd44d6ac63a428fe5425c22421d0
Antonio Gallardo Rivera <antonio@apache.org>
2006-05-06 20:33:27 +0000

    Remove unused import.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@400361 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/treeprocessor/sitemap/ViewNodeBuilder.java
+++ src/java/org/apache/cocoon/components/treeprocessor/sitemap/ViewNodeBuilder.java
@@ -17,7 +17,6 @@ package org.apache.cocoon.components.treeprocessor.sitemap;
 
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.components.treeprocessor.NamedContainerNodeBuilder;
 import org.apache.cocoon.components.treeprocessor.NamedProcessingNode;
 import org.apache.cocoon.components.treeprocessor.ProcessingNode;
