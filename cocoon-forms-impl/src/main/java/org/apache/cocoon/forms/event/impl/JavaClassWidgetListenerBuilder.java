d32f20a7f2cbf83157c745452c43632641282722
Carsten Ziegeler <cziegeler@apache.org>
2006-05-18 13:52:37 +0000

    CForms: Java event listeners can now implement Configurable, LogEnabled, and Contextualizable.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/blocks/cocoon-forms@407551 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/event/impl/JavaClassWidgetListenerBuilder.java
+++ cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/event/impl/JavaClassWidgetListenerBuilder.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,11 @@ import org.apache.cocoon.forms.event.WidgetListener;
 import org.apache.cocoon.forms.event.WidgetListenerBuilder;
 import org.apache.cocoon.forms.util.DomHelper;
 import org.apache.cocoon.util.ClassUtils;
+import org.apache.avalon.framework.configuration.ConfigurationUtil;
+import org.apache.avalon.framework.context.Context;
+import org.apache.avalon.framework.context.ContextException;
+import org.apache.avalon.framework.context.Contextualizable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.service.ServiceException;
 import org.apache.avalon.framework.service.ServiceManager;
 import org.apache.avalon.framework.service.Serviceable;
@@ -36,20 +41,37 @@ import org.w3c.dom.Element;
  *
  * @version $Id$
  */
-public class JavaClassWidgetListenerBuilder implements WidgetListenerBuilder, ThreadSafe, Serviceable {
-	ServiceManager manager;
+public class JavaClassWidgetListenerBuilder
+    extends AbstractLogEnabled
+    implements WidgetListenerBuilder, ThreadSafe, Contextualizable, Serviceable {
 
+    protected ServiceManager manager;
+
+    protected Context context;
+
+	/**
+	 * @see org.apache.avalon.framework.context.Contextualizable#contextualize(org.apache.avalon.framework.context.Context)
+	 */
+	public void contextualize(Context context) throws ContextException {
+        this.context = context;
+    }
+
+    /**
+	 * @see org.apache.avalon.framework.service.Serviceable#service(org.apache.avalon.framework.service.ServiceManager)
+	 */
 	public void service(ServiceManager manager) throws ServiceException {
 		this.manager = manager;
 	}
 
+	/**
+	 * @see org.apache.cocoon.forms.event.WidgetListenerBuilder#buildListener(org.w3c.dom.Element, java.lang.Class)
+	 */
 	public WidgetListener buildListener(Element element, Class listenerClass) throws Exception {
-
         String name = DomHelper.getAttribute(element, "class");
 
         Object listener = ClassUtils.newInstance(name);
         if (listenerClass.isAssignableFrom(listener.getClass())) {
-            LifecycleHelper.setupComponent(listener, null, null, manager, null);
+            LifecycleHelper.setupComponent(listener, this.getLogger(), this.context, manager, ConfigurationUtil.toConfiguration(element));
             return (WidgetListener)listener;
         } else {
             throw new Exception("Class " + listener.getClass() + " is not a " + listenerClass);
