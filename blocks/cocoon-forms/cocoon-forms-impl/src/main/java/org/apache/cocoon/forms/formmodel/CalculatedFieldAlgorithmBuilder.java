4207ef6a84a900d42b95bbbf07a05432e54a5c16
Simone Gianni <simoneg@apache.org>
2006-05-26 00:53:05 +0000

    Calculated fields
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@409526 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blocks/cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/formmodel/CalculatedFieldAlgorithmBuilder.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 1999-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.forms.formmodel;
+
+import org.w3c.dom.Element;
+
+/**
+ * Common interface for {@link org.apache.cocoon.forms.formmodel.CalculatedFieldAlgorithm} builders.
+ * @version $Id$
+ */
+public interface CalculatedFieldAlgorithmBuilder {
+
+    static final String ROLE = CalculatedFieldAlgorithmBuilder.class.getName();
+
+    CalculatedFieldAlgorithm build(Element algorithmElement) throws Exception;
+    
+}
