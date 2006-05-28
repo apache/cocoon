b738f31fccc15eca25ff1c15e77784bcd622e47b
Par Daniel Fagerstrohm <danielf@apache.org>
2006-05-28 20:09:44 +0000

    Reorganized the Java packages in cocoon-bootstrap so that we don't have any packages that are split between cocoon-bootstrap and cocoon-core. Added a manifest file so that it works as a bundle and also updated the manifest in the cocoon-core to reflect that some packages where moved and some jars updated to new versions.
    Part I
    
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/core/cocoon-bootstrap@409990 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/cocoon/bootstrap/servlet/BootstrapClassLoaderManager.java
+++ src/main/java/org/apache/cocoon/bootstrap/servlet/BootstrapClassLoaderManager.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cocoon.servlet;
+package org.apache.cocoon.bootstrap.servlet;
 
 import java.io.IOException;
 import java.io.InputStream;
--- src/main/java/org/apache/cocoon/classloader/WildcardHelper.java
+++ src/main/java/org/apache/cocoon/classloader/WildcardHelper.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cocoon.util;
+package org.apache.cocoon.classloader;
 
 import java.util.HashMap;
 
