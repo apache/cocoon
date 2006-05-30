8b19f6c9d01e5e2e8c989267ccf0aa558fe2b41c
Carsten Ziegeler <cziegeler@apache.org>
2006-05-30 15:30:08 +0000

    Add testcase that show a bug in the wildcard matcher!
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/core/cocoon-core@410303 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/cocoon/matching/WildcardURIMatcherTestCase.java
+++ src/test/java/org/apache/cocoon/matching/WildcardURIMatcherTestCase.java
@@ -16,47 +16,47 @@
 package org.apache.cocoon.matching;
 
 import java.util.Map;
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import junit.textui.TestRunner;
+
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.SitemapComponentTestCase;
 
 
 public class WildcardURIMatcherTestCase extends SitemapComponentTestCase {
 
-    /**
-     * Run this test suite from commandline
-     *
-     * @param args commandline arguments (ignored)
-     */
-    public static void main( String[] args ) {
-        TestRunner.run(suite());
-    }
-    
-    /** Create a test suite.
-     * This test suite contains all test cases of this class.
-     * @return the Test object containing all test cases.
-     */
-    public static Test suite() {
-        TestSuite suite = new TestSuite(WildcardURIMatcherTestCase.class);
-        return suite;
-    }
-    
     public void testWildcardURIMatch() throws Exception {
         getRequest().setRequestURI("/test/foo/bla/end");
 
-        Parameters parameters = new Parameters();
+        final Parameters parameters = new Parameters();
 
         Map result = match("wildcard-uri", "**", parameters);
-        System.out.println(result);
         assertNotNull("Test if resource exists", result);
         assertEquals("Test for **", "test/foo/bla/end", result.get("1"));
         
         result = match("wildcard-uri", "**/bla/*", parameters);
-        System.out.println(result);
         assertNotNull("Test if resource exists", result);
         assertEquals("Test for **/bla/* {1}", "test/foo", result.get("1"));
         assertEquals("Test for **/bla/* {2}", "end", result.get("2"));
     }
+
+    public void testWildcardURIMatchSimplePattern() throws Exception {
+        getRequest().setRequestURI("/test");
+
+        final Parameters parameters = new Parameters();
+
+        Map result = match("wildcard-uri", "*", parameters);
+        assertNotNull("Test if resource exists", result);
+        assertEquals("Test for *", "test", result.get("1"));
+    }
+
+    public void testWildcardURIMatchDoublePattern() throws Exception {
+        getRequest().setRequestURI("/test/something.xmlbla.xml");
+
+        final Parameters parameters = new Parameters();
+
+        Map result = match("wildcard-uri", "*/*.xml", parameters);
+        assertNotNull("Test if resource exists", result);
+        assertEquals("Test for */*.xml", "test", result.get("1"));
+        assertEquals("Test for */*.xml", "something.xmlbla", result.get("2"));
+    }
+
 }
