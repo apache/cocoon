0bb016ef978bc6a175b3126a001a6b8ecd9d3d9e
Antonio Gallardo Rivera <antonio@apache.org>
2006-06-07 00:36:01 +0000

    Update dojo to 2006-06-05.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/blocks/cocoon-ajax@412242 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-ajax-impl/src/main/resources/org/apache/cocoon/ajax/resources/js/__package__.js
+++ cocoon-ajax-impl/src/main/resources/org/apache/cocoon/ajax/resources/js/__package__.js
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-dojo.hostenv.conditionalLoadModule({
+dojo.kwCompoundRequire({
         common: [
                 "cocoon.ajax.BUHandler"
         ]
 });
-s
-dojo.hostenv.moduleLoaded("cocoon.ajax.*");
+
+dojo.provide("cocoon.ajax.*");
