378153a695d50ec06692b9b74b22951a7160fa14
Carsten Ziegeler <cziegeler@apache.org>
2006-06-27 14:14:26 +0000

    Rename paranoid to something more common...
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/core@417463 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-bootstrap/src/main/java/org/apache/cocoon/bootstrap/servlet/ShieldingServletFilter.java
+++ cocoon-bootstrap/src/main/java/org/apache/cocoon/bootstrap/servlet/ShieldingServletFilter.java
@@ -30,7 +30,7 @@ import javax.servlet.ServletResponse;
  *
  * @version $Id$
  */
-public class ParanoidServletFilter implements Filter {
+public class ShieldingServletFilter implements Filter {
 
     protected Filter filter;
 
