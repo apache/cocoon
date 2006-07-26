ebffc1ac51cccb13043150dbcbb9e98d75a43566
Antonio Gallardo Rivera <antonio@apache.org>
2006-07-26 05:07:26 +0000

    <action dev="AG" type="fix" fixes-bug="COCOON-1885" due-to="Ard Schrijvers" due-to-email="a.schrijvers@hippo.nl">   Core: EHDefaultStore returns in the size() method the wrong number of keys. </action>
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@425627 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/components/store/impl/EHDefaultStore.java
+++ src/java/org/apache/cocoon/components/store/impl/EHDefaultStore.java
@@ -439,8 +439,10 @@ implements Store, Contextualizable, Serviceable, Parameterizable, Initializable,
      */
     public int size() {
         try {
-            return this.cache.getSize();
-        } catch (CacheException e) {
+            // cast to int due ehcache implementation returns a long instead of int.
+            // See: http://ehcache.sourceforge.net/javadoc/net/sf/ehcache/Cache.html#getMemoryStoreSize()
+            return (int)this.cache.getMemoryStoreSize();
+        } catch (IllegalStateException e) {
             if (getLogger().isWarnEnabled()) {
                 getLogger().warn("Error while getting cache size", e);
             }
