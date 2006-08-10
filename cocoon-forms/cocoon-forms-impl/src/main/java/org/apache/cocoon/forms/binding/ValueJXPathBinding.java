70a81f86effe562677d03986192bd3373b714e22
Marc Portier <mpo@apache.org>
2006-08-10 13:35:08 +0000

    Reverting inappropriate fix. - facts to undo: http://svn.apache.org/viewvc?view=rev&revision=369507 - discussion: http://marc.theaimsgroup.com/?t=115502476800003&r=1&w=2 - original patch-issue: https://issues.apache.org/jira/browse/COCOON-1687
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/blocks@430378 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/binding/ValueJXPathBinding.java
+++ cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/binding/ValueJXPathBinding.java
@@ -129,11 +129,7 @@ public class ValueJXPathBinding extends JXPathBindingBase {
 
         if ((value == null && oldValue != null) || value != null && !value.equals(oldValue)) {
             // first update the value itself
-            if(value != null) {
-                jxpc.createPathAndSetValue(this.xpath, value);
-            } else {
-                jxpc.removePath(this.xpath);
-            }
+            jxpc.createPathAndSetValue(this.xpath, value);
 
             // now perform any other bindings that need to be performed when the value is updated
             JXPathContext subContext = null;
