c32459e0514ccf757a04748ae00b6df840d010b9
Carsten Ziegeler <cziegeler@apache.org>
2006-09-03 14:40:20 +0000

    Clean up logging configuration Move avalon related stuff into avalon package
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/core@439784 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-core/src/main/java/org/apache/cocoon/core/container/spring/avalon/ClassLoaderUtils.java
+++ cocoon-core/src/main/java/org/apache/cocoon/core/container/spring/avalon/ClassLoaderUtils.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cocoon.core.container.util;
+package org.apache.cocoon.core.container.spring.avalon;
 
 import java.util.Iterator;
 
