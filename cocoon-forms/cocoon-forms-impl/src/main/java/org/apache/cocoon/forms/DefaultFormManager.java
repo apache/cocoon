09cb7991d733845e8ac8b497050d6119e5464fcb
Vadim Gritsenko <vgritsenko@apache.org>
2006-09-16 20:43:50 +0000

    Add CacheManager.remove() method. Cleanup.
    <action dev="VG" type="update">
      CForms: FormManager now caches form definitions created from Source or URI.
    </action>
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/blocks@446950 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/DefaultFormManager.java
+++ cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/DefaultFormManager.java
@@ -26,11 +26,14 @@ import org.apache.avalon.framework.context.Context;
 import org.apache.avalon.framework.context.ContextException;
 import org.apache.avalon.framework.context.Contextualizable;
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.logger.Logger;
 import org.apache.avalon.framework.service.ServiceException;
 import org.apache.avalon.framework.service.ServiceManager;
-import org.apache.avalon.framework.service.ServiceSelector;
 import org.apache.avalon.framework.service.Serviceable;
 import org.apache.avalon.framework.thread.ThreadSafe;
+import org.apache.excalibur.source.Source;
+import org.apache.excalibur.source.SourceResolver;
+
 import org.apache.cocoon.forms.formmodel.Form;
 import org.apache.cocoon.forms.formmodel.FormDefinition;
 import org.apache.cocoon.forms.formmodel.FormDefinitionBuilder;
@@ -39,8 +42,7 @@ import org.apache.cocoon.forms.formmodel.library.LibraryManager;
 import org.apache.cocoon.forms.formmodel.library.LibraryManagerImpl;
 import org.apache.cocoon.forms.util.DomHelper;
 import org.apache.cocoon.forms.util.SimpleServiceSelector;
-import org.apache.excalibur.source.Source;
-import org.apache.excalibur.source.SourceResolver;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
@@ -50,23 +52,38 @@ import org.xml.sax.InputSource;
  *
  * @version $Id$
  */
-public class DefaultFormManager
-  extends AbstractLogEnabled
-  implements FormManager, Contextualizable, ThreadSafe, Serviceable, Disposable, Configurable, Component, Initializable {
-// FIXME: Component is there to allow this block to also run in the 2.1 branch
+public class DefaultFormManager extends AbstractLogEnabled
+                                implements FormManager, Contextualizable, Serviceable, Configurable,
+                                           Initializable, Disposable, ThreadSafe, Component {
+    // NOTE: Component is there to allow this block to run in the 2.1 branch
 
     protected static final String PREFIX = "CocoonForm:";
-    protected ServiceManager manager;
-    protected Configuration configuration;
-    protected SimpleServiceSelector widgetDefinitionBuilderSelector;
-    protected CacheManager cacheManager;
-    
-    protected LibraryManagerImpl libraryManager;
 
     private Context avalonContext;
+    protected ServiceManager manager;
+    protected SimpleServiceSelector widgetDefinitionBuilderSelector;
+    protected CacheManager cacheManager;
+
+    protected LibraryManagerImpl libraryManager;
+
+    //
+    // Lifecycle
+    //
+
+    public DefaultFormManager() {
+        libraryManager = new LibraryManagerImpl();
+        widgetDefinitionBuilderSelector = new SimpleServiceSelector("widget", WidgetDefinitionBuilder.class);
+    }
+
+    public void enableLogging(Logger logger) {
+        super.enableLogging(logger);
+        libraryManager.enableLogging(getLogger().getChildLogger("library"));
+    }
+
     public void contextualize(Context context) throws ContextException {
-		this.avalonContext = context;
-	}
+        this.avalonContext = context;
+        widgetDefinitionBuilderSelector.contextualize(avalonContext);
+    }
 
     /** Temporary internal method, don't rely on it's existence! Needed to access the context from flowscript. */
     // FIXME (SW). Extending the FOM is needed.
@@ -77,51 +94,76 @@ public class DefaultFormManager
     public void service(ServiceManager manager) throws ServiceException {
         this.manager = manager;
         this.cacheManager = (CacheManager)manager.lookup(CacheManager.ROLE);
+        libraryManager.service(new FormServiceManager());
+        widgetDefinitionBuilderSelector.service(new FormServiceManager());
     }
 
-    /**
-     * Configurable
-     */
     public void configure(Configuration configuration) throws ConfigurationException {
-        this.configuration = configuration;
+        libraryManager.configure(configuration.getChild("libraries"));
+        widgetDefinitionBuilderSelector.configure(configuration.getChild("widgets"));
     }
 
     public void initialize() throws Exception {
-
-        libraryManager = new LibraryManagerImpl();
-        libraryManager.enableLogging(getLogger().getChildLogger("library"));
-        libraryManager.service(new FormServiceManager());
-        libraryManager.configure(configuration.getChild("libraries"));
-        
-        widgetDefinitionBuilderSelector = new SimpleServiceSelector("widget", WidgetDefinitionBuilder.class);
-        widgetDefinitionBuilderSelector.contextualize(avalonContext);
-        widgetDefinitionBuilderSelector.service(new FormServiceManager());
-        widgetDefinitionBuilderSelector.configure(configuration.getChild("widgets"));
-        
         libraryManager.initialize();
     }
-    
-    public ServiceSelector getWidgetDefinitionBuilderSelector() {
-    	return this.widgetDefinitionBuilderSelector;
+
+    public void dispose() {
+        if (this.widgetDefinitionBuilderSelector != null) {
+            this.widgetDefinitionBuilderSelector.dispose();
+            this.widgetDefinitionBuilderSelector = null;
+        }
+        if(this.libraryManager != null) {
+            this.libraryManager.dispose();
+            this.libraryManager = null;
+        }
+        if (this.cacheManager != null) {
+            this.manager.release(this.cacheManager);
+            this.cacheManager = null;
+        }
+        this.manager = null;
+    }
+
+    //
+    // Business Methods
+    //
+
+    public Form createForm(String uri) throws Exception {
+        SourceResolver resolver = null;
+        Source source = null;
+        try {
+            resolver = (SourceResolver) manager.lookup(SourceResolver.ROLE);
+            source = resolver.resolveURI(uri);
+            return createForm(source);
+        } finally {
+            if (source != null) {
+                resolver.release(source);
+            }
+            if (resolver != null) {
+                manager.release(resolver);
+            }
+        }
     }
 
     public Form createForm(Source source) throws Exception {
-        FormDefinition formDefinition = getFormDefinition(source);
-        Form form = (Form)formDefinition.createInstance();
+        FormDefinition formDefinition = createFormDefinition(source);
+        Form form = (Form) formDefinition.createInstance();
         form.initialize();
         return form;
     }
 
-    public Form createForm(String uri) throws Exception {
+    public Form createForm(Element formElement) throws Exception {
+        Form form = (Form) createFormDefinition(formElement).createInstance();
+        form.initialize();
+        return form;
+    }
+
+    public FormDefinition createFormDefinition(String uri) throws Exception {
         SourceResolver sourceResolver = null;
         Source source = null;
-
         try {
             sourceResolver = (SourceResolver)manager.lookup(SourceResolver.ROLE);
-
             source = sourceResolver.resolveURI(uri);
-            Form form = createForm(source);
-            return form;
+            return createFormDefinition(source);
         } finally {
             if (source != null) {
                 sourceResolver.release(source);
@@ -132,27 +174,17 @@ public class DefaultFormManager
         }
     }
 
-    public Form createForm(Element formElement) throws Exception {
-        Form form = (Form)getFormDefinition(formElement).createInstance();
-        form.initialize();
-        return form;
-    }
+    public FormDefinition createFormDefinition(Source source) throws Exception {
+        FormDefinition formDefinition = (FormDefinition) this.cacheManager.get(source, PREFIX);
+        if (formDefinition != null && formDefinition.getLocalLibrary().dependenciesHaveChanged()) {
+            formDefinition = null; // invalidate
+        }
 
-    public FormDefinition createFormDefinition(Element formElement) throws Exception {
-        return getFormDefinition(formElement);
-    }
-
-    public FormDefinition getFormDefinition(Source source) throws Exception {
-        FormDefinition formDefinition = (FormDefinition)this.cacheManager.get(source, PREFIX);
-        
-        if(formDefinition != null && formDefinition.getLocalLibrary().dependenciesHaveChanged())
-        	formDefinition = null; // invalidate
-        
         if (formDefinition == null) {
-        	
-        	if(getLogger().isDebugEnabled())
-        		getLogger().debug("Building Form: "+source.getURI());
-        	
+            if (getLogger().isDebugEnabled()) {
+                getLogger().debug("Building Form: " + source.getURI());
+            }
+
             Document formDocument;
             try {
                 InputSource inputSource = new InputSource(source.getInputStream());
@@ -164,94 +196,51 @@ public class DefaultFormManager
             }
 
             Element formElement = formDocument.getDocumentElement();
-            formDefinition = getFormDefinition(formElement);
+            formDefinition = createFormDefinition(formElement);
             this.cacheManager.set(formDefinition, source, PREFIX);
         }
         return formDefinition;
     }
 
-    public FormDefinition getFormDefinition(Element formElement) throws Exception {
+    public FormDefinition createFormDefinition(Element formElement) throws Exception {
         // check that the root element is a fd:form element
-        if (!(formElement.getLocalName().equals("form") || FormsConstants.DEFINITION_NS.equals(formElement.getNamespaceURI())))
+        if (!FormsConstants.DEFINITION_NS.equals(formElement.getNamespaceURI()) || !formElement.getLocalName().equals("form")) {
             throw new Exception("Expected a Cocoon Forms form element at " + DomHelper.getLocation(formElement));
-
-        FormDefinitionBuilder formDefinitionBuilder = (FormDefinitionBuilder)widgetDefinitionBuilderSelector.select("form");
-        return (FormDefinition)formDefinitionBuilder.buildWidgetDefinition(formElement);
-    }
-
-    public FormDefinition createFormDefinition(String uri) throws Exception {
-        SourceResolver sourceResolver = null;
-        Source source = null;
-        Document formDocument = null;
-
-        try {
-            sourceResolver = (SourceResolver)manager.lookup(SourceResolver.ROLE);
-            source = sourceResolver.resolveURI(uri);
-
-            try {
-                InputSource inputSource = new InputSource(source.getInputStream());
-                inputSource.setSystemId(source.getURI());
-                formDocument = DomHelper.parse(inputSource, this.manager);
-            } catch (Exception e) {
-                throw new CascadingException("Could not parse form definition from " +
-                                             source.getURI(), e);
-            }
-
-        } finally {
-            if (source != null)
-                sourceResolver.release(source);
-            if (sourceResolver != null)
-                manager.release(sourceResolver);
         }
 
-        Element formElement = formDocument.getDocumentElement();
-        return getFormDefinition(formElement);
+        FormDefinitionBuilder builder = (FormDefinitionBuilder) widgetDefinitionBuilderSelector.select("form");
+        return (FormDefinition) builder.buildWidgetDefinition(formElement);
     }
 
-    /**
-     * Disposable
-     */
-    public void dispose() {
-        if (this.widgetDefinitionBuilderSelector != null) {
-            this.widgetDefinitionBuilderSelector.dispose();
-            this.widgetDefinitionBuilderSelector = null;
-        }
-        if(this.libraryManager != null) {
-        	this.libraryManager.dispose();
-        	this.libraryManager = null;
-        }
-        this.manager.release(this.cacheManager);
-        this.cacheManager = null;
-        this.manager = null;
-    }
-    
-    
-    public class FormServiceManager implements ServiceManager {
+
+    private class FormServiceManager implements ServiceManager {
         final String WIDGET_DEFINITION_BUILDER_SELECTOR_ROLE = WidgetDefinitionBuilder.class.getName() + "Selector";
         final String LIBRARY_MANAGER_ROLE = LibraryManager.ROLE;
 
         public Object lookup(String name) throws ServiceException {
-            if (WIDGET_DEFINITION_BUILDER_SELECTOR_ROLE.equals(name))
+            if (WIDGET_DEFINITION_BUILDER_SELECTOR_ROLE.equals(name)) {
                 return widgetDefinitionBuilderSelector;
-            else if(LIBRARY_MANAGER_ROLE.equals(name))
-            	return libraryManager;
-            else
+            } else if(LIBRARY_MANAGER_ROLE.equals(name)) {
+                return libraryManager;
+            } else {
                 return manager.lookup(name);
+            }
         }
 
         public boolean hasService(String name) {
-            if (WIDGET_DEFINITION_BUILDER_SELECTOR_ROLE.equals(name))
+            if (WIDGET_DEFINITION_BUILDER_SELECTOR_ROLE.equals(name)) {
                 return true;
-            else if(LIBRARY_MANAGER_ROLE.equals(name))
-            	return true;
-            else
+            } else if(LIBRARY_MANAGER_ROLE.equals(name)) {
+                return true;
+            } else {
                 return manager.hasService(name);
+            }
         }
 
         public void release(Object service) {
-            if (service != widgetDefinitionBuilderSelector
-            		&& service != libraryManager)
+            if (service != widgetDefinitionBuilderSelector && service != libraryManager) {
                 manager.release(service);
+            }
         }
     }
 }
