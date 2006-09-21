6b211118e9e6500ef23130a9723d598655f87da7
Leszek Gawron <lgawron@apache.org>
2006-09-21 10:00:36 +0000

    wrong license header
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@448505 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-apples/cocoon-apples-impl/src/main/java/org/apache/cocoon/components/flow/apples/ServiceApplesProcessor.java
+++ blocks/cocoon-apples/cocoon-apples-impl/src/main/java/org/apache/cocoon/components/flow/apples/ServiceApplesProcessor.java
@@ -1,22 +1,18 @@
 /*
- * $Id$
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Created on 2006-09-05
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright (c) 2006, MobileBox sp. z o.o.
- * All rights reserved.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.cocoon.components.flow.apples;
 
@@ -26,17 +22,17 @@ import org.apache.cocoon.ProcessingException;
  * A special version of ApplesProcessor that interprets the parameter passed to
  * instantiateController as service/bean name instead of classname. The class is
  * probably most useful with spring container integration.
- * 
+ *
  * Declare your flow in sitemap as &lt;map:flow language=&quot;service-apples&quot/&gt>
  * Define your AppleController beans in block/config/spring/ and call them from
  * sitemap by &lt;map:call function=&quot;beanName&quot/&gt>
- * 
+ *
  * Please remember to declare your StatelessAppleControllers as singletons. If
  * you wish to use continuations beans have to be declared as non-singletons.
- * 
+ *
  * You are of course free to use any container features in your beans like
  * dependency injection.
- * 
+ *
  * @version $Id$
  */
 public class ServiceApplesProcessor extends ApplesProcessor {
