2c66d27078cb73bd6fef2f45bd57727e658b2d9a
Vadim Gritsenko <vgritsenko@apache.org>
2006-10-02 17:17:06 +0000

    COCOON-1049: Add docos on getMimeType
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@452130 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/serialization/AbstractSerializer.java
+++ src/java/org/apache/cocoon/serialization/AbstractSerializer.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,15 +39,13 @@ extends AbstractXMLPipe implements Serializer {
      * Set the {@link OutputStream} where the requested resource should
      * be serialized.
      */
-    public void setOutputStream(OutputStream out) 
+    public void setOutputStream(OutputStream out)
     throws IOException {
         this.output = out;
     }
 
     /**
      * Get the mime-type of the output of this <code>Serializer</code>
-     * This default implementation returns null to indicate that the
-     * mime-type specified in the sitemap is to be used
      */
     public String getMimeType() {
         return null;
--- src/java/org/apache/cocoon/sitemap/SitemapOutputComponent.java
+++ src/java/org/apache/cocoon/sitemap/SitemapOutputComponent.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,7 +24,7 @@ import java.io.OutputStream;
 /**
  * This interface marks a component as a sitemap component that produces
  * a response, like a serializer or a reader.
- * 
+ *
  * @author <a href="mailto:Giacomo.Pati@pwr.ch">Giacomo Pati</a>
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
  * @version CVS $Id$
@@ -38,7 +38,16 @@ public interface SitemapOutputComponent extends Component {
     void setOutputStream(OutputStream out) throws IOException;
 
     /**
-     * Get the mime-type of the output of this <code>Component</code>.
+     * Get the media type of the output of this <code>Component</code>
+     * to be used in <code>Content-Type</code> header in the response.
+     * <br>
+     * Example content type value: <code>text/html; charset=utf-8</code>.
+     * <br>
+     * The returned value is used if no <code>mime-type</code> attribute is set
+     * in the sitemap.
+     *
+     * @see org.apache.cocoon.components.pipeline.AbstractProcessingPipeline#setMimeTypeForSerializer
+     * @see org.apache.cocoon.components.pipeline.AbstractProcessingPipeline#setMimeTypeForReader(org.apache.cocoon.environment.Environment) 
      */
     String getMimeType();
 
