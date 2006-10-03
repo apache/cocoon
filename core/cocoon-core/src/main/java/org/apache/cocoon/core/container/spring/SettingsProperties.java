e7dd2e5a3b9b76c5a79a3aa6612c0ee7000a16a0
Carsten Ziegeler <cziegeler@apache.org>
2006-10-03 12:56:59 +0000

    Add support for spring property override configuration
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@452470 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/cocoon-core/src/main/java/org/apache/cocoon/core/container/spring/SettingsProperties.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.core.container.spring;
+
+import java.util.Properties;
+
+import org.apache.cocoon.configuration.Settings;
+
+/**
+ * A properties implementation using the settings object.
+ * @version $Id$
+ */
+public class SettingsProperties extends Properties {
+
+    protected final Settings settings;
+
+    public SettingsProperties(Settings s) {
+        this.settings = s;
+    }
+
+    /**
+     * @see java.util.Properties#getProperty(java.lang.String, java.lang.String)
+     */
+    public String getProperty(String key, String defaultValue) {
+        return this.settings.getProperty(key, defaultValue);
+    }
+
+    /**
+     * @see java.util.Properties#getProperty(java.lang.String)
+     */
+    public String getProperty(String key) {
+        return this.settings.getProperty(key);
+    }
+    
+}
