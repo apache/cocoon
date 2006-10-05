82c011c49985aafdc0a9c5ca4b3e1dd61a6a97f7
Carsten Ziegeler <cziegeler@apache.org>
2006-10-05 14:59:27 +0000

    Continue property override configuration functionality Fix namespaces (as reported by Felix)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@453261 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-core/src/main/java/org/apache/cocoon/core/container/spring/avalon/AvalonServiceManager.java
+++ core/cocoon-core/src/main/java/org/apache/cocoon/core/container/spring/avalon/AvalonServiceManager.java
@@ -18,7 +18,6 @@ package org.apache.cocoon.core.container.spring.avalon;
 
 import org.apache.avalon.framework.service.ServiceException;
 import org.apache.avalon.framework.service.ServiceManager;
-import org.apache.cocoon.configuration.Settings;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
