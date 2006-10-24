13cadfe9daa3c878f4ae2155841c8cc1a8c3e05d
Par Daniel Fagerstrohm <danielf@apache.org>
2006-10-24 21:05:51 +0000

    Empty servlet implementation in the BlockCallHttpServletRequest (Jira COCOON-1940). Pathch due to Alexander Klimetschek.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/core/cocoon-blocks-fw/cocoon-blocks-fw-impl@467483 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/cocoon/blocks/util/BlockCallHttpServletRequest.java
+++ src/main/java/org/apache/cocoon/blocks/util/BlockCallHttpServletRequest.java
@@ -27,10 +27,14 @@ import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
+
+import org.apache.commons.collections.map.AbstractHashedMap;
 
 
 /**
@@ -340,6 +344,95 @@ public class BlockCallHttpServletRequest implements HttpServletRequest{
      */
     public HttpSession getSession(boolean create) {
         // TODO Auto-generated method stub
+        if (create) {
+            return new HttpSession() {
+
+                public Object getAttribute(String name) {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                public Enumeration getAttributeNames() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                public long getCreationTime() {
+                    // TODO Auto-generated method stub
+                    return 0;
+                }
+
+                public String getId() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                public long getLastAccessedTime() {
+                    // TODO Auto-generated method stub
+                    return 0;
+                }
+
+                public int getMaxInactiveInterval() {
+                    // TODO Auto-generated method stub
+                    return 0;
+                }
+
+                public ServletContext getServletContext() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                public HttpSessionContext getSessionContext() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                public Object getValue(String name) {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                public String[] getValueNames() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                public void invalidate() {
+                    // TODO Auto-generated method stub
+                    
+                }
+
+                public boolean isNew() {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+
+                public void putValue(String name, Object value) {
+                    // TODO Auto-generated method stub
+                    
+                }
+
+                public void removeAttribute(String name) {
+                    // TODO Auto-generated method stub
+                    
+                }
+
+                public void removeValue(String name) {
+                    // TODO Auto-generated method stub
+                    
+                }
+
+                public void setAttribute(String name, Object value) {
+                    // TODO Auto-generated method stub
+                    
+                }
+
+                public void setMaxInactiveInterval(int interval) {
+                    // TODO Auto-generated method stub
+                    
+                }
+           };     
+        }
         return null;
     }
 
