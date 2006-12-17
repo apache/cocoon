ae0da6c8c72ca5669f07ed5f6f69d43e5891ba28
Joerg Heinick <joerg@apache.org>
2006-12-17 21:33:21 +0000

    COCOON-1811: Allow dynamic loading of JavaScript objects even when scope is locked. (forgot the actual fix)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@488053 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-flowscript/cocoon-flowscript-impl/src/main/java/org/apache/cocoon/components/flow/javascript/fom/FOM_JavaScriptInterpreter.java
+++ blocks/cocoon-flowscript/cocoon-flowscript-impl/src/main/java/org/apache/cocoon/components/flow/javascript/fom/FOM_JavaScriptInterpreter.java
@@ -301,7 +301,7 @@ public class FOM_JavaScriptInterpreter extends CompilingInterpreter
         public void put(String name, Scriptable start, Object value) {
             //Allow setting values to existing variables, or if this is a
             //java class (used by importClass & importPackage)
-            if (this.locked && !has(name, start) && !(value instanceof NativeJavaClass)) {
+            if (this.locked && !has(name, start) && !(value instanceof NativeJavaClass) && !(value instanceof Function)) {
                 // Need to wrap into a runtime exception as Scriptable.put has no throws clause...
                 throw new WrappedException (new JavaScriptException("Implicit declaration of global variable '" + name +
                   "' forbidden. Please ensure all variables are explicitely declared with the 'var' keyword"));
