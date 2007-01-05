13b166104a095d0ff3bd7ee106c04c2ef50fcc7d
Poetz Reinhard <reinhard@apache.org>
2007-01-05 13:01:53 +0000

    fix profile name
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@493019 13f79535-47bb-0310-9956-ffa450edef68

--- build-docs.sh
+++ build-docs.sh
@@ -2,4 +2,4 @@
 mvn install -N
 cd site
 mvn site-deploy -P localDocs -Ddocs.repoUrl=http://localhost:9263
-mvn site-deploy -P localDocs,all-reports,simian-report -Ddocs.repoUrl=http://localhost:9263
+mvn site-deploy -P localDocs-all-reports,all-reports,simian-report -Ddocs.repoUrl=http://localhost:9263
