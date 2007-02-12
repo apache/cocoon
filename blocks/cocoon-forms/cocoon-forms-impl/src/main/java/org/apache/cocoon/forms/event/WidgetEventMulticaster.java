e3d1b411a95b4d70c079439bda6c6837b6e0bd13
Simone Gianni <simoneg@apache.org>
2007-02-12 22:22:48 +0000

    Fixed an error with listener removal
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@506703 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/event/WidgetEventMulticaster.java
+++ blocks/cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/event/WidgetEventMulticaster.java
@@ -134,4 +134,15 @@ public class WidgetEventMulticaster extends AWTEventMulticaster implements
         return (RepeaterListener)removeInternal(l, oldl);
     }
     
+    protected EventListener remove(EventListener oldl) {
+        if (oldl == a)  return b;
+        if (oldl == b)  return a;
+        EventListener a2 = removeInternal(a, oldl);
+        EventListener b2 = removeInternal(b, oldl);
+        if (a2 == a && b2 == b) {
+            return this;        // it's not here
+        }
+        return addInternal(a2, b2);
+    }
+    
 }
