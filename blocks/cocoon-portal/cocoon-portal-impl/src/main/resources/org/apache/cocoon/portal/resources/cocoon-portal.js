97be1ab17338f70a3e69b73c50694bdc880275b4
Carsten Ziegeler <cziegeler@apache.org>
2007-03-06 16:04:29 +0000

    Fix ajax handling after recent refactorings of ajax and forms block
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@515168 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-portal/cocoon-portal-impl/src/main/resources/org/apache/cocoon/portal/resources/cocoon-portal.js
+++ blocks/cocoon-portal/cocoon-portal-impl/src/main/resources/org/apache/cocoon/portal/resources/cocoon-portal.js
@@ -14,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+dojo.registerModulePath("cocoon.ajax", "../ajax/js");
+dojo.require("cocoon.ajax.BUHandler");
+
 cocoon.portal = {};
 
 cocoon.portal.process = function(uri) {
@@ -22,47 +25,36 @@ cocoon.portal.process = function(uri) {
   } else {
     uri = uri + "&cocoon-ajax=true";
   }
-  var req = dojo.hostenv.getXmlhttpObject()
-  req.open("POST", uri, true);
-  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-  req.onreadystatechange = function() {
-      if (req.readyState == 4) {
-          cocoon.portal.handleBrowserUpdate(req);
-      }
-  }
-  req.send(null);
+  dojo.io.bind({
+    url : uri,
+    load : function(type, data, evt) {
+      cocoon.portal.handleBrowserUpdate(data);
+    },
+    method: "POST",
+    mimetype: "text/xml"
+  });
 }
 
-cocoon.portal.handleBrowserUpdate = function(req) {
+cocoon.portal.handleBrowserUpdate = function(doc) {
   var updater = new cocoon.ajax.BUHandler();
-  if (req.status == 200) {
-    // Handle browser update directives
-    var doc = req.responseXML;
-    if (!doc) {
-        cocoon.ajax.BUHandler.handleError("No xml answer", req);
-        return;
+  updater.handlers['coplet'] = function(element) {
+    var content = dojo.dom.getFirstChildElement(element);
+    var id = content.getAttribute("id");
+    if (!id) {
+      alert("no id found on update element");
+      return;
     }
-    updater.handlers['coplet'] = function(element) {
-      var content = dojo.dom.getFirstChildElement(element);
-      var id = content.getAttribute("id");
-      if (!id) {
-        alert("no id found on update element");
-        return;
-      }
-      var oldElement = document.getElementById(id);
-      // in some cases, the server sends a coplet that
-      // is currently not displayed. So it's not an
-      // error if the element can't be found.
-      if (oldElement) {
-        var newElement = cocoon.ajax.insertionHelper.importNode(content, document).element;
-        // Warn: it's replace(new, old)!!
-        oldElement.parentNode.replaceChild(newElement, oldElement);
-        // Ensure the new node has the correct id
-        newElement.setAttribute("id", id);
-      }    
+    var oldElement = document.getElementById(id);
+    // in some cases, the server sends a coplet that
+    // is currently not displayed. So it's not an
+    // error if the element can't be found.
+    if (oldElement) {
+      var newElement = cocoon.ajax.insertionHelper.importNode(content, document).element;
+      // Warn: it's replace(new, old)!!
+      oldElement.parentNode.replaceChild(newElement, oldElement);
+      // Ensure the new node has the correct id
+      newElement.setAttribute("id", id);
     }
-    updater.processResponse(doc, req);
-  } else {
-    updater.handleError("Request failed - status=" + req.status, req);
-  }
+  };
+  updater.processResponse(doc);
 }
