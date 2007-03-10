316c1fe51d3f6e7a8ddc3bc54b42795386706164
Reinhard Pötz <reinhard@apache.org>
2007-03-10 12:38:53 +0000

    rename packages (don't use Cocoon core namespace)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@516696 13f79535-47bb-0310-9956-ffa450edef68

--- tools/cocoon-rcl/cocoon-rcl-webapp-wrapper/src/main/java/org/apache/cocoon/tools/rcl/wrapper/servlet/ReloadingNotificationSubscriber.java
+++ tools/cocoon-rcl/cocoon-rcl-webapp-wrapper/src/main/java/org/apache/cocoon/tools/rcl/wrapper/servlet/ReloadingNotificationSubscriber.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cocoon.servlet;
+package org.apache.cocoon.tools.rcl.wrapper.servlet;
 
 public interface ReloadingNotificationSubscriber {
 
