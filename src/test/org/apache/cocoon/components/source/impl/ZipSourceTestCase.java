ddd46bc03c8b46eed3c31e730b54d43db3f9bb03
Joerg Heinick <joerg@apache.org>
2007-03-29 19:22:43 +0000

    fix for the fix for the fix ... sorry, stupid me :(
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@523805 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/cocoon/components/source/impl/ZipSourceTestCase.java
+++ src/test/org/apache/cocoon/components/source/impl/ZipSourceTestCase.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.cocoon.components.source.impl;
 
 import org.apache.avalon.framework.service.ServiceException;
@@ -11,7 +27,8 @@ import org.apache.excalibur.source.SourceResolver;
 public class ZipSourceTestCase extends ContainerTestCase {
 
     public void testURIHandling() throws Exception {
-        String zipSourceUri = "zip:file://test.zip!/test.xml";
+        final String zipSourceUri = "zip:file://test.zip!/test.xml";
+        final String brokenMacOsXSourceUri = "zip:file:/test.zip!/test.xml";
         Source zipSource;
         SourceResolver resolver = null;
         try {
@@ -34,7 +51,7 @@ public class ZipSourceTestCase extends ContainerTestCase {
             //        removed after an update of Excalibur source resolve
             //        - https://issues.apache.org/jira/browse/COCOON-2022
             //        - http://thread.gmane.org/gmane.comp.apache.excalibur.devel/2107
-            assertEquals("Uri is wrong.", zipSourceUri.substring(0, index + 6).concat(zipSourceUri.substring(index + 7)), actualZipSourceUri);
+            assertEquals("Uri is wrong.", brokenMacOsXSourceUri, actualZipSourceUri);
         }
     }
     
