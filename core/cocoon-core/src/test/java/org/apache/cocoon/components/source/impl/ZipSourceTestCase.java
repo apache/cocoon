594ba1b000167f769fe58c50343cc068fe19fc56
Joerg Heinicke <joerg@apache.org>
2007-03-29 00:13:24 +0000

    work around for OS specific source resolving
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@523513 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-core/src/test/java/org/apache/cocoon/components/source/impl/ZipSourceTestCase.java
+++ core/cocoon-core/src/test/java/org/apache/cocoon/components/source/impl/ZipSourceTestCase.java
@@ -25,7 +25,17 @@ public class ZipSourceTestCase extends ContainerTestCase {
         assertTrue("Resolved Source is not an instance of ZipSource.", 
                    zipSource instanceof ZipSource);
         assertEquals("Scheme/protocol is wrong.", "zip", zipSource.getScheme());
-        assertEquals("Uri is wrong.", zipSourceUri, zipSource.getURI());
+        String actualZipSourceUri = zipSource.getURI();
+        int index = actualZipSourceUri.indexOf("file://");
+        if (index != -1) {
+            assertEquals("Uri is wrong.", zipSourceUri, actualZipSourceUri);
+        } else {
+            // FIXME: special treatment for OS specific return value, should be
+            //        removed after an update of Excalibur source resolve
+            //        - https://issues.apache.org/jira/browse/COCOON-2022
+            //        - http://thread.gmane.org/gmane.comp.apache.excalibur.devel/2107
+            assertEquals("Uri is wrong.", zipSourceUri.substring(0, index + 6).concat(zipSourceUri.substring(index + 7)), actualZipSourceUri);
+        }
     }
     
 }
