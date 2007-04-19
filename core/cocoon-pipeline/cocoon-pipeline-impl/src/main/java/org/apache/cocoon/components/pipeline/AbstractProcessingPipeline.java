f19142a3b4ff2de0bd61607b7252cdb8e26a01fc
Reinhard Pötz <reinhard@apache.org>
2007-04-19 13:28:04 +0000

    we can't set the response status here as it overrides the status-code value which was set by the serializer
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@530409 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-pipeline/cocoon-pipeline-impl/src/main/java/org/apache/cocoon/components/pipeline/AbstractProcessingPipeline.java
+++ core/cocoon-pipeline/cocoon-pipeline-impl/src/main/java/org/apache/cocoon/components/pipeline/AbstractProcessingPipeline.java
@@ -24,8 +24,6 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.avalon.excalibur.pool.Recyclable;
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.parameters.ParameterException;
@@ -548,8 +546,6 @@ public abstract class AbstractProcessingPipeline
             handleException(e);
         }
 
-        //Request has been succesfully processed, set approporiate status code
-        environment.setStatus(HttpServletResponse.SC_OK);
         return true;
     }
 
@@ -661,8 +657,6 @@ public abstract class AbstractProcessingPipeline
             handleException(e);
         }
         
-        //Request has been succesfully processed, set approporiate status code
-        environment.setStatus(HttpServletResponse.SC_OK);
         return true;
     }
 
