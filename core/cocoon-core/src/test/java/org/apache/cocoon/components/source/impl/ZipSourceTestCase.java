dd2a787de95d28e5d7bc5cfe7ee1289d43746120
Jorg Heymans <jheymans@apache.org>
2007-05-02 22:31:04 +0000

    this is fixed in the latest sourceresolver release
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@534640 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-core/src/test/java/org/apache/cocoon/components/source/impl/ZipSourceTestCase.java
+++ core/cocoon-core/src/test/java/org/apache/cocoon/components/source/impl/ZipSourceTestCase.java
@@ -28,7 +28,6 @@ public class ZipSourceTestCase extends ContainerTestCase {
 
     public void testURIHandling() throws Exception {
         final String zipSourceUri = "zip:file://test.zip!/test.xml";
-        final String brokenMacOsXSourceUri = "zip:file:/test.zip!/test.xml";
         Source zipSource;
         SourceResolver resolver = null;
         try {
@@ -43,16 +42,7 @@ public class ZipSourceTestCase extends ContainerTestCase {
                    zipSource instanceof ZipSource);
         assertEquals("Scheme/protocol is wrong.", "zip", zipSource.getScheme());
         String actualZipSourceUri = zipSource.getURI();
-        int index = actualZipSourceUri.indexOf("file://");
-        if (index != -1) {
-            assertEquals("Uri is wrong.", zipSourceUri, actualZipSourceUri);
-        } else {
-            // FIXME: special treatment for OS specific return value, should be
-            //        removed after an update of Excalibur source resolve
-            //        - https://issues.apache.org/jira/browse/COCOON-2022
-            //        - http://thread.gmane.org/gmane.comp.apache.excalibur.devel/2107
-            assertEquals("Uri is wrong.", brokenMacOsXSourceUri, actualZipSourceUri);
-        }
+        assertEquals("Uri is wrong.", zipSourceUri, actualZipSourceUri);
     }
     
 }
