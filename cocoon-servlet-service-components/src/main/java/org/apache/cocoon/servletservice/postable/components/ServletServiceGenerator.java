b1f2ea513158c98a20ea8ea1354a6b5c26d65177
Grzegorz Kossakowski <gkossakowski@apache.org>
2007-05-03 16:03:48 +0000

    Reverting changes made in r534394 and r534392. They break everything :(
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk/core/cocoon-servlet-service@534919 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon-servlet-service-components/src/main/java/org/apache/cocoon/servletservice/postable/components/ServletServiceGenerator.java
+++ cocoon-servlet-service-components/src/main/java/org/apache/cocoon/servletservice/postable/components/ServletServiceGenerator.java
@@ -32,7 +32,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.excalibur.source.Source;
 import org.apache.excalibur.source.SourceException;
-import org.apache.cocoon.core.xml.SAXParser;
+import org.apache.excalibur.xml.sax.SAXParser;
 import org.xml.sax.SAXException;
 
 /**
@@ -86,9 +86,9 @@ public class ServletServiceGenerator extends AbstractGenerator {
             throw SourceUtil.handle("Error during resolving of '" + src + "'.", se);
         }
         
-        if (logger.isDebugEnabled()) {
-        	logger.debug("Source " + service + " resolved to " + servletSource.getURI());
-            logger.debug("Source " + super.source + " resolved to " + inputSource.getURI());
+        if (getLogger().isDebugEnabled()) {
+        	getLogger().debug("Source " + service + " resolved to " + servletSource.getURI());
+            getLogger().debug("Source " + super.source + " resolved to " + inputSource.getURI());
         }
         
         IOUtils.copy(inputSource.getInputStream(), servletSource.getOutputStream());
