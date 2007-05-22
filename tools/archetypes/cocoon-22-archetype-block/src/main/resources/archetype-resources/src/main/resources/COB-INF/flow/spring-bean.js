4a211976ae85c5b2426e620c03febd853501af7d
Reinhard Pötz <reinhard@apache.org>
2007-05-22 06:02:59 +0000

    - move all resources that are part of the demo into a demo directory - add a commented pipeline demo - use released dependencies
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@540432 13f79535-47bb-0310-9956-ffa450edef68

--- tools/archetypes/cocoon-22-archetype-block/src/main/resources/archetype-resources/src/main/resources/COB-INF/flow/spring-bean.js
+++ tools/archetypes/cocoon-22-archetype-block/src/main/resources/archetype-resources/src/main/resources/COB-INF/flow/spring-bean.js
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +15,13 @@
  * limitations under the License.
  */
 //
-// @version $Id$
+// @version $Id: demo.js 448451 2006-09-21 04:04:16Z crossley $
 //
 
 function demo() {
     var demoBean = cocoon.getComponent("demo");
-    cocoon.sendPage("screens/demo", 
-        { 
+    cocoon.sendPage("screens/spring-bean",
+        {
             "message" : demoBean.message
         }
     );
