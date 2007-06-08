69699dc6dbabe1310ebf88ed9f01f6d1ae3da313
Reinhard Pötz <reinhard@apache.org>
2007-06-08 14:06:51 +0000

    give a hint how to solve this problem
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@545508 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-servlet-service/cocoon-servlet-service-components/src/main/java/org/apache/cocoon/servletservice/components/BlockPathModule.java
+++ core/cocoon-servlet-service/cocoon-servlet-service-components/src/main/java/org/apache/cocoon/servletservice/components/BlockPathModule.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,7 +36,7 @@ import org.apache.cocoon.servletservice.ServletServiceContext;
  * @version $Id$
  */
 public class BlockPathModule implements InputModule {
-    
+
     private ServletContext servletContext;
 
     public void setServletContext(ServletContext servletContext) {
@@ -61,7 +61,9 @@ public class BlockPathModule implements InputModule {
             URI uri = new URI(name);
             absoluteURI = ((ServletServiceContext)servletContext).absolutizeURI(uri).toString();
         } catch (URISyntaxException e) {
-            throw new ConfigurationException("Couldn't absolutize " + name);
+            throw new ConfigurationException("Couldn't absolutize " + name +
+                    ". Make sure that the configuration of your servlet-service " +
+                    "contains a connection to '" + name + "'.");
         }
         return absoluteURI;
     }
