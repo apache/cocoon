96a0b15fbdf6e948d683dffb2f0d6c59e5b4f655
Reinhard Pötz <reinhard@apache.org>
2007-07-03 09:23:54 +0000

    break build
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@552759 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-core/src/main/java/org/apache/cocoon/servlet/SitemapServlet.java
+++ core/cocoon-core/src/main/java/org/apache/cocoon/servlet/SitemapServlet.java
@@ -31,7 +31,7 @@ import javax.servlet.http.HttpServletResponse;
 
 // FIXME: This class should be merged with the o.a.c.sitemap.SitemapServlet
 // that is used in block mode.
-public class SitemapServlet extends HttpServlet {
+public class SitemapServletttttt extends HttpServlet {
 
     /** The Cocoon request processor. */
     protected RequestProcessor processor;
