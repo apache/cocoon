aa8e8116e12456ef06b06da4fffa9303ffd245cf
Carsten Ziegeler <cziegeler@apache.org>
2007-07-20 13:28:11 +0000

    Correc test case after update of excalibur source resolve.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@557994 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-core/src/test/java/org/apache/cocoon/components/source/impl/ZipSourceTestCase.java
+++ core/cocoon-core/src/test/java/org/apache/cocoon/components/source/impl/ZipSourceTestCase.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,8 +27,7 @@ import org.apache.excalibur.source.SourceResolver;
 public class ZipSourceTestCase extends ContainerTestCase {
 
     public void testURIHandling() throws Exception {
-        final String zipSourceUri = "zip:file://test.zip!/test.xml";
-        final String brokenMacOsXSourceUri = "zip:file:/test.zip!/test.xml";
+        final String zipSourceUri = "zip:file:///test.zip!/test.xml";
         Source zipSource;
         SourceResolver resolver = null;
         try {
@@ -39,20 +38,11 @@ public class ZipSourceTestCase extends ContainerTestCase {
         } finally {
             getManager().release(resolver);
         }
-        assertTrue("Resolved Source is not an instance of ZipSource.", 
+        assertTrue("Resolved Source is not an instance of ZipSource.",
                    zipSource instanceof ZipSource);
         assertEquals("Scheme/protocol is wrong.", "zip", zipSource.getScheme());
         String actualZipSourceUri = zipSource.getURI();
-        int index = actualZipSourceUri.indexOf("file://");
-        if (index != -1) {
-            assertEquals("Uri is wrong.", zipSourceUri, actualZipSourceUri);
-        } else {
-            // FIXME: special treatment for OS specific return value, should be
-            //        removed after an update of Excalibur source resolve
-            //        - https://issues.apache.org/jira/browse/COCOON-2022
-            //        - http://thread.gmane.org/gmane.comp.apache.excalibur.devel/2107
-            assertEquals("Uri is wrong.", brokenMacOsXSourceUri, actualZipSourceUri);
-        }
+        assertEquals("Uri is wrong.", zipSourceUri, actualZipSourceUri);
     }
-    
+
 }
