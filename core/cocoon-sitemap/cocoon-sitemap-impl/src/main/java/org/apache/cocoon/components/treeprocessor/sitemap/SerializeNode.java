apache/cocoon

commit 0c31225344d5bc2848ce65bf554f7fd2c659af7c

COCOON-1354 Variable resultion in <map:serialize status-code=""/> is supported. Voting thread: http://marc.info/?l=xml-cocoon-dev&m=117549655404542&w=2
git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@530406 13f79535-47bb-0310-9956-ffa450edef68

core/cocoon-sitemap/cocoon-sitemap-impl/src/main/java/org/apache/cocoon/components/treeprocessor/sitemap/SerializeNode.java
@@ -41,7 +41,7 @@ public class SerializeNode extends PipelineEventComponentProcessingNode
 
     private VariableResolver mimeType;
 
-    private int statusCode;
+    private VariableResolver statusCode;
 
     private Map parameters;
 
@@ -55,8 +55,8 @@ public class SerializeNode extends PipelineEventComponentProcessingNode
      */
     public SerializeNode(String name,
                          VariableResolver source,
-                         VariableResolver mimeType,
-                         int statusCode) {
+                         VariableResolver mimeType, 
+                         VariableResolver statusCode) {
         this.serializerName = name;
         this.source = source;
         this.mimeType = mimeType;
@@ -125,9 +125,14 @@ public class SerializeNode extends PipelineEventComponentProcessingNode
                                desc.mimeType);
 
         // Set status code if there is one
-        if (this.statusCode >= 0) {
-            env.setStatus(this.statusCode);
+        int statusCodeInt = -1;
+        try {
+            statusCodeInt = new Integer(this.statusCode.resolve(context, env.getObjectModel())).intValue();
+        } catch (NumberFormatException nfe) {
+            this.getLogger().warn("It was tried to set a non-integer as status code. " +
+                    "This value was ignored and default status code remains.", nfe);
         }
+        env.setStatus(statusCodeInt);        
 
         if (!context.isBuildingPipelineOnly()) {
             // Process pipeline

core/cocoon-sitemap/cocoon-sitemap-impl/src/main/java/org/apache/cocoon/components/treeprocessor/sitemap/SerializeNodeBuilder.java
@@ -61,7 +61,7 @@ public class SerializeNodeBuilder extends AbstractProcessingNodeBuilder
             type,
             VariableResolverFactory.getResolver(config.getAttribute("src", null), this.manager),
             VariableResolverFactory.getResolver(mimeType, this.manager),
-            config.getAttributeAsInteger("status-code", -1)
+            VariableResolverFactory.getResolver(config.getAttribute("status-code", null), this.manager)
         );
         this.node.setPipelineHints(this.pipelineHints);
         return this.treeBuilder.setupNode(node, config);

commit 028c5acbd6af30ab75bcf84f2fd706917f371099

Fix regression introduced in r530406, r532869. Update changes document: RC1 is long released.
git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@562199 13f79535-47bb-0310-9956-ffa450edef68

core/cocoon-sitemap/cocoon-sitemap-impl/pom.xml
@@ -17,13 +17,16 @@
   specific language governing permissions and limitations
   under the License.
 -->
+
 <!--+
     | @version $Id$
     +-->
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
-  
-  <modelVersion>4.0.0</modelVersion>  
-  <packaging>jar</packaging>    
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+
+  <modelVersion>4.0.0</modelVersion>
+  <packaging>jar</packaging>
   <parent>
     <artifactId>cocoon-core-modules</artifactId>
     <groupId>org.apache.cocoon</groupId>
@@ -35,22 +38,22 @@
   <description>
     The tree processor implementation of the sitemap with dependencies.
     The Spring packages for Avalon support. Abstract base classes
-    and support classes for making it easier to write sitemap components. 
+    and support classes for making it easier to write sitemap components.
   </description>
-  <url>http://cocoon.apache.org/${docs.m.sitemap-impl.relPath}</url>    
+  <url>http://cocoon.apache.org/${docs.m.sitemap-impl.relPath}</url>
 
   <properties>
-    <docs.name>${project.name}</docs.name>    
+    <docs.name>${project.name}</docs.name>
     <docs.version>${docs.m.sitemap-impl.version}</docs.version>
-  </properties>  
-  
+  </properties>
+
   <distributionManagement>
     <site>
       <id>website</id>
       <url>${docs.deploymentBaseUrl}/${docs.m.sitemap-impl.relPath}</url>
     </site>
-  </distributionManagement>     
-  
+  </distributionManagement>
+
   <dependencies>
     <!-- Cocoon -->
     <dependency>
@@ -124,9 +127,9 @@
           <artifactId>ant-optional</artifactId>
           <groupId>ant</groupId>
         </exclusion>
-        <!-- 
+        <!--
           excluding this because it brings avalon-framework which is NOT ignored by eclipse:eclipse
-          even though we excluded it in our root pom 
+          even though we excluded it in our root pom
         -->
         <exclusion>
           <artifactId>commons-logging</artifactId>
@@ -138,11 +141,11 @@
     <dependency>
       <groupId>org.springframework</groupId>
       <artifactId>spring-core</artifactId>
-    </dependency>   
+    </dependency>
     <dependency>
       <groupId>org.springframework</groupId>
       <artifactId>spring-context</artifactId>
-    </dependency>    
+    </dependency>
     <dependency>
       <groupId>org.springframework</groupId>
       <artifactId>spring-beans</artifactId>
@@ -152,7 +155,7 @@
           <artifactId>avalon-framework</artifactId>
         </exclusion>
       </exclusions>
-    </dependency>   
+    </dependency>
     <dependency>
       <groupId>org.springframework</groupId>
       <artifactId>spring-aop</artifactId>
@@ -161,8 +164,8 @@
           <groupId>avalon-framework</groupId>
           <artifactId>avalon-framework</artifactId>
         </exclusion>
-      </exclusions>      
-    </dependency>      
+      </exclusions>
+    </dependency>
     <dependency>
       <groupId>org.springframework</groupId>
       <artifactId>spring-web</artifactId>

core/cocoon-sitemap/cocoon-sitemap-impl/src/main/java/org/apache/cocoon/components/treeprocessor/sitemap/SerializeNode.java
@@ -55,7 +55,7 @@ public class SerializeNode extends PipelineEventComponentProcessingNode
      */
     public SerializeNode(String name,
                          VariableResolver source,
-                         VariableResolver mimeType, 
+                         VariableResolver mimeType,
                          VariableResolver statusCode) {
         this.serializerName = name;
         this.source = source;
@@ -124,19 +124,20 @@ public class SerializeNode extends PipelineEventComponentProcessingNode
                                desc.hintParameters,
                                desc.mimeType);
 
-        // Set status code if there is one
-        int statusCodeInt = 200;
-        try {
-            String statusCodeString = this.statusCode.resolve(context, env.getObjectModel());
-            if(null != statusCodeString) {
+        // Set status code *only* if there is one - do not override status
+        // code if it was set elsewhere.
+        String statusCodeString = this.statusCode.resolve(context, env.getObjectModel());
+        if (statusCodeString != null) {
+            int statusCodeInt = 200;
+            try {
                 Integer resolvedStatusCode = new Integer(this.statusCode.resolve(context, env.getObjectModel()));
                 statusCodeInt = resolvedStatusCode.intValue();
+            } catch (NumberFormatException e) {
+                getLogger().warn("Status code value '" + statusCodeString + "' is not an integer. " +
+                                 "Using " + statusCodeInt + " instead.", e);
             }
-        } catch (NumberFormatException nfe) {
-            this.getLogger().warn("It was tried to set a non-integer as status code. " +
-                    "This value was ignored and default status code remains.", nfe);
+            env.setStatus(statusCodeInt);
         }
-        env.setStatus(statusCodeInt);        
 
         if (!context.isBuildingPipelineOnly()) {
             // Process pipeline