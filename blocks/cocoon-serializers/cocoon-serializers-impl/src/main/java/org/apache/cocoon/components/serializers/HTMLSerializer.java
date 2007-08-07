e3439eee25f4256053e49de766b436550e5724cf
Carsten Ziegeler <cziegeler@apache.org>
2007-08-07 18:16:36 +0000

    Move charsets to own module and add convenient impls for serializers.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@563600 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-serializers/cocoon-serializers-impl/src/main/java/org/apache/cocoon/components/serializers/HTMLSerializer.java
+++ blocks/cocoon-serializers/cocoon-serializers-impl/src/main/java/org/apache/cocoon/components/serializers/HTMLSerializer.java
@@ -16,11 +16,22 @@
  */
 package org.apache.cocoon.components.serializers;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.avalon.excalibur.pool.Recyclable;
+import org.apache.avalon.framework.configuration.Configurable;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.cocoon.components.serializers.encoding.HTMLEncoder;
-import org.apache.cocoon.components.serializers.util.DocType;
-import org.apache.cocoon.components.serializers.util.SGMLDocType;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.environment.http.HttpEnvironment;
+import org.apache.cocoon.serialization.Serializer;
+import org.apache.cocoon.sitemap.SitemapModelComponent;
 import org.xml.sax.SAXException;
 
 /**
@@ -49,204 +60,43 @@ import org.xml.sax.SAXException;
  *
  * @version $Id$
  */
-public class HTMLSerializer extends XHTMLSerializer {
-
-    /** A cross-browser compatible very simple document type declaration. */
-    public static final DocType HTML401_DOCTYPE_COMPATIBLE = new SGMLDocType(
-            "HTML", "-//W3C//DTD HTML 4.01 Transitional//EN", null);
-
-    /** A representation of the HTML 4.01 strict document type. */
-    public static final DocType HTML401_DOCTYPE_STRICT = new SGMLDocType(
-            "HTML", "-//W3C//DTD HTML 4.01//EN",
-            "http://www.w3.org/TR/html4/strict.dtd");
-
-    /** A representation of the HTML 4.01 transitional document type. */
-    public static final DocType HTML401_DOCTYPE_TRANSITIONAL = new SGMLDocType(
-            "HTML", "-//W3C//DTD HTML 4.01 Transitional//EN",
-            "http://www.w3.org/TR/html4/loose.dtd");
-
-    /** A representation of the HTML 4.01 frameset document type. */
-    public static final DocType HTML401_DOCTYPE_FRAMESET = new SGMLDocType(
-            "HTML", "-//W3C//DTD HTML 4.01 Frameset//EN",
-            "http://www.w3.org/TR/html4/frameset.dtd");
-
-    /* ====================================================================== */
-
-    private static final HTMLEncoder HTML_ENCODER = new HTMLEncoder();
-
-    protected boolean encodeCharacters = true;
+public class HTMLSerializer
+    extends org.apache.cocoon.components.serializers.util.HTMLSerializer
+    implements Serializer, SitemapModelComponent, Recyclable, Configurable  {
 
     /**
-     * Create a new instance of this <code>HTMLSerializer</code>
+     * @see org.apache.cocoon.sitemap.SitemapModelComponent#setup(org.apache.cocoon.environment.SourceResolver, java.util.Map, java.lang.String, org.apache.avalon.framework.parameters.Parameters)
      */
-    public HTMLSerializer() {
-        super(HTML_ENCODER);
+    public void setup(SourceResolver resolver,
+                      Map            objectModel,
+                      String         src,
+                      Parameters     par)
+    throws ProcessingException, SAXException, IOException {
+        this.setup((HttpServletRequest) objectModel.get(HttpEnvironment.HTTP_REQUEST_OBJECT));
+    }
+
+
+    /**
+     * Test if the component wants to set the content length.
+     */
+    public boolean shouldSetContentLength() {
+        return false;
     }
 
     /**
-     * Configure this instance by selecting the default document type to use.
+     * Set the configurations for this serializer.
      */
     public void configure(Configuration conf)
     throws ConfigurationException {
-        super.configure(conf);
-
-        String doctype = conf.getChild("doctype-default").getValue(null);
-        if ("none".equalsIgnoreCase(doctype)) {
-            this.doctype_default = null;
-        } else if ("compatible".equalsIgnoreCase(doctype)) {
-            this.doctype_default = HTML401_DOCTYPE_COMPATIBLE;
-        } else if ("strict".equalsIgnoreCase(doctype)) {
-            this.doctype_default = HTML401_DOCTYPE_STRICT;
-        } else if ("loose".equalsIgnoreCase(doctype)) {
-            this.doctype_default = HTML401_DOCTYPE_TRANSITIONAL;
-        } else if ("frameset".equalsIgnoreCase(doctype)) {
-            this.doctype_default = HTML401_DOCTYPE_FRAMESET;
-        } else {
-            /* Default is compatible (MSIE hack) */
-            this.doctype_default = HTML401_DOCTYPE_COMPATIBLE;
-        }
-    }
-
-    /* ====================================================================== */
-
-    /** Empty namespaces declaration. */
-    private static final String NAMESPACES[][] = new String[0][0];
-
-    /** Check if the URI is allowed by this serializer. */
-    private boolean checkNamespace(String nsuri) {
-        if (nsuri.length() == 0) return(true);
-        if (XHTML1_NAMESPACE.equals(nsuri)) return(true);
-        return(false);
-    }
-
-    /* ====================================================================== */
-
-    /**
-     * Write the XML document header.
-     * <p>
-     * This method overrides the default <code>XMLSerializer</code>.behaviour.
-     * </p>
-     */
-    public void head()
-    throws SAXException {
-        // NO NOTHING!
-    }
-
-    /**
-     * Receive notification of the beginning of the document body.
-     *
-     * @param nsuri The namespace URI of the root element.
-     * @param local The local name of the root element.
-     * @param qual The fully-qualified name of the root element.
-     */
-    public void body(String nsuri, String local, String qual)
-    throws SAXException {
-        String name = local.toUpperCase();
-        if (! this.checkNamespace(nsuri)) {
-            throw new SAXException("Unsupported namespace \"" + nsuri + "\" "
-                                   + "for HTML root element \"" + qual + "\""
-                                   + this.getLocation());
+        String encoding = conf.getChild("encoding").getValue(null);
+        try {
+            this.setEncoding(encoding);
+        } catch (UnsupportedEncodingException exception) {
+            throw new ConfigurationException("Encoding not supported: "
+                                             + encoding, exception);
         }
 
-        if (this.doctype == null) this.doctype = this.doctype_default;
-
-        if (XHTML1_DOCTYPE_STRICT.equals(this.doctype)) {
-            this.doctype = HTML401_DOCTYPE_STRICT;
-        } else if (XHTML1_DOCTYPE_TRANSITIONAL.equals(this.doctype)) {
-            this.doctype = HTML401_DOCTYPE_TRANSITIONAL;
-        } else if (XHTML1_DOCTYPE_FRAMESET.equals(this.doctype)) {
-            this.doctype = HTML401_DOCTYPE_FRAMESET;
-        } else if (this.doctype != null) {
-            /* The root element is uppercase, always!!! */
-            this.doctype = new SGMLDocType(this.doctype.getName().toUpperCase(),
-                                       this.doctype.getPublicId(),
-                                       this.doctype.getSystemId());
-        }
-        super.body(XHTML1_NAMESPACE, name, name);
-    }
-
-    /**
-     * Receive notification of the beginning of an element.
-     */
-    public void startElementImpl(String nsuri, String local, String qual,
-                             String namespaces[][], String attributes[][])
-    throws SAXException {
-        String name = local.toUpperCase();
-        if (! this.checkNamespace(nsuri)) {
-            throw new SAXException("Unsupported namespace \"" + nsuri + "\" "
-                                   + "for HTML element \"" + qual + "\""
-                                   + this.getLocation());
-        }
-
-        int length = 0;
-        for (int x = 0; x < attributes.length; x ++) {
-            if (checkNamespace(attributes[x][ATTRIBUTE_NSURI])) length ++;
-        }
-
-        String at[][] = new String[length][ATTRIBUTE_LENGTH];
-        length = 0;
-        for (int x = 0; x < attributes.length; x ++) {
-            if (!checkNamespace(attributes[x][ATTRIBUTE_NSURI])) continue;
-
-            String at_name = attributes[x][ATTRIBUTE_LOCAL].toLowerCase();
-            at[length][ATTRIBUTE_NSURI] = XHTML1_NAMESPACE;
-            at[length][ATTRIBUTE_LOCAL] = at_name;
-            at[length][ATTRIBUTE_QNAME] = at_name;
-            at[length][ATTRIBUTE_VALUE] = attributes[x][ATTRIBUTE_VALUE];
-            length++;
-        }
-
-        // script and style are CDATA sections by default, so no encoding
-        if ( "SCRIPT".equals(name) || "STYLE".equals(name) ) {
-            this.encodeCharacters = false;
-        }
-        super.startElementImpl(XHTML1_NAMESPACE, name, name, NAMESPACES, at);
-    }
-
-    /**
-     * Receive notification of the end of an element.
-     */
-    public void endElementImpl(String nsuri, String local, String qual)
-    throws SAXException {
-        this.closeElement(false);
-
-        String name = local.toUpperCase();
-        if (! this.checkNamespace(nsuri)) {
-            throw new SAXException("Unsupported namespace \"" + nsuri + "\" "
-                                   + "for HTML element \"" + qual + "\""
-                                   + this.getLocation());
-        }
-
-        if (name.equals("AREA")) return;
-        if (name.equals("BASE")) return;
-        if (name.equals("BASEFONT")) return;
-        if (name.equals("BR")) return;
-        if (name.equals("COL")) return;
-        if (name.equals("FRAME")) return;
-        if (name.equals("HR")) return;
-        if (name.equals("IMG")) return;
-        if (name.equals("INPUT")) return;
-        if (name.equals("ISINDEX")) return;
-        if (name.equals("LINK")) return;
-        if (name.equals("META")) return;
-        if (name.equals("PARAM")) return;
-
-        // script and style are CDATA sections by default, so no encoding
-        if ( "SCRIPT".equals(name) || "STYLE".equals(name) ) {
-            this.encodeCharacters = true;
-        }
-        super.endElementImpl(XHTML1_NAMESPACE, name, name);
-    }
-
-    /**
-     * Encode and write a specific part of an array of characters.
-     */
-    protected void encode(char data[], int start, int length)
-    throws SAXException {
-        if ( !this.encodeCharacters ) {
-            this.write(data, start, length);
-            return;
-        }
-        super.encode(data, start, length);
+        this.setIndentPerLevel(conf.getChild("indent").getValueAsInteger(0));
+        this.setDoctypeDefault(conf.getChild("doctype-default").getValue(null));
     }
 }
