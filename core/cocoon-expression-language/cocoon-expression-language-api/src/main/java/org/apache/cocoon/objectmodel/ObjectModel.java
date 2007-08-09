9f07aec37940a5f86a8bbde1f973794e64a6327b
Grzegorz Kossakowski <gkossakowski@apache.org>
2007-08-09 20:37:51 +0000

    Formatting.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@564375 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-expression-language/cocoon-expression-language-api/src/main/java/org/apache/cocoon/objectmodel/ObjectModel.java
+++ core/cocoon-expression-language/cocoon-expression-language-api/src/main/java/org/apache/cocoon/objectmodel/ObjectModel.java
@@ -19,47 +19,52 @@ package org.apache.cocoon.objectmodel;
 import java.util.Map;
 
 /**
- * ObjectModel is a special {@link Map} that cannot be modified using standard {@link Map} methods, except {@link #put(Object, Object)} method.   
+ * ObjectModel is a special {@link Map} that cannot be modified using standard {@link Map} methods, except
+ * {@link #put(Object, Object)} method.
  */
 public interface ObjectModel extends Map {
-    
+
     static public String ROLE = ObjectModel.class.getName();
-    
+
     public static final String CONTEXTBEAN = "contextBean";
+
     public static final String NAMESPACE = "namespace";
-    
+
     /**
-     * @return a {@link org.apache.commons.collections.MultiMap} that contains all stored values in all context in LIFO-compliant order.
-     * Returned {@link org.apache.commons.collections.MultiMap} is {@link org.apache.commons.collections.Unmodifiable}.
+     * @return a {@link org.apache.commons.collections.MultiMap} that contains all stored values in all context in
+     *         LIFO-compliant order. Returned {@link org.apache.commons.collections.MultiMap} is
+     *         {@link org.apache.commons.collections.Unmodifiable}.
      */
     public Map getAll();
-    
+
     /**
-     * Works exactly the same way as {@link Map#put(Object, Object)} but previous value associated to <code>key</code> is not lost in
-     * a case {@link #markLocalContext()} was called before. The previous value is stored and can be recovered by calling 
-     * {@link #markLocalContext()}.
+     * Works exactly the same way as {@link Map#put(Object, Object)} but previous value associated to <code>key</code>
+     * is not lost in a case {@link #markLocalContext()} was called before. The previous value is stored and can be
+     * recovered by calling {@link #markLocalContext()}.
      */
     public Object put(Object key, Object value);
-    
-    /** 
+
+    /**
      * @see #put(Object, Object)
      */
     public void putAll(Map mapToCopy);
-    
+
     /**
-     * Marks new local context. Such mark is useful to do a clean up of entries. 
+     * Marks new local context. Such mark is useful to do a clean up of entries.
      */
     public void markLocalContext();
-    
+
     /**
      * Cleans up entries put to ObjectModel since last {@link #markLocalContext()} call.
      */
     public void cleanupLocalContext();
-    
+
     /**
-     * <p>Copies properties (both static and dynamic) of current context bean to the Object Model. The method is useful
-     * when you want, for example, to access properties of context bean in JEXL expression and omit <code>contextBean.</code>
-     * part.</p>
+     * <p>
+     * Copies properties (both static and dynamic) of current context bean to the Object Model. The method is useful
+     * when you want, for example, to access properties of context bean in JEXL expression and omit
+     * <code>contextBean.</code> part.
+     * </p>
      * 
      * @see org.apache.commons.jxpath.JXPathBeanInfo.isDynamic() for description of static and dynamic properties
      */
