b2bd6153be0302031e73bc31ecc57a223db63784
Grzegorz Kossakowski <gkossakowski@apache.org>
2007-08-17 10:00:23 +0000

    COCOON-2115: Moved LegacyStringTemplaterParser back to cocoon-expression-language-impl module.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@566995 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-expression-language/cocoon-expression-language-impl/src/main/java/org/apache/cocoon/el/impl/parsing/LegacyStringTemplateParser.java
+++ core/cocoon-expression-language/cocoon-expression-language-impl/src/main/java/org/apache/cocoon/el/impl/parsing/LegacyStringTemplateParser.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cocoon.components.treeprocessor.variables;
+package org.apache.cocoon.el.impl.parsing;
 
 import java.io.Reader;
 import java.util.LinkedList;
