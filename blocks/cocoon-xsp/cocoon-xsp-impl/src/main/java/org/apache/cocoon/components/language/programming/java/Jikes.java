02ab9e8928215352065fad0f44effb8169496903
Carsten Ziegeler <cziegeler@apache.org>
2007-08-21 07:08:39 +0000

    Use new package names for thread module.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@567987 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-xsp/cocoon-xsp-impl/src/main/java/org/apache/cocoon/components/language/programming/java/Jikes.java
+++ blocks/cocoon-xsp/cocoon-xsp-impl/src/main/java/org/apache/cocoon/components/language/programming/java/Jikes.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ import org.apache.avalon.framework.service.ServiceException;
 import org.apache.avalon.framework.service.ServiceManager;
 import org.apache.avalon.framework.service.Serviceable;
 import org.apache.cocoon.components.language.programming.CompilerError;
-import org.apache.cocoon.components.thread.RunnableManager;
+import org.apache.cocoon.thread.RunnableManager;
 import EDU.oswego.cs.dl.util.concurrent.CountDown;
 
 /**
@@ -46,7 +46,7 @@ public class Jikes extends AbstractJavaCompiler implements Serviceable {
     static final int BUFFER_SIZE = 512;
 
     private ServiceManager m_serviceManager;
-    
+
     private class StreamPumper implements Runnable {
 
         private BufferedInputStream stream;
