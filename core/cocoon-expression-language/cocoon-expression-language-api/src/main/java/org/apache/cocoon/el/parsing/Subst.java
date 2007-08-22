bc04dc71e0dc8cf04eec5bb89c36447fe6b4ce40
Carsten Ziegeler <cziegeler@apache.org>
2007-08-22 18:34:47 +0000

    Clean up interfaces a little bit.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@568724 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-expression-language/cocoon-expression-language-api/src/main/java/org/apache/cocoon/el/parsing/Subst.java
+++ core/cocoon-expression-language/cocoon-expression-language-api/src/main/java/org/apache/cocoon/el/parsing/Subst.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,51 +25,23 @@ import org.apache.cocoon.objectmodel.ObjectModel;
  */
 public interface Subst {
 
-    public Object getCompiledExpression();
+    Object getCompiledExpression();
 
-    public Object getNode(ObjectModel objectModel) throws Exception;
+    Object getNode(ObjectModel objectModel) throws Exception;
 
-    public Iterator getIterator(ObjectModel objectModel) throws Exception;
+    Iterator getIterator(ObjectModel objectModel) throws Exception;
 
-    public Boolean getBooleanValue(ObjectModel objectModel) throws Exception;
+    Boolean getBooleanValue(ObjectModel objectModel) throws Exception;
 
-    public String getStringValue(ObjectModel objectModel) throws Exception;
+    String getStringValue(ObjectModel objectModel) throws Exception;
 
-    public Number getNumberValue(ObjectModel objectModel) throws Exception;
+    Number getNumberValue(ObjectModel objectModel) throws Exception;
 
-    public int getIntValue(ObjectModel objectModel) throws Exception;
+    int getIntValue(ObjectModel objectModel) throws Exception;
 
-    public Object getValue(ObjectModel objectModel) throws Exception;
+    Object getValue(ObjectModel objectModel) throws Exception;
 
-    public void setLenient(Boolean lenient);
+    void setLenient(Boolean lenient);
 
-    public String getRaw();
-
-    public static final Iterator EMPTY_ITER = new Iterator() {
-            public boolean hasNext() {
-                return false;
-            }
-    
-            public Object next() {
-                return null;
-            }
-    
-            public void remove() {
-                // EMPTY
-            }
-        };
-    public static final Iterator NULL_ITER = new Iterator() {
-            public boolean hasNext() {
-                return true;
-            }
-    
-            public Object next() {
-                return null;
-            }
-    
-            public void remove() {
-                // EMPTY
-            }
-        };
-    // VOID
+    String getRaw();
 }
--- core/cocoon-expression-language/cocoon-expression-language-api/src/main/java/org/apache/cocoon/objectmodel/ObjectModel.java
+++ core/cocoon-expression-language/cocoon-expression-language-api/src/main/java/org/apache/cocoon/objectmodel/ObjectModel.java
@@ -26,52 +26,52 @@ import org.apache.commons.collections.MultiMap;
  */
 public interface ObjectModel extends Map {
 
-    static public String ROLE = ObjectModel.class.getName();
+    String ROLE = ObjectModel.class.getName();
 
-    public static final String CONTEXTBEAN = "contextBean";
+    String CONTEXTBEAN = "contextBean";
 
-    public static final String NAMESPACE = "namespace";
-    
-    public static final String PARAMETERS_PATH = "cocoon/parameters";
+    String NAMESPACE = "namespace";
+
+    String PARAMETERS_PATH = "cocoon/parameters";
 
     /**
      * @return a {@link MultiMap} that contains all stored values in all context in LIFO-compliant order.
      *         Returned {@link MultiMap} is {@link org.apache.commons.collections.Unmodifiable}.
      */
-    public MultiMap getAll();
+    MultiMap getAll();
 
     /**
      * Works exactly the same way as {@link Map#put(Object, Object)} but previous value associated to <code>key</code>
      * is not lost in a case {@link #markLocalContext()} was called before. The previous value is stored and can be
      * recovered by calling {@link #markLocalContext()}.
      */
-    public Object put(Object key, Object value);
+    Object put(Object key, Object value);
 
     /**
      * @see #put(Object, Object)
      */
-    public void putAll(Map mapToCopy);
+    void putAll(Map mapToCopy);
 
     /**
      * Marks new local context. Such mark is useful to do a clean up of entries.
      */
-    public void markLocalContext();
+    void markLocalContext();
 
     /**
      * Cleans up entries put to ObjectModel since last {@link #markLocalContext()} call.
      */
-    public void cleanupLocalContext();
+    void cleanupLocalContext();
 
     /**
      * Puts object at certain <code>path</code>. Each segment of path is separated by "/" symbol. This method
      * supports only traversing through objects implementing {@link Map} interface. If certain segment does not exist it
      * will be created automatically.
-     * 
+     *
      * @param path
      *            where the <code>value</code> should be put at
      * @param value that is going to be put
      */
-    public void putAt(String path, Object value);
+    void putAt(String path, Object value);
 
     /**
      * <p>
@@ -79,15 +79,15 @@ public interface ObjectModel extends Map {
      * when you want, for example, to access properties of context bean in JEXL expression and omit
      * <code>contextBean.</code> part.
      * </p>
-     * 
+     *
      * @see org.apache.commons.jxpath.JXPathBeanInfo.isDynamic() for description of static and dynamic properties
      */
-    public void fillContext();
-    
+    void fillContext();
+
     /**
      * <p>Sets parent object model so newly created instance can inherit values from parent but cannot modify it.</p>
-     * 
+     *
      * @param parentObjectModel
      */
-    public void setParent(ObjectModel parentObjectModel);
+    void setParent(ObjectModel parentObjectModel);
 }
