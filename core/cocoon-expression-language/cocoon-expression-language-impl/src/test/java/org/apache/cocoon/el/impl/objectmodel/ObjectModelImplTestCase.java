de32491b650ebdda62c9e0170306b67ccf11768b
Carsten Ziegeler <cziegeler@apache.org>
2007-08-23 09:46:14 +0000

    Refactoring of expression language to use o.a.c.el package names (part 7)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@568910 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-expression-language/cocoon-expression-language-impl/src/test/java/org/apache/cocoon/el/impl/objectmodel/ObjectModelImplTestCase.java
+++ core/cocoon-expression-language/cocoon-expression-language-impl/src/test/java/org/apache/cocoon/el/impl/objectmodel/ObjectModelImplTestCase.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cocoon.objectmodel;
+package org.apache.cocoon.el.impl.objectmodel;
 
 import java.util.Collection;
 import java.util.Map;
