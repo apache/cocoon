8214a6750bdf6fbd0966328d5cc0c0df9f8cb2ce
Grzegorz Kossakowski <gkossakowski@apache.org>
2007-09-14 21:58:27 +0000

    COCOON-2108: Construct xmodule:flow-attr does not accept document object. Thanks to Hugh Sparks for contributing the patch and Kazo Csaba for reporting the issue on mailing list.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@575803 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-sitemap/cocoon-sitemap-impl/src/main/java/org/apache/cocoon/components/modules/input/JXPathHelper.java
+++ core/cocoon-sitemap/cocoon-sitemap-impl/src/main/java/org/apache/cocoon/components/modules/input/JXPathHelper.java
@@ -92,9 +92,7 @@ public class JXPathHelper {
         try {
             JXPathContext jxContext = JXPathContext.newContext(contextObj);
             setup(setup, jxContext, modeConf);
-
-            Object obj = jxContext.getValue(name);
-            return obj;
+            return jxContext.selectSingleNode(name);
         } catch (Exception e) {
             throw new ConfigurationException("Module does not support <" + name + ">" + "attribute.", e);
         }
