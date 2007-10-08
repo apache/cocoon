1b2c541af19534ab99c4de7f1f62c63c326e1be3
Leszek Gawron <lgawron@apache.org>
2007-10-08 16:17:57 +0000

    a little hacky way to have cocoon beans configured
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@582863 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-sitemap/cocoon-sitemap-impl/src/test/java/org/apache/cocoon/MockWebApplicationContext.java
+++ core/cocoon-sitemap/cocoon-sitemap-impl/src/test/java/org/apache/cocoon/MockWebApplicationContext.java
@@ -2,11 +2,17 @@ package org.apache.cocoon;
 
 import javax.servlet.ServletContext;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
 
 public class MockWebApplicationContext extends GenericApplicationContext implements WebApplicationContext {
     ServletContext sc;
+
+    public MockWebApplicationContext(DefaultListableBeanFactory parent, ServletContext context) {
+        super(parent);
+        this.sc = context;
+    }
 
     public MockWebApplicationContext(ServletContext context) {
         this.sc = context;
