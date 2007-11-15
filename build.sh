0add655a34cdc14f9001f29432a99cd1720c8a6a
Vadim Gritsenko <vgritsenko@apache.org>
2007-11-15 22:38:34 +0000

    trying to make it run on solaris
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@595477 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 
 # Java compiler requires at least 128m of memory for cocoon-serializers block,
 # plus another 128m for maven itself to fit its dependency tree.
-export MAVEN_OPTS=-Xmx256m
+export MAVEN_OPTS="-Xmx256m"
 
 # Unit tests debugging option
 # -Dmaven.surefire.debug
