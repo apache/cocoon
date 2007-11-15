268e08b4c986f91bf013fe817eef986e2e098fcb
Vadim Gritsenko <vgritsenko@apache.org>
2007-11-15 23:51:12 +0000

    pass arguments
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@595492 13f79535-47bb-0310-9956-ffa450edef68

--- cocoon.sh
+++ cocoon.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
-cd core/cocoon-webapp && echo "Starting in `pwd`" && mvn -Djava.awt.headless=true -Dorg.apache.cocoon.mode=dev jetty:run
+cd core/cocoon-webapp && echo "Starting in `pwd`" && mvn -Djava.awt.headless=true -Dorg.apache.cocoon.mode=dev $* jetty:run
 
