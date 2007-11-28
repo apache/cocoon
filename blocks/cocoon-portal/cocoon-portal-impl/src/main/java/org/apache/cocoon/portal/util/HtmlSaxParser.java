291d8911492530769a3e79ee77f3fabfd1d74b1d
Carsten Ziegeler <cziegeler@apache.org>
2007-11-28 00:40:36 +0000

    Create bean describing a layout type to OM. Start using bean maps for layout and coplet types. Fix Avalon configuration for layouts.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@598833 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-portal/cocoon-portal-impl/src/main/java/org/apache/cocoon/portal/util/HtmlSaxParser.java
+++ blocks/cocoon-portal/cocoon-portal-impl/src/main/java/org/apache/cocoon/portal/util/HtmlSaxParser.java
@@ -21,13 +21,12 @@ import java.io.StringReader;
 import java.util.Iterator;
 import java.util.Properties;
 
-import org.apache.cocoon.xml.ContentHandlerWrapper;
-import org.apache.excalibur.xml.sax.XMLConsumer;
 import org.apache.xerces.parsers.AbstractSAXParser;
 import org.cyberneko.html.HTMLConfiguration;
 import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
+import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
 import org.xml.sax.ext.LexicalHandler;
 
@@ -72,17 +71,24 @@ public class HtmlSaxParser extends AbstractSAXParser {
         }
     }
 
-    public static XMLConsumer getContentFilter(ContentHandler ch) {
+    /**
+     * Return a content filter which streams every element except body and html.
+     * @param ch
+     * @return A content handler.
+     */
+    public static ContentHandler getContentFilter(ContentHandler ch) {
+        if ( ch instanceof LexicalHandler ) {
+            return new ExtendedContentFilter(ch);
+        }
         return new ContentFilter(ch);
     }
 
-    protected static final class ContentFilter extends ContentHandlerWrapper {
+    protected static class ContentFilter implements ContentHandler {
+
+        protected final ContentHandler ch;
 
         public ContentFilter(ContentHandler ch) {
-            this.setContentHandler(ch);
-            if ( ch instanceof LexicalHandler ) {
-                this.setLexicalHandler((LexicalHandler)ch);
-            }
+            this.ch = ch;
         }
 
         /**
@@ -90,7 +96,7 @@ public class HtmlSaxParser extends AbstractSAXParser {
          */
         public void endElement(String uri, String loc, String raw) throws SAXException {
             if ( !loc.equals("html") && !loc.equals("body") ) {
-                super.endElement(uri, loc, raw);
+                this.ch.endElement(uri, loc, raw);
             }
         }
 
@@ -99,8 +105,115 @@ public class HtmlSaxParser extends AbstractSAXParser {
          */
         public void startElement(String uri, String loc, String raw, Attributes a) throws SAXException {
             if ( !loc.equals("html") && !loc.equals("body") ) {
-                super.startElement(uri, loc, raw, a);
+                this.ch.startElement(uri, loc, raw, a);
             }
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#characters(char[], int, int)
+         */
+        public void characters(char[] ch, int start, int length)
+                throws SAXException {
+            this.ch.characters(ch, start, length);
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#endDocument()
+         */
+        public void endDocument() throws SAXException {
+            ch.endDocument();
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#endPrefixMapping(java.lang.String)
+         */
+        public void endPrefixMapping(String prefix) throws SAXException {
+            ch.endPrefixMapping(prefix);
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#ignorableWhitespace(char[], int, int)
+         */
+        public void ignorableWhitespace(char[] ch, int start, int length)
+                throws SAXException {
+            this.ch.ignorableWhitespace(ch, start, length);
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#processingInstruction(java.lang.String, java.lang.String)
+         */
+        public void processingInstruction(String target, String data)
+                throws SAXException {
+            ch.processingInstruction(target, data);
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#setDocumentLocator(org.xml.sax.Locator)
+         */
+        public void setDocumentLocator(Locator locator) {
+            ch.setDocumentLocator(locator);
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#skippedEntity(java.lang.String)
+         */
+        public void skippedEntity(String name) throws SAXException {
+            ch.skippedEntity(name);
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#startDocument()
+         */
+        public void startDocument() throws SAXException {
+            ch.startDocument();
+        }
+
+        /**
+         * @see org.xml.sax.ContentHandler#startPrefixMapping(java.lang.String, java.lang.String)
+         */
+        public void startPrefixMapping(String prefix, String uri)
+                throws SAXException {
+            ch.startPrefixMapping(prefix, uri);
+        }
+    }
+
+    protected static class ExtendedContentFilter extends ContentFilter implements LexicalHandler {
+
+        protected final LexicalHandler lh;
+
+        public ExtendedContentFilter(final ContentHandler ch) {
+            super(ch);
+            this.lh = (LexicalHandler)ch;
+        }
+
+        public void comment(char[] arg0, int arg1, int arg2)
+                throws SAXException {
+            lh.comment(arg0, arg1, arg2);
+        }
+
+        public void endCDATA() throws SAXException {
+            lh.endCDATA();
+        }
+
+        public void endDTD() throws SAXException {
+            lh.endDTD();
+        }
+
+        public void endEntity(String arg0) throws SAXException {
+            lh.endEntity(arg0);
+        }
+
+        public void startCDATA() throws SAXException {
+            lh.startCDATA();
+        }
+
+        public void startDTD(String arg0, String arg1, String arg2)
+                throws SAXException {
+            lh.startDTD(arg0, arg1, arg2);
+        }
+
+        public void startEntity(String arg0) throws SAXException {
+            lh.startEntity(arg0);
         }
     }
 }
