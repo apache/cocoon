3cc1bea6e1e5ddc08ea48bbdea42bae21a647556
Poetz Reinhard <reinhard@apache.org>
2007-11-29 22:06:23 +0000

    - pass attributes, headers and parameters from parent request to sub request - expand sample
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@599594 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-servlet-service/cocoon-servlet-service-sample/src/main/java/org/apache/cocoon/servletservice/demo2/DemoServlet.java
+++ core/cocoon-servlet-service/cocoon-servlet-service-sample/src/main/java/org/apache/cocoon/servletservice/demo2/DemoServlet.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,7 +32,14 @@ public class DemoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/plain");
         PrintWriter writer = response.getWriter();
-        writer.println("Demo2");
+        writer.println("Content From: " + this.getClass().getName());
+        writer.println("******************************************************************");
+        writer.println("request.getAttribute(\"foo\"): " + request.getAttribute("foo"));
+        writer.println("request.getParameter(\"x\"): " + request.getParameter("x"));
+        writer.println("request.getHeader(\"User-Agent\"): " + request.getHeader("User-Agent"));
+        // set a request attribute
+        request.setAttribute("foo1", "bar1");
+        writer.println("request.getAttribute(\"foo1\") [from sub request]: " + request.getAttribute("foo1"));
         writer.close();
     }
 
