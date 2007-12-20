8b1f9601b41ce0a891604f1df667b8cb9949dfb6
Carsten Ziegeler <cziegeler@apache.org>
2007-12-20 09:16:33 +0000

    Move sitemap related class into sitemap module.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@605841 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-portal/cocoon-portal-sitemap/src/main/java/org/apache/cocoon/portal/sitemap/InputModuleHelper.java
+++ blocks/cocoon-portal/cocoon-portal-sitemap/src/main/java/org/apache/cocoon/portal/sitemap/InputModuleHelper.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cocoon.portal.util;
+package org.apache.cocoon.portal.sitemap;
 
 import java.util.HashMap;
 import java.util.Iterator;
