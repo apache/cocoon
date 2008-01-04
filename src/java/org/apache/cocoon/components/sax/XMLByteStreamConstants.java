4076b9d909e3a2a447c5219c54b33f97494cc005
Antonio Gallardo Rivera <antonio@apache.org>
2008-01-04 19:40:50 +0000

    Backporting refactoring from cocoon 2.2.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@608968 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/cocoon/components/sax/XMLByteStreamConstants.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.components.sax;
+
+/**
+ * The common constants for SAX byte streaming
+ *
+ * @version $Id: XMLByteStreamConstants.java 587751 2007-10-24 02:41:36Z vgritsenko $
+  */
+public interface XMLByteStreamConstants {
+
+    int START_DOCUMENT         = 0;
+    int END_DOCUMENT           = 1;
+    int START_PREFIX_MAPPING   = 2;
+    int END_PREFIX_MAPPING     = 3;
+    int START_ELEMENT          = 4;
+    int END_ELEMENT            = 5;
+    int CHARACTERS             = 6;
+    int IGNORABLE_WHITESPACE   = 7;
+    int PROCESSING_INSTRUCTION = 8;
+    int COMMENT                = 9;
+    int LOCATOR                = 10;
+    int START_DTD              = 11;
+    int END_DTD                = 12;
+    int START_CDATA            = 13;
+    int END_CDATA              = 14;
+    int SKIPPED_ENTITY         = 15;
+    int START_ENTITY           = 16;
+    int END_ENTITY             = 17;
+}
---
+++ src/java/org/apache/cocoon/components/sax/XMLOutputStreamCompiler.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cocoon.components.sax;
+
+import org.apache.cocoon.components.sax.AbstractXMLByteStreamCompiler;
+
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+/**
+ * This class compiles SAX events to an OutputStream.
+ * If you want to reuse this instance, make sure to call {@link #recycle(OutputStream)} to set a new OutputStream
+ * and reset the class inbetween two compilation tasks.
+ *
+ * @version $Id: XMLOutputStreamCompiler.java 587751 2007-10-24 02:41:36Z vgritsenko $
+ */
+public final class XMLOutputStreamCompiler
+    extends AbstractXMLByteStreamCompiler {
+
+    /** The buffer for the compile xml byte stream. */
+    private OutputStream os;
+
+    public XMLOutputStreamCompiler(final OutputStream out)
+        throws IOException {
+        super();
+        this.recycle(out);
+    }
+
+    public void recycle(final OutputStream out) {
+        this.os = out;
+        super.recycle();
+    }
+
+    public void recycle() {
+        this.recycle(this.os);
+    }
+
+    protected void write(final int b)
+        throws SAXException {
+        try {
+            this.os.write((byte)b);
+        } catch(final IOException e) {
+            throw new SAXException(e);
+        }
+    }
+}
