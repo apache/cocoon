28984d962608876158253ea17ff7c3ea657e76a2
Grzegorz Kossakowski <gkossakowski@apache.org>
2008-01-09 21:12:34 +0000

    <action dev="gkossakowski" type="add" issue="COCOON-2133" due-to="Robin Wyles" due-to-email="reb@robinwyles.com">   Addition of "allow-enlarge" parameter to ImageOp resize operation. Many thanks to Robin Wyles for providing test-cases covering added functionality. </action>
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@610567 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-imageop/cocoon-imageop-impl/src/test/java/org/apache/cocoon/reading/imageop/ImageOpReaderTestCase.java
+++ blocks/cocoon-imageop/cocoon-imageop-impl/src/test/java/org/apache/cocoon/reading/imageop/ImageOpReaderTestCase.java
@@ -16,6 +16,12 @@
  */
 package org.apache.cocoon.reading.imageop;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+
+import javax.imageio.ImageIO;
+
+import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.SitemapComponentTestCase;
 import org.apache.commons.io.IOUtils;
 import org.apache.excalibur.source.Source;
@@ -32,5 +38,91 @@ public class ImageOpReaderTestCase extends SitemapComponentTestCase {
         byte[] expectedBinaryArray = IOUtils.toByteArray(expectedSource.getInputStream());
         
         assertIdentical(expectedBinaryArray, read("imageop-no-effects", SitemapComponentTestCase.EMPTY_PARAMS, inputURI));
+    }
+    
+    public void testResizeOperationAllowEnlargeTrue() throws Exception {
+        BufferedImage resizedImage = null;
+        try {
+            String inputURI = imgBase + "4x2.jpg";
+
+            Parameters params = new Parameters();
+            
+            // Both dimensions being enlarged, image should be resized in both dimensions
+            params.setParameter("resize-allow-enlarge", "true");
+            params.setParameter("resize-width", "8");
+            params.setParameter("resize-height", "4");
+            byte[] returnedBinaryArray = read("imageop-resize", params,
+                    inputURI);
+            resizedImage = ImageIO.read(new ByteArrayInputStream(
+                    returnedBinaryArray));
+            assertEquals(8, resizedImage.getWidth());
+            assertEquals(4, resizedImage.getHeight());
+            
+            // One dimension being enlarged, image should be resized in one dimension
+            params.setParameter("resize-height", "2");
+            returnedBinaryArray = read("imageop-resize", params, inputURI);
+            resizedImage = ImageIO.read(new ByteArrayInputStream(
+                    returnedBinaryArray));
+            assertEquals(8, resizedImage.getWidth());
+            assertEquals(2, resizedImage.getHeight());
+            
+            // One dimension being enlarged with preserve-ratio, image should be resized proportionally
+            params.removeParameter("resize-height");
+            params.setParameter("resize-preserve-ratio", "true");
+            returnedBinaryArray = read("imageop-resize", params, inputURI);
+            resizedImage = ImageIO.read(new ByteArrayInputStream(
+                    returnedBinaryArray));
+            assertEquals(8, resizedImage.getWidth());
+            assertEquals(4, resizedImage.getHeight());
+            
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (resizedImage != null)
+                resizedImage.flush();
+        }
+    }
+
+    public void testResizeOperationAllowEnlargeFalse() throws Exception {
+        BufferedImage resizedImage = null;
+        try {
+            String inputURI = imgBase + "4x2.jpg";
+
+            Parameters params = new Parameters();
+            params.setParameter("resize-allow-enlarge", "false");
+            
+            // Both dimensions being enlarged, image should not be resized
+            params.setParameter("resize-width", "8");
+            params.setParameter("resize-height", "4");
+            byte[] returnedBinaryArray = read("imageop-resize", params,
+                    inputURI);
+            resizedImage = ImageIO.read(new ByteArrayInputStream(
+                    returnedBinaryArray));
+            assertEquals(4, resizedImage.getWidth());
+            assertEquals(2, resizedImage.getHeight());
+            
+            // One dimension being enlarged, image should not be resized
+            params.setParameter("resize-height", "2");
+            returnedBinaryArray = read("imageop-resize", params, inputURI);
+            resizedImage = ImageIO.read(new ByteArrayInputStream(
+                    returnedBinaryArray));
+            assertEquals(4, resizedImage.getWidth());
+            assertEquals(2, resizedImage.getHeight());
+            
+            // One dimension being enlarged with preserve-ratio, image should not be resized
+            params.removeParameter("resize-height");
+            params.setParameter("resize-preserve-ratio", "true");
+            returnedBinaryArray = read("imageop-resize", params, inputURI);
+            resizedImage = ImageIO.read(new ByteArrayInputStream(
+                    returnedBinaryArray));
+            assertEquals(4, resizedImage.getWidth());
+            assertEquals(2, resizedImage.getHeight());
+            
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (resizedImage != null)
+                resizedImage.flush();
+        }
     }
 }
