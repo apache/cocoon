267b5684e0d4191d897b546495d2c76e3216e392
Poetz Reinhard <reinhard@apache.org>
2008-01-22 08:28:37 +0000

    enable Cobertura for integration tests
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@614143 13f79535-47bb-0310-9956-ffa450edef68

--- tools/cocoon-it-fw/src/main/java/org/apache/cocoon/maven/test/jetty/JettyStopperMojo.java
+++ tools/cocoon-it-fw/src/main/java/org/apache/cocoon/maven/test/jetty/JettyStopperMojo.java
@@ -4,7 +4,7 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 /**
- * Stop all the JettyContainer instance.
+ * Stop the Jetty instance.
  *
  * @goal jetty-stop
  */
@@ -20,11 +20,20 @@ public class JettyStopperMojo extends AbstractMojo {
             this.getLog().info("Skip starting server environment.");
             return;
         }
-
         try {
             new JettyContainer().stop();
         } catch (Exception e) {
             this.getLog().error("Can't stop JettyContainer. ", e);
+        }
+        // flush information collected by Cobertura
+        try {
+            String className = "net.sourceforge.cobertura.coveragedata.ProjectData";
+            String methodName = "saveGlobalProjectData";
+            Class saveClass = Class.forName(className);
+            java.lang.reflect.Method saveMethod = saveClass.getDeclaredMethod(methodName, new Class[0]);
+            saveMethod.invoke(null, new Object[0]);
+        } catch (Throwable t) {
+            this.getLog().debug("Error while flushing information collected by Cobertura.");
         }
     }
 }
