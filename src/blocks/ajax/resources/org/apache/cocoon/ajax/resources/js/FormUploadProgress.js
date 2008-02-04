3bd41e3ede0a42a5cac8258a94f54ce3f27b4f85
Antonio Gallardo Rivera <antonio@apache.org>
2008-02-04 17:30:21 +0000

    Explicitely turn cache off.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@618335 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/ajax/resources/org/apache/cocoon/ajax/resources/js/FormUploadProgress.js
+++ src/blocks/ajax/resources/org/apache/cocoon/ajax/resources/js/FormUploadProgress.js
@@ -14,19 +14,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
  /*
     FormUploadProgress Widget
-    
+
     This is a Dojo Widget for Cocoon that monitors the progress of uploads from a form.
-    
+
     It works by sending periodical XMLHttp calls to Cocoon in the background, while the files are being uploaded.
     Cocoon responds with JSON data which is used to draw the progress bar and status strings.
 
     @version $Id$
-    
+
  */
- 
+
 dojo.provide("cocoon.ajax.FormUploadProgress");
 dojo.require("dojo.event");
 dojo.require("dojo.widget.HtmlWidget");
@@ -41,31 +41,31 @@ dojo.widget.defineWidget(
         color: "#336699",           // the colour of the text and border
         ready: "Ready.",            // Strings the widget needs, that may require external i18n transformation
         connecting: "Connecting ...",
-        
+
         // widget API properties
         ns: "forms",
         widgetType: "FormUploadProgress",
         isContainer: true,
         templateString: '<div class="FormUpload">' +
-                            '<div class="FormUploadUser" dojoAttachPoint="containerNode"></div>' + 
+                            '<div class="FormUploadUser" dojoAttachPoint="containerNode"></div>' +
                             '<div class="FormUploadProgress" style="border:1px solid ${this.color};margin:2px;height: 16px;position:relative;">' +
-                                '<div style="width:0px;background:${this.background};position:absolute;top:0;left:0;width:0;right:0;" class="bar" dojoAttachPoint="barNode">&nbsp;</div>' + 
-                                '<div style="color:${this.color};position:absolute;top:0;left:0;bottom:0;right:0;background:none;padding:2px;font-size:10px;overflow:hidden;" class="data" dojoAttachPoint="msgNode">${this.ready}</div>' + 
+                                '<div style="width:0px;background:${this.background};position:absolute;top:0;left:0;width:0;right:0;" class="bar" dojoAttachPoint="barNode">&nbsp;</div>' +
+                                '<div style="color:${this.color};position:absolute;top:0;left:0;bottom:0;right:0;background:none;padding:2px;font-size:10px;overflow:hidden;" class="data" dojoAttachPoint="msgNode">${this.ready}</div>' +
                             '</div>' +
                          '</div>',
-                        
+
         // DOM Nodes in the template
         containerNode: null,   // the node containing the user content
         msgNode: null,         // the node containing the progress status text
         barNode: null,         // the progress bar node
-        
+
         // private properties
         _form: null,           // this widget's form
         _target: null,         // the form's *Form widget
         _timer: null,          // timer for periodical updates
         _busy: false,          // busy getting the status
         _delay: 2000,          // 2 secs between status calls
-        
+
         // widget interface
         postCreate: function(){
             // work out where to attach the event listener
@@ -83,7 +83,7 @@ dojo.widget.defineWidget(
                 throw new Error("The FormUploadProgress widget may only be used inside a form.");
             }
         },
-    
+
         // widget interface
         destroy: function(){
             if (this._target) {
@@ -112,14 +112,15 @@ dojo.widget.defineWidget(
             dojo.io.bind({
                 url: new dojo.uri.Uri(djConfig["baseRelativePath"], "../../../_cocoon/system/ajax/upload/status"),
                 mimetype: "text/json",
+                useCache: false,
                 handle: dojo.lang.hitch(this, function(type, data, evt) {
                     if (type == "load") {
                         this._update(data);
                         this._busy = false;
                     } else if (type == "error") {
                         dojo.debug("FormUploadProgress - status request failed");
-                    }           
-                })
+                    }
+                }),
             });
             this._busy = true;
         },
@@ -133,7 +134,7 @@ dojo.widget.defineWidget(
                     this._timer.stop();
                 } else {
                     this.barNode.style.width = status.percent + "%";
-                }               
+                }
             }
             if (status.message) this.msgNode.innerHTML = status.message;
         },
@@ -150,6 +151,4 @@ dojo.widget.defineWidget(
             });
             return hasFile;
         }
-
 });
-
