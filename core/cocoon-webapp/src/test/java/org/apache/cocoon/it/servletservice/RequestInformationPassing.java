963b0307e08b60fadd65ff0415b7779b87688bf2
Reinhard Pötz <reinhard@apache.org>
2008-03-11 15:53:12 +0000

    add integration tests for the servlet-service framework
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@635983 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-webapp/src/test/java/org/apache/cocoon/it/servletservice/RequestInformationPassing.java
+++ core/cocoon-webapp/src/test/java/org/apache/cocoon/it/servletservice/RequestInformationPassing.java
@@ -7,8 +7,50 @@ import org.apache.cocoon.tools.it.HtmlUnitTestCase;
 public class RequestInformationPassing extends HtmlUnitTestCase {
 
     public void testAttributes() throws Exception {
-        this.loadResponse("test1/test1");
+        this.webClient.addRequestHeader("my-header-param", "6");
+        this.loadXmlPage("test1/test4?foo=5");
         Assert.assertTrue(this.response.getStatusCode() == 200);
+
+        // external request
+        // ~~~~~~~~~~~~~~~~
+
+        assertXPath("/page/request-parameters/parameter[@name='foo']/value", "5");
+        assertXPath("/page/header-parameters/parameter[@name='my-header-param']/value", "6");
+        assertXPath("/page/request-attributes/attribute[@name='foo']/value", "bar");
+
+
+        // sub request: requst attributes, parameters and header
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        assertXPath("/page/sub-request/page/request-parameters/parameter[@name='foo']", "5");  // from the original request
+        assertXPath("/page/sub-request/page/request-parameters/parameter[@name='xyz']", "5");  // passed value
+        assertXPath("/page/sub-request/page/header-parameters/parameter[@name='my-header-param']/value", "6"); // from the original request
+        assertXPath("/page/sub-request/page/request-attributes/attribute[@name='foo1']/value", "bar1");
+        assertXPath("/page/check-sub/request-attribute[@name='foo1']/value", "null");
+
+
+        // sub request: session handling
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        // a session attribute set from within the called request
+        assertXPath("/page/sub-request[@name='demo2-test1']/page/session-attributes/attribute[@name='attribute-from-called-request']",
+                        "84");
+
+        // a session attribute set from within the calling request
+        assertXPath("/page/sub-request[@name='demo2-test1']/page/session-attributes/attribute[@name='attribute-from-calling-request']",
+                        "42");
+
+        // check if a session attribute set in a called request is not accessible in the calling request session
+        assertXPath("/page/check-sub/session-attribute[@name='attribute-from-called-request']/value", "null");
+
+        // check if the attribute set the called request before, is still available
+        assertXPath("/page/sub-request[@name='demo2-test2']/page/session-attributes/attribute[@name='attribute-from-called-request']",
+                    "84");
+        // check if the attribute set the calling request is still available
+        assertXPath("/page/sub-request[@name='demo2-test2']/page/session-attributes/attribute[@name='attribute-from-calling-request']",
+                    "42");  // a session attribute set from within the calling request
+
+
     }
 
 }
