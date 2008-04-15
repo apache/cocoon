49b1fcf2b909f64dca4f4905f9d4ef1dc4e41e00
Reinhard Pötz <reinhard@apache.org>
2008-04-15 07:40:21 +0000

    lazy initialization (... have to figure out why an advice around the init() method on servlets doesn't work as expected)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@648145 13f79535-47bb-0310-9956-ffa450edef68

--- core/cocoon-core/src/main/java/org/apache/cocoon/sitemap/SitemapServlet.java
+++ core/cocoon-core/src/main/java/org/apache/cocoon/sitemap/SitemapServlet.java
@@ -63,6 +63,8 @@ public class SitemapServlet extends HttpServlet {
      */
     private static final String PARAM_PASS_THROUGH = "pass-through";
 
+    private boolean initialized;
+
 
     protected RequestProcessor processor;
 
@@ -72,7 +74,6 @@ public class SitemapServlet extends HttpServlet {
      */
     public void init() throws ServletException {
         super.init();
-        this.processor = new RequestProcessor(getServletContext());
     }
 
     /**
@@ -80,6 +81,10 @@ public class SitemapServlet extends HttpServlet {
      */
     protected void service(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        if (!initialized) {
+            this.processor = new RequestProcessor(getServletContext());
+            initialized = true;
+        }
 
         this.processor.service(request, response);
     }
