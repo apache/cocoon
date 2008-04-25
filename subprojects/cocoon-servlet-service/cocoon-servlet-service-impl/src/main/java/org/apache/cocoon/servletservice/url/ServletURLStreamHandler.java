64cc6a0cdaab76ebce76e8e81240d8db3a8aa91b
Grzegorz Kossakowski <gkossakowski@apache.org>
2008-04-25 20:43:29 +0000

    Moved implementation of ServletURLStreamHandler to package that belongs to SSF.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@651697 13f79535-47bb-0310-9956-ffa450edef68

--- subprojects/cocoon-servlet-service/cocoon-servlet-service-impl/src/main/java/org/apache/cocoon/servletservice/url/ServletURLStreamHandler.java
+++ subprojects/cocoon-servlet-service/cocoon-servlet-service-impl/src/main/java/org/apache/cocoon/servletservice/url/ServletURLStreamHandler.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.cocoon.url;
+package org.apache.cocoon.servletservice.url;
 
 import java.io.IOException;
 import java.net.URL;
