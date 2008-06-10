88c2361ccfba0b09562a41ec155b929e61826a3a
Joerg Heinicke <joerg@apache.org>
2008-06-10 02:40:16 +0000

    as a follow-up of COCOON-2168 (http://marc.info/?t=120899868500003&r=1&w=4): Cocoon's pipeline buffer increases from an initial buffer size of 8192 bytes to the configurable flush buffer size rather than allocating the complete buffer beforehand.
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X@665954 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cocoon/util/BufferedOutputStream.java
+++ src/java/org/apache/cocoon/util/BufferedOutputStream.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,63 +21,93 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * This class is like the {@link java.io.BufferedOutputStream} but it
- * extends it with a logic to count the number of bytes written to
- * the output stream.
- * 
+ * This class is similar to the {@link java.io.BufferedOutputStream}. In
+ * addition it provides an increasing buffer, the possibility to reset the
+ * buffer and it counts the number of bytes written to the output stream.
+ *
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
  * @version CVS $Id$
  * @since   2.1
  */
-public final class BufferedOutputStream extends FilterOutputStream {
-    
-    protected byte buf[];
+public class BufferedOutputStream extends FilterOutputStream {
 
-    protected int count;
-    
+    private byte buffer[];
+
+    private int count;
+    private int totalCount;
+
+    private final int flushBufferSize;
+
     /**
-     * Creates a new buffered output stream to write data to the 
-     * specified underlying output stream with a default 8192-byte
-     * buffer size.
+     * Creates a new buffered output stream to write data to the specified
+     * underlying output stream with a default flush buffer size of 32768 bytes
+     * and a default initial buffer size of 8192 bytes.
      *
      * @param   out   the underlying output stream.
      */
-    public BufferedOutputStream(OutputStream out) {
-        this(out, 8192);
+    public BufferedOutputStream(final OutputStream out) {
+        this(out, 32768);
     }
 
     /**
-     * Creates a new buffered output stream to write data to the 
-     * specified underlying output stream with the specified buffer 
-     * size. 
+     * Creates a new buffered output stream to write data to the specified
+     * underlying output stream with the specified flush buffer size and a
+     * default initial buffer size of 8192 bytes.
      *
-     * @param   out    the underlying output stream.
-     * @param   size   the buffer size.
-     * @exception IllegalArgumentException if size <= 0.
+     * @param   out   the underlying output stream.
      */
-    public BufferedOutputStream(OutputStream out, int size) {
-        super(out);
-        if (size <= 0) {
-            throw new IllegalArgumentException("Buffer size <= 0");
-        }
-        this.buf = new byte[size];
+    public BufferedOutputStream(final OutputStream out, final int flushBufferSize) {
+        this(out, flushBufferSize, 8192);
     }
 
     /**
-     * Writes the specified byte to this buffered output stream. 
+     * Creates a new buffered output stream to write data to the specified
+     * underlying output stream with the specified buffer sizes.
+     *
+     * @param out    the underlying output stream.
+     * @param flushBufferSize  the buffer size when the stream is flushed. Must
+     *                         be greater than 0 or -1 meaning the stream never
+     *                         flushes itself.
+     * @param initialBufferSize  the initial buffer size. Must be greater than 0.
+     *                           Will be limited to the flush buffer size.
+     */
+    public BufferedOutputStream(final OutputStream out,
+                                final int flushBufferSize,
+                                final int initialBufferSize) {
+        super(out);
+        if (flushBufferSize <= 0 && flushBufferSize != -1) {
+            throw new IllegalArgumentException("Flush buffer size <= 0 && != -1");
+        }
+        if (initialBufferSize <= 0) {
+            throw new IllegalArgumentException("Initial buffer size <= 0");
+        }
+        int actualInitialBufferSize =
+            flushBufferSize > 0 && initialBufferSize > flushBufferSize ? flushBufferSize
+                                                                       : initialBufferSize;
+        this.buffer = new byte[actualInitialBufferSize];
+        this.flushBufferSize = flushBufferSize;
+    }
+
+    /**
+     * Writes the specified byte to this buffered output stream.
      *
      * @param      b   the byte to be written.
      * @exception  IOException  if an I/O error occurs.
      */
-    public void write(int b) throws IOException {
-        if (this.count >= this.buf.length) {
-            this.incBuffer();
+    public void write(final int b) throws IOException {
+        if (this.count == this.buffer.length) {
+            // No need to check return value, can NEVER be 0.
+            this.increaseBuffer(1);
         }
-        this.buf[count++] = (byte)b;
+
+        this.buffer[this.count++] = (byte)b;
+        this.totalCount++;
+
+        checkForFlush();
     }
 
     /**
-     * Writes <code>len</code> bytes from the specified byte array 
+     * Writes <code>len</code> bytes from the specified byte array
      * starting at offset <code>off</code> to this buffered output stream.
      *
      * <p> Ordinarily this method stores bytes from the given array into this
@@ -92,22 +122,54 @@ public final class BufferedOutputStream extends FilterOutputStream {
      * @param      len   the number of bytes to write.
      * @exception  IOException  if an I/O error occurs.
      */
-    public void write(byte b[], int off, int len) throws IOException {
-        while (len > buf.length - count) {
-            this.incBuffer();
+    public void write(final byte[] b, final int off, final int len) throws IOException {
+        int free = this.buffer.length - this.count;
+        int necessaryIncrease = len - free;
+        if (necessaryIncrease > 0) {
+            int actualIncrease = this.increaseBuffer(necessaryIncrease);
+            if (actualIncrease < necessaryIncrease) {
+                free += actualIncrease;
+                // Needs to be written in chunks by recursive calls to this method.
+                writeToBuffer(b, off, free);
+                int newOff = off + free;
+                int newLen = len - free;
+                while (newLen > 0) {
+                    writeToBuffer(b, newOff, Math.min(newLen, this.flushBufferSize));
+                    newOff += this.flushBufferSize;
+                    newLen -= this.flushBufferSize;
+                }
+                return;
+            }
         }
-        System.arraycopy(b, off, buf, count, len);
-        count += len;
+
+        writeToBuffer(b, off, len);
+    }
+
+    private void writeToBuffer(final byte[] b, final int off, final int len) throws IOException {
+        System.arraycopy(b, off, this.buffer, this.count, len);
+        this.count += len;
+        this.totalCount += len;
+
+        checkForFlush();
+    }
+
+    private void checkForFlush() throws IOException {
+        if (this.count == this.flushBufferSize) {
+            flush();
+        }
     }
 
     /**
-     * Flushes this buffered output stream. 
-     * We don't flush here, flushing is done during closing.
+     * Flushes this buffered output stream.
      *
      * @exception  IOException  if an I/O error occurs.
      */
     public void flush() throws IOException {
-        // nothing
+        if (this.count > 0) {
+            this.out.write(this.buffer, 0, this.count);
+            this.count = 0;
+        }
+        this.out.flush();
     }
 
     /**
@@ -117,52 +179,73 @@ public final class BufferedOutputStream extends FilterOutputStream {
      * @exception  IOException  if an I/O error occurs.
      */
     public void close() throws IOException {
-        realFlush();
-        super.close ();
+        flush();
+        super.close();
     }
 
     /**
-     * Flushes this buffered output stream. 
+     * Increase the buffer by at least as many bytes as specified via the
+     * parameter, but not exceeding the flushBufferSize. The actual increase is
+     * returned.
+     *
+     * @return  increase in buffer size.
      */
-    public void realFlush() throws IOException {
-        this.writeBuffer();
-        this.out.flush();
-    }
-    
-    /**
-     * Write the buffer
-     */
-    private void writeBuffer() 
-    throws IOException {
-        if (this.count > 0) {
-            this.out.write(this.buf, 0, this.count);
-            this.clearBuffer();
+    private int increaseBuffer(final int increase) {
+        int oldLength = this.buffer.length;
+        if (oldLength == this.flushBufferSize) {
+            // fast way out
+            return 0;
         }
+
+        int newLength = oldLength;
+        int actualIncrease;
+        do {
+            newLength = newLength * 2;
+            if (this.flushBufferSize > 0 && newLength >= this.flushBufferSize) {
+                newLength = this.flushBufferSize;
+                actualIncrease = newLength - oldLength;
+                break;
+            }
+            actualIncrease = newLength - oldLength;
+        } while (actualIncrease < increase);
+
+        // Because of the "fast way out" above at this point there should always be an increase.
+        byte[] newBuffer = new byte[newLength];
+        if (this.count > 0) {
+            System.arraycopy(this.buffer, 0, newBuffer, 0, this.count);
+        }
+        this.buffer = newBuffer;
+        return actualIncrease;
     }
 
     /**
-     * Increment the buffer
-     */
-    private void incBuffer() {
-        // currently we double the buffer size
-        // this is not so fast but is a very simple logic
-        byte[] newBuf = new byte[this.buf.length * 2];
-        System.arraycopy(this.buf, 0, newBuf, 0, this.buf.length);
-        this.buf = newBuf;
-    }
-    
-    /**
-     * Clear/reset the buffer
+     * Clear the buffer.
+     * @deprecated Public access is deprecated. Use {@link #reset()} instead.
      */
     public void clearBuffer() {
+        this.totalCount -= this.count;
         this.count = 0;
+    }
+
+    /**
+     * Reset the BufferedOutputStream to the last {@link #flush()}.
+     */
+    public void reset() {
+        clearBuffer();
+    }
+
+    /**
+     * @return if it is possible to reset the buffer completely, i.e. nothing has been flushed yet.
+     */
+    public boolean isResettable() {
+        return this.count == this.totalCount;
     }
 
     /**
      * Return the size of the current buffer
      */
     public int getCount() {
-        return this.count;
+        return this.totalCount;
     }
-}
 
+}
