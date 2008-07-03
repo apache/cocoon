edd2d474d964d71d24f85393be56c5b007077089
Reinhard Pötz <reinhard@apache.org>
2008-07-03 16:25:17 +0000

    migrate the EventAwareGenerator to Spring (done together with Lukas Lang)
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@673733 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-eventcache/cocoon-eventcache-impl/src/main/java/org/apache/cocoon/samples/EventAwareGenerator.java
+++ blocks/cocoon-eventcache/cocoon-eventcache-impl/src/main/java/org/apache/cocoon/samples/EventAwareGenerator.java
@@ -18,75 +18,81 @@ package org.apache.cocoon.samples;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.caching.validity.EventValidity;
 import org.apache.cocoon.caching.validity.NamedEvent;
+import org.apache.cocoon.el.objectmodel.ObjectModel;
 import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.SourceResolver;
 import org.apache.cocoon.template.JXTemplateGenerator;
 import org.apache.excalibur.source.SourceValidity;
 import org.xml.sax.SAXException;
 
 /**
- * This is a sample generator to demonstrate the event aware caching.
- * We simply extend the JXTG.
+ * This is a sample generator to demonstrate the event aware caching. We simply extend the JXTG.
+ * 
  * @version $Id$
  */
 public class EventAwareGenerator extends JXTemplateGenerator {
 
+    private Map oldObjectModel;
+
     /**
      * Generate the unique key for the cache.
-     *
-     * This key must be unique inside the space of this XSP page, it is used
-     * to find the page contents in the cache (if getValidity says that the
-     * contents are still valid).
-     *
+     * 
+     * This key must be unique inside the space of this XSP page, it is used to find the page contents in the cache (if
+     * getValidity says that the contents are still valid).
+     * 
      * This method will be invoked before the getValidity() method.
-     *
-     * @return The generated key or null if the component
-     *         is currently not cacheable.
+     * 
+     * @return The generated key or null if the component is currently not cacheable.
      */
     public Serializable getKey() {
-        final Request request = ObjectModelHelper.getRequest(this.getObjectModel());
+        final Request request = ObjectModelHelper.getRequest(oldObjectModel);
         // for our test, pages having the same value of "pageKey" will share
         // the same cache location
-        String key = request.getParameter("pageKey") ;
-        return ((key==null||"".equals(key)) ? "one" : key);
+        String key = request.getParameter("pageKey");
+        return ((key == null || "".equals(key)) ? "one" : key);
     }
 
     /**
-     * Generate the validity object, tells the cache how long to
-     * keep contents having this key around.  In this case, it will 
-     * be until an Event is retrieved matching the NamedEvent created below.
-     *
-     * Before this method can be invoked the getKey() method
-     * will be invoked.
-     *
-     * @return The generated validity object or null if the
-     *         component is currently not cacheable.
+     * Generate the validity object, tells the cache how long to keep contents having this key around. In this case, it
+     * will be until an Event is retrieved matching the NamedEvent created below.
+     * 
+     * Before this method can be invoked the getKey() method will be invoked.
+     * 
+     * @return The generated validity object or null if the component is currently not cacheable.
      */
     public SourceValidity getValidity() {
-        final Request request = ObjectModelHelper.getRequest(this.getObjectModel());
-        String key = request.getParameter("pageKey") ;
-        return new EventValidity(
-                   new NamedEvent(
-                       (key==null||"".equals(key)) ? "one" : key));
+        final Request request = ObjectModelHelper.getRequest(this.oldObjectModel);
+        String key = request.getParameter("pageKey");
+        return new EventValidity(new NamedEvent((key == null || "".equals(key)) ? "one" : key));
+    }
+    
+    public void setup(SourceResolver resolver, Map objectModel, String src, Parameters parameters)
+            throws ProcessingException, SAXException, IOException {
+        this.oldObjectModel = objectModel;
+        super.setup(resolver, objectModel, src, parameters);
     }
 
-
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.apache.cocoon.generation.Generator#generate()
      */
-    public void generate() 
-    throws IOException, SAXException, ProcessingException {
+    public void generate() throws IOException, SAXException, ProcessingException {
         super.generate();
         // slowdown page generation.
         long DELAY_SECS = this.parameters.getParameterAsLong("DELAY_SECS", 2);
         try {
-          Thread.sleep(DELAY_SECS * 1000L);
+            Thread.sleep(DELAY_SECS * 1000L);
         } catch (InterruptedException ie) {
-          // Not much that can be done...
+            // Not much that can be done...
         }
     }
 }
