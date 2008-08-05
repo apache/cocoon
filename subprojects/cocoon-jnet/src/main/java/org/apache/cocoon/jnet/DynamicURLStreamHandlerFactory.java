fd0462a34adc98f45dbe54d5e81ec612e8063936
Poetz Reinhard <reinhard@apache.org>
2008-08-05 12:55:37 +0000

    remove unused Excalibur stuff
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@682701 13f79535-47bb-0310-9956-ffa450edef68

--- subprojects/cocoon-jnet/src/main/java/org/apache/cocoon/jnet/DynamicURLStreamHandlerFactory.java
+++ subprojects/cocoon-jnet/src/main/java/org/apache/cocoon/jnet/DynamicURLStreamHandlerFactory.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.excalibur.sourceresolve.jnet;
+package org.apache.cocoon.jnet;
 
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
