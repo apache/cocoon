c180281f702a36d32cd72e20cec449ea219439be
Poetz Reinhard <reinhard@apache.org>
2008-08-17 21:57:37 +0000

    - add license header - formatting
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@686647 13f79535-47bb-0310-9956-ffa450edef68

--- subprojects/cocoon-block-deployment/src/main/java/org/apache/cocoon/blockdeployment/DefaultBlockResourcesHolder.java
+++ subprojects/cocoon-block-deployment/src/main/java/org/apache/cocoon/blockdeployment/DefaultBlockResourcesHolder.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.cocoon.blockdeployment;
 
 import java.util.Map;
@@ -6,11 +22,11 @@ import javax.servlet.ServletContext;
 
 public class DefaultBlockResourcesHolder implements BlockResourcesHolder,
         org.apache.cocoon.spring.configurator.BlockResourcesHolder {
-    
+
     ServletContext servletContext;
 
     public Map getBlockContexts() {
-        return (Map)servletContext.getAttribute(BlockDeploymentServletContextListener.BLOCK_CONTEXT_MAP);
+        return (Map) servletContext.getAttribute(BlockDeploymentServletContextListener.BLOCK_CONTEXT_MAP);
     }
 
     public ServletContext getServletContext() {
@@ -20,5 +36,4 @@ public class DefaultBlockResourcesHolder implements BlockResourcesHolder,
     public void setServletContext(ServletContext servletContext) {
         this.servletContext = servletContext;
     }
-
 }
