7df5e81f8b05c505e84bee974dd6edd2007a834b
Jeremy Quinn <jeremy@apache.org>
2008-09-11 18:32:25 +0000

    new samples, cleaning up to be done
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/branches/BRANCH_2_1_X-dojo1_1@694401 13f79535-47bb-0310-9956-ffa450edef68

--- src/blocks/forms/samples/flow/forms_flow_example.js
+++ src/blocks/forms/samples/flow/forms_flow_example.js
@@ -15,6 +15,16 @@
 * limitations under the License.
 */
 cocoon.load("resource://org/apache/cocoon/forms/flow/javascript/Form.js");
+cocoon.load("resource://org/apache/cocoon/forms/system/System.Collections.js");
+importClass (Packages.java.util.Locale);
+importClass (Packages.com.ibm.icu.text.NumberFormat);
+importClass (Packages.com.ibm.icu.text.SimpleDateFormat);
+importClass (Packages.java.math.BigDecimal);
+
+
+// TODO: Remove all XSP Pipelines, replace with JXT
+// TODO: finish cleaningup selection-list generation
+
 
 function form1(form) {
     var locale = determineLocale();
@@ -152,10 +162,10 @@ function do_dojoRepeaters() {
     var form = new Form("forms/dynamicrepeaters.xml");
     form.setAttribute("counter1", new java.lang.Integer(0));
     form.setAttribute("counter2", new java.lang.Integer(0));
-    form.setAttribute("counter3", new java.lang.Integer(0));
+    //form.setAttribute("counter3", new java.lang.Integer(0));
     form.getChild("addcontact1").performAction(); // to increment the counter
     form.getChild("addcontact2").performAction(); // to increment the counter
-    form.getChild("addcontact3").performAction(); // to increment the counter
+    //form.getChild("addcontact3").performAction(); // to increment the counter
     form.showForm("dynamicrepeaters_dojo-display-pipeline.jx");
 
     var doc = Packages.javax.xml.parsers.DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
@@ -279,13 +289,175 @@ function do_inplace() {
             "\ncomments: " + form.getChild("comments") });
 }
 
-function do_calculatedfields(form) {
+function do_calculatedfields() {
     var form = new Form("forms/calculatedfields.xml");
     form.showForm("calculatedfields-display-pipeline.jx");
     cocoon.sendPage("textresult-display-pipeline.jx",
         {title: "Calculated results", text: "Grand total of your order: " + form.getChild("grandtotal").value});
 }
 
+function do_groupStyles() {
+    var form = new Form("forms/group_styles.xml");
+    var locale = determineLocale();
+    if (locale != null) form.locale = locale;
+    var model = form.getModel();
+    model.dieselprice = new Packages.java.math.BigDecimal("100");
+    
+    /*
+      The way to set a Currency on a Widget, from FlowScript 
+      form.getChild("dieselprice").getDatatype().getConvertor().setCurrency("GBP");
+    */
+    form.showForm("group_styles-display-pipeline.jx", {locales: NumberFormat.getAvailableLocales()});
+    cocoon.sendPage("textresult-display-pipeline.jx",
+        {title: "Group Style Samples", text: "TODO"});
+
+}
+
+function getCurrencyName(form, id) {
+  return form.getChild(id).getDefinition().getDatatype().getConvertor().getCurrencyName(form.locale);
+}
+
+function do_numbers() {
+    var form = new Form("forms/numbers.xml");
+    var chosenlocale = determineLocale();
+    form.locale = chosenlocale || Locale.getDefault();
+    var model = form.getModel();
+    var names = new Packages.java.util.HashMap();
+    model.EUR = new BigDecimal("98765432.10"); 
+    names.put("EUR", getCurrencyName(form, "EUR"));
+    model.USD = new BigDecimal("98765432.10");
+    names.put("USD", getCurrencyName(form, "USD"));
+    model.CNY = new BigDecimal("98765432.10");
+    names.put("CNY", getCurrencyName(form, "CNY"));
+    model.JPY = new BigDecimal("98765432.10");
+    names.put("JPY", getCurrencyName(form, "JPY"));
+    model.GBP = new BigDecimal("98765432.10");
+    names.put("GBP", getCurrencyName(form, "GBP"));
+    model.RUB = new BigDecimal("98765432.10");
+    names.put("RUB", getCurrencyName(form, "RUB"));
+    model.XXX = new BigDecimal("98765432.10");
+    names.put("XXX", getCurrencyName(form, "XXX"));
+    model.plainDecimal = new BigDecimal("5432.1");
+    model.formattedDecimal = new BigDecimal("5432.1");
+    model.formattedPercent = 1.23;
+    
+    form.showForm("numbers-display-pipeline.jx", {currencies: names, locales: NumberFormat.getAvailableLocales()});
+    
+    var message = "Cash successfully transfered to my private Swiss bank account:" + 
+                   "\n Euros             " + model.EUR + 
+                   "\n US Dollars        " + model.USD + 
+                   "\n Yuan              " + model.CNY + 
+                   "\n Yen               " + model.JPY + 
+                   "\n Pounds            " + model.GBP + 
+                   "\n Roubles           " + model.RUB + 
+                   "\n Default           " + model.XXX +
+                   "\n ------------------" +
+                   "\n plainInteger      " + model.plainInteger +
+                   "\n plainLong         " + model.plainLong +
+                   "\n plainDecimal      " + model.plainDecimal +
+                   "\n plainDouble       " + model.plainDouble +
+                   "\n plainFloat        " + model.plainFloat +
+                   "\n ------------------" +
+                   "\n formattedInteger  " + model.formattedInteger +
+                   "\n formattedLong     " + model.formattedLong +
+                   "\n formattedDecimal  " + model.formattedDecimal +
+                   "\n formattedDouble   " + model.formattedDouble +
+                   "\n formattedFloat    " + model.formattedFloat +
+                   "\n formattedPercent  " + model.formattedPercent ;
+             
+    cocoon.sendPage("textresult-display-pipeline.jx", {title: "Number Samples", text: message});
+
+}
+
+function do_calendars() {
+    var form = new Form("forms/calendars.xml");
+    var chosenlocale = determineLocale();
+    form.locale = chosenlocale || Locale.getDefault();
+    var model = form.getModel();
+    var d = new Packages.java.util.Date();
+    model.date0 = d;
+    model.date1 = d;
+    model.date2 = d;
+    model.time0 = d;
+    model.time1 = d;
+    model.time2 = d;
+    model.datetime0 = d;
+    model.datetime1 = d;
+    model.datetime2 = d;
+    
+    
+    form.showForm("calendars-display-pipeline.jx", {locales: SimpleDateFormat.getAvailableLocales()});
+    
+    var message = "Calendar Results:" +
+                  "\n date0  " + model.date0 +
+                  "\n date1  " + model.date1 +
+                  "\n date2  " + model.date2 +
+                  "\n time0  " + model.time0 +
+                  "\n time1  " + model.time1 +
+                  "\n time2  " + model.time2;
+             
+    cocoon.sendPage("textresult-display-pipeline.jx", {title: "Calendar Samples", text: message});
+
+}
+
+// TODO: write a version of determinLocale that takes the browser Locale into account 
+// or at least test whats going on with people from other /native/ locales from me
+
+function do_lists() {
+    var form = new Form("forms/lists.xml");
+    var chosenlocale = determineLocale();
+    form.locale = chosenlocale || Packages.java.util.Locale.getDefault();
+    var model = form.getModel();
+    var fish = [
+      {value:"Oncorhynchus mykiss", label:"Rainbow Trout"},
+      {value:"Ignoratio elenchi", label:"Red Herring"},
+      {value:"Pygocentrus rhombeus", label:"Black Piranha"},
+      {value:"Lutjanus campechanus", label:"Red Snapper"},
+      {value:"Cypselurus melanurus", label:"Flying Fish"}
+    ]; // if any of these are wrong, blame wikipedia
+    
+    System.Collections.registerSuggestionList("countries", System.Collections.getCountryGenerator());
+    
+    
+    form.showForm("lists-display-pipeline.jx", {fish: fish, locales: SimpleDateFormat.getAvailableLocales()});
+    
+    var message = "List Results:" +
+                  "\n Height:                 [" + model.height + "]" +
+                  "\n Characters:             [" + model.fourchars + "]" +
+                  "\n Birth Date:             [" + model.altbirthdate + "]" +
+                  "\n Other Birth Date:       [" + model.altbirthdate2 + "]" +
+                  "\n Pet Fish:               [" + model.petfish + "]" +
+                  "\n Fish Gender:            [" + model.fishgender + "]" +
+                  "\n Vegetable:              [" + model.vegetables + "]" +
+                  "\n Workplace:              [" + model.workplace + "]";
+             
+    cocoon.sendPage("textresult-display-pipeline.jx", {title: "List Samples", text: message});
+
+}
+
+function do_textareas() {
+    var form = new Form("forms/textareas.xml");
+    var chosenlocale = determineLocale();
+    form.locale = chosenlocale || Locale.getDefault();
+    var model = form.getModel();
+    
+    model.simple = "A simple textarea\ncan have more than one line";
+    model.validating = "A validating textarea\nMust have content";
+    model.rich = "<p>A <a href=\"http://dojotoolkit.org\">dijit.Editor</a></p><p><i>edit Rich Text</i></p>";
+    model.inplacearea = "Click on me\nto edit";
+    //model.inplacerich = "<p>This will not work</p><p><i>until</i> Dojo 1.2 is released.</p>";
+    
+    form.showForm("textareas-display-pipeline.jx", {locales: SimpleDateFormat.getAvailableLocales()});
+
+    var message = "Output from TextAreas : " +
+                  "\n Simple TextArea:        [" + model.simple + "]" +
+                  "\n Validating TextArea:    [" + model.validating + "]" +
+                  "\n Rich TextArea:          [" + model.rich + "]" +
+                  "\n Inplace TextArea:       [" + model.inplacearea + "]" +
+                  "\n Inplace RichText:       [" + model.inplacerich + "]";
+
+    cocoon.sendPage("textresult-display-pipeline.jx", {title: "TextArea Samples", text: message});
+}
 function do_widgetstates() {
     var form = new Form("forms/widgetstates.xml");
     form.showForm("widgetstates-display-pipeline.jx");
@@ -306,3 +478,4 @@ function do_multiValueDoubleList() {
     cocoon.sendPage("textresult-display-pipeline.jx",
         { title: "Values selected", text: "_field_1: "+_string1 });
 }
+
