7d66862c3a87462f357444eb69df4c521c32d01f
Reinhard Pötz <reinhard@apache.org>
2009-04-04 13:00:00 +0000

    [maven-release-plugin]  copy for tag cocoon-jnet-1.1.0
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/tags/cocoon-jnet@761925 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cocoon-jnet-1.1.0/src/main/java/org/apache/cocoon/jnet/URLHandlerFactoryCollector.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cocoon.jnet;
+
+import java.net.URLStreamHandlerFactory;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class URLHandlerFactoryCollector {
+
+    private Map urlHandlerFactories = Collections.EMPTY_MAP;
+
+    private boolean urlStreamHandlerInstalled;
+
+    public Object installURLHandlers(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        try {
+            pushUrlHandlerFactories();
+            return proceedingJoinPoint.proceed();
+        } finally {
+            popUrlHandlerFactories();
+        }
+    }
+    
+    public void pushUrlHandlerFactories() throws Exception {
+        if (!this.urlStreamHandlerInstalled) {
+            URLStreamHandlerFactoryInstaller.setURLStreamHandlerFactory(new DynamicURLStreamHandlerFactory());
+            this.urlStreamHandlerInstalled = true;
+        }
+
+        for (Iterator i = this.urlHandlerFactories.values().iterator(); i.hasNext();) {
+            URLStreamHandlerFactory streamHandlerFactory = (URLStreamHandlerFactory) i.next();
+            DynamicURLStreamHandlerFactory.push(streamHandlerFactory);
+        }
+    }
+    
+    public void popUrlHandlerFactories() {
+        for (Iterator i = this.urlHandlerFactories.values().iterator(); i.hasNext(); i.next()) {
+            DynamicURLStreamHandlerFactory.pop();
+        }
+    }
+
+    public void setUrlHandlerFactories(Map urlHandlerFactories) {
+        this.urlHandlerFactories = urlHandlerFactories;
+    }
+}
