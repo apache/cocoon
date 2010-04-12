9e01f6580ed51d4f410fb1f3a4a8ac3d9c80add9
Jeroen Reijn <jreijn@apache.org>
2010-04-12 21:09:57 +0000

    COCOON-2035: Use spaces not tabs
    git-svn-id: https://svn.apache.org/repos/asf/cocoon/trunk@933416 13f79535-47bb-0310-9956-ffa450edef68

--- blocks/cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/binding/RepeaterJXPathAdapter.java
+++ blocks/cocoon-forms/cocoon-forms-impl/src/main/java/org/apache/cocoon/forms/binding/RepeaterJXPathAdapter.java
@@ -27,130 +27,131 @@ import java.util.*;
  */
 public class RepeaterJXPathAdapter implements RepeaterAdapter {
 
-	private int progressive = 100000;
+    private int progressive = 100000;
 
-	private EnhancedRepeaterJXPathBinding binding;
-	private RepeaterJXPathCollection jxCollection;
-	private List sortedItems;
+    private EnhancedRepeaterJXPathBinding binding;
+    private RepeaterJXPathCollection jxCollection;
+    private List sortedItems;
 
 
-	public RepeaterFilter getFilter() {
-		return new RepeaterJXPathFilter();
-	}
+    public RepeaterFilter getFilter() {
+        return new RepeaterJXPathFilter();
+    }
 
-	private String findPathFor(String field) {
-		JXPathBindingBase[] childBindings = binding.getRowBinding().getChildBindings();
-		String path = null;
-		for (int i = 0; i < childBindings.length; i++) {
-			if (childBindings[i] instanceof ValueJXPathBinding) {
-				ValueJXPathBinding bnd = (ValueJXPathBinding) childBindings[i];
-				if (bnd.getFieldId().equals(field)) {
-					path = bnd.getXPath();
-					break;
-				}
-			}
-		}
-		return path;
-	}
+    private String findPathFor(String field) {
+        JXPathBindingBase[] childBindings = binding.getRowBinding().getChildBindings();
+        String path = null;
+        for (int i = 0; i < childBindings.length; i++) {
+            if (childBindings[i] instanceof ValueJXPathBinding) {
+                ValueJXPathBinding bnd = (ValueJXPathBinding) childBindings[i];
+                if (bnd.getFieldId().equals(field)) {
+                    path = bnd.getXPath();
+                    break;
+                }
+            }
+        }
+        return path;
+    }
 
-	public RepeaterSorter sortBy(String field) {
-		if (field == null) {
-			sortedItems = null;
-			return new NormalOrderJXPathSorter();
-		}
-		String path = findPathFor(field);
-		if (path == null) throw new IllegalStateException("Cannot find a path for sorting on widget " + field);
-		RepeaterSorter sort = new RepeaterJXPathSorter(path, field, false);
-		if (sortedItems == null) {
-			List tsortedItems = new ArrayList();
-			int i = 0;
-			RepeaterItem item = getItem(i);
-			while (item != null) {
-				tsortedItems.add(item);
-				i++;
-				item = getItem(i);
-			}
-			this.sortedItems = tsortedItems;
-		}
-		Collections.sort(sortedItems, sort);
-		return sort;
-	}
+    public RepeaterSorter sortBy(String field) {
+        if (field == null) {
+            sortedItems = null;
+            return new NormalOrderJXPathSorter();
+        }
+        String path = findPathFor(field);
+        if (path == null) throw new IllegalStateException("Cannot find a path for sorting on widget " + field);
+        RepeaterSorter sort = new RepeaterJXPathSorter(path, field, false);
+        if (sortedItems == null) {
+            List tsortedItems = new ArrayList();
+            int i = 0;
+            RepeaterItem item = getItem(i);
+            while (item != null) {
+                tsortedItems.add(item);
+                i++;
+                item = getItem(i);
+            }
+            this.sortedItems = tsortedItems;
+        }
+        Collections.sort(sortedItems, sort);
+        return sort;
+    }
 
-	public void setBinding(EnhancedRepeaterJXPathBinding binding) {
-		this.binding = binding;
-	}
+    public void setBinding(EnhancedRepeaterJXPathBinding binding) {
+        this.binding = binding;
+    }
 
-	public void setCollection(Collection c) {
-	}
+    public void setCollection(Collection c) {
+    }
 
-	public void setJXCollection(RepeaterJXPathCollection collection) {
-		this.jxCollection = collection;
-	}
+    public void setJXCollection(RepeaterJXPathCollection collection) {
+        this.jxCollection = collection;
+    }
 
     public RepeaterItem getItem(int i) {
-    	if (i < 0) return null;
-		if (i >= jxCollection.getOriginalCollectionSize()) return null;
-    	if (this.sortedItems == null) {
-	    	JXPathContext storageContext = this.jxCollection.getStorageContext();
-	        Pointer pointer = storageContext.getPointer(binding.getRowPath() + "[" + (i+1) + "]");
-	        JXPathContext rowContext = storageContext.getRelativeContext(pointer);
-	        RepeaterItem item = new RepeaterItem(new Integer(i + 1));
-	        item.setContext(rowContext);
-	        return item;
-    	} else {
-    		return (RepeaterItem) sortedItems.get(i);
-    	}
+        if (i < 0) return null;
+        if (i >= jxCollection.getOriginalCollectionSize()) return null;
+        if (this.sortedItems == null) {
+            JXPathContext storageContext = this.jxCollection.getStorageContext();
+            Pointer pointer = storageContext.getPointer(binding.getRowPath() + "[" + (i + 1) + "]");
+            JXPathContext rowContext = storageContext.getRelativeContext(pointer);
+            RepeaterItem item = new RepeaterItem(new Integer(i + 1));
+            item.setContext(rowContext);
+            return item;
+        } else {
+            return (RepeaterItem) sortedItems.get(i);
+        }
     }
 
 
-	class RepeaterJXPathFilter implements RepeaterFilter {
+    class RepeaterJXPathFilter implements RepeaterFilter {
 
-		private Map fieldsPaths = new HashMap();
-		private Map fieldsValues = new HashMap();
+        private Map fieldsPaths = new HashMap();
+        private Map fieldsValues = new HashMap();
 
-		public boolean shouldDisplay(RepeaterItem item) {
-			for (Iterator iter = fieldsValues.keySet().iterator(); iter.hasNext();) {
-				String field = (String) iter.next();
-				Object value = fieldsValues.get(field);
-				Object acvalue = null;
-				if (item.getRow() == null) {
-					String path = (String) fieldsPaths.get(field);
-					acvalue = item.getContext().getValue(path);
-				} else {
-					acvalue = item.getRow().getChild(field).getValue();
-				}
-				if (acvalue == null) return false;
-				if (acvalue instanceof String && value instanceof String) {
-					return ((String)acvalue).startsWith((String)value);
-				} else {
-					return acvalue.equals(value);
-				}
-			}
-			return true;
-		}
+        public boolean shouldDisplay(RepeaterItem item) {
+            for (Iterator iter = fieldsValues.keySet().iterator(); iter.hasNext();) {
+                String field = (String) iter.next();
+                Object value = fieldsValues.get(field);
+                Object acvalue = null;
+                if (item.getRow() == null) {
+                    String path = (String) fieldsPaths.get(field);
+                    acvalue = item.getContext().getValue(path);
+                } else {
+                    acvalue = item.getRow().getChild(field).getValue();
+                }
+                if (acvalue == null) return false;
+                if (acvalue instanceof String && value instanceof String) {
+                    return ((String) acvalue).startsWith((String) value);
+                } else {
+                    return acvalue.equals(value);
+                }
+            }
+            return true;
+        }
 
-		public void setFilter(String field, Object value) {
-			if (value == null || ((value instanceof String) && ((String)value).length() == 0)) {
-				fieldsPaths.remove(field);
-				fieldsValues.remove(field);
-			} else {
-				String path = findPathFor(field);
-				if (path == null) throw new IllegalStateException("Cannot find a path for filtering on widget " + field);
-				fieldsPaths.put(field, path);
-				fieldsValues.put(field, value);
-			}
-		}
+        public void setFilter(String field, Object value) {
+            if (value == null || ((value instanceof String) && ((String) value).length() == 0)) {
+                fieldsPaths.remove(field);
+                fieldsValues.remove(field);
+            } else {
+                String path = findPathFor(field);
+                if (path == null)
+                    throw new IllegalStateException("Cannot find a path for filtering on widget " + field);
+                fieldsPaths.put(field, path);
+                fieldsValues.put(field, value);
+            }
+        }
 
-	}
+    }
 
 
     /**
      * Sorter for JXPath based repeaters
      */
-	static class RepeaterJXPathSorter implements RepeaterSorter {
+    static class RepeaterJXPathSorter implements RepeaterSorter {
 
-		private String path;
-		private String field;
+        private String path;
+        private String field;
         private boolean nullsAreHigher;
 
         /**
@@ -161,67 +162,67 @@ public class RepeaterJXPathAdapter implements RepeaterAdapter {
          * When this value is set to false, it  means that nulls are lower than anything else.
          */
         public RepeaterJXPathSorter(String path, String field, boolean nullsAreHigher) {
-			this.path = path;
-			this.field = field;
+            this.path = path;
+            this.field = field;
             this.nullsAreHigher = nullsAreHigher;
-		}
+        }
 
-		public void setCollection(Collection c) {
-		}
+        public void setCollection(Collection c) {
+        }
 
-		public int compare(Object o1, Object o2) {
-			RepeaterItem i1 = (RepeaterItem) o1;
-			RepeaterItem i2 = (RepeaterItem) o2;
+        public int compare(Object o1, Object o2) {
+            RepeaterItem i1 = (RepeaterItem) o1;
+            RepeaterItem i2 = (RepeaterItem) o2;
 
             Object val1;
-			if (i1.getRow() != null) {
-				val1 = i1.getRow().getChild(field).getValue();
-			} else {
-				val1 = i1.getContext().getValue(path);
-			}
+            if (i1.getRow() != null) {
+                val1 = i1.getRow().getChild(field).getValue();
+            } else {
+                val1 = i1.getContext().getValue(path);
+            }
 
             Object val2;
-			if (i2.getRow() != null) {
-				val2 = i2.getRow().getChild(field).getValue();
-			} else {
-				val2 = i2.getContext().getValue(path);
-			}
+            if (i2.getRow() != null) {
+                val2 = i2.getRow().getChild(field).getValue();
+            } else {
+                val2 = i2.getContext().getValue(path);
+            }
 
-			if(val1 == null) {
+            if(val1 == null) {
                 return (this.nullsAreHigher ? 1 : -1);
             }
-	        if(val2 == null) {
+            if(val2 == null) {
                 return (this.nullsAreHigher ? -1 : 1);
             }
-			
+
             if (val1 instanceof Comparable) {
                 return ((Comparable) val1).compareTo(val2);
             }
             return val1.toString().compareTo(val2.toString());
-		}
+        }
 
-	}
+    }
 
-	static class NormalOrderJXPathSorter implements RepeaterSorter {
+    static class NormalOrderJXPathSorter implements RepeaterSorter {
 
-		public void setCollection(Collection c) {
-		}
+        public void setCollection(Collection c) {
+        }
 
-		public int compare(Object o1, Object o2) {
-			RepeaterItem i1 = (RepeaterItem) o1;
-			RepeaterItem i2 = (RepeaterItem) o2;
+        public int compare(Object o1, Object o2) {
+            RepeaterItem i1 = (RepeaterItem) o1;
+            RepeaterItem i2 = (RepeaterItem) o2;
             return ((Integer) i1.getHandle()).compareTo((Integer) i2.getHandle());
         }
-	}
+    }
 
-	public RepeaterItem generateItem(RepeaterRow row) {
-		RepeaterItem item = new RepeaterItem(new Integer(progressive++));
-		item.setRow(row);
-		return item;
-	}
+    public RepeaterItem generateItem(RepeaterRow row) {
+        RepeaterItem item = new RepeaterItem(new Integer(progressive++));
+        item.setRow(row);
+        return item;
+    }
 
-	public void populateRow(RepeaterItem item) throws BindingException {
+    public void populateRow(RepeaterItem item) throws BindingException {
         binding.getRowBinding().loadFormFromModel(item.getRow(), item.getContext());
-	}
+    }
 
 }
